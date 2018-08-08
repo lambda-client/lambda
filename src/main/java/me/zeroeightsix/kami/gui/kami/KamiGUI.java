ed06454313831bb12b3a2f4b2d9fc18aa2de99b6
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2018-08-08 21:49:20 +0200

    text radar width, height markers

--- src/main/java/me/zeroeightsix/kami/gui/kami/KamiGUI.java
+++ src/main/java/me/zeroeightsix/kami/gui/kami/KamiGUI.java
@@ -2,7 +2,10 @@ package me.zeroeightsix.kami.gui.kami;
 
 import me.zeroeightsix.kami.KamiMod;
 import me.zeroeightsix.kami.command.Command;
+import me.zeroeightsix.kami.gui.kami.component.ActiveModules;
 import me.zeroeightsix.kami.gui.kami.component.Radar;
+import me.zeroeightsix.kami.gui.kami.component.SettingsPanel;
+import me.zeroeightsix.kami.gui.kami.theme.kami.KamiTheme;
 import me.zeroeightsix.kami.gui.rgui.GUI;
 import me.zeroeightsix.kami.gui.rgui.component.container.use.Frame;
 import me.zeroeightsix.kami.gui.rgui.component.container.use.Scrollpane;
@@ -13,9 +16,6 @@ import me.zeroeightsix.kami.gui.rgui.component.use.Label;
 import me.zeroeightsix.kami.gui.rgui.render.theme.Theme;
 import me.zeroeightsix.kami.gui.rgui.util.ContainerHelper;
 import me.zeroeightsix.kami.gui.rgui.util.Docking;
-import me.zeroeightsix.kami.gui.kami.component.ActiveModules;
-import me.zeroeightsix.kami.gui.kami.component.SettingsPanel;
-import me.zeroeightsix.kami.gui.kami.theme.kami.KamiTheme;
 import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.module.ModuleManager;
 import me.zeroeightsix.kami.util.ColourHolder;
@@ -40,7 +40,7 @@ public class KamiGUI extends GUI {
     public static final RootFontRenderer fontRenderer = new RootFontRenderer(1);
     public Theme theme;
 
-    public static ColourHolder primaryColour = new ColourHolder(29,29,29);
+    public static ColourHolder primaryColour = new ColourHolder(29, 29, 29);
 
     public KamiGUI() {
         super(new KamiTheme());
@@ -55,10 +55,10 @@ public class KamiGUI extends GUI {
     @Override
     public void initializeGUI() {
         HashMap<Module.Category, Pair<Scrollpane, SettingsPanel>> categoryScrollpaneHashMap = new HashMap<>();
-        for (Module module : ModuleManager.getModules()){
+        for (Module module : ModuleManager.getModules()) {
             if (module.getCategory().isHidden()) continue;
             Module.Category moduleCategory = module.getCategory();
-            if (!categoryScrollpaneHashMap.containsKey(moduleCategory)){
+            if (!categoryScrollpaneHashMap.containsKey(moduleCategory)) {
                 Stretcherlayout stretcherlayout = new Stretcherlayout(1);
                 stretcherlayout.setComponentOffsetWidth(0);
                 Scrollpane scrollpane = new Scrollpane(getTheme(), stretcherlayout, 300, 260);
@@ -75,10 +75,10 @@ public class KamiGUI extends GUI {
             checkButton.addMouseListener(new MouseListener() {
                 @Override
                 public void onMouseDown(MouseButtonEvent event) {
-                    if (event.getButton() == 1){ // Right click
+                    if (event.getButton() == 1) { // Right click
                         pair.getValue().setModule(module);
-                        pair.getValue().setX(event.getX()+checkButton.getX());
-                        pair.getValue().setY(event.getY()+checkButton.getY());
+                        pair.getValue().setX(event.getX() + checkButton.getX());
+                        pair.getValue().setY(event.getY() + checkButton.getY());
                     }
                 }
 
@@ -105,7 +105,7 @@ public class KamiGUI extends GUI {
             checkButton.addPoof(new CheckButton.CheckButtonPoof<CheckButton, CheckButton.CheckButtonPoof.CheckButtonPoofInfo>() {
                 @Override
                 public void execute(CheckButton component, CheckButtonPoofInfo info) {
-                    if (info.getAction().equals(CheckButton.CheckButtonPoof.CheckButtonPoofInfo.CheckButtonPoofInfoAction.TOGGLE)){
+                    if (info.getAction().equals(CheckButton.CheckButtonPoof.CheckButtonPoofInfo.CheckButtonPoofInfoAction.TOGGLE)) {
                         module.setEnabled(checkButton.isToggled());
                     }
                 }
@@ -116,7 +116,7 @@ public class KamiGUI extends GUI {
         int x = 10;
         int y = 10;
         int nexty = y;
-        for (Map.Entry<Module.Category, Pair<Scrollpane, SettingsPanel>> entry : categoryScrollpaneHashMap.entrySet()){
+        for (Map.Entry<Module.Category, Pair<Scrollpane, SettingsPanel>> entry : categoryScrollpaneHashMap.entrySet()) {
             Stretcherlayout stretcherlayout = new Stretcherlayout(1);
             stretcherlayout.COMPONENT_OFFSET_Y = 1;
             Frame frame = new Frame(getTheme(), stretcherlayout, entry.getKey().getName());
@@ -134,21 +134,21 @@ public class KamiGUI extends GUI {
 
             nexty = Math.max(y + frame.getHeight() + 10, nexty);
             x += frame.getWidth() + 10;
-            if (x > Wrapper.getMinecraft().displayWidth / 1.2f){
+            if (x > Wrapper.getMinecraft().displayWidth / 1.2f) {
                 y = nexty;
                 nexty = y;
             }
         }
 
         this.addMouseListener(new MouseListener() {
-            private boolean isBetween(int min, int val, int max){
+            private boolean isBetween(int min, int val, int max) {
                 return !(val > max || val < min);
             }
 
             @Override
             public void onMouseDown(MouseButtonEvent event) {
                 List<SettingsPanel> panels = ContainerHelper.getAllChildren(SettingsPanel.class, KamiGUI.this);
-                for (SettingsPanel settingsPanel : panels){
+                for (SettingsPanel settingsPanel : panels) {
                     if (!settingsPanel.isVisible()) continue;
                     int[] real = GUI.calculateRealPosition(settingsPanel);
                     int pX = event.getX() - real[0];
@@ -195,8 +195,8 @@ public class KamiGUI extends GUI {
         information.addTickListener(() -> {
             information.setText("");
             information.addLine("\u00A7b" + KamiMod.KAMI_KANJI + "\u00A73 " + KamiMod.MODVER);
-            information.addLine("\u00A7b" + Math.round(LagCompensator.INSTANCE.getTickRate()) + Command.SECTIONSIGN() +  "3 tps");
-            information.addLine("\u00A7b" + Wrapper.getMinecraft().debugFPS + Command.SECTIONSIGN() +  "3 fps");
+            information.addLine("\u00A7b" + Math.round(LagCompensator.INSTANCE.getTickRate()) + Command.SECTIONSIGN() + "3 tps");
+            information.addLine("\u00A7b" + Wrapper.getMinecraft().debugFPS + Command.SECTIONSIGN() + "3 fps");
 
 //            information.addLine("[&3" + Sprint.getSpeed() + "km/h&r]");
 
@@ -207,39 +207,35 @@ public class KamiGUI extends GUI {
 
         frame = new Frame(getTheme(), new Stretcherlayout(1), "Text Radar");
         Label list = new Label("");
-        list.addTickListener(new TickListener() {
-            @Override
-            public void onTick() {
-                if (!list.isVisible()) return;
+        list.addTickListener(() -> {
+            if (!list.isVisible()) return;
+            list.setText("");
+
+            Minecraft mc = Wrapper.getMinecraft();
+
+            if (mc.player == null) return;
+            List<EntityPlayer> entityList = mc.world.playerEntities;
+
+            Map<String, Integer> players = new HashMap<>();
+            for (Entity e : entityList) {
+                if (e.getName().equals(mc.player.getName())) continue;
+                players.put(e.getName() + " " + (e.posY > mc.player.posY ? "(+)" : (e.posY == mc.player.posY ? "(=)" : "(-)")), (int) mc.player.getDistance(e));
+            }
+
+            if (players.isEmpty()) {
                 list.setText("");
+                return;
+            }
 
-                Minecraft mc = Wrapper.getMinecraft();
+            players = sortByValue(players);
 
-                if (mc.player == null) return;
-                List<EntityPlayer> entityList = mc.world.playerEntities;
-
-                Map<String, Integer> players = new HashMap<>();
-                for(Entity e : entityList){
-                    if (e.getName().equals(mc.player.getName())) continue;
-                    players.put(e.getName(), (int)mc.player.getDistance(e));
-                }
-
-                if (players.isEmpty()){
-                    list.setText("");
-                    return;
-                }
-
-                players = sortByValue(players);
-
-                for (Map.Entry<String, Integer> player : players.entrySet()){
-                    list.addLine("\u00A77" + player.getKey() + " \u00A78" + player.getValue());
-                }
-
-//                list.getParent().setHeight(list.getLines().length * (list.getTheme().getFontRenderer().getFontHeight()+1) + 3);
+            for (Map.Entry<String, Integer> player : players.entrySet()) {
+                list.addLine(Command.SECTIONSIGN() + "7" + player.getKey() + " " + Command.SECTIONSIGN() + "8" + player.getValue());
             }
         });
         frame.setCloseable(false);
         frame.setPinneable(true);
+        frame.setMinimumWidth(75);
         list.setShadow(true);
         frame.addChild(list);
         list.setFontRenderer(fontRenderer);
@@ -255,7 +251,7 @@ public class KamiGUI extends GUI {
             public void onTick() {
                 if (mc.player == null || !entityLabel.isVisible()) return;
                 List<Entity> entityList = mc.world.loadedEntityList;
-                if (entityList.size()<=1){
+                if (entityList.size() <= 1) {
                     entityLabel.setText("");
                     return;
                 }
@@ -269,41 +265,41 @@ public class KamiGUI extends GUI {
 
                     int add = 1;
 
-                    if (e instanceof EntityItem){
+                    if (e instanceof EntityItem) {
                         name = Command.SECTIONSIGN() + "3" + ((EntityItem) e).getItem().getItem().getItemStackDisplayName(((EntityItem) e).getItem());
                         add = ((EntityItem) e).getItem().getCount();
                     }
-                    if (e instanceof EntityWitherSkull){
+                    if (e instanceof EntityWitherSkull) {
                         name = Command.SECTIONSIGN() + "8" + "Wither skull";
                     }
-                    if (e instanceof EntityEnderCrystal){
+                    if (e instanceof EntityEnderCrystal) {
                         name = Command.SECTIONSIGN() + "d" + "End crystal";
                     }
-                    if (e instanceof EntityEnderPearl){
+                    if (e instanceof EntityEnderPearl) {
                         name = "Thrown ender pearl";
                     }
-                    if (e instanceof EntityMinecart){
+                    if (e instanceof EntityMinecart) {
                         name = "Minecart";
                     }
-                    if (e instanceof EntityItemFrame){
+                    if (e instanceof EntityItemFrame) {
                         name = "Item frame";
                     }
-                    if (e instanceof EntityEgg){
+                    if (e instanceof EntityEgg) {
                         name = "Thrown egg";
                     }
-                    if (e instanceof EntitySnowball){
+                    if (e instanceof EntitySnowball) {
                         name = "Thrown snowball";
                     }
 
                     int count = entityMap.containsKey(name) ? entityMap.get(name) : 0;
-                    entityMap.put(name,count+add);
+                    entityMap.put(name, count + add);
                 }
 
                 entityMap = sortByValue(entityMap);
 
                 entityLabel.setText("");
-                for (Map.Entry<String, Integer> entry : entityMap.entrySet()){
-                    entityLabel.addLine(Command.SECTIONSIGN() + "7"+entry.getKey() + " " + Command.SECTIONSIGN() + "8x" + entry.getValue());
+                for (Map.Entry<String, Integer> entry : entityMap.entrySet()) {
+                    entityLabel.addLine(Command.SECTIONSIGN() + "7" + entry.getKey() + " " + Command.SECTIONSIGN() + "8x" + entry.getValue());
                 }
 
 //                entityLabel.getParent().setHeight(entityLabel.getLines().length * (entityLabel.getTheme().getFontRenderer().getFontHeight()+1) + 3);
@@ -321,6 +317,7 @@ public class KamiGUI extends GUI {
         Label coordsLabel = new Label("");
         coordsLabel.addTickListener(new TickListener() {
             Minecraft mc = Minecraft.getMinecraft();
+
             @Override
             public void onTick() {
                 boolean inHell = (mc.world.getBiome(mc.player.getPosition()).getBiomeName().equals("Hell"));
@@ -353,7 +350,7 @@ public class KamiGUI extends GUI {
                         Command.SECTIONSIGN(),
                         hposZ,
                         Command.SECTIONSIGN()
-                        ));
+                ));
             }
         });
         frame.addChild(coordsLabel);
@@ -371,13 +368,13 @@ public class KamiGUI extends GUI {
         frame.setHeight(100);
         frames.add(frame);
 
-        for (Frame frame1 : frames){
+        for (Frame frame1 : frames) {
             frame1.setX(x);
             frame1.setY(y);
 
             nexty = Math.max(y + frame1.getHeight() + 10, nexty);
             x += frame1.getWidth() + 10;
-            if (x*DisplayGuiScreen.getScale() > Wrapper.getMinecraft().displayWidth / 1.2f){
+            if (x * DisplayGuiScreen.getScale() > Wrapper.getMinecraft().displayWidth / 1.2f) {
                 y = nexty;
                 nexty = y;
                 x = 10;
@@ -387,16 +384,14 @@ public class KamiGUI extends GUI {
         }
     }
 
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map )
-    {
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
         List<Map.Entry<K, V>> list =
-                new LinkedList<>( map.entrySet() );
+                new LinkedList<>(map.entrySet());
         Collections.sort(list, Comparator.comparing(o -> (o.getValue())));
 
         Map<K, V> result = new LinkedHashMap<K, V>();
-        for (Map.Entry<K, V> entry : list)
-        {
-            result.put( entry.getKey(), entry.getValue() );
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
         }
         return result;
     }
@@ -407,6 +402,7 @@ public class KamiGUI extends GUI {
     }
 
     private static final int DOCK_OFFSET = 0;
+
     public static void dock(Frame component) {
         Docking docking = component.getDocking();
         if (docking.isTop())
