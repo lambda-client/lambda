a3987df978f7fd635bd8b3f2bb4a7ddd0f439a6b
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2018-08-08 23:18:31 +0200

    add NoPacketKick

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinNetworkManager.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinNetworkManager.java
@@ -3,6 +3,7 @@ package me.zeroeightsix.kami.mixin.client;
 import io.netty.channel.ChannelHandlerContext;
 import me.zeroeightsix.kami.KamiMod;
 import me.zeroeightsix.kami.event.events.PacketEvent;
+import me.zeroeightsix.kami.module.modules.misc.NoPacketKick;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.CPacketCustomPayload;
@@ -12,6 +13,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.IOError;
+import java.io.IOException;
 
 /**
  * Created by 086 on 13/11/2017.
@@ -37,6 +41,11 @@ public class MixinNetworkManager {
         if (event.isCancelled()) {
             callbackInfo.cancel();
         }
+    }
+
+    @Inject(method = "exceptionCaught", at = @At("HEAD"), cancellable = true)
+    private void exceptionCaught(ChannelHandlerContext p_exceptionCaught_1_, Throwable p_exceptionCaught_2_, CallbackInfo info) {
+        if (p_exceptionCaught_2_ instanceof IOException && NoPacketKick.isEnabled()) info.cancel();
     }
 
 }
---
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/NoPacketKick.java
@@ -0,0 +1,22 @@
+package me.zeroeightsix.kami.module.modules.misc;
+
+import me.zeroeightsix.kami.module.Module;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author 086
+ * @see me.zeroeightsix.kami.mixin.client.MixinNetworkManager
+ */
+@Module.Info(name = "NoPacketKick", category = Module.Category.MISC, description = "Prevent large packets from kicking you")
+public class NoPacketKick {
+    private static NoPacketKick INSTANCE;
+
+    public NoPacketKick() {
+        INSTANCE = this;
+    }
+
+    public static boolean isEnabled() {
+        return INSTANCE.isEnabled();
+    }
+
+}
