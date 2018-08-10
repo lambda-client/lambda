e355089bc031d01c6806a9d1f6696f4a172c1095
Ridan Vandenbergh <RidanVandenbergh@gmail.com>
2018-08-10 15:46:36 +0200

    add nohurtcam

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntityRenderer.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntityRenderer.java
@@ -2,6 +2,7 @@ package me.zeroeightsix.kami.mixin.client;
 
 import me.zeroeightsix.kami.module.ModuleManager;
 import me.zeroeightsix.kami.module.modules.render.AntiFog;
+import me.zeroeightsix.kami.module.modules.render.NoHurtCam;
 import me.zeroeightsix.kami.util.Wrapper;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
@@ -27,22 +28,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderer.class)
 public class MixinEntityRenderer {
 
-    // all your fields and methods belong to me
-    @Shadow public float farPlaneDistance;
-    @Shadow public int frameCount;
-    @Shadow public boolean debugView;
-    @Shadow public boolean renderHand;
-    @Shadow public boolean isDrawBlockOutline() {return true;}
-    @Shadow public void updateFogColor(float partialTicks) {}
-    @Shadow public void setupFog(int startCoords, float partialTicks) {}
-    @Shadow public void setupCameraTransform(float partialTicks, int pass) {}
-    @Shadow public float getFOVModifier(float partialTicks, boolean useFOVSetting) { return 0f; }
-    @Shadow public void renderCloudsCheck(RenderGlobal renderGlobalIn, float partialTicks, int pass, double x, double y, double z) {}
-    @Shadow public void enableLightmap() {}
-    @Shadow public void disableLightmap() {}
-    @Shadow private void renderRainSnow(float partialTicks) {}
-    @Shadow public void renderHand(float partialTicks, int pass) {}
-
     Minecraft mc = Wrapper.getMinecraft();
 
     @Redirect(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;rayTraceBlocks(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/RayTraceResult;"))
@@ -65,194 +50,9 @@ public class MixinEntityRenderer {
         return ActiveRenderInfo.getBlockStateAtEntityViewpoint(worldIn, entityIn, p_186703_2_);
     }
 
-    /*@Inject(method = "renderWorldPass", at = @At(value = "HEAD"), cancellable = true)
-    public void renderWorldPass(int pass, float partialTicks, long finishTimeNano, CallbackInfo info) {
-        info.cancel();
-
-        RenderGlobal renderglobal = this.mc.renderGlobal;
-        ParticleManager particlemanager = this.mc.effectRenderer;
-        boolean flag = this.isDrawBlockOutline();
-        GlStateManager.enableCull();
-        this.mc.mcProfiler.endStartSection("clear");
-        GlStateManager.viewport(0, 0, this.mc.displayWidth, this.mc.displayHeight);
-        this.updateFogColor(partialTicks);
-        GlStateManager.clear(16640);
-        this.mc.mcProfiler.endStartSection("camera");
-        this.setupCameraTransform(partialTicks, pass);
-        ActiveRenderInfo.updateRenderInfo(this.mc.player, this.mc.gameSettings.thirdPersonView == 2);
-        this.mc.mcProfiler.endStartSection("frustum");
-        ClippingHelperImpl.getInstance();
-        this.mc.mcProfiler.endStartSection("culling");
-        ICamera icamera = new Frustum();
-        Entity entity = this.mc.getRenderViewEntity();
-        double d0 = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * (double)partialTicks;
-        double d1 = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * (double)partialTicks;
-        double d2 = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * (double)partialTicks;
-        icamera.setPosition(d0, d1, d2);
-
-        if (this.mc.gameSettings.renderDistanceChunks >= 4)
-        {
-            this.setupFog(-1, partialTicks);
-            this.mc.mcProfiler.endStartSection("sky");
-            GlStateManager.matrixMode(5889);
-            GlStateManager.loadIdentity();
-            Project.gluPerspective(this.getFOVModifier(partialTicks, true), (float)this.mc.displayWidth / (float)this.mc.displayHeight, 0.05F, this.farPlaneDistance * 2.0F);
-            GlStateManager.matrixMode(5888);
-            renderglobal.renderSky(partialTicks, pass);
-            GlStateManager.matrixMode(5889);
-            GlStateManager.loadIdentity();
-            Project.gluPerspective(this.getFOVModifier(partialTicks, true), (float)this.mc.displayWidth / (float)this.mc.displayHeight, 0.05F, this.farPlaneDistance * MathHelper.SQRT_2);
-            GlStateManager.matrixMode(5888);
-        }
-
-        this.setupFog(0, partialTicks);
-        GlStateManager.shadeModel(7425);
-
-        if (entity.posY + (double)entity.getEyeHeight() < 128.0D)
-        {
-            this.renderCloudsCheck(renderglobal, partialTicks, pass, d0, d1, d2);
-        }
-
-        this.mc.mcProfiler.endStartSection("prepareterrain");
-        this.setupFog(0, partialTicks);
-        this.mc.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        RenderHelper.disableStandardItemLighting();
-        this.mc.mcProfiler.endStartSection("terrain_setup");
-        renderglobal.setupTerrain(entity, (double)partialTicks, icamera, this.frameCount++, this.mc.player.isSpectator());
-
-        if (pass == 0 || pass == 2)
-        {
-            this.mc.mcProfiler.endStartSection("updatechunks");
-            this.mc.renderGlobal.updateChunks(finishTimeNano);
-        }
-
-        this.mc.mcProfiler.endStartSection("terrain");
-
-        if (Wireframe.INSTANCE.isEnabled()) {
-            GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
-            GL11.glEnable(GL11.GL_POLYGON_OFFSET_FILL);
-            GL11.glPolygonOffset(-1f, -1f);
-            GL11.glLineWidth(1f);
-            GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
-        }
-
-        GlStateManager.matrixMode(5888);
-        GlStateManager.pushMatrix();
-        renderglobal.renderBlockLayer(BlockRenderLayer.SOLID, (double)partialTicks, pass, entity);
-        GlStateManager.enableAlpha();
-        renderglobal.renderBlockLayer(BlockRenderLayer.CUTOUT_MIPPED, (double)partialTicks, pass, entity);
-        this.mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
-        renderglobal.renderBlockLayer(BlockRenderLayer.CUTOUT, (double)partialTicks, pass, entity);
-
-        if (Wireframe.INSTANCE.isEnabled())
-            GL11.glPopAttrib();
-
-        this.mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
-        GlStateManager.shadeModel(7424);
-        GlStateManager.alphaFunc(516, 0.1F);
-
-        if (!this.debugView)
-        {
-            GlStateManager.matrixMode(5888);
-            GlStateManager.popMatrix();
-            GlStateManager.pushMatrix();
-            RenderHelper.enableStandardItemLighting();
-            this.mc.mcProfiler.endStartSection("entities");
-            net.minecraftforge.client.ForgeHooksClient.setRenderPass(0);
-            renderglobal.renderEntities(entity, icamera, partialTicks);
-            net.minecraftforge.client.ForgeHooksClient.setRenderPass(0);
-            RenderHelper.disableStandardItemLighting();
-            this.disableLightmap();
-        }
-
-        GlStateManager.matrixMode(5888);
-        GlStateManager.popMatrix();
-
-        if (flag && this.mc.objectMouseOver != null && !entity.isInsideOfMaterial(Material.WATER))
-        {
-            EntityPlayer entityplayer = (EntityPlayer)entity;
-            GlStateManager.disableAlpha();
-            this.mc.mcProfiler.endStartSection("outline");
-            if (!net.minecraftforge.client.ForgeHooksClient.onDrawBlockHighlight(renderglobal, entityplayer, mc.objectMouseOver, 0, partialTicks))
-                renderglobal.drawSelectionBox(entityplayer, this.mc.objectMouseOver, 0, partialTicks);
-            GlStateManager.enableAlpha();
-        }
-
-        if (this.mc.debugRenderer.shouldRender())
-        {
-            this.mc.debugRenderer.renderDebug(partialTicks, finishTimeNano);
-        }
-
-        this.mc.mcProfiler.endStartSection("destroyProgress");
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        this.mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
-        renderglobal.drawBlockDamageTexture(Tessellator.getInstance(), Tessellator.getInstance().getBuffer(), entity, partialTicks);
-        this.mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
-        GlStateManager.disableBlend();
-
-        if (!this.debugView)
-        {
-            this.enableLightmap();
-            this.mc.mcProfiler.endStartSection("litParticles");
-            particlemanager.renderLitParticles(entity, partialTicks);
-            RenderHelper.disableStandardItemLighting();
-            this.setupFog(0, partialTicks);
-            this.mc.mcProfiler.endStartSection("particles");
-            particlemanager.renderParticles(entity, partialTicks);
-            this.disableLightmap();
-        }
-
-        GlStateManager.depthMask(false);
-        GlStateManager.enableCull();
-        this.mc.mcProfiler.endStartSection("weather");
-        this.renderRainSnow(partialTicks);
-        GlStateManager.depthMask(true);
-        renderglobal.renderWorldBorder(entity, partialTicks);
-        GlStateManager.disableBlend();
-        GlStateManager.enableCull();
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        GlStateManager.alphaFunc(516, 0.1F);
-        this.setupFog(0, partialTicks);
-        GlStateManager.enableBlend();
-        GlStateManager.depthMask(false);
-        this.mc.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        GlStateManager.shadeModel(7425);
-        this.mc.mcProfiler.endStartSection("translucent");
-        renderglobal.renderBlockLayer(BlockRenderLayer.TRANSLUCENT, (double)partialTicks, pass, entity);
-        if (!this.debugView) //Only render if render pass 0 happens as well.
-        {
-            RenderHelper.enableStandardItemLighting();
-            this.mc.mcProfiler.endStartSection("entities");
-            net.minecraftforge.client.ForgeHooksClient.setRenderPass(1);
-            renderglobal.renderEntities(entity, icamera, partialTicks);
-            // restore blending function changed by RenderGlobal.preRenderDamagedBlocks
-            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-            net.minecraftforge.client.ForgeHooksClient.setRenderPass(-1);
-            RenderHelper.disableStandardItemLighting();
-        }
-        GlStateManager.shadeModel(7424);
-        GlStateManager.depthMask(true);
-        GlStateManager.enableCull();
-        GlStateManager.disableBlend();
-        GlStateManager.disableFog();
-
-        if (entity.posY + (double)entity.getEyeHeight() >= 128.0D)
-        {
-            this.mc.mcProfiler.endStartSection("aboveClouds");
-            this.renderCloudsCheck(renderglobal, partialTicks, pass, d0, d1, d2);
-        }
-
-        this.mc.mcProfiler.endStartSection("forge_render_last");
-        net.minecraftforge.client.ForgeHooksClient.dispatchRenderLast(renderglobal, partialTicks);
-
-        this.mc.mcProfiler.endStartSection("hand");
-
-        if (this.renderHand)
-        {
-            GlStateManager.clear(256);
-            this.renderHand(partialTicks, pass);
-        }
-    }*/
+    @Inject(method = "hurtCameraEffect", at = @At("HEAD"), cancellable = true)
+    public void hurtCameraEffect(float ticks, CallbackInfo info) {
+        if (NoHurtCam.shouldDisable()) info.cancel();
+    }
 
 }
