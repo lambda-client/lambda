f43b4a64974fb3c57e3846f1aa9c0b26c8c25368
zeroeightsix <RidanVandenbergh@gmail.com>
2018-10-10 19:19:43 +0200

    Prefix command

---
+++ src/main/java/me/zeroeightsix/kami/command/commands/PrefixCommand.java
@@ -0,0 +1,26 @@
+package me.zeroeightsix.kami.command.commands;
+
+import me.zeroeightsix.kami.command.Command;
+import me.zeroeightsix.kami.command.syntax.ChunkBuilder;
+
+/**
+ * Created by 086 on 10/10/2018.
+ */
+public class PrefixCommand extends Command {
+
+    public PrefixCommand() {
+        super("prefix", new ChunkBuilder().append("character").build());
+    }
+
+    @Override
+    public void call(String[] args) {
+        if (args.length == 0) {
+            Command.sendChatMessage("Please specify a new prefix!");
+            return;
+        }
+
+        Command.COMMAND_PREFIX = args[0];
+        Command.sendChatMessage("Prefix set to &b" + Command.COMMAND_PREFIX);
+    }
+
+}
