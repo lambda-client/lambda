e736bc4442a4e144b4fb1e59c0aa189c16f6e1a7
zeroeightsix <RidanVandenbergh@gmail.com>
2018-10-11 22:29:39 +0200

    Implement SafeWalk Closes #44

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntity.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntity.java
@@ -2,6 +2,7 @@ package me.zeroeightsix.kami.mixin.client;
 
 import me.zeroeightsix.kami.KamiMod;
 import me.zeroeightsix.kami.event.events.EntityEvent;
+import me.zeroeightsix.kami.module.modules.movement.SafeWalk;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,6 +25,11 @@ public class MixinEntity {
         entity.motionZ += z;
 
         entity.isAirBorne = true;
+    }
+
+    @Redirect(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isSneaking()Z"))
+    public boolean isSneaking(Entity entity) {
+        return SafeWalk.shouldSafewalk() || entity.isSneaking();
     }
 
 }
