03833565843a639273794a2447f0d2d66beaec4b
zeroeightsix <RidanVandenbergh@gmail.com>
2018-10-13 23:22:04 +0200

    oops!! corrected converters

--- src/main/java/me/zeroeightsix/kami/setting/converter/AbstractBoxedNumberConverter.java
+++ src/main/java/me/zeroeightsix/kami/setting/converter/AbstractBoxedNumberConverter.java
@@ -1,15 +1,17 @@
 package me.zeroeightsix.kami.setting.converter;
 
 import com.google.common.base.Converter;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 
 /**
  * Created by 086 on 13/10/2018.
  */
-public abstract class AbstractBoxedNumberConverter<T extends Number> extends Converter<T, String> {
+public abstract class AbstractBoxedNumberConverter<T extends Number> extends Converter<T, JsonElement> {
 
     @Override
-    protected String doForward(T t) {
-        return t.toString();
+    protected JsonElement doForward(T t) {
+        return new JsonPrimitive(t);
     }
 
 }
