3f8f1e511b6a18ba48a55376d6131c02c0182ed1
zeroeightsix <RidanVandenbergh@gmail.com>
2018-10-14 11:34:27 +0200

    misc

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/PortalChat.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/PortalChat.java
@@ -4,10 +4,8 @@ import me.zeroeightsix.kami.module.Module;
 
 /**
  * Created by 086 on 12/12/2017.
+ * @see me.zeroeightsix.kami.mixin.client.MixinEntityPlayerSP
  */
 @Module.Info(name = "PortalChat", category = Module.Category.MISC, description = "Allows you to open GUIs in portals")
 public class PortalChat extends Module {
-
-    // Code in mixin
-
 }
