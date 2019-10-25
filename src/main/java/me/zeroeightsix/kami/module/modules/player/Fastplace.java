374664e8edf6ce7baab68a370418dd162e541ef0
S-B99 <bstallman99@gmail.com>
2019-10-25 16:37:43 -0400

    update fastplace to take values

--- src/main/java/me/zeroeightsix/kami/module/modules/player/Fastplace.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/Fastplace.java
@@ -1,15 +1,20 @@
 package me.zeroeightsix.kami.module.modules.player;
 
 import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
 
 /**
  * @author 086
+ * Updated by S-B99 on 25/10/19
  */
 @Module.Info(name = "Fastplace", category = Module.Category.PLAYER, description = "Nullifies block place delay")
 public class Fastplace extends Module {
 
+	private Setting<Integer> delay = register(Settings.i("Delay", 0));
+
     @Override
     public void onUpdate() {
-        mc.rightClickDelayTimer = 0;
+        mc.rightClickDelayTimer = delay.getValue();
     }
 }
