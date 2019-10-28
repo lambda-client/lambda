b6e2f72941005d66debbb9395ab4e7f5b2f7d358
S-B99 <bstallman99@gmail.com>
2019-10-27 21:19:48 -0400

    start working on rpc

---
+++ src/main/java/me/zeroeightsix/kami/DiscordRPC.java
@@ -0,0 +1,16 @@
+package me.zeroeightsix.kami;
+
+import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
+
+//import club.minnced.discord.rpc.*;
+
+/**
+ * @author 086
+ * Updated by S-B99 on 27/10/19
+ */
+@Module.Info(name = "DiscordRPC", category = Module.Category.PLAYER, description = "Discord Rich Presence")
+public class DiscordRPC extends Module {
+
+}
