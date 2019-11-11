94ee71ebecd3777f0b2729d6e542e5aba5746112
S-B99 <bstallman99@gmail.com>
2019-11-11 15:54:52 -0500

    clean up guicolor code

--- src/main/java/me/zeroeightsix/kami/module/modules/sdashb/experimental/GUIColor.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/sdashb/experimental/GUIColor.java
@@ -1,16 +1,10 @@
-//package me.robeart.voidclient.module.modules.render;
 package me.zeroeightsix.kami.module.modules.sdashb.experimental;
 
-//1//import me.robeart.voidclient.module.Module;
-//1//import me.robeart.voidclient.setting.Setting;
-//1//import me.robeart.voidclient.setting.Settings;
-//1//import me.robeart.voidclient.setting.builder.numerical.NumericalSettingBuilder;
-//1//import me.robeart.voidclient.setting.impl.numerical.NumberSetting;
 import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
-import me.zeroeightsix.kami.setting.builder.numerical.NumericalSettingBuilder;
-import me.zeroeightsix.kami.setting.impl.numerical.NumberSetting;
+//import me.zeroeightsix.kami.setting.builder.numerical.NumericalSettingBuilder;
+//import me.zeroeightsix.kami.setting.impl.numerical.NumberSetting;
 
 @Module.Info(name="GUI Color", description="Change GUI Colors", category=Module.Category.EXPERIMENTAL)
 public class GUIColor extends Module {
