19bb3b2d9c82df8d1c631f12490d8d0cc4e9273f
S-B99 <bstallman99@gmail.com>
2019-11-11 20:55:47 -0500

    change gui color defaults

--- src/main/java/me/zeroeightsix/kami/module/modules/sdashb/experimental/GUIColor.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/sdashb/experimental/GUIColor.java
@@ -8,8 +8,8 @@ import me.zeroeightsix.kami.setting.Settings;
 
 @Module.Info(name="GUI Color", description="Change GUI Colors", category=Module.Category.EXPERIMENTAL)
 public class GUIColor extends Module {
-    public Setting<Integer> red = this.register(Settings.integerBuilder("Red").withMinimum(0).withValue(80).withMaximum(255).build());
-    public Setting<Integer> green = this.register(Settings.integerBuilder("Green").withMinimum(0).withValue(80).withMaximum(255).build());
-    public Setting<Integer> blue = this.register(Settings.integerBuilder("Blue").withMinimum(0).withValue(80).withMaximum(255).build());
-    public Setting<Integer> alpha = this.register(Settings.integerBuilder("Alpha").withMinimum(0).withValue(200).withMaximum(255).build());
+    public Setting<Integer> red = this.register(Settings.integerBuilder("Red").withMinimum(0).withValue(13).withMaximum(255).build());
+    public Setting<Integer> green = this.register(Settings.integerBuilder("Green").withMinimum(0).withValue(13).withMaximum(255).build());
+    public Setting<Integer> blue = this.register(Settings.integerBuilder("Blue").withMinimum(0).withValue(13).withMaximum(255).build());
+    public Setting<Integer> alpha = this.register(Settings.integerBuilder("Alpha").withMinimum(0).withValue(117).withMaximum(255).build());
 }
