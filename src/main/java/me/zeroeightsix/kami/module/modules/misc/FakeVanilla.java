78ddacb37dca05ac92ea870c0da79f4a75b4ea32
Bella <bella.who.two@gmail.com>
2019-11-20 17:32:03 -0500

    rename fakevanilla to fakevanillaclient to make it more clear

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/FakeVanilla.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/FakeVanilla.java
@@ -5,6 +5,6 @@ import me.zeroeightsix.kami.module.Module;
 /**
  * Created by 086 on 9/04/2018.
  */
-@Module.Info(name = "FakeVanilla", description = "Fakes a modless client when connecting", category = Module.Category.MISC)
+@Module.Info(name = "FakeVanillaClient", description = "Fakes a modless client when connecting", category = Module.Category.MISC)
 public class FakeVanilla extends Module {
 }
