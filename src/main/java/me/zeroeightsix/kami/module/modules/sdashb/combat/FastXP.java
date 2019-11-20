efcdd961c186f64fff79d55cf2c561a695d1292a
Bella <bella.who.two@gmail.com>
2019-11-20 17:18:45 -0500

    add fast xp

---
+++ src/main/java/me/zeroeightsix/kami/module/modules/sdashb/combat/FastXP.java
@@ -0,0 +1,16 @@
+package me.zeroeightsix.kami.module.modules.sdashb.combat;
+
+import me.zeroeightsix.kami.module.Module;
+import net.minecraft.item.ItemExpBottle;
+
+/***
+ * @author Jamie
+ */
+@Module.Info(name = "FastXP", category = Module.Category.COMBAT, description = "Makes XP Faster for PvP")
+public class FastXP extends Module {
+    public void onUpdate() {
+        if (mc.player.inventory.getCurrentItem().getItem() instanceof ItemExpBottle) {
+            mc.rightClickDelayTimer = 0;
+        }
+    }
+}
