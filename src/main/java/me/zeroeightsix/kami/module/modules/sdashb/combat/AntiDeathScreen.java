14261a43e0eab32630231a50540c06e22a8b5bee
Bella <bella.who.two@gmail.com>
2019-11-27 09:07:51 -0500

    finish and close #48

--- src/main/java/me/zeroeightsix/kami/module/modules/sdashb/combat/AntiDeathScreen.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/sdashb/combat/AntiDeathScreen.java
@@ -1,29 +1,24 @@
 package me.zeroeightsix.kami.module.modules.sdashb.combat;
 
-import java.util.function.Predicate;
-import me.zero.alpine.listener.EventHandler;
-import me.zero.alpine.listener.Listener;
 import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
 import net.minecraft.client.gui.GuiGameOver;
-import net.minecraft.client.gui.GuiScreen;
 
-@Module.Info(name = "AntiDeathScreen", description = "Fixs random death screen glitches", category = Module.Category.EXPERIMENTAL)
+@Module.Info(name = "AntiDeathScreen", description = "Fixs random death screen glitches", category = Module.Category.COMBAT)
 public class AntiDeathScreen extends Module {
-    private Setting<Boolean> respawn = this.register(Settings.b("Respawn", true));
-    @EventHandler
-    public Listener listener;
 
-    public AntiDeathScreen() {
-        listener = new Listener((event) -> {
-    //        if (event.getScreen() instanceof GuiGameOver && (Boolean)this.respawn.getValue()) {
-    //            mc.player.respawnPlayer();
-    //            if (!mc.player.isDead) {
-    //                mc.displayGuiScreen((GuiScreen)null);
-    //            }
-    //        }
+    //private Setting<Boolean> respawn = this.register(Settings.b("Respawn", true));
 
-        }, new Predicate[0]);
+    @Override
+    public void onUpdate()
+    {
+        if (!this.isEnabled()) return;
+        if (mc.player.getHealth() > 0 && mc.currentScreen instanceof GuiGameOver)
+        {
+            mc.player.respawnPlayer();
+            mc.displayGuiScreen(null);
+        }
     }
+
 }
