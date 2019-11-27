2c36648d0ad8046f9d288ca66a1bb044e7317ef6
Bella <bella.who.two@gmail.com>
2019-11-27 09:00:37 -0500

    make autoqmain have better run states

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/AutoQMain.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/AutoQMain.java
@@ -10,7 +10,7 @@ import net.minecraft.network.play.client.CPacketChatMessage;
 
 /**
  * Created by d1gress/Qther on 24/11/2019.
- * Updated by @S-B99 on 25/11/19
+ * Updated by @S-B99 on 27/11/19
  */
 @Module.Info(name = "AutoQMain", description = "Automatically does \"/queue main\" every 7.1 minutes.", category = Module.Category.MISC)
 public class AutoQMain extends Module {
@@ -18,30 +18,31 @@ public class AutoQMain extends Module {
     private Setting<Boolean> debug = register(Settings.b("Debug", true));
 
     private static long startTime = 0;
-    private static long startTimeCon = 0;
 
     @Override
     public void onUpdate() {
         if (mc.player == null) return;
 
-        if (Minecraft.getMinecraft().getCurrentServerData() == null || (Minecraft.getMinecraft().getCurrentServerData() !=null && !Minecraft.getMinecraft().getCurrentServerData().serverIP.equalsIgnoreCase("2b2t.org"))) {
-            startTime = 0;
-            if (startTimeCon == 0 || startTimeCon <= System.currentTimeMillis() - 600000) startTimeCon = System.currentTimeMillis();
-            if (startTimeCon + 600000 <= System.currentTimeMillis()) {
-                Command.sendWarningMessage("&cYou are not connected to 2b2t.org, so AutoQMain will not function on this server.");
-                startTimeCon = System.currentTimeMillis();
-            }
-        }
-        else {
+        if ((Minecraft.getMinecraft().getCurrentServerData() !=null)) {
+
             if (startTime == 0 || startTime <= System.currentTimeMillis() - 427000) startTime = System.currentTimeMillis();
             if (startTime + 426000 <= System.currentTimeMillis()) {
-                if (debug.getValue()) {
-                    Command.sendChatMessage("&7Run &b/queue main&7 at " + System.currentTimeMillis());
+                if (Minecraft.getMinecraft().getCurrentServerData() == null) {
+                    Command.sendWarningMessage("&cYou are currently in SinglePlayer, so AutoQMain will not run");
                 }
-                Minecraft.getMinecraft().playerController.connection.sendPacket(new CPacketChatMessage("/queue main"));
-                Command.sendChatMessage("Sent \"/queue main\" at " + System.currentTimeMillis());
+                if (Minecraft.getMinecraft().getCurrentServerData().serverIP.equalsIgnoreCase("2b2t.org")) {
+                    if (debug.getValue()) {
+                        Command.sendChatMessage("&7Run &b/queue main&7 at " + System.currentTimeMillis());
+                    }
+                    Minecraft.getMinecraft().playerController.connection.sendPacket(new CPacketChatMessage("/queue main"));
+                    Command.sendChatMessage("Sent \"/queue main\" at " + System.currentTimeMillis());
+                }
+                if (!Minecraft.getMinecraft().getCurrentServerData().serverIP.equalsIgnoreCase("2b2t.org")) {
+                    Command.sendWarningMessage("&cYou are not connected to 2b2t.org, so AutoQMain will not function on this server.");
+                }
                 startTime = System.currentTimeMillis();
             }
+            startTime = 0;
         }
     }
 }
