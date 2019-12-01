067a29887b6aa1d29f330c68d298894dd47a9d0a
Bella <bella.who.two@gmail.com>
2019-12-01 18:32:16 -0500

    fix credit for fastxp xD

--- src/main/java/me/zeroeightsix/kami/module/modules/sdashb/combat/FastXP.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/sdashb/combat/FastXP.java
@@ -4,7 +4,7 @@ import me.zeroeightsix.kami.module.Module;
 import net.minecraft.item.ItemExpBottle;
 
 /***
- * @author Jamie
+ * @author S-B99
  */
 @Module.Info(name = "FastXP", category = Module.Category.COMBAT, description = "Makes XP Faster for PvP")
 public class FastXP extends Module {
