c3410709dc38fb09bb05f4696080fdf81aa36261
Bella <bella.who.two@gmail.com>
2019-12-03 09:01:16 -0500

    changed to case enum modes instead

--- src/main/java/me/zeroeightsix/kami/module/modules/player/Fastuse.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/Fastuse.java
@@ -1,43 +1,74 @@
 package me.zeroeightsix.kami.module.modules.player;
 
-import me.zero.alpine.listener.EventHandler;
-import me.zero.alpine.listener.Listener;
-import me.zeroeightsix.kami.event.events.PacketEvent;
 import me.zeroeightsix.kami.module.Module;
-// info goes here
-import me.zeroeightsix.kami.module.modules.misc.BookCrash;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
-import net.minecraft.init.Items;
-
-//import me.zeroeightsix.kami.module.Module.Info;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemEndCrystal;
+import net.minecraft.item.ItemExpBottle;
 
 /**
  * Created by S-B99 on 23/10/2019
  * @author S-B99
  * Updated by d1gress/Qther on 2/12/2019
+ * Updated by S-B99 on 03/12/19
  */
 @Module.Info(category = Module.Category.PLAYER, description = "Changes delay when holding right click", name = "FastUse")
 public class Fastuse extends Module {
 
-	private Setting<Boolean> exp = register(Settings.b("EXP Bottles", false));
-	private Setting<Boolean> crystal = register(Settings.b("Crystals", false));
-	private Setting<Boolean> all = register(Settings.b("Everything", true));
-	private Setting<Integer> delay = register(Settings.i("Delay", 0));
+
+//	private Setting<Integer> delay = register(Settings.i("Delay", 0));
+	private Setting<Double> delay = register(Settings.doubleBuilder("Delay").withMinimum(0.0).withValue(0.0).withMaximum(10.0).build());
+	private Setting<Mode> mode = register(Settings.e("Mode", Mode.BOTH));
 	private static long time = 0;
-
-
+//	private int modeCase;
+	private enum Mode {
+		ALL, BOTH, EXP, CRYSTAL
+	}
 	@Override
 	public void onUpdate() {
+		Item itemMain = mc.player.getHeldItemMainhand().getItem();
+		Item itemOff = mc.player.getHeldItemOffhand().getItem();
+		boolean doExpMain = itemMain instanceof ItemExpBottle;
+		boolean doExpOff = itemOff instanceof ItemExpBottle;
+		boolean doCrystalMain = itemMain instanceof ItemEndCrystal;
+		boolean doCrystalOff = itemOff instanceof ItemEndCrystal;
+
 		if (mc.player != null && delay.getValue() <= 0) {
-			if (!all.getValue() && (mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE || mc.player.getHeldItemOffhand().getItem() == Items.EXPERIENCE_BOTTLE) && exp.getValue())  mc.rightClickDelayTimer = 0;
-			if (!all.getValue() && (mc.player.getHeldItemMainhand().getItem() == Items.END_CRYSTAL || mc.player.getHeldItemOffhand().getItem() == Items.END_CRYSTAL) && crystal.getValue())  mc.rightClickDelayTimer = 0;
-			if (all.getValue())  mc.rightClickDelayTimer = 0;
+			switch(mode.getValue()){
+				case ALL:
+					mc.rightClickDelayTimer = 0;
+				case BOTH:
+					if (doExpMain || doExpOff || doCrystalMain || doCrystalOff) {
+						mc.rightClickDelayTimer = 0;
+					}
+				case EXP:
+					if (doExpMain || doExpOff) {
+						mc.rightClickDelayTimer = 0;
+					}
+				case CRYSTAL:
+					if (doCrystalMain || doCrystalOff) {
+						mc.rightClickDelayTimer = 0;
+					}
+			}
 		}
 		else if (mc.player != null && time == 0 || time + delay.getValue() * 50 <= System.currentTimeMillis()) { //scales weird
-			if (!all.getValue() && (mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE || mc.player.getHeldItemOffhand().getItem() == Items.EXPERIENCE_BOTTLE) && exp.getValue())  mc.rightClickDelayTimer = 0;
-			if (!all.getValue() && (mc.player.getHeldItemMainhand().getItem() == Items.END_CRYSTAL || mc.player.getHeldItemOffhand().getItem() == Items.END_CRYSTAL) && crystal.getValue())  mc.rightClickDelayTimer = 0;
-			if (all.getValue())  mc.rightClickDelayTimer = 0;
+			switch(mode.getValue()){
+				case ALL:
+					mc.rightClickDelayTimer = 0;
+				case BOTH:
+					if (doExpMain || doExpOff || doCrystalMain || doCrystalOff) {
+						mc.rightClickDelayTimer = 0;
+					}
+				case EXP:
+					if (doExpMain || doExpOff) {
+						mc.rightClickDelayTimer = 0;
+					}
+				case CRYSTAL:
+					if (doCrystalMain || doCrystalOff) {
+						mc.rightClickDelayTimer = 0;
+					}
+			}
 			time = System.currentTimeMillis();
 		}
 	}
