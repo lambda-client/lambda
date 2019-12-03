a13778287c38ad96e8f2733541e3fb2562bd3bae
d1gress <55198830+d1gress@users.noreply.github.com>
2019-12-03 21:23:30 +0800

    Merge fastplace into fastuse and make fastuse better (#202)
    * Merge fastplace into fastuse pt 1
    
    * finished
    
    * wait what ok fix
    
    * yeet

--- src/main/java/me/zeroeightsix/kami/module/modules/player/Fastuse.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/Fastuse.java
@@ -5,6 +5,9 @@ import me.zero.alpine.listener.Listener;
 import me.zeroeightsix.kami.event.events.PacketEvent;
 import me.zeroeightsix.kami.module.Module;
 // info goes here
+import me.zeroeightsix.kami.module.modules.misc.BookCrash;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
 import net.minecraft.init.Items;
 
 //import me.zeroeightsix.kami.module.Module.Info;
@@ -12,18 +15,30 @@ import net.minecraft.init.Items;
 /**
  * Created by S-B99 on 23/10/2019
  * @author S-B99
+ * Updated by d1gress/Qther on 2/12/2019
  */
-@Module.Info(category = Module.Category.PLAYER, description = "Removes delay when holding right click", name = "FastUse")
+@Module.Info(category = Module.Category.PLAYER, description = "Changes delay when holding right click", name = "FastUse")
 public class Fastuse extends Module {
-	
-	@EventHandler
-	private Listener<PacketEvent.Receive> receiveListener = new Listener<>(event ->
-	{
 
-		if (mc.player != null && (mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE || mc.player.getHeldItemOffhand().getItem() == Items.EXPERIENCE_BOTTLE))
-		{
-			mc.rightClickDelayTimer = 0;
+	private Setting<Boolean> exp = register(Settings.b("EXP Bottles", false));
+	private Setting<Boolean> crystal = register(Settings.b("Crystals", false));
+	private Setting<Boolean> all = register(Settings.b("Everything", true));
+	private Setting<Integer> delay = register(Settings.i("Delay", 0));
+	private static long time = 0;
+
+
+	@Override
+	public void onUpdate() {
+		if (mc.player != null && delay.getValue() <= 0) {
+			if (!all.getValue() && (mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE || mc.player.getHeldItemOffhand().getItem() == Items.EXPERIENCE_BOTTLE) && exp.getValue())  mc.rightClickDelayTimer = 0;
+			if (!all.getValue() && (mc.player.getHeldItemMainhand().getItem() == Items.END_CRYSTAL || mc.player.getHeldItemOffhand().getItem() == Items.END_CRYSTAL) && crystal.getValue())  mc.rightClickDelayTimer = 0;
+			if (all.getValue())  mc.rightClickDelayTimer = 0;
+		}
+		else if (mc.player != null && time == 0 || time + delay.getValue() * 50 <= System.currentTimeMillis()) { //scales weird
+			if (!all.getValue() && (mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE || mc.player.getHeldItemOffhand().getItem() == Items.EXPERIENCE_BOTTLE) && exp.getValue())  mc.rightClickDelayTimer = 0;
+			if (!all.getValue() && (mc.player.getHeldItemMainhand().getItem() == Items.END_CRYSTAL || mc.player.getHeldItemOffhand().getItem() == Items.END_CRYSTAL) && crystal.getValue())  mc.rightClickDelayTimer = 0;
+			if (all.getValue())  mc.rightClickDelayTimer = 0;
+			time = System.currentTimeMillis();
 		}
 	}
-			);
 }
