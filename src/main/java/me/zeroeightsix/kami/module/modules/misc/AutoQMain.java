08f626095364cb632cb5585688a0d9ed60b8277c
d1gress <55198830+d1gress@users.noreply.github.com>
2019-12-04 23:47:35 +0800

    Revert "Revert "Change AutoQMain to use ticks (#219)""
    This reverts commit 2b14bcae03a0268370a6ad43e1b4b0f720266d30.

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/AutoQMain.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/AutoQMain.java
@@ -10,35 +10,38 @@ import net.minecraft.network.play.client.CPacketChatMessage;
 /***
  * @author S-B99
  * Updated by @S-B99 on 29/11/19
+ * Updated by d1gress/Qther on 4/12/19
  */
-@Module.Info(name = "AutoQMain", description = "Automatically does \"/queue main\" every 7.1 minutes.", category = Module.Category.MISC)
+@Module.Info(name = "AutoQMain", description = "Automatically does \"/queue main\" every X minutes.", category = Module.Category.MISC)
 public class AutoQMain extends Module {
 
     private Setting<Boolean> debug = register(Settings.b("Debug", true));
     private Setting<Boolean> debugWarn = register(Settings.b("Connection Warning", true));
     private Setting<Double> delay = this.register(Settings.doubleBuilder("Wait time").withMinimum(0.2).withValue(7.1).withMaximum(10.0).build());
 
-    private static long startTime = 0;
     private double delayTime;
 
     @Override
     public void onUpdate() {
-        delayTime = 60000.0 * delay.getValue(); //426000
-        if (mc.player == null) return;
-        if (Minecraft.getMinecraft().getCurrentServerData() == null) return;
-
-        if (Minecraft.getMinecraft().getCurrentServerData() != null){
-            if (startTime == 0) startTime = System.currentTimeMillis();
-            if (startTime + delayTime <= System.currentTimeMillis()) {
-                if (!Minecraft.getMinecraft().getCurrentServerData().serverIP.equalsIgnoreCase("2b2t.org") && debugWarn.getValue()) {
-                    Command.sendWarningMessage("[AutoQMain] &l&6Warning: &r&6You are not connected to 2b2t.org");
-                }
-                if (debug.getValue()) {
-                    Command.sendChatMessage("&7Run &b/queue main&7 at " + System.currentTimeMillis());
-                }
-                Minecraft.getMinecraft().playerController.connection.sendPacket(new CPacketChatMessage("/queue main"));
-                startTime = System.currentTimeMillis();
-            }
+        if (delayTime <= 0) {
+            delayTime = (int) (delay.getValue() * 3000);
         }
+        else if (delayTime > 0) {
+            delayTime--;
+            return;
+        }
+        if (mc.player == null) return;
+
+        if (Minecraft.getMinecraft().getCurrentServerData() == null) {
+            Command.sendWarningMessage("[AutoQMain] &l&6Warning: &r&6You are on singleplayer");
+            return;
+        }
+        if (!Minecraft.getMinecraft().getCurrentServerData().serverIP.equalsIgnoreCase("2b2t.org") && debugWarn.getValue()) {
+            Command.sendWarningMessage("[AutoQMain] &l&6Warning: &r&6You are not connected to 2b2t.org");
+        }
+        if (debug.getValue()) {
+            Command.sendChatMessage("&7Run &b/queue main&7 at " + System.currentTimeMillis());
+        }
+        Minecraft.getMinecraft().playerController.connection.sendPacket(new CPacketChatMessage("/queue main"));
     }
 }
