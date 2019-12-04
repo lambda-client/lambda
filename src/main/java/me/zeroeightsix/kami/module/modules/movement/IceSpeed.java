986319a37bdaa8093f537346b5aa29e9d0d14e19
Bella <bella.who.two@gmail.com>
2019-12-03 23:09:23 -0500

    give ice speed a description

--- src/main/java/me/zeroeightsix/kami/module/modules/movement/IceSpeed.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/movement/IceSpeed.java
@@ -9,7 +9,7 @@ import net.minecraft.init.Blocks;
  * Created on 26 October 2019 by hub
  * Updated 24 November 2019 by hub
  */
-@Module.Info(name = "IceSpeed", description = "Ice Speed", category = Module.Category.MOVEMENT)
+@Module.Info(name = "IceSpeed", description = "Changes how slippery ice is", category = Module.Category.MOVEMENT)
 public class IceSpeed extends Module {
 
     private Setting<Float> slipperiness = register(Settings.floatBuilder("Slipperiness").withMinimum(0.2f).withValue(0.4f).withMaximum(1.0f).build());
