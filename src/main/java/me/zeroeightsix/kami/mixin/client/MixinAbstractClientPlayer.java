9f177b4f65675a12cf81b4dd7eafb65c2a317fb1
Bella <bella.who.two@gmail.com>
2019-12-13 09:23:05 -0500

    readd mixins

---
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinAbstractClientPlayer.java
@@ -0,0 +1,30 @@
+package me.zeroeightsix.kami.mixin.client;
+
+import com.mojang.authlib.GameProfile;
+import me.zeroeightsix.kami.module.ModuleManager;
+import me.zeroeightsix.kami.module.modules.sdashb.capes.LayerCape;
+import me.zeroeightsix.kami.module.modules.sdashb.render.CapeGUI;
+import me.zeroeightsix.kami.util.Wrapper;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+//i love crystallinqq <3
+
+@Mixin(AbstractClientPlayer.class)
+public class MixinAbstractClientPlayer {
+
+    @Inject(method = "<init>", at = @At(value = "RETURN"))
+    public void AbstractClientPlayer(World worldIn, GameProfile playerProfile, CallbackInfo callbackInfo) {
+        for (RenderPlayer renderPlayer : Wrapper.getMinecraft().getRenderManager().getSkinMap().values()) {
+            //if (ModuleManager.getModuleByName("Cape").isEnabled()) {
+                LayerCape cape = new LayerCape(renderPlayer);
+                renderPlayer.addLayer(cape);
+            //}
+        }
+    }
+}
