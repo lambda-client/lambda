b44ab17ed65caa2577ce86441745badd68d9ef9d
Bella <bella.who.two@gmail.com>
2019-12-14 20:29:10 -0500

    Revert "missed credit"
    This reverts commit 844c1b08cf22a8d35648d55b4f342ba59f3c8eff.

--- src/main/java/me/zeroeightsix/kami/module/modules/render/NoRender.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/render/NoRender.java
@@ -12,7 +12,6 @@ import net.minecraftforge.client.event.RenderBlockOverlayEvent;
 
 /**
  * Created by 086 on 4/02/2018.
- * Updated by S-B99 on 14/12/19
  */
 @Module.Info(name = "NoRender", category = Module.Category.RENDER, description = "Ignore entity spawn packets")
 public class NoRender extends Module {
