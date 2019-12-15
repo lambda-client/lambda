9b9ab682c66dc5bfec35adcd639d6bde032fab70
Bella <bella.who.two@gmail.com>
2019-12-14 23:24:58 -0500

    close #172

---
+++ src/main/java/me/zeroeightsix/kami/command/commands/CreditsCommand.java
@@ -0,0 +1,20 @@
+package me.zeroeightsix.kami.command.commands;
+
+import me.zeroeightsix.kami.command.Command;
+
+/**
+ * Created by S-B99 on 01/12/2019.
+ */
+
+public class CreditsCommand extends Command {
+
+    public CreditsCommand() {
+        super("credits", null);
+        setDescription("Prints KAMI Blue's authors and contributors");
+    }
+
+    @Override
+    public void call(String[] args) {
+        Command.sendChatMessage("\n&l&9Author: \n&b086\n&l&9Contributors: \n&bBella (S-B99)\n&bhub (blockparole)\n&bQther (d1gress)\n&bSasha (EmotionalLove)\n&bHHSGPA\n&bcats (Cuhnt)\n&b20kdc\n&bVonr\n&bKatatje\n&bDeauthorized\n&bkdb424\n&bElementars\n&bfsck\n&bJamie (jamie27)\n&bTBM\n&bWaizy\n&bcookiedragon234");
+    }
+}
