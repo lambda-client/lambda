cadc6a8453c773fe8836e60751a082d2db783634
Bella <bella.who.two@gmail.com>
2019-12-14 19:45:48 -0500

    color > colour

--- src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/experimental/GUIColour.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/experimental/GUIColour.java
@@ -6,8 +6,8 @@ import me.zeroeightsix.kami.setting.Settings;
 //import me.zeroeightsix.kami.setting.builder.numerical.NumericalSettingBuilder;
 //import me.zeroeightsix.kami.setting.impl.numerical.NumberSetting;
 
-@Module.Info(name = "GUI Color", description = "Change GUI Colors", category = Module.Category.EXPERIMENTAL)
-public class GUIColor extends Module {
+@Module.Info(name = "GUI Colour", description = "Change GUI Colours", category = Module.Category.EXPERIMENTAL)
+public class GUIColour extends Module {
     public Setting<Integer> red = this.register(Settings.integerBuilder("Red").withMinimum(0).withValue(13).withMaximum(255).build());
     public Setting<Integer> green = this.register(Settings.integerBuilder("Green").withMinimum(0).withValue(13).withMaximum(255).build());
     public Setting<Integer> blue = this.register(Settings.integerBuilder("Blue").withMinimum(0).withValue(13).withMaximum(255).build());
