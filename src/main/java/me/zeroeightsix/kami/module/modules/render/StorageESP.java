e087060f3ed567461b73b2f97debfeff6d80d540
Bella <bella.who.two@gmail.com>
2019-12-14 20:30:22 -0500

    missed credits

--- src/main/java/me/zeroeightsix/kami/module/modules/render/StorageESP.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/render/StorageESP.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 
 /**
  * Created by 086 on 10/12/2017.
+ * Updated by S-B99 on 14/12/19
  */
 @Module.Info(name = "StorageESP", description = "Draws nice little lines around storage items", category = Module.Category.RENDER)
 public class StorageESP extends Module {
