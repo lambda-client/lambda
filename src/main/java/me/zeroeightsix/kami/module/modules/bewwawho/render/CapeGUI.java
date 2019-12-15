6a7a001410dfefe55e48359d4efce8599d59752c
Bella <bella.who.two@gmail.com>
2019-12-14 22:50:21 -0500

    clean up cape code

--- src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/render/CapeGUI.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/render/CapeGUI.java
@@ -1,6 +1,5 @@
 package me.zeroeightsix.kami.module.modules.bewwawho.render;
 
-import me.zeroeightsix.kami.command.Command;
 import me.zeroeightsix.kami.module.Module;
 
 /***
@@ -8,28 +7,7 @@ import me.zeroeightsix.kami.module.Module;
  */
 @Module.Info(name = "Cape", category = Module.Category.RENDER, description = "Shiny custom donator cape", showOnArray = false)
 public class CapeGUI extends Module {
-
-    public void onEnable() {
-        if (mc.player == null) return;
-        Command.sendWarningMessage("[Cape] Note: you need donator jar and to restart Minecraft is cape is not working");
-    }
-
     public void onDisable() {
         this.enable();
     }
-
-//    private static CapeGUI INSTANCE;
-//
-//    public CapeGUI() {
-//        INSTANCE = this;
-//    }
-//
-//    public static boolean shouldCape() {
-//        return INSTANCE.isEnabled();
-//    }
-
-//    public boolean isEnabled() {
-//        return INSTANCE.isEnabled();
-//    }
-
 }
