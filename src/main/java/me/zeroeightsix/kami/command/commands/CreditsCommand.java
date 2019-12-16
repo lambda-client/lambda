594bdf4c76d24cd413c2f9fe00fe4c131879aa13
Bella <bella.who.two@gmail.com>
2019-12-15 19:42:00 -0500

    add 0x2E to credits

--- src/main/java/me/zeroeightsix/kami/command/commands/CreditsCommand.java
+++ src/main/java/me/zeroeightsix/kami/command/commands/CreditsCommand.java
@@ -15,6 +15,6 @@ public class CreditsCommand extends Command {
 
     @Override
     public void call(String[] args) {
-        Command.sendChatMessage("\n&l&9Author: \n&b086\n&l&9Contributors: \n&bBella (S-B99)\n&bhub (blockparole)\n&bQther (d1gress)\n&bSasha (EmotionalLove)\n&bHHSGPA\n&bcats (Cuhnt)\n&b20kdc\n&bVonr\n&bKatatje\n&bDeauthorized\n&bkdb424\n&bElementars\n&bfsck\n&bJamie (jamie27)\n&bTBM\n&bWaizy\n&bcookiedragon234");
+        Command.sendChatMessage("\n&l&9Author: \n&b086\n&l&9Contributors: \n&bBella (S-B99)\n&bhub (blockparole)\n&bQther (d1gress)\n&bSasha (EmotionalLove)\n&bHHSGPA\n&bcats (Cuhnt)\n&b20kdc\n&bVonr\n&bKatatje\n&bDeauthorized\n&bkdb424\n&bElementars\n&bfsck\n&bJamie (jamie27)\n&bTBM\n&bWaizy\n&bcookiedragon234\n&b0x2E (PretendingToCode)");
     }
 }
