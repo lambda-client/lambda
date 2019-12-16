ed9a8d0a1f9f8069cfbf552f13320fc05d8cf0d1
d1gress <55198830+d1gress@users.noreply.github.com>
2019-12-16 20:04:42 +0800

    Update EntityStatsCommand.java
    cookie is pissy

--- src/main/java/me/zeroeightsix/kami/command/commands/EntityStatsCommand.java
+++ src/main/java/me/zeroeightsix/kami/command/commands/EntityStatsCommand.java
@@ -8,7 +8,8 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
- * Created by d1gress/Qther on 25/11/2017.
+ * Created by d1gress/Qther on 25/11/2017, updated on 16/12/2019
+ * Updated by EmotionalLove on 16/12/2019
  */
 
 public class EntityStatsCommand extends Command {
@@ -23,23 +24,14 @@ public class EntityStatsCommand extends Command {
         String maxHealth;
         String speed;
         if (mc.player.getRidingEntity() != null && mc.player.getRidingEntity() instanceof AbstractHorse) {
-            // EntityHorse, EntityMule, etc etc all extend AbstractHorse
-            // no need to check if the riding entity is an instanceof everything @d1gress
-            AbstractHorse h = (AbstractHorse) mc.player.getRidingEntity();
-
-            maxHealth = h.getMaxHealth() + " \2472HP";
-            speed = round(43.17 * h.getAIMoveSpeed(), 2) + " \2472m/s";
-            String jumpHeight = round(-0.1817584952 * Math.pow(h.getHorseJumpStrength(), 3) + 3.689713992 * Math.pow(h.getHorseJumpStrength(), 2) + 2.128599134 * h.getHorseJumpStrength() - 0.343930367, 4) + " \2472m";
-            String tamer = h.getOwnerUniqueId() == null ? "Not tamed." : h.getOwnerUniqueId().toString();
+            AbstractHorse horse = (AbstractHorse) mc.player.getRidingEntity();
             // TODO: Function that resolves UUID's to Minecraft usernames. @bella
-            Command.sendChatMessage("\2476Entity Stats:\n\247cMax Health: \247b" + maxHealth + "\n\247cSpeed: \247b" + speed + "\n\247cJump: \247b" + jumpHeight + "\n\247cOwner UUID: \247b" + tamer);
+            Command.sendChatMessage("&6Entity Stats:\n&cMax Health: &b" + horse.getMaxHealth() + " &2HP" + "\n&cSpeed: &b" + round(43.17 * horse.getAIMoveSpeed(), 2) + " &2m/s" + "\n&cJump: &b" + round(-0.1817584952 * Math.pow(horse.getHorseJumpStrength(), 3) + 3.689713992 * Math.pow(horse.getHorseJumpStrength(), 2) + 2.128599134 * horse.getHorseJumpStrength() - 0.343930367, 4) + " &2m" + "\n&cOwner UUID: &b" + horse.getOwnerUniqueId() == null ? "Not tamed." : horse.getOwnerUniqueId().toString());
         } else if (mc.player.getRidingEntity() instanceof EntityLivingBase) {
-            EntityLivingBase l = (EntityLivingBase) mc.player.getRidingEntity();
-            maxHealth = l.getMaxHealth() + " \2472HP";
-            speed = round(43.17 * l.getAIMoveSpeed(), 2) + " \2472m/s";
-            Command.sendChatMessage("\2476Entity Stats:\n\247cMax Health: \247b" + maxHealth + "\n\247cSpeed: \247b" + speed);
+            EntityLivingBase entity = (EntityLivingBase) mc.player.getRidingEntity();
+            Command.sendChatMessage("&6Entity Stats:\n&cMax Health: &b" + entity.getMaxHealth() + " &2HP" + "\n&cSpeed: &b" + round(43.17 * entity.getAIMoveSpeed(), 2) + " &2m/s";);
         } else {
-            Command.sendChatMessage("\2474\247lERROR: \247cNot riding a compatible entity.");
+            Command.sendChatMessage("&4&lERROR: &cNot riding a compatible entity.");
         }
     }
 
