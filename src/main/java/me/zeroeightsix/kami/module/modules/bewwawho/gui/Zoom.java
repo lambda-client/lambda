849778507262c047cbc3e55e33c4f9389bcf11a1
Bella <bella.who.two@gmail.com>
2019-12-22 10:06:48 -0500

    change defaults of zoom

--- src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/gui/Zoom.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/gui/Zoom.java
@@ -7,6 +7,7 @@ import me.zeroeightsix.kami.setting.Settings;
 /***
  * @author S-B99
  * Created by S-B99 on 20/12/19
+ * Updated by S-B99 on 22/12/19
  */
 @Module.Info(name = "Zoom", category = Module.Category.GUI, description = "Configures FOV", showOnArray = Module.ShowOnArray.OFF)
 public class Zoom extends Module {
@@ -14,8 +15,8 @@ public class Zoom extends Module {
     private float fov = 0;
     private float sensi = 0;
 
-    private Setting<Integer> fovChange = register(Settings.integerBuilder("FOV").withMinimum(30).withValue(50).withMaximum(150).build());
-    private Setting<Float> sensChange = register(Settings.floatBuilder("Sensitivity").withMinimum(0.25F).withValue(1.25F).withMaximum(2F).build());
+    private Setting<Integer> fovChange = register(Settings.integerBuilder("FOV").withMinimum(30).withValue(30).withMaximum(150).build());
+    private Setting<Float> sensChange = register(Settings.floatBuilder("Sensitivity").withMinimum(0.25F).withValue(1.3F).withMaximum(2F).build());
     private Setting<Boolean> smoothCamera = register(Settings.b("Cinematic Camera", true));
     private Setting<Boolean> sens = register(Settings.b("Sensitivity", true));
 
