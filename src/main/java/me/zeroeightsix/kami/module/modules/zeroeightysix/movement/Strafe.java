f561046f0b91f4c096dfd7ce1f54962bb5425ca1
Bella <bella.who.two@gmail.com>
2019-12-28 19:53:37 -0500

    refactor sprint to strafe

--- src/main/java/me/zeroeightsix/kami/module/modules/zeroeightysix/movement/Strafe.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/zeroeightysix/movement/Strafe.java
@@ -5,8 +5,8 @@ import me.zeroeightsix.kami.module.Module;
 /**
  * Created by 086 on 23/08/2017.
  */
-@Module.Info(name = "Sprint", description = "Automatically makes the player sprint", category = Module.Category.MOVEMENT, showOnArray = Module.ShowOnArray.OFF)
-public class Sprint extends Module {
+@Module.Info(name = "Strafe", description = "Automatically makes the player sprint", category = Module.Category.MOVEMENT, showOnArray = Module.ShowOnArray.OFF)
+public class Strafe extends Module {
 
     @Override
     public void onUpdate() {
