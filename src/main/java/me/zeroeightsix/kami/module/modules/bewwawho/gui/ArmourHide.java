80d4787d2e272e839b26fa412826445c1425fffc
Bella <bella.who.two@gmail.com>
2019-12-28 19:25:49 -0500

    aksjfa msaaa formatting

--- src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/gui/ArmourHide.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/gui/ArmourHide.java
@@ -3,5 +3,5 @@ package me.zeroeightsix.kami.module.modules.bewwawho.gui;
 import me.zeroeightsix.kami.module.Module;
 
 @Module.Info(name = "ArmourHide", category = Module.Category.GUI, description = "Hides entity armour", showOnArray = Module.ShowOnArray.OFF)
-public class ArmourHide extends Module{
+public class ArmourHide extends Module {
 }
