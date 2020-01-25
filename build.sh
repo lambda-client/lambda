27eb2b74d27ce28a188104970e780253a7ea88d0
Bella <bella.who.two@gmail.com>
2020-01-25 10:02:01 -0500

    clean up build script

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@
 START_TIME="$(date +%s)"
 echo "BUILD: started at $(date -d "@$START_TIME")"
 
-echo "BUILD: cd back"
 cd ../
 
-echo "BUILD: copied"
 mkdir KAMI-Blue-beta/
 cp -r kamiblue/gradle* KAMI-Blue-beta/ 
 cp -r kamiblue/gradlew* KAMI-Blue-beta/
@@ -14,32 +12,24 @@ cp -r kamiblue/gradle/ KAMI-Blue-beta/
 cp -r kamiblue/build.sh KAMI-Blue-beta/
 cp -r kamiblue/build.gradle KAMI-Blue-beta/
 cp -r kamiblue/src/ KAMI-Blue-beta/
+echo "BUILD: copied"
 
-echo "BUILD: cd in"
 cd KAMI-Blue-beta/ || exit
-
-echo "BUILD: removing keygen"
-rm -rf src/main/java/me/zeroeightsix/kami/module/modules/sdashb/experimental/Gen.java
 
 echo "BUILD: cleaning"
 ./gradlew clean
 
-#echo "BUILD: setup workspace"
-#./gradlew setupDecompWorkspace
-
-echo "BUILD: build command"
+echo "BUILD: building"
 ./gradlew build rmOld copy
 
-echo "BUILD: cd back"
 cd ../
 
-echo "BUILD: deleting"
 rm -rf KAMI-Blue-beta/
+echo "BUILD: deleted"
 
-echo "BUILD: cd in"
 cd kamiblue/ || exit
 
-echo "BUILD: finished"
+echo "BUILD: SUCCESFUL"
 
 END_TIME="$(date +%s)"
 echo "BUILD: took" $(($(date +%s)-$START_TIME)) "seconds"
