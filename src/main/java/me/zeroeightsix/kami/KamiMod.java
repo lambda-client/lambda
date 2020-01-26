dd9585d1bf32912ff55171211aaa4760209c5d7d
Bella <bella.who.two@gmail.com>
2020-01-26 00:39:49 -0500

    fix repo refs

--- src/main/java/me/zeroeightsix/kami/KamiMod.java
+++ src/main/java/me/zeroeightsix/kami/KamiMod.java
@@ -68,9 +68,9 @@ public class KamiMod {
     public static final String MODVER = "v1.1.2";
     public static final String APP_ID = "638403216278683661";
 
-    static final String UPDATE_JSON = "https://raw.githubusercontent.com/S-B99/KAMI/assets/assets/updateChecker.json";
-    public static final String DONATORS_JSON = "https://raw.githubusercontent.com/S-B99/KAMI/assets/assets/donators.json";
-    public static final String CAPES_JSON = "https://raw.githubusercontent.com/S-B99/KAMI/assets/assets/capes.json";
+    static final String UPDATE_JSON = "https://raw.githubusercontent.com/S-B99/kamiblue/assets/assets/updateChecker.json";
+    public static final String DONATORS_JSON = "https://raw.githubusercontent.com/S-B99/kamiblue/assets/assets/donators.json";
+    public static final String CAPES_JSON = "https://raw.githubusercontent.com/S-B99/kamiblue/assets/assets/capes.json";
 
 //    public static final String KAMI_HIRAGANA = "\u304B\u307F";
 //    public static final String KAMI_KATAKANA = "\u30AB\u30DF";
