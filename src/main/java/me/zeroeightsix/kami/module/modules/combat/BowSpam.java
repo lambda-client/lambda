6d860cd9c07de9a41593a7f2198c2467c2fab0b7
Bella <bella.who.two@gmail.com>
2020-01-28 19:15:55 -0500

    fix credit message

--- src/main/java/me/zeroeightsix/kami/module/modules/combat/BowSpam.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/combat/BowSpam.java
@@ -8,8 +8,10 @@ import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
 import net.minecraft.util.math.BlockPos;
 
 /**
- * @author Seppukudevelopment
- * https://github.com/seppukudevelopment/seppuku
+ * Code like this does not have an author, as it is literally one function. It's nothing unique.
+ * This is just one thing. Anybody can write it the exact same way on accident.
+ * There is nothing to credit here.
+ * Updated by S-B99 on 28/01/20
  */
 @Module.Info(name = "BowSpam", description = "Makes you spam arrows", category = Module.Category.COMBAT)
 public class BowSpam extends Module {
