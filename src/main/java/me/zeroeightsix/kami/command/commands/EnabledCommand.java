2fa807e830e16ca64367ed9423b8accce6723b8a
Bella <bella.who.two@gmail.com>
2020-02-07 15:53:00 -0500

    cleanup and fix enabledcommand

--- src/main/java/me/zeroeightsix/kami/command/commands/EnabledCommand.java
+++ src/main/java/me/zeroeightsix/kami/command/commands/EnabledCommand.java
@@ -3,10 +3,12 @@ package me.zeroeightsix.kami.command.commands;
 import me.zeroeightsix.kami.command.Command;
 import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.module.ModuleManager;
+import net.minecraft.util.text.TextFormatting;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
 
 /**
  * @author S-B99
@@ -14,23 +16,22 @@ import java.util.stream.Collectors;
  */
 public class EnabledCommand extends Command {
     public EnabledCommand() {
-        super("enabledlist", null, "enabled");
+        super("enabledlist", null, "enabled", "enabledmodules");
         setDescription("Prints Enabled Modules");
     }
 
     @Override
     public void call(String[] args) {
         AtomicReference<String> enabled = new AtomicReference<>("");
-        List<Module> mods = ModuleManager.getModules().stream()
-                .filter(Module::isEnabled)
-                .collect(Collectors.toList());
+        List<Module> mods = new ArrayList<>(ModuleManager.getModules());
 
         mods.forEach(module -> {
             if (module.isEnabled()) {
-                enabled.set(String.join(", ", module.getName()));
+                enabled.set(enabled + module.getName() + ", ");
             }
         });
-        Command.sendChatMessage("Enabled modules: \n" + enabled);
+        enabled.set(StringUtils.chop(StringUtils.chop(String.valueOf(enabled)))); // this looks horrible but I don't know how else to do it sorry
+        Command.sendChatMessage("Enabled modules: \n" + TextFormatting.GRAY + enabled);
     }
 
 }
