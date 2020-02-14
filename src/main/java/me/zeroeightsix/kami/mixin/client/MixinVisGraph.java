dc0f0656f23446539208bf3b0231dfd8a34f9a74
20kdc <asdd2808@gmail.com>
2020-02-14 20:15:58 +0000

    Prevent rendering issues with Freecam in walls (Should fix KAMI#168) (Revised Again)
    It's over. I think. I have a broken down arrow key, and the complexities of how this works... I'm not strictly speaking sure, I've changed so much ; but I do believe I have successfully and I do hope properly slain 168.
    
    Do excuse me if I left any unnecessary AT entries.
    
    I really cannot be bothered to fix them, seeing as this was a much tougher job than it probably looks from the result.
    
    As testing is complete, I will now prepare the sister commit for kamiblue.
    
    Do excuse my previous PR; I had thought it correct, but now with this knowledge I see that it only seemed correct because of a difference in OptiFine that I find quite... odd.

---
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinVisGraph.java
@@ -0,0 +1,30 @@
+package me.zeroeightsix.kami.mixin.client;
+
+import java.util.EnumSet;
+import java.util.Set;
+import net.minecraft.client.renderer.chunk.VisGraph;
+import net.minecraft.util.EnumFacing;
+import me.zeroeightsix.kami.module.ModuleManager;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Created by 20kdc on 14/02/2020, but really 15/02/2020 because this is basically being recycled
+ */
+@Mixin(VisGraph.class)
+public class MixinVisGraph {
+
+    @Inject(method = "getVisibleFacings", at = @At("HEAD"), cancellable = true)
+    public void getVisibleFacings(CallbackInfoReturnable<Set<EnumFacing>> callbackInfo) {
+        // WebringOfTheDamned
+        // This part prevents the "block-level culling". OptiFine does this for you but vanilla doesn't.
+        // We have to implement this here or else OptiFine causes trouble.
+        if (ModuleManager.isModuleEnabled("Freecam"))
+            callbackInfo.setReturnValue(EnumSet.<EnumFacing>allOf(EnumFacing.class));
+    }
+
+}
