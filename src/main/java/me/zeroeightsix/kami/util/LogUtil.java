8fa3f6ee574cf0ac82706f3e2ec4c79c4512ff58
Bella <bella.who.two@gmail.com>
2020-02-18 10:20:27 -0500

    inline vars and clean up

--- src/main/java/me/zeroeightsix/kami/util/LogUtil.java
+++ src/main/java/me/zeroeightsix/kami/util/LogUtil.java
@@ -2,7 +2,6 @@ package me.zeroeightsix.kami.util;
 
 import net.minecraft.client.Minecraft;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,17 +10,14 @@ import java.io.IOException;
  * Created by S-B99 on 18/02/20
  */
 public class LogUtil {
-    private static String COORDS_FILE_NAME = "KAMIBlueCoords.txt";
-    private static final File coordsFileName = new File(COORDS_FILE_NAME);
-
     public static void writePlayerCoords(String locationName) {
         Minecraft mc = Minecraft.getMinecraft();
-        writeTo((int) mc.player.posX, (int) mc.player.posY, (int) mc.player.posZ, locationName);
+        writeCoords((int) mc.player.posX, (int) mc.player.posY, (int) mc.player.posZ, locationName);
     }
 
-    public static void writeTo(int x, int y, int z, String locationName) {
+    public static void writeCoords(int x, int y, int z, String locationName) {
         try {
-            FileWriter fW = new FileWriter(COORDS_FILE_NAME);
+            FileWriter fW = new FileWriter("KAMIBlueCoords.txt");
             fW.write(formatter(x, y, z, locationName));
             fW.close();
         } catch (IOException e) {
