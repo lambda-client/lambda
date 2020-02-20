fb623b7df0df61f6ab4c915d57e7139fc4c976cc
Bella <bella.who.two@gmail.com>
2020-02-19 20:06:32 -0500

    close #451

---
+++ fullVer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Created by S-B99 on 19/02/20
+# echo "Usage: ./fullVer.sh v2.0.0"
+
+sed -i "s/modVersion=.*/modVersion=$1/" gradle.properties
+sed -i "s/MODVER = \".*\";/MODVER = \"$1\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
+sed -i "s/\"version\": \".*\",/\"version\": \"${1:1}\",/" src/main/resources/mcmod.info
+
+git reset
+git add gradle.properties src/main/java/me/zeroeightsix/kami/KamiMod.java src/main/resources/mcmod.info
+git commit -m "AutoBumpVer: $1"
