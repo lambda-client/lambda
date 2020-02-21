40b6d5704be704a21f364c61d7e58c4e9e2cf4bb
Bella <bella.who.two@gmail.com>
2020-02-21 12:29:14 -0500

    hide prefixchat by default

--- src/main/java/me/zeroeightsix/kami/module/modules/gui/PrefixChat.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/gui/PrefixChat.java
@@ -4,7 +4,7 @@ import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
 
-@Module.Info(name = "PrefixChat", category = Module.Category.GUI, description = "Opens chat with prefix inside when prefix is pressed.")
+@Module.Info(name = "PrefixChat", category = Module.Category.GUI, description = "Opens chat with prefix inside when prefix is pressed.", showOnArray = Module.ShowOnArray.OFF)
 public class PrefixChat extends Module {
     public Setting<Boolean> startupGlobal = register(Settings.b("Enable Automatically", true));
 }
