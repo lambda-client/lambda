75f57b6c3123bf93c1bc29b67a1d2af8a30d7390
Bella <bella.who.two@gmail.com>
2020-02-22 21:33:24 -0500

    switch month and day ver

--- ver.sh
+++ ver.sh
@@ -3,13 +3,13 @@
 # Created by S-B99 on 19/02/20
 # echo "Usage: ./ver.sh v2.0.0 01"
 
-dateD=$(date +'%d')
 dateM=$(date +'%m')
+dateD=$(date +'%d')
 
-sed -i "s/modVersion=.*/modVersion=$1\-${dateD}\-${dateM}\-$2/" gradle.properties
-sed -i "s/MODVER = \".*\";/MODVER = \"$1\-${dateD}\-${dateM}\-$2\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
+sed -i "s/modVersion=.*/modVersion=$1\-${dateM}\-${dateD}\-$2/" gradle.properties
+sed -i "s/MODVER = \".*\";/MODVER = \"$1\-${dateM}\-${dateD}\-$2\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
+sed -i "s/\"version\": \".*\",/\"version\": \"${1:1}\-${dateM}\-${dateD}\-$2\",/" src/main/resources/mcmod.info
 sed -i "s/MODVERSMALL = \".*\";/MODVERSMALL = \"$1\-beta\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
-sed -i "s/\"version\": \".*\",/\"version\": \"${1:1}\-${dateD}\-${dateM}\-$2\",/" src/main/resources/mcmod.info
 
 git reset
 git add gradle.properties src/main/java/me/zeroeightsix/kami/KamiMod.java src/main/resources/mcmod.info
