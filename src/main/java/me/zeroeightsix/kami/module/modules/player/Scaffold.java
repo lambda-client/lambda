263937031fb90d88238ed75dc026b524054b3f68
Bella <bella.who.two@gmail.com>
2020-02-29 14:16:13 -0500

    clean up garbage code in scaffold

--- src/main/java/me/zeroeightsix/kami/module/modules/player/Scaffold.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/Scaffold.java
@@ -24,14 +24,18 @@ import static me.zeroeightsix.kami.util.BlockInteractionHelper.*;
 /**
  * Created by 086 on 20/01/19
  * Updated by Polymer on 16/01/20
+ * @see me.zeroeightsix.kami.mixin.client.MixinEntity
  */
 @Module.Info(name = "Scaffold", category = Module.Category.PLAYER, description = "Places blocks under you")
 public class Scaffold extends Module {
 
-    private Setting<Integer> future = register(Settings.integerBuilder("Ticks").withMinimum(0).withMaximum(60).withValue(2));
-    private Setting legitBridge = register(Settings.b("Legit Bridge", false));
-    private Setting autoPlace = register(Settings.b("AutoPlace", false));
-    boolean shouldSlow = false;
+    private Setting<Boolean> placeBlocks = register(Settings.b("Place Blocks", true));
+    private Setting<Mode> modeSetting = register(Settings.enumBuilder(Mode.class).withName("Mode").withValue(Mode.TOWER).build());
+    private Setting<Boolean> randomDelay = register(Settings.booleanBuilder("Random Delay").withValue(false).withVisibility(v -> modeSetting.getValue().equals(Mode.LEGIT)).build());
+    private Setting<Integer> delayRange = register(Settings.integerBuilder("Delay Range").withMinimum(0).withValue(3).withMaximum(10).withVisibility(v -> modeSetting.getValue().equals(Mode.LEGIT) && randomDelay.getValue()).build());
+    private Setting<Integer> ticks = register(Settings.integerBuilder("Ticks").withMinimum(0).withMaximum(60).withValue(2).withVisibility(v -> !modeSetting.getValue().equals(Mode.LEGIT)).build());
+
+    private boolean shouldSlow = false;
 
     private static Scaffold INSTANCE;
 
@@ -42,54 +46,64 @@ public class Scaffold extends Module {
     public static boolean shouldScaffold() {
         return INSTANCE.isEnabled();
     }
+
+    private enum Mode {
+        NEITHER, TOWER, LEGIT
+    }
+
     @EventHandler
     private Listener<InputUpdateEvent> eventListener = new Listener<>(event -> {
-        if ((boolean)legitBridge.getValue() && shouldSlow == true) {
-            event.getMovementInput().moveStrafe *= 0.2f;
-            event.getMovementInput().moveForward *= 0.2f;
+        if (modeSetting.getValue().equals(Mode.LEGIT) && shouldSlow) {
+            if (randomDelay.getValue()) {
+                event.getMovementInput().moveStrafe *= 0.2f + getRandomInRange();
+                event.getMovementInput().moveForward *= 0.2f + getRandomInRange();
+            }
+            else {
+                event.getMovementInput().moveStrafe *= 0.2f;
+                event.getMovementInput().moveForward *= 0.2f;
+            }
         }
     });
 
     @Override
     public void onUpdate() {
+        if (mc.player == null || ModuleManager.isModuleEnabled("Freecam")) return;
         shouldSlow = false;
-        if (isDisabled() || mc.player == null || ModuleManager.isModuleEnabled("Freecam")) return;
-        Vec3d vec3d = EntityUtil.getInterpolatedPos(mc.player, future.getValue());
-        if ((boolean) legitBridge.getValue()) vec3d = EntityUtil.getInterpolatedPos(mc.player, 0);
+
+        Vec3d vec3d = EntityUtil.getInterpolatedPos(mc.player, ticks.getValue());
+        if (modeSetting.getValue().equals(Mode.LEGIT)) vec3d = EntityUtil.getInterpolatedPos(mc.player, 0);
+
         BlockPos blockPos = new BlockPos(vec3d).down();
         BlockPos belowBlockPos = blockPos.down();
         BlockPos legitPos = new BlockPos(EntityUtil.getInterpolatedPos(mc.player, 2));
-        //check if block behind player is air or other replaceable block and if it is, make the player crouch when legitBridge is enabled
-        if (Wrapper.getWorld().getBlockState(legitPos.down()).getMaterial().isReplaceable() && (boolean)legitBridge.getValue()&& mc.player.onGround == true) {
+
+        /* when legitBridge is enabled */
+        /* check if block behind player is air or other replaceable block and if it is, make the player crouch */
+        if (modeSetting.getValue().equals(Mode.LEGIT) && Wrapper.getWorld().getBlockState(legitPos.down()).getMaterial().isReplaceable() && mc.player.onGround) {
             shouldSlow = true;
             mc.player.movementInput.sneak = true;
             mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, Action.START_SNEAKING));
         }
-        // check if block is already placed
-        if (!Wrapper.getWorld().getBlockState(blockPos).getMaterial().isReplaceable() ) {
+
+        /* check if block is already placed */
+        if (!Wrapper.getWorld().getBlockState(blockPos).getMaterial().isReplaceable()) {
             return;
         }
-        // search blocks in hotbar
+
+        /* search blocks in hotbar */
         int newSlot = -1;
         for (int i = 0; i < 9; i++) {
-            // filter out non-block items
-            ItemStack stack =
-                    Wrapper.getPlayer().inventory.getStackInSlot(i);
+            /* filter out non-block items */
+            ItemStack stack = Wrapper.getPlayer().inventory.getStackInSlot(i);
+            if (stack == ItemStack.EMPTY || !(stack.getItem() instanceof ItemBlock)) continue;
 
-            if (stack == ItemStack.EMPTY || !(stack.getItem() instanceof ItemBlock)) {
-                continue;
-            }
             Block block = ((ItemBlock) stack.getItem()).getBlock();
-            if (blackList.contains(block) || block instanceof BlockContainer) {
-                continue;
-            }
+            if (blackList.contains(block) || block instanceof BlockContainer) continue;
 
-            // filter out non-solid blocks
-            if (!Block.getBlockFromItem(stack.getItem()).getDefaultState()
-                    .isFullBlock())
-                continue;
+            /* filter out non-solid blocks */
+            if (!Block.getBlockFromItem(stack.getItem()).getDefaultState().isFullBlock()) continue;
 
-            // don't use falling blocks if it'd fall
+            /* don't use falling blocks if it'd fall */
             if (((ItemBlock) stack.getItem()).getBlock() instanceof BlockFalling) {
                 if (Wrapper.getWorld().getBlockState(belowBlockPos).getMaterial().isReplaceable()) continue;
             }
@@ -98,26 +112,25 @@ public class Scaffold extends Module {
             break;
         }
 
-        // check if any blocks were found
-        if (newSlot == -1)
-            return;
-
-        // set slot
+        /* check if any blocks were found, and if they were then set the slot */
+        if (newSlot == -1) return;
         int oldSlot = Wrapper.getPlayer().inventory.currentItem;
         Wrapper.getPlayer().inventory.currentItem = newSlot;
 
-        // check if we don't have a block adjacent to blockpos
-        if (!checkForNeighbours(blockPos)) {
-            return;
-        }
-        // place block
-        if ((boolean)autoPlace.getValue()) placeBlockScaffold(blockPos);
+        /* check if we don't have a block adjacent to the blockPos */
+        if (!checkForNeighbours(blockPos)) return;
+
+        /* place the block */
+        if (placeBlocks.getValue()) placeBlockScaffold(blockPos);
         mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, Action.STOP_SNEAKING));
         shouldSlow = false;
 
-        // reset slot
+        /* reset slot back to the original one */
         Wrapper.getPlayer().inventory.currentItem = oldSlot;
     }
 
+    public float getRandomInRange() {
+        return 0.11f + (float) Math.random() * ((delayRange.getValue() / 10.0f) - 0.11f);
+    }
 }
 
