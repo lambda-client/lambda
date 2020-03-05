95c78e88766a87c74a06b1bdf599528a31b52664
Bella <bella.who.two@gmail.com>
2020-03-05 16:11:26 -0500

    oneliners uwu

--- src/main/java/me/zeroeightsix/kami/util/ColourConverter.java
+++ src/main/java/me/zeroeightsix/kami/util/ColourConverter.java
@@ -5,13 +5,9 @@ package me.zeroeightsix.kami.util;
  * Updated by S-B99 on 04/03/20
  */
 public class ColourConverter {
-    public static float toF(int i) {
-        return i / 255f;
-    }
+    public static float toF(int i) { return i / 255f; }
 
-    public static float toF(double d) {
-        return (float) (d / 255f);
-    }
+    public static float toF(double d) { return (float) (d / 255f); }
 
     public static int settingsToInt(int r, int g, int b, int a) { return (r << 16) | (g << 8) | (b) | (a << 24); }
 }
