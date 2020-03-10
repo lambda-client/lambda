0b358b6c70748ae665f17bbb7fab5b0464a49ea0
Bella <bella.who.two@gmail.com>
2020-03-10 11:43:39 -0400

    remove backticks for safety

--- scripts/hookName.sh
+++ scripts/hookName.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BRANCH="$(git branch 2>/dev/null | grep '^*' | colrm 1 2)"
-curl -H "Content-Type: application/json" -X POST -d '{"username": "Github Actions", "content": "**Branch:** `'$BRANCH'`"}' "$WEBHOOK"
+curl -H "Content-Type: application/json" -X POST -d '{"username": "Github Actions", "content": "**Branch:** '$BRANCH'"}' "$WEBHOOK"
