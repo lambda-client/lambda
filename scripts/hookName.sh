3f25cafc40fa920cd7c0e7ecef92590d477dcbcc
Bella <bella.who.two@gmail.com>
2020-03-10 11:17:32 -0400

    change how hookname greps the branch

--- scripts/hookName.sh
+++ scripts/hookName.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-BRANCH="$(git branch | grep "*")"
-BRANCH="${BRANCH:2}"
+BRANCH="$(git branch | grep '^\*')"
+BRANCH="${BRANCH#??}"
 curl -H "Content-Type: application/json" -X POST -d '{"username": "Github Actions", "content": "**Branch:** `'$BRANCH'`"}' "$WEBHOOK"
