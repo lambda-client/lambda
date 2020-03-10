2743a3479d0712756b82823c5bba1c009b93bad7
Bella <bella.who.two@gmail.com>
2020-03-10 11:43:56 -0400

    add debugging

--- scripts/hookName.sh
+++ scripts/hookName.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 BRANCH="$(git branch 2>/dev/null | grep '^*' | colrm 1 2)"
+echo "'$BRANCH'"
 curl -H "Content-Type: application/json" -X POST -d '{"username": "Github Actions", "content": "**Branch:** '$BRANCH'"}' "$WEBHOOK"
