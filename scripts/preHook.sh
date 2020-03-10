84f42be4a56806ee725df62d8bfd6d5b77e945b9
Bella <bella.who.two@gmail.com>
2020-03-10 19:24:30 -0400

    prehook change mcmod as well

--- scripts/preHook.sh
+++ scripts/preHook.sh
@@ -2,3 +2,4 @@
 
 CUR_VER="$(cat ./scripts/curVer)"
 sed -i "s/modVersion=.*/modVersion=${CUR_VER:1}-$TRAVIS_COMMIT/" gradle.properties
+sed -i "s/\"version\": \".*\",/\"version\": \"${CUR_VER:1}-$TRAVIS_COMMIT\",/" src/main/resources/mcmod.info
