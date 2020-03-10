dcaf617788b3600938b4568affdda1f4a4cf58c3
Bella <bella.who.two@gmail.com>
2020-03-10 19:27:14 -0400

    include beta in mcmod and kamimod

--- scripts/ver.sh
+++ scripts/ver.sh
@@ -7,9 +7,9 @@ CUR_VER="$(cat ./scripts/curVer)"
 CUR_BETA="$(cat ./scripts/curBeta)"
 
 sed -i "s/modVersion=.*/modVersion=${CUR_VER:1}$CUR_BETA/" gradle.properties
-sed -i "s/MODVER = \".*\";/MODVER = \"$CUR_VER\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
+sed -i "s/MODVER = \".*\";/MODVER = \"$CUR_VER$CUR_BETA\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
 sed -i "s/MODVERSMALL = \".*\";/MODVERSMALL = \"$CUR_VER$CUR_BETA\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
-sed -i "s/\"version\": \".*\",/\"version\": \"${CUR_VER:1}\",/" src/main/resources/mcmod.info
+sed -i "s/\"version\": \".*\",/\"version\": \"${CUR_VER:1}$CUR_BETA\",/" src/main/resources/mcmod.info
 
 git reset
 git add gradle.properties src/main/java/me/zeroeightsix/kami/KamiMod.java src/main/resources/mcmod.info
