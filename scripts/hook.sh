89a40224761bb83db8daf7416b3bc7b16de75ae5
Bella <bella.who.two@gmail.com>
2020-03-10 09:55:46 -0400

    send branch name on push

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-DIR="$(readlink -f ./build/libs/)"
-JARDIR="$(ls "$DIR" | grep "release")"
+CURDIR="$(readlink -f ./build/libs/)"
+JARDIR="$(ls "$CURDIR" | grep "release")"
+BRANCH="$(git branch | grep "*")"
+BRANCH="${BRANCH:2}"
+curl -H "Content-Type: application/json" -X POST -d '{"username": "Github Actions", "content": "Branch: '$BRANCH'"}' "$WEBHOOK"
 curl -F content=@""$DIR"/"$JARDIR"" "$WEBHOOK"
-#echo ""$DIR"/"$UWUOWO""
