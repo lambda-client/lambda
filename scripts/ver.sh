cc79b160fb465a9f4a97c4590b4e2d6806645295
Bella <bella.who.two@gmail.com>
2020-03-10 19:21:57 -0400

    gradle default building change versioning system

--- scripts/ver.sh
+++ scripts/ver.sh
@@ -6,7 +6,7 @@
 CUR_VER="$(cat ./scripts/curVer)"
 CUR_BETA="$(cat ./scripts/curBeta)"
 
-sed -i "s/modVersion=.*/modVersion=$CUR_VER/" gradle.properties
+sed -i "s/modVersion=.*/modVersion=${CUR_VER:1}$CUR_BETA/" gradle.properties
 sed -i "s/MODVER = \".*\";/MODVER = \"$CUR_VER\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
 sed -i "s/MODVERSMALL = \".*\";/MODVERSMALL = \"$CUR_VER$CUR_BETA\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
 sed -i "s/\"version\": \".*\",/\"version\": \"${CUR_VER:1}\",/" src/main/resources/mcmod.info
