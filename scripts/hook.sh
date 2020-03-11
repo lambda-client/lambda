11df8e0a6e674423f8d61b3d7fe762615d21c319
Bella <bella.who.two@gmail.com>
2020-03-10 22:55:55 -0400

    exit 0 if the branch doesn't match

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# Send message with branch name 
-curl -H "Content-Type: application/json" -X POST -d '{"username": "KAMI Blue Releases", "content": "**Branch:** `'$BRANCH'`"}' "$WEBHOOK"
+if [[ "$BRANCH" == "feature/master" ]]; then
+    # Send message with branch name
+    curl -H "Content-Type: application/json" -X POST -d '{"username": "KAMI Blue Releases", "content": "**Branch:** `'$BRANCH'`"}' "$WEBHOOK"
 
-# Upload the release file 
-BUILD_DIR="$(readlink -f ./build/libs/)"
-JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
-curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
+    # Upload the release file
+    BUILD_DIR="$(readlink -f ./build/libs/)"
+    JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
+    curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
+elif [[ "$BRANCH" == "feature/safecrystal#480" ]]; then
+    # Send message with branch name
+    curl -H "Content-Type: application/json" -X POST -d '{"username": "KAMI Blue Releases", "content": "**Branch:** `'$BRANCH'`"}' "$WEBHOOK"
+
+    # Upload the release file
+    BUILD_DIR="$(readlink -f ./build/libs/)"
+    JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
+    curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
+else
+    exit 0
+fi
