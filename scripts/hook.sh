05db6161266a624370589bf3b4eb49aa14c0f548
Bella Who <bella.who.two@gmail.com>
2020-03-11 14:57:02 +0000

    Update combat improvements  (#561)
    * exit 0 if the branch doesn't match
    
    * use commit ver for MODVER
    
    * exit if it's a pull request hook
    
    * use embeds and add more information for hook
    
    * add full commit link

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-# Send message with branch name 
-curl -H "Content-Type: application/json" -X POST -d '{"username": "KAMI Blue Releases", "content": "**Branch:** `'$BRANCH'`"}' "$WEBHOOK"
+if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then exit 0; else echo "">/dev/null; fi
 
-# Upload the release file 
-BUILD_DIR="$(readlink -f ./build/libs/)"
-JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
-curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
+COMMIT_TRIM="${TRAVIS_COMMIT::7}"
+COMMIT_MSG="$TRAVIS_COMMIT_MESSAGE"
+
+if [[ "$BRANCH" == "feature/master" ]]; then
+    # Send message with branch name
+    curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commits/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
+
+    # Upload the release file
+    BUILD_DIR="$(readlink -f ./build/libs/)"
+    JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
+    curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
+elif [[ "$BRANCH" == "feature/safecrystal#480" ]]; then
+    # Send message with branch name
+    curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commits/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
+
+    # Upload the release file
+    BUILD_DIR="$(readlink -f ./build/libs/)"
+    JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
+    curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
+else
+    exit 0
+fi
--- scripts/preHook.sh
+++ scripts/preHook.sh
@@ -2,5 +2,7 @@
 
 CUR_VER="$(cat ./scripts/curVer)"
 COMMIT_TRIM="${TRAVIS_COMMIT::7}"
+
 sed -i "s/modVersion=.*/modVersion=${CUR_VER:1}-$COMMIT_TRIM/" gradle.properties
 sed -i "s/\"version\": \".*\",/\"version\": \"${CUR_VER:1}-$COMMIT_TRIM\",/" src/main/resources/mcmod.info
+sed -i "s/MODVER = \".*\";/MODVER = \"$CUR_VER-$COMMIT_TRIM\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
