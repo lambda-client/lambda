9c1b4e3143cbaadb1d442cf8305b858f687cfdb0
Bella <bella.who.two@gmail.com>
2020-03-11 17:50:04 -0400

    change embed color

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -7,7 +7,7 @@ COMMIT_MSG="$TRAVIS_COMMIT_MESSAGE"
 
 if [[ "$BRANCH" == "feature/master" ]]; then
     # Send message with branch name
-    curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commit/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
+    curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","color": 10195199,"description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commit/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
 
     # Upload the release file
     BUILD_DIR="$(readlink -f ./build/libs/)"
@@ -15,7 +15,7 @@ if [[ "$BRANCH" == "feature/master" ]]; then
     curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
 elif [[ "$BRANCH" == "feature/safecrystal#480" ]]; then
     # Send message with branch name
-    curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commit/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
+    curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","color": 10195199,"description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commit/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
 
     # Upload the release file
     BUILD_DIR="$(readlink -f ./build/libs/)"
