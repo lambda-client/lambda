f288b08ccb576392d7073f9d861ef290554621d4
Bella <bella.who.two@gmail.com>
2020-03-12 11:44:24 -0400

    revert properly this time

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -5,7 +5,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then exit 0; else echo "">/dev/null; 
 COMMIT_TRIM="${TRAVIS_COMMIT::7}"
 COMMIT_MSG="$TRAVIS_COMMIT_MESSAGE"
 
-if [[ "$BRANCH" == "feature/master" ]] && [[ "$TRAVISBRANCH" == "1" ]]; then
+if [[ "$BRANCH" == "feature/master" ]]; then
     # Send message with branch name
     curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","color": 10195199,"description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commit/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
 
@@ -13,7 +13,7 @@ if [[ "$BRANCH" == "feature/master" ]] && [[ "$TRAVISBRANCH" == "1" ]]; then
     BUILD_DIR="$(readlink -f ./build/libs/)"
     JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
     curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
-elif [[ "$BRANCH" == "feature/safecrystal#480" ]] && [[ "$TRAVISBRANCH" == "2" ]]; then
+elif [[ "$BRANCH" == "feature/safecrystal#480" ]]; then
     # Send message with branch name
     curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","color": 10195199,"description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commit/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
 
