303763de7db44ecddde02afb3fee5837b73bbef2
Bella <bella.who.two@gmail.com>
2020-03-13 10:38:56 -0400

    make holfill spoof yaw packets

--- src/main/java/me/zeroeightsix/kami/module/modules/experimental/HoleFiller.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/experimental/HoleFiller.java
@@ -1,9 +1,13 @@
 package me.zeroeightsix.kami.module.modules.experimental;
 
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
 import me.zeroeightsix.kami.command.Command;
+import me.zeroeightsix.kami.event.events.PacketEvent;
 import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.module.ModuleManager;
 import me.zeroeightsix.kami.module.modules.combat.CrystalAura;
+import me.zeroeightsix.kami.module.modules.player.NoBreakAnimation;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
 import me.zeroeightsix.kami.util.BlockInteractionHelper;
@@ -12,10 +16,13 @@ import me.zeroeightsix.kami.util.Wrapper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockObsidian;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.CPacketHeldItemChange;
+import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
@@ -24,6 +31,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static me.zeroeightsix.kami.module.modules.combat.CrystalAura.getPlayerPos;
+import static me.zeroeightsix.kami.util.EntityUtil.calculateLookAt;
 
 /**
  * @author hub
@@ -36,20 +44,23 @@ public class HoleFiller extends Module {
     private Setting<Double> distance = register(Settings.d("Range", 4.0));
     private Setting<Boolean> render = register(Settings.b("Render Filled Blocks", false));
     private Setting<Boolean> holeCheck = register(Settings.b("Only Fill in Hole", true));
-   
+
     public List<BlockPos> blockPosList;
     List<Entity> entities = new ArrayList<>();
-    
+
     public boolean isHole;
-    
+    static boolean isSpoofingAngles;
+    static float yaw;
+    static float pitch;
+
     private final BlockPos[] surroundOffset = {
-            new BlockPos(0, -1, 0), // down
-            new BlockPos(0, 0, -1), // north
-            new BlockPos(1, 0, 0), // east
-            new BlockPos(0, 0, 1), // south
-            new BlockPos(-1, 0, 0) // west
+        new BlockPos(0, -1, 0), // down
+        new BlockPos(0, 0, -1), // north
+        new BlockPos(1, 0, 0), // east
+        new BlockPos(0, 0, 1), // south
+        new BlockPos(-1, 0, 0) // west
     };
-    
+
     private int findObiInHotbar() {
         int slot = -1;
         for (int i = 0; i < 9; ++i) {
@@ -64,32 +75,32 @@ public class HoleFiller extends Module {
         }
         return slot;
     }
-    
+
     @Override
     public void onUpdate() {
         /* mc.player can only be null if the world is null, so checking if the mc.player is null *should be sufficient */
-       if (mc.player == null && mc.world == null) return;
-       
-       Vec3d[] holeOffset = {
-           	mc.player.getPositionVector().add(1, 0, 0),
-           	mc.player.getPositionVector().add(-1, 0, 0),
-           	mc.player.getPositionVector().add(0, 0, 1),
-           	mc.player.getPositionVector().add(0, 0, -1),
-           	mc.player.getPositionVector().add(0, -1, 0)
-       };
+        if (mc.player == null && mc.world == null) return;
+
+        Vec3d[] holeOffset = {
+            mc.player.getPositionVector().add(1, 0, 0),
+            mc.player.getPositionVector().add(-1, 0, 0),
+            mc.player.getPositionVector().add(0, 0, 1),
+            mc.player.getPositionVector().add(0, 0, -1),
+            mc.player.getPositionVector().add(0, -1, 0)
+        };
 
 
-    	entities.addAll(mc.world.playerEntities.stream().filter(entityPlayer -> !Friends.isFriend(entityPlayer.getName())).collect(Collectors.toList()));
+        entities.addAll(mc.world.playerEntities.stream().filter(entityPlayer -> !Friends.isFriend(entityPlayer.getName())).collect(Collectors.toList()));
         int range = (int) Math.ceil(distance.getValue());
-    	CrystalAura ca = (CrystalAura) ModuleManager.getModuleByName("CrystalAura");
-    	blockPosList = ca.getSphere(getPlayerPos(), range, range, false, true, 0);
+        CrystalAura ca = (CrystalAura) ModuleManager.getModuleByName("CrystalAura");
+        blockPosList = ca.getSphere(getPlayerPos(), range, range, false, true, 0);
 
-    	if (blockPosList == null) return;
-    	for (BlockPos p: blockPosList) {
-    	    if (p == null) return;
+        if (blockPosList == null) return;
+        for (BlockPos p: blockPosList) {
+            if (p == null) return;
 
-    		isHole = true;
-    		// block gotta be air
+            isHole = true;
+            // block gotta be air
             if (!mc.world.getBlockState(p).getBlock().equals(Blocks.AIR)) continue;
 
             // block 1 above gotta be air
@@ -105,39 +116,72 @@ public class HoleFiller extends Module {
                     break;
                 }
             }
-            
+
             int h = 0;
-            if (holeCheck.getValue()) {	
-            	for (Vec3d o: holeOffset) {
-            		BlockPos q = new BlockPos (o.x, o.y, o.z);
-            		Block b = mc.world.getBlockState(q).getBlock();
-            		if (b == Blocks.OBSIDIAN || b == Blocks.BEDROCK) {
-            			h++;
-            		}	
-            	}
-            	if (h != 5) return;
+            if (holeCheck.getValue()) {
+                for (Vec3d o: holeOffset) {
+                    BlockPos q = new BlockPos (o.x, o.y, o.z);
+                    Block b = mc.world.getBlockState(q).getBlock();
+                    if (b == Blocks.OBSIDIAN || b == Blocks.BEDROCK) {
+                        h++;
+                    }
+                }
+                if (h != 5) return;
             }
-            
+
             if (isHole) {
-            	if (mc.player.getPositionVector().squareDistanceTo(p.x, p.y, p.z) <= 1.2) {
-            		return;
-            	}
-            	for (Entity e : entities) {
-            		if (e.getPositionVector().squareDistanceTo(p.x, p.y, p.z) <= 1.2) {
-                		return;
-                	}
-            	}
-            	int oldSlot = mc.player.inventory.currentItem;
-            	int obiSlot = -1;
-            	obiSlot = findObiInHotbar();
-            	if (obiSlot == -1) {
-            		Command.sendChatMessage("&cError: &rNo obsidian in hotbar! disabling.");
-            		this.disable();
-            	}
+                if (mc.player.getPositionVector().squareDistanceTo(p.x, p.y, p.z) <= 1.2) {
+                    return;
+                }
+                for (Entity e : entities) {
+                    if (e.getPositionVector().squareDistanceTo(p.x, p.y, p.z) <= 1.2) {
+                        return;
+                    }
+                }
+                int oldSlot = mc.player.inventory.currentItem;
+                int obiSlot = -1;
+                obiSlot = findObiInHotbar();
+                if (obiSlot == -1) {
+                    Command.sendChatMessage("&cError: &rNo obsidian in hotbar! disabling.");
+                    this.disable();
+                }
                 mc.player.connection.sendPacket(new CPacketHeldItemChange(obiSlot));
-        		BlockInteractionHelper.placeBlockScaffold(p);
+                lookAtPacket(p.x,p.y,p.z,mc.player);
+                BlockInteractionHelper.placeBlockScaffold(p);
+                if (ModuleManager.getModuleByName("NoBreakAnimation").isEnabled()) {
+                    ((NoBreakAnimation) ModuleManager.getModuleByName("NoBreakAnimation")).resetMining();
+                }
+                resetRotation();
                 mc.player.connection.sendPacket(new CPacketHeldItemChange(oldSlot));
             }
-    	}
+        }
     }
+    private void lookAtPacket(double px, double py, double pz, EntityPlayer me) {
+        double[] v = calculateLookAt(px, py, pz, me);
+        setYawAndPitch((float) v[0], (float) v[1]+1f);
+    }
+    private static void setYawAndPitch(float yaw1, float pitch1) {
+        yaw = yaw1;
+        pitch = pitch1;
+        isSpoofingAngles = true;
+    }
+
+    private static void resetRotation() {
+        if (isSpoofingAngles) {
+            yaw = mc.player.rotationYaw;
+            pitch = mc.player.rotationPitch;
+            isSpoofingAngles = false;
+        }
+    }
+
+    @EventHandler
+    private Listener<PacketEvent.Send> cPacketListener = new Listener<>(event -> {
+        Packet packet = event.getPacket();
+        if (packet instanceof CPacketPlayer) {
+            if (isSpoofingAngles) {
+                ((CPacketPlayer) packet).yaw = (float) yaw;
+                ((CPacketPlayer) packet).pitch = (float) pitch;
+            }
+        }
+    });
 }
