c99a1a041a3acdd79c12bc692c402875413ddff6
Bella <bella.who.two@gmail.com>
2020-03-25 09:54:04 -0400

    fix config crash 2

--- src/main/java/me/zeroeightsix/kami/module/modules/gui/InfoOverlay.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/gui/InfoOverlay.java
@@ -5,6 +5,7 @@ import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.module.modules.movement.TimerSpeed;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
+import me.zeroeightsix.kami.util.ColourTextFormatting;
 import me.zeroeightsix.kami.util.InfoCalculator;
 import me.zeroeightsix.kami.util.TimeUtil;
 import net.minecraft.client.Minecraft;
@@ -16,12 +17,13 @@ import net.minecraft.util.text.TextFormatting;
 import java.util.ArrayList;
 
 import static me.zeroeightsix.kami.command.Command.sendDisableMessage;
+import static me.zeroeightsix.kami.util.ColourTextFormatting.toTextMap;
 
 /**
  * @author S-B99
  * Created by S-B99 on 04/12/19
- * Updated by S-B99 on 05/03/20
  * PVP Information by Polymer on 04/03/20
+ * Updated by S-B99 on 25/03/20
  */
 @Module.Info(name = "InfoOverlay", category = Module.Category.GUI, description = "Configures the game information overlay", showOnArray = Module.ShowOnArray.OFF)
 public class InfoOverlay extends Module {
@@ -48,8 +50,52 @@ public class InfoOverlay extends Module {
     private Setting<TimeUtil.TimeType> timeTypeSetting = register(Settings.enumBuilder(TimeUtil.TimeType.class).withName("Time Format").withValue(TimeUtil.TimeType.HHMMSS).withVisibility(v -> page.getValue().equals(Page.THREE) && time.getValue()).build());
     private Setting<TimeUtil.TimeUnit> timeUnitSetting = register(Settings.enumBuilder(TimeUtil.TimeUnit.class).withName("Time Unit").withValue(TimeUtil.TimeUnit.H12).withVisibility(v -> page.getValue().equals(Page.THREE) && time.getValue()).build());
     private Setting<Boolean> doLocale = register(Settings.booleanBuilder("Time Show AMPM").withValue(true).withVisibility(v -> page.getValue().equals(Page.THREE) && time.getValue()).build());
-    public Setting<TextFormatting> firstColour = register(Settings.enumBuilder(TextFormatting.class).withName("First Colour").withValue(TextFormatting.valueOf("WHITE")).withVisibility(v -> page.getValue().equals(Page.THREE)).build());
-    public Setting<TextFormatting> secondColour = register(Settings.enumBuilder(TextFormatting.class).withName("Second Colour").withValue(TextFormatting.valueOf("BLUE")).withVisibility(v -> page.getValue().equals(Page.THREE)).build());
+    public Setting<ColourTextFormatting.ColourCode> firstColour = register(Settings.enumBuilder(ColourTextFormatting.ColourCode.class).withName("First Colour").withValue(ColourTextFormatting.ColourCode.WHITE).withVisibility(v -> page.getValue().equals(Page.THREE)).build());
+    public Setting<ColourTextFormatting.ColourCode> secondColour = register(Settings.enumBuilder(ColourTextFormatting.ColourCode.class).withName("Second Colour").withValue(ColourTextFormatting.ColourCode.BLUE).withVisibility(v -> page.getValue().equals(Page.THREE)).build());
+
+    public static String getStringColour(TextFormatting c) {
+        return c.toString();
+    }
+
+    private TextFormatting setToText(ColourTextFormatting.ColourCode colourCode) {
+        return toTextMap.get(colourCode);
+    }
+
+    public ArrayList<String> infoContents() {
+        ArrayList<String> infoContents = new ArrayList<>();
+        if (version.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + KamiMod.KAMI_KANJI + getStringColour(setToText(secondColour.getValue())) + " " + KamiMod.MODVERSMALL);
+        } if (username.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + "Welcome" + getStringColour(setToText(secondColour.getValue())) + " " + mc.getSession().getUsername() + "!");
+        } if (time.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + TimeUtil.getFinalTime(setToText(secondColour.getValue()), setToText(firstColour.getValue()), timeUnitSetting.getValue(), timeTypeSetting.getValue(), doLocale.getValue()));
+        } if (tps.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + InfoCalculator.tps() + getStringColour(setToText(secondColour.getValue())) + " tps");
+        } if (fps.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + Minecraft.debugFPS + getStringColour(setToText(secondColour.getValue())) + " fps");
+        } if (speed.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + InfoCalculator.speed(useUnitKmH()) + getStringColour(setToText(secondColour.getValue())) + " " + unitType(speedUnit.getValue()));
+        } if (timerSpeed.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + TimerSpeed.returnGui() + getStringColour(setToText(secondColour.getValue())) + "t");
+        } if (ping.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + InfoCalculator.ping() + getStringColour(setToText(secondColour.getValue())) + " ms");
+        } if (durability.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + InfoCalculator.dura() + getStringColour(setToText(secondColour.getValue())) + " dura");
+        } if (memory.getValue()) {
+            infoContents.add(getStringColour(setToText(firstColour.getValue())) + InfoCalculator.memory() + getStringColour(setToText(secondColour.getValue())) + "mB free");
+        } if (totems.getValue()) {
+        	infoContents.add(getStringColour(setToText(firstColour.getValue())) + getItems(Items.TOTEM_OF_UNDYING) + getStringColour(setToText(secondColour.getValue())) + " Totems");
+        } if (endCrystals.getValue()) {
+        	infoContents.add(getStringColour(setToText(firstColour.getValue())) + getItems(Items.END_CRYSTAL) + getStringColour(setToText(secondColour.getValue())) + " Crystals");
+        } if (expBottles.getValue()) {
+        	infoContents.add(getStringColour(setToText(firstColour.getValue())) + getItems(Items.EXPERIENCE_BOTTLE) + getStringColour(setToText(secondColour.getValue())) + " EXP Bottles");
+        } if (godApples.getValue()) {
+        	infoContents.add(getStringColour(setToText(firstColour.getValue())) + getItems(Items.GOLDEN_APPLE) + getStringColour(setToText(secondColour.getValue())) + " God Apples");
+        }
+        return infoContents;
+    }
+
+    public void onDisable() { sendDisableMessage(getName()); }
 
     private enum SpeedUnit { MPS, KMH }
 
@@ -59,6 +105,10 @@ public class InfoOverlay extends Module {
         return speedUnit.getValue().equals(SpeedUnit.KMH);
     }
 
+    public static int getItems(Item i) {
+        return mc.player.inventory.mainInventory.stream().filter(itemStack -> itemStack.getItem() == i).mapToInt(ItemStack::getCount).sum() + mc.player.inventory.offHandInventory.stream().filter(itemStack -> itemStack.getItem() == i).mapToInt(ItemStack::getCount).sum();
+    }
+
     private String unitType(SpeedUnit s) {
         switch (s) {
             case MPS: return "m/s";
@@ -66,48 +116,4 @@ public class InfoOverlay extends Module {
             default: return "Invalid unit type (mps or kmh)";
         }
     }
-
-    public static String getStringColour(TextFormatting c) {
-        return c.toString();
-    }
-
-    public static int getItems(Item i) {
-        return mc.player.inventory.mainInventory.stream().filter(itemStack -> itemStack.getItem() == i).mapToInt(ItemStack::getCount).sum() + mc.player.inventory.offHandInventory.stream().filter(itemStack -> itemStack.getItem() == i).mapToInt(ItemStack::getCount).sum();
-    }
-
-    public ArrayList<String> infoContents() {
-        ArrayList<String> infoContents = new ArrayList<>();
-        if (version.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + KamiMod.KAMI_KANJI + getStringColour(secondColour.getValue()) + " " + KamiMod.MODVERSMALL);
-        } if (username.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + "Welcome" + getStringColour(secondColour.getValue()) + " " + mc.getSession().getUsername() + "!");
-        } if (time.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + TimeUtil.getFinalTime(secondColour.getValue(), firstColour.getValue(), timeUnitSetting.getValue(), timeTypeSetting.getValue(), doLocale.getValue()));
-        } if (tps.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + InfoCalculator.tps() + getStringColour(secondColour.getValue()) + " tps");
-        } if (fps.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + Minecraft.debugFPS + getStringColour(secondColour.getValue()) + " fps");
-        } if (speed.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + InfoCalculator.speed(useUnitKmH()) + getStringColour(secondColour.getValue()) + " " + unitType(speedUnit.getValue()));
-        } if (timerSpeed.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + TimerSpeed.returnGui() + getStringColour(secondColour.getValue()) + "t");
-        } if (ping.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + InfoCalculator.ping() + getStringColour(secondColour.getValue()) + " ms");
-        } if (durability.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + InfoCalculator.dura() + getStringColour(secondColour.getValue()) + " dura");
-        } if (memory.getValue()) {
-            infoContents.add(getStringColour(firstColour.getValue()) + InfoCalculator.memory() + getStringColour(secondColour.getValue()) + "mB free");
-        } if (totems.getValue()) {
-        	infoContents.add(getStringColour(firstColour.getValue()) + getItems(Items.TOTEM_OF_UNDYING) + getStringColour(secondColour.getValue()) + " Totems");
-        } if (endCrystals.getValue()) {
-        	infoContents.add(getStringColour(firstColour.getValue()) + getItems(Items.END_CRYSTAL) + getStringColour(secondColour.getValue()) + " Crystals");
-        } if (expBottles.getValue()) {
-        	infoContents.add(getStringColour(firstColour.getValue()) + getItems(Items.EXPERIENCE_BOTTLE) + getStringColour(secondColour.getValue()) + " EXP Bottles");
-        } if (godApples.getValue()) {
-        	infoContents.add(getStringColour(firstColour.getValue()) + getItems(Items.GOLDEN_APPLE) + getStringColour(secondColour.getValue()) + " God Apples");
-        }
-        return infoContents;
-    }
-
-    public void onDisable() { sendDisableMessage(getName()); }
 }
