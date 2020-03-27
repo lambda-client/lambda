3e637ae84097424804c21935052cb385c20ed0ce
d1gress <dragoclanleader@gmail.com>
2020-03-27 17:48:58 +0800

    VectorUtil changes. Check desc
    rename getVecsInArea to getIntPositionsInArea, now returns List<BlockPos> and has an added overload for calling function with BlockPos-es
    
    Remove convertType
    
    readd advanceVec to return given vec which is moved towards a given direction for given distance.
    
    add getDistance to get distance between two vectors
    
    add extendVec to get all vectors between two given vecs with a point every (distance between the vecs) / (given steps)

--- src/main/java/me/zeroeightsix/kami/util/VectorUtil.java
+++ src/main/java/me/zeroeightsix/kami/util/VectorUtil.java
@@ -1,15 +1,37 @@
 package me.zeroeightsix.kami.util;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class VectorUtil {
 
-    public static List<Vec3d> getVecsInArea(Vec3d pos1, Vec3d pos2) {
-        List<Vec3d> returnList = null;
+    public static double getDistance(Vec3d vecA, Vec3d vecB) {
+        return MathHelper.sqrt((vecA.x - vecB.x) * (vecA.x - vecB.x) + (vecA.y - vecB.y) * (vecA.y - vecB.y) + (vecA.z - vecB.z) * (vecA.z - vecB.z));
+    }
 
+    public static List<Vec3d> extendVec(Vec3d startVec, Vec3d destinationVec, int steps) {
+        List<Vec3d> returnList = new ArrayList<>();
+        double stepDistance = getDistance(startVec, destinationVec) / steps;
+
+        for (int i = 0; i < Math.max(steps, 1) + 1; i++) {
+            returnList.add(advanceVec(startVec, destinationVec, stepDistance * i));
+        }
+
+        return returnList;
+    }
+
+    public static Vec3d advanceVec(Vec3d startVec, Vec3d destinationVec, double distance) {
+        Vec3d advanceDirection = destinationVec.subtract(startVec).normalize();
+        if (destinationVec.distanceTo(startVec) < distance) return destinationVec;
+        return advanceDirection.scale(distance);
+    }
+
+    public static List<BlockPos> getIntPositionsInArea(Vec3d pos1, Vec3d pos2) {
         int minX = (int) Math.round(Math.min(pos1.x, pos2.x));
         int maxX = (int) Math.round(Math.max(pos1.x, pos2.x));
 
@@ -19,15 +41,35 @@ public class VectorUtil {
         int minZ = (int) Math.round(Math.min(pos1.z, pos2.z));
         int maxZ = (int) Math.round(Math.max(pos1.z, pos2.z));
 
+        List<BlockPos> returnList = Arrays.asList(new BlockPos[(maxX - minX) * (maxY-minY) * (maxZ-minZ)]);
+
         for (int x = minX; x < maxX; x++) {
             for (int y = minY; y < maxY; y++) {
-                for (int z = minZ; z < maxZ; z++) { returnList.add(new Vec3d(x, y, z)); }
+                for (int z = minZ; z < maxZ; z++) { returnList.add(new BlockPos(x, y, z)); }
             }
         }
 
         return returnList;
     }
 
-    public static BlockPos convertType(Vec3d vec3d) { return new BlockPos(vec3d.x, vec3d.y, vec3d.z); }
-    public static Vec3d convertType(BlockPos blockpos) { return new Vec3d(blockpos.x, blockpos.y, blockpos.z); }
+    public static List<BlockPos> getIntPositionsInArea(BlockPos pos1, BlockPos pos2) {
+        int minX = (int) Math.round(Math.min(pos1.x, pos2.x));
+        int maxX = (int) Math.round(Math.max(pos1.x, pos2.x));
+
+        int minY = (int) Math.round(Math.min(pos1.y, pos2.y));
+        int maxY = (int) Math.round(Math.max(pos1.y, pos2.y));
+
+        int minZ = (int) Math.round(Math.min(pos1.z, pos2.z));
+        int maxZ = (int) Math.round(Math.max(pos1.z, pos2.z));
+
+        List<BlockPos> returnList = Arrays.asList(new BlockPos[(maxX - minX) * (maxY-minY) * (maxZ-minZ)]);
+
+        for (int x = minX; x < maxX; x++) {
+            for (int y = minY; y < maxY; y++) {
+                for (int z = minZ; z < maxZ; z++) { returnList.add(new BlockPos(x, y, z)); }
+            }
+        }
+
+        return returnList;
+    }
 }
