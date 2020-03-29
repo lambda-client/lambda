d682c6a2936f6d19e25519c007088942eeab639b
Bella <bella.who.two@gmail.com>
2020-03-29 18:37:22 -0400

    remove safecrystal from hook

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -13,14 +13,6 @@ if [[ "$BRANCH" == "feature/master" ]]; then
     BUILD_DIR="$(readlink -f ./build/libs/)"
     JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
     curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
-elif [[ "$BRANCH" == "feature/safecrystal#480" ]]; then
-    # Send message with branch name
-    curl -H "Content-Type: application/json" -X POST -d '{"embeds": [{"title": "","color": 10195199,"description": "**Changelog:** '"$COMMIT_MSG"'\nBranch: `'"$BRANCH"'`\nCommit: ['${COMMIT_TRIM}'](https://github.com/S-B99/kamiblue/commits/'${COMMIT_TRIM}') Full: ['${COMMIT_TRIM}'-](https://github.com/S-B99/kamiblue/commit/'${TRAVIS_COMMIT}') "}]}' "$WEBHOOK"
-
-    # Upload the release file
-    BUILD_DIR="$(readlink -f ./build/libs/)"
-    JAR_DIR="$(ls "$BUILD_DIR" | grep "release")"
-    curl -F content=@"$BUILD_DIR/$JAR_DIR" "$WEBHOOK"
 else
     exit 0
 fi
