0ff218b5cc1bfce6b1686a11a2ab5366c4bd3b98
Bella <bella.who.two@gmail.com>
2020-04-06 10:43:29 -0400

    close #50

---
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinMovementInputFromOptions.java
@@ -0,0 +1,35 @@
+package me.zeroeightsix.kami.mixin.client;
+
+import me.zeroeightsix.kami.module.modules.movement.InventoryMove;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.MovementInput;
+import net.minecraft.util.MovementInputFromOptions;
+import org.lwjgl.input.Keyboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static me.zeroeightsix.kami.KamiMod.MODULE_MANAGER;
+
+/**
+ * @author Finz0
+ * Used with LGPLv3 license permission
+ * https://github.com/S-B99/osiris/blob/master/src/main/java/me/finz0/osiris/mixin/mixins/MixinMovementInputFromOptions.java
+ *
+ * @see InventoryMove
+ */
+@Mixin(value = MovementInputFromOptions.class)
+public abstract class MixinMovementInputFromOptions extends MovementInput {
+    @Redirect(method = "updatePlayerMoveState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/settings/KeyBinding;isKeyDown()Z"))
+    public boolean isKeyPressed(KeyBinding keyBinding) {
+        if (Minecraft.getMinecraft().player != null
+                && MODULE_MANAGER.isModuleEnabled(InventoryMove.class)
+                && Minecraft.getMinecraft().currentScreen != null
+                && !(Minecraft.getMinecraft().currentScreen instanceof GuiChat)) {
+            return Keyboard.isKeyDown(keyBinding.getKeyCode());
+        }
+        return keyBinding.isKeyDown();
+    }
+}
---
+++ src/main/java/me/zeroeightsix/kami/module/modules/movement/InventoryMove.java
@@ -0,0 +1,33 @@
+package me.zeroeightsix.kami.module.modules.movement;
+
+import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
+import net.minecraft.client.gui.GuiChat;
+import org.lwjgl.input.Keyboard;
+
+/**
+ * @author S-B99
+ * Created by S-B99 on 06/04/20
+ * @see me.zeroeightsix.kami.mixin.client.MixinMovementInputFromOptions
+ */
+@Module.Info(name = "InventoryMove", description = "Allows you to walk around with GUIs opened", category = Module.Category.MOVEMENT)
+public class InventoryMove extends Module {
+    private Setting<Integer> speed = register(Settings.i("Look speed", 20));
+    public void onUpdate() {
+        if (mc.player == null || mc.currentScreen == null || mc.currentScreen instanceof GuiChat) return;
+        if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+            mc.player.rotationYaw = mc.player.rotationYaw - speed.getValue();
+        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+            mc.player.rotationYaw = mc.player.rotationYaw + speed.getValue();
+        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+            mc.player.rotationPitch = mc.player.rotationPitch - speed.getValue();
+        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+            mc.player.rotationPitch = mc.player.rotationPitch + speed.getValue();
+        }
+    }
+}
+
