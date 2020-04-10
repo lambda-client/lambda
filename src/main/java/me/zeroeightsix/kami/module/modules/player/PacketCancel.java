b4343c064e2256b167eddccf0796eec01848f469
cookiedragon234 <c.o.okiedragon234@gmail.com>
2020-04-10 17:41:30 +0100

    Cleaner and faster packet cancel

--- src/main/java/me/zeroeightsix/kami/module/modules/player/PacketCancel.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/PacketCancel.java
@@ -23,39 +23,34 @@ public class PacketCancel extends Module {
     private final Setting<Boolean> packetEntityAction = register(Settings.booleanBuilder("CPacketEntityAction").withValue(true).withVisibility(v -> !all.getValue()));
     private final Setting<Boolean> packetUseEntity = register(Settings.booleanBuilder("CPacketUseEntity").withValue(true).withVisibility(v -> !all.getValue()));
     private final Setting<Boolean> packetVehicleMove = register(Settings.booleanBuilder("CPacketVehicleMove").withValue(true).withVisibility(v -> !all.getValue()));
-
-    Queue<Packet> packets = new LinkedList<>();
+    private int numPackets;
+    
     @EventHandler
     private final Listener<PacketEvent.Send> sendListener = new Listener<>(event -> {
-        if (all.getValue() && event.getPacket() != null) event.cancel();
-        if (packetInput.getValue() && event.getPacket() instanceof CPacketInput) {
+        if (
+            (all.getValue())
+            ||
+            (packetInput.getValue() && event.getPacket() instanceof CPacketInput)
+            ||
+            (packetPlayer.getValue() && event.getPacket() instanceof CPacketPlayer)
+            ||
+            (packetEntityAction.getValue() && event.getPacket() instanceof CPacketEntityAction)
+            ||
+            (packetUseEntity.getValue() && event.getPacket() instanceof CPacketUseEntity)
+            ||
+            (packetVehicleMove.getValue() && event.getPacket() instanceof CPacketVehicleMove)
+        ) {
             event.cancel();
-            packets.add(event.getPacket());
-        }
-        if (packetPlayer.getValue() && event.getPacket() instanceof CPacketPlayer) {
-            event.cancel();
-            packets.add(event.getPacket());
-        }
-        if (packetEntityAction.getValue() && event.getPacket() instanceof CPacketEntityAction) {
-            event.cancel();
-            packets.add(event.getPacket());
-        }
-        if (packetUseEntity.getValue() && event.getPacket() instanceof CPacketUseEntity) {
-            event.cancel();
-            packets.add(event.getPacket());
-        }
-        if (packetVehicleMove.getValue() && event.getPacket() instanceof CPacketVehicleMove) {
-            event.cancel();
-            packets.add(event.getPacket());
+            numPackets++;
         }
     });
 
     public void onDisable() {
-        packets.clear();
+        numPackets = 0;
     }
 
     @Override
     public String getHudInfo() {
-        return String.valueOf(packets.size());
+        return Integer.toString(numPackets);
     }
 }
