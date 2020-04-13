25362d0de38c3640cf0ecae3068354965378d0ac
Bella <bella.who.two@gmail.com>
2020-04-12 22:29:05 -0400

    start working on new changes

---
+++ src/main/java/me/zeroeightsix/kami/event/events/PlayerTravelEvent.java
@@ -0,0 +1,5 @@
+package me.zeroeightsix.kami.event.events;
+
+import me.zeroeightsix.kami.event.KamiEvent;
+
+public class PlayerTravelEvent extends KamiEvent {}
---
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntityPlayer.java
@@ -0,0 +1,30 @@
+package me.zeroeightsix.kami.mixin.client;
+
+import me.zeroeightsix.kami.KamiMod;
+import me.zeroeightsix.kami.event.events.PlayerTravelEvent;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.MoverType;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = EntityPlayer.class, priority = Integer.MAX_VALUE)
+public abstract class MixinEntityPlayer extends EntityLivingBase {
+
+    public MixinEntityPlayer(World worldIn) {
+        super(worldIn);
+    }
+
+    @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
+    public void travel(float strafe, float vertical, float forward, CallbackInfo info) {
+        PlayerTravelEvent event = new PlayerTravelEvent();
+        KamiMod.EVENT_BUS.post(event);
+        if (event.isCancelled()) {
+            move(MoverType.SELF, motionX, motionY, motionZ);
+            info.cancel();
+        }
+    }
+}
