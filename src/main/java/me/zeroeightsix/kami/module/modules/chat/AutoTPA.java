e134d865de0137de6e24d2cd2eedf4a09a5b26da
Bella <bella.who.two@gmail.com>
2020-04-12 20:22:47 -0400

    add friends check to autotpa

--- src/main/java/me/zeroeightsix/kami/module/modules/chat/AutoTPA.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/chat/AutoTPA.java
@@ -6,41 +6,42 @@ import me.zeroeightsix.kami.event.events.PacketEvent;
 import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
+import me.zeroeightsix.kami.util.Friends;
 import me.zeroeightsix.kami.util.Wrapper;
 import net.minecraft.network.play.server.SPacketChat;
 
-import java.util.function.Predicate;
+import static me.zeroeightsix.kami.util.MessageSendHelper.sendServerMessage;
 
 /*
  * By Katatje 8 Dec 2019
+ * Updated by S-B99 on 12/04/20
  */
-
 @Module.Info(name = "AutoTPA", description = "Automatically decline or accept TPA requests", category = Module.Category.CHAT)
 public class AutoTPA extends Module {
-
+    private Setting<Boolean> friends = register(Settings.b("Always accept friends", true));
     private Setting<mode> mod = register(Settings.e("Response", mode.DENY));
 
     @EventHandler
-    public Listener<PacketEvent.Receive> receiveListener;
-
-    public AutoTPA() {
-        this.receiveListener = new Listener<PacketEvent.Receive>(event -> {
-            SPacketChat packet;
-            if (event.getPacket() instanceof SPacketChat && (packet = (SPacketChat) event.getPacket()).getChatComponent().getUnformattedText().contains(" has requested to teleport to you.")) {
-                switch (mod.getValue()) {
-                    case ACCEPT:
-                        Wrapper.getPlayer().sendChatMessage("/tpaccept");
-                        break;
-                    case DENY:
-                        Wrapper.getPlayer().sendChatMessage("/tpdeny");
-                        break;
-                }
+    public Listener<PacketEvent.Receive> receiveListener = new Listener<>(event -> {
+        if (event.getPacket() instanceof SPacketChat && ((SPacketChat) event.getPacket()).getChatComponent().getUnformattedText().contains(" has requested to teleport to you.")) {
+            /* I tested that getting the first word is compatible with chat timestamp, and it as, as this is Receive and chat timestamp is after Receive */
+            String firstWord = ((SPacketChat) event.getPacket()).getChatComponent().getUnformattedText().split("\\s+")[0];
+            if (friends.getValue() && Friends.isFriend(firstWord)) {
+                sendServerMessage("/tpaccept");
+                return;
             }
-        }, (Predicate<PacketEvent.Receive>[]) new Predicate[0]);
-    }
+            switch (mod.getValue()) {
+                case ACCEPT:
+                    sendServerMessage("/tpaccept");
+                    break;
+                case DENY:
+                    sendServerMessage("/tpdeny");
+                    break;
+            }
+        }
+    });
 
     public enum mode {
         ACCEPT, DENY
     }
-
 }
