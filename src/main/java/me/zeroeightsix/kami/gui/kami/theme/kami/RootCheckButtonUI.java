981f08ca9cdec5f4bc36457b26c8b4625365f61e
Bella <bella.who.two@gmail.com>
2020-04-13 11:14:46 -0400

    start adding outlines

--- src/main/java/me/zeroeightsix/kami/gui/kami/theme/kami/RootCheckButtonUI.java
+++ src/main/java/me/zeroeightsix/kami/gui/kami/theme/kami/RootCheckButtonUI.java
@@ -7,6 +7,7 @@ import me.zeroeightsix.kami.gui.rgui.component.container.Container;
 import me.zeroeightsix.kami.gui.rgui.component.use.CheckButton;
 import me.zeroeightsix.kami.gui.rgui.render.AbstractComponentUI;
 import me.zeroeightsix.kami.gui.rgui.render.font.FontRenderer;
+import org.lwjgl.opengl.GL11;
 
 import static me.zeroeightsix.kami.gui.kami.theme.kami.KamiGuiColors.GuiC;
 import static me.zeroeightsix.kami.util.ColourConverter.toF;
@@ -36,9 +37,16 @@ public class RootCheckButtonUI<T extends CheckButton> extends AbstractComponentU
             c = (c & GuiC.buttonHoveredN.color.getRGB()) << 1;
             if (component.hasDescription()) {
                 Command.sendChatMessage(component.getName() + ": " + component.getDescription());
+
                 glDisable(GL_SCISSOR_TEST);
                 glDepthRange(0, 0.01);
-                RenderHelper.drawFilledRectangle(component.getWidth() + 14, 0, component.getWidth() * 2, component.getHeight());
+
+                GL11.glColor4f(0.17F, 0.17F, 0.18F, 0.9F);
+                RenderHelper.drawFilledRectangle(component.getWidth() + 14, -4, 50, component.getHeight() + 4);
+                GL11.glColor3f(.60f, .56f, 1.00f);
+                GL11.glLineWidth(1.5F);
+                RenderHelper.drawRectangle(component.getWidth() + 14, -4, 50, component.getHeight() + 4);
+
                 glEnable(GL_SCISSOR_TEST);
                 glDepthRange(0, 1.0);
             }
