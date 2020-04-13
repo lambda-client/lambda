fd5718f614eaf1e2ed77babe0badc78ebca3d793
Bella <bella.who.two@gmail.com>
2020-04-13 12:13:19 -0400

    fix MODVERBROAD for new beta system

--- scripts/ver.sh
+++ scripts/ver.sh
@@ -5,10 +5,11 @@
 
 CUR_VER="$(cat ./scripts/curVer)"
 CUR_BETA="$(cat ./scripts/curBeta)"
+CUR_RELEASE="$(cat ./scripts/curRelease)"
 
 sed -i "s/modVersion=.*/modVersion=${CUR_VER:1}$CUR_BETA/" gradle.properties
 sed -i "s/MODVER = \".*\";/MODVER = \"$CUR_VER$CUR_BETA\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
 sed -i "s/MODVERSMALL = \".*\";/MODVERSMALL = \"$CUR_VER$CUR_BETA\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
-sed -i "s/MODVERBROAD = \".*\";/MODVERBROAD = \"$CUR_VER\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
+sed -i "s/MODVERBROAD = \".*\";/MODVERBROAD = \"$CUR_RELEASE\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
 sed -i "s/\"version\": \".*\",/\"version\": \"${CUR_VER:1}$CUR_BETA\",/" src/main/resources/mcmod.info
 
