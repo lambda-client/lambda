34053297b647f20894173666fc878deba1041b94
Dewy REDACTED <dljcoolguy@gmail.com>
2020-04-16 08:56:42 +0100

    Revert "aimbot settings"
    This reverts commit 0eb230e2c02edd5e63016b5957a1a4d226b03530.

--- src/main/java/me/zeroeightsix/kami/module/modules/combat/AimBot.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/combat/AimBot.java
@@ -2,20 +2,14 @@ package me.zeroeightsix.kami.module.modules.combat;
 
 import me.zeroeightsix.kami.KamiMod;
 import me.zeroeightsix.kami.module.Module;
-import me.zeroeightsix.kami.setting.Setting;
-import me.zeroeightsix.kami.setting.Settings;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.MathHelper;
 
-/**
- * Created by Dewy on the 16th of April, 2020
- */
+// poop aimbot gonna finish tomorrow
 @Module.Info(name = "AimBot", description = "Automatically aims at entities for you.", category = Module.Category.COMBAT)
 public class AimBot extends Module {
-
-    private Setting<Integer> fov = register(Settings.integerBuilder("FOV").withMinimum(90).withMaximum(360).withValue(360));
 
     @Override
     public void onUpdate() {
@@ -41,21 +35,9 @@ public class AimBot extends Module {
         double diffY = mc.player.posY + (double) mc.player.getEyeHeight() - (entity.posY + (double) entity.getEyeHeight());
         double xz = MathHelper.sqrt(diffX * diffX + diffZ * diffZ);
 
-        float yaw = (float) normalizeAngle((Math.atan2(diffZ, diffX) * 180.0 / Math.PI) - 90.0f);
-        float pitch = (float) normalizeAngle((-Math.atan2(diffY, xz) * 180.0 / Math.PI));
+        float yaw = (float) (Math.atan2(diffZ, diffX) * 180.0 / Math.PI) - 90.0f;
+        float pitch = (float) (-Math.atan2(diffY, xz) * 180.0 / Math.PI);
 
         mc.player.setPositionAndRotation(mc.player.posX, mc.player.posY, mc.player.posZ, yaw, -pitch);
-    }
-
-    private static double normalizeAngle(double angle) {
-        while (angle <= -180.0) {
-            angle += 360.0;
-        }
-
-        while (angle > 180.0) {
-            angle -= 360.0;
-        }
-
-        return angle;
     }
 }
