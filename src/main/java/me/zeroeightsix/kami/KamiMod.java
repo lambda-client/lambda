1b8837596c65b786fac2f4794c8f66a4387243d4
Bella <bella.who.two@gmail.com>
2020-04-17 12:27:05 -0400

    bump ver for v1.1.4 beta

--- src/main/java/me/zeroeightsix/kami/KamiMod.java
+++ src/main/java/me/zeroeightsix/kami/KamiMod.java
@@ -66,8 +66,8 @@ public class KamiMod {
 
     public static final String MODNAME = "KAMI Blue";
     public static final String MODID = "kamiblue";
-    public static final String MODVER = "v1.1.3"; // this is changed to v1.1.2-commit for debugging during travis releases
-    public static final String MODVERSMALL = "v1.1.3"; // shown to the user
+    public static final String MODVER = "v1.1.4-beta"; // this is changed to v1.1.2-commit for debugging during travis releases
+    public static final String MODVERSMALL = "v1.1.4-beta"; // shown to the user
     public static final String MODVERBROAD = "v1.1.3"; // used for update checking
 
     public static final String MCVER = "1.12.2";
