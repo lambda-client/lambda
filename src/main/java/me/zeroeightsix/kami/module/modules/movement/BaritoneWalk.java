d1833ef757303a59847f48692aaeafcafa3dd6f4
Dewy REDACTED <dljcoolguy@gmail.com>
2020-04-18 10:16:56 +0100

    improved BaritoneWalk

--- src/main/java/me/zeroeightsix/kami/module/modules/movement/BaritoneWalk.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/movement/BaritoneWalk.java
@@ -2,7 +2,13 @@ package me.zeroeightsix.kami.module.modules.movement;
 
 import baritone.api.BaritoneAPI;
 import baritone.api.pathing.goals.GoalXZ;
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import me.zeroeightsix.kami.KamiMod;
+import me.zeroeightsix.kami.event.events.ServerDisconnectedEvent;
 import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
 
 import static me.zeroeightsix.kami.util.MathsUtils.normalizeAngle;
 
@@ -11,12 +17,17 @@ import static me.zeroeightsix.kami.util.MathsUtils.normalizeAngle;
  */
 @Module.Info(name = "BaritoneWalk", description = "AutoWalk with Baritone pathfinding.", category = Module.Category.MOVEMENT)
 public class BaritoneWalk extends Module {
-    
+    private Setting<Boolean> sprint = register(Settings.booleanBuilder("Allow Sprinting").withValue(true));
+    private Setting<Boolean> parkour = register(Settings.booleanBuilder("Allow Parkour").withValue(true).withVisibility(v -> sprint.getValue().equals(true)));
+    private Setting<Boolean> lockView = register(Settings.booleanBuilder("Lock View").withValue(false));
+
     private String direction;
 
     // Very shittily done, but this check is not that taxing on performance cos it is NOT performed every tick.
     @Override
     protected void onEnable() {
+
+
         if (normalizeAngle(mc.player.rotationYaw) >= -22.5 && normalizeAngle(mc.player.rotationYaw) <= 22.5) { // +Z
             BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoalAndPath(new GoalXZ((int) mc.player.posX, (int) mc.player.posZ + 1068));
 
@@ -53,6 +64,16 @@ public class BaritoneWalk extends Module {
     }
 
     @Override
+    public void onUpdate() {
+        BaritoneAPI.getSettings().allowSprint.value = sprint.getValue();
+        BaritoneAPI.getSettings().freeLook.value = !lockView.getValue();
+
+        if (sprint.getValue()) {
+            BaritoneAPI.getSettings().allowParkour.value = parkour.getValue();
+        }
+    }
+
+    @Override
     public String getHudInfo() {
         return direction;
     }
@@ -60,5 +81,18 @@ public class BaritoneWalk extends Module {
     @Override
     protected void onDisable() {
         BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoal(null);
+
+        BaritoneAPI.getSettings().freeLook.reset();
+        BaritoneAPI.getSettings().allowParkour.reset();
+        BaritoneAPI.getSettings().allowSprint.reset();
     }
+
+    @EventHandler
+    private Listener<ServerDisconnectedEvent> disconnectedEventListener = new Listener<>(event -> {
+        System.out.println("bBBBB");
+
+        if (KamiMod.MODULE_MANAGER.getModuleT(BaritoneWalk.class).isEnabled()) {
+            KamiMod.MODULE_MANAGER.getModuleT(BaritoneWalk.class).destroy();
+        }
+    });
 }
