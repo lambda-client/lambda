ac01e6fc97e9c93158c79a8b4e364892ed37ebcc
Dewy REDACTED <dljcoolguy@gmail.com>
2020-04-20 11:26:42 +0100

    added antioverlay

---
+++ src/main/java/me/zeroeightsix/kami/module/modules/render/AntiOverlay.java
@@ -0,0 +1,54 @@
+package me.zeroeightsix.kami.module.modules.render;
+
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
+import net.minecraftforge.client.event.RenderBlockOverlayEvent;
+
+/**
+ * Created by Dewy on the 20th April, 2020
+ */
+@Module.Info(
+          name = "AntiOverlay",
+          description = "Prevents rendering of fire, water and block texture overlays.",
+          category = Module.Category.RENDER
+)
+public class AntiOverlay extends Module {
+    private Setting<Boolean> fire = register(Settings.booleanBuilder("Fire").withValue(true));
+    private Setting<Boolean> water = register(Settings.booleanBuilder("Water").withValue(true));
+    private Setting<Boolean> blocks = register(Settings.booleanBuilder("Blocks").withValue(true));
+
+    @EventHandler
+    public Listener<RenderBlockOverlayEvent> renderBlockOverlayEventListener = new Listener<>(event -> {
+        boolean shouldCancel = false;
+
+        if (!isEnabled()) {
+            return;
+        }
+
+        switch (event.getOverlayType()) {
+            case FIRE:
+                if (fire.getValue()) {
+                    shouldCancel = true;
+                }
+
+                break;
+            case WATER:
+                if (water.getValue()) {
+                    shouldCancel = true;
+                }
+
+                break;
+            case BLOCK:
+                if (blocks.getValue()) {
+                    shouldCancel = true;
+                }
+
+                break;
+        }
+
+        event.setCanceled(shouldCancel);
+    });
+}
