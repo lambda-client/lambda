22596384d407499aa38b377d0c219cd8f8c38642
Bella <bella.who.two@gmail.com>
2020-04-27 12:30:59 -0400

    remove travis from prehHook.sh and use git

--- scripts/preHook.sh
+++ scripts/preHook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CUR_VER="$(cat ./scripts/curVer)"
-COMMIT_TRIM="${TRAVIS_COMMIT::7}"
+COMMIT_TRIM="$(git log --format=%h -1)"
 
 sed -i "s/modVersion=.*/modVersion=${CUR_VER:1}-$COMMIT_TRIM/" gradle.properties
 sed -i "s/\"version\": \".*\",/\"version\": \"${CUR_VER:1}-$COMMIT_TRIM\",/" src/main/resources/mcmod.info
