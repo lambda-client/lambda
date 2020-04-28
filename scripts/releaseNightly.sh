24e095e3c2b676e42ea1d9f27977547a9330124b
Bella <bella.who.two@gmail.com>
2020-04-28 12:48:54 -0400

    Added support for diffs in nightly

--- scripts/releaseNightly.sh
+++ scripts/releaseNightly.sh
@@ -2,15 +2,19 @@
   
 cd ~/kamiblue/
 export COMMIT_LAST="$(git log --format=%h -1)"
-git fetch kamiblue
+export COMMIT_LAST_FULL="$(git log --format=%H -1)"
+
+git fetch kamiblue master
+git fetch origin master
 git reset --hard kamiblue/master
+git push --force origin master
 
 export COMMIT_TRIM="$(git log --format=%h -1)"
 
 if [ "$COMMIT_LAST" == "$COMMIT_TRIM" ]; then
     exit 0
-fi
 
+fi
 rm -rf build/libs
 
 ./scripts/preHook.sh
