a28d7c1179435a259e2c2ef971b9fe9f675543d9
Bella <bella.who.two@gmail.com>
2020-04-29 17:50:50 -0400

    added documentation to releaseNightly

--- scripts/releaseNightly.sh
+++ scripts/releaseNightly.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-  
+
+#  REQUIREMENTS:
+#
+#  ~/.profile:
+#    $GITHUB_RELEASE_ACCESS_TOKEN with repo:status, repo_deployment, public_repo
+#    $GITHUB_RELEASE_REPOSITORY="kami-blue/nightly-releases"
+#    $WEBHOOK= webhook url
+#
+#  ~/kamiblue
+#    git clone git@github.com:kami-blue/nightly-releases.git
+#    git remote add kamiblue git@github.com:kami-blue/client.git
+#
+#  ~/Java JDK 8 is also required
+#
+#  ~/https://github.com/buildkite/github-release/releases is also required
+#
+#  crontab -e
+#    0 0,12 * * * /home/user/releaseNightly >/tmp/cron1.log 2>&1
+
 cd ~/kamiblue/
 export COMMIT_LAST="$(git log --format=%h -1)"
 export COMMIT_LAST_FULL="$(git log --format=%H -1)"
@@ -11,10 +29,10 @@ git push --force origin HEAD:master
 
 export COMMIT_TRIM="$(git log --format=%h -1)"
 
-if [ "$COMMIT_LAST" == "$COMMIT_TRIM" ]; then
+if [[ "$COMMIT_LAST" == "$COMMIT_TRIM" ]]; then
     exit 0
-
 fi
+
 rm -rf build/libs
 
 ./scripts/preHook.sh
