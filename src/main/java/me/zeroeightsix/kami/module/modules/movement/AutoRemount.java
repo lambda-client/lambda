9b79fd2cc150df9ef8c80ef7b1fc42b17051ba08
ionar2 <64785720+ionar2@users.noreply.github.com>
2020-05-05 15:09:59 -0400

    AutoRemount now works with other mountable entities

--- src/main/java/me/zeroeightsix/kami/module/modules/movement/AutoRemount.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/movement/AutoRemount.java
@@ -4,49 +4,87 @@ import me.zeroeightsix.kami.module.Module;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.EntityDonkey;
-import net.minecraft.entity.passive.EntityHorse;
+import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.passive.*;
 import net.minecraft.util.EnumHand;
+
+import java.util.Comparator;
 
 /**
  * @author dominikaaaa
  * Created by dominikaaaa on 05/04/20
  * Updated by dominikaaaa on 07/04/20
+ * updated by ionar2 on 04/05/20
  */
 @Module.Info(
         name = "AutoRemount",
         description = "Automatically remounts your horse",
         category = Module.Category.MOVEMENT
 )
-public class AutoRemount extends Module {
-    private Setting<Mode> modeSetting = register(Settings.e("Mode", Mode.HORSE));
+public class AutoRemount extends Module
+{
+    private Setting<Boolean> boat = register(Settings.b("Boats", true));
+    private Setting<Boolean> horse = register(Settings.b("Horse", true));
+    private Setting<Boolean> skeletonHorse = register(Settings.b("SkeletonHorse", true));
+    private Setting<Boolean> donkey = register(Settings.b("Donkey", true));
+    private Setting<Boolean> pig = register(Settings.b("Pig", true));
+    private Setting<Boolean> llama = register(Settings.b("Llama", true));
     private Setting<Float> range = register(Settings.floatBuilder("Range").withMinimum(1.0f).withValue(2.0f).withMaximum(10.0f).build());
 
-    private enum Mode { HORSE, DONKEY }
+    public void onUpdate()
+    {
+        // we don't need to do anything if we're already riding.
+        if (mc.player.isRiding())
+            return;
 
-    public void onUpdate() {
-        switch (modeSetting.getValue()) {
-            case HORSE:
-                for (Entity e : mc.world.getLoadedEntityList()) {
-                    if (e instanceof EntityHorse && !(mc.player.isRidingHorse())) {
-                        final EntityHorse horse = (EntityHorse) e;
-                        if (mc.player.getDistance(horse) <= range.getValue()) {
-                            mc.playerController.interactWithEntity(mc.player, horse, EnumHand.MAIN_HAND);
-                        }
-                    }
-                }
-                break;
-            case DONKEY:
-                for (Entity e : mc.world.getLoadedEntityList()) {
-                    if (e instanceof EntityDonkey && !(mc.player.isRidingHorse())) {
-                        final EntityDonkey donkey = (EntityDonkey) e;
-                        if (mc.player.getDistance(donkey) <= range.getValue()) {
-                            mc.playerController.interactWithEntity(mc.player, donkey, EnumHand.MAIN_HAND);
-                        }
-                    }
-                }
-                break;
+        Entity entity = mc.world.loadedEntityList.stream()
+                .filter(en -> isValidEntity(en))
+                .min(Comparator.comparing(en -> mc.player.getDistance(en)))
+                .orElse(null);
+
+        if (entity != null)
+            mc.playerController.interactWithEntity(mc.player, entity, EnumHand.MAIN_HAND);
+    }
+
+    private boolean isValidEntity(Entity entity)
+    {
+        if (entity.getDistance(mc.player) > range.getValue())
+            return false;
+
+        if (entity instanceof AbstractHorse)
+        {
+            AbstractHorse horse = (AbstractHorse) entity;
+
+            /// no animal abuse done in this module, no thanks.
+            if (horse.isChild())
+                return false;
         }
 
+        if (entity instanceof EntityBoat && boat.getValue())
+            return true;
+
+        if (entity instanceof EntityHorse && horse.getValue())
+            return true;
+
+        if (entity instanceof EntitySkeletonHorse && skeletonHorse.getValue())
+            return true;
+
+        if (entity instanceof EntityDonkey && donkey.getValue())
+            return true;
+
+        if (entity instanceof EntityPig && pig.getValue())
+        {
+            EntityPig pig = (EntityPig) entity;
+
+            if (pig.getSaddled())
+                return true;
+
+            return false;
+        }
+
+        if (entity instanceof EntityLlama && llama.getValue())
+            return true;
+
+        return false;
     }
 }
