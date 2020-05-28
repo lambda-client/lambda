4eb892e6b398f390e0c56c713a3213fd128869ef
Dominika <sokolov.dominika@gmail.com>
2020-05-28 12:05:30 -0400

    Run renderPlayerlist on another thread to reduce lag and close #895

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinGuiPlayerTabOverlay.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinGuiPlayerTabOverlay.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Created by 086 on 8/04/2018.
@@ -18,9 +19,19 @@ import java.util.List;
 @Mixin(GuiPlayerTabOverlay.class)
 public class MixinGuiPlayerTabOverlay {
 
+    private AtomicReference<List> list1 = null;
+
     @Redirect(method = "renderPlayerlist", at = @At(value = "INVOKE", target = "Ljava/util/List;subList(II)Ljava/util/List;", remap = false))
     public List subList(List list, int fromIndex, int toIndex) {
-        return list.subList(fromIndex, ExtraTab.INSTANCE.isEnabled() ? Math.min(ExtraTab.INSTANCE.tabSize.getValue(), list.size()) : toIndex);
+        if (list1 == null) {
+            list1 = new AtomicReference<>(list.subList(fromIndex, Math.min(1, list.size())));
+        }
+
+        new Thread(() -> {
+            list1.set(list.subList(fromIndex, ExtraTab.INSTANCE.isEnabled() ? Math.min(ExtraTab.INSTANCE.tabSize.getValue(), list.size()) : toIndex));
+        }).start();
+        
+        return list1.get();
     }
 
     @Inject(method = "getPlayerName", at = @At("HEAD"), cancellable = true)
