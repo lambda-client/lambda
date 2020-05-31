81d15bb3fa69dd4e32464bb8f25f5ff7bf840b62
Dominika <sokolov.dominika@gmail.com>
2020-05-31 16:12:03 -0400

    Fix bugs with Zoom and cleanup code

--- src/main/java/me/zeroeightsix/kami/module/modules/render/Zoom.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/render/Zoom.kt
@@ -1,12 +1,13 @@
 package me.zeroeightsix.kami.module.modules.render
 
 import me.zeroeightsix.kami.module.Module
+import me.zeroeightsix.kami.setting.Setting
 import me.zeroeightsix.kami.setting.Settings
 
 /**
  * @author dominikaaaa
  * Created by dominikaaaa on 20/12/19
- * Updated by dominikaaaa on 22/12/19
+ * Updated by dominikaaaa on 31/05/20
  */
 @Module.Info(
         name = "Zoom",
@@ -17,27 +18,32 @@ import me.zeroeightsix.kami.setting.Settings
 class Zoom : Module() {
     private var fov = 0f
     private var sensi = 0f
-    private val fovChange = register(Settings.integerBuilder("FOV").withMinimum(30).withValue(30).withMaximum(150).build())
-    private val sensChange = register(Settings.floatBuilder("Sensitivity").withMinimum(0.25f).withValue(1.3f).withMaximum(2f).build())
-    private val smoothCamera = register(Settings.b("Cinematic Camera", true))
-    private val sens = register(Settings.b("Sensitivity", true))
-    public override fun onEnable() {
+
+    private val fovChange = register(Settings.integerBuilder("FOV").withRange(1, 130).withValue(40).build())
+    private val sens = register(Settings.b("Change Sensitivity", true))
+    private val sensChange = register(Settings.floatBuilder("Sensitivity").withMinimum(0.25f).withValue(1.3f).withMaximum(2f).withVisibility { sens.value }.build())
+    private val smoothCamera = register(Settings.b("Cinematic Camera", false))
+
+    init {
+        fovChange.settingListener = Setting.SettingListeners { if (isEnabled && mc.player != null) mc.gameSettings.fovSetting = fovChange.value.toFloat() }
+        sens.settingListener = Setting.SettingListeners { if (isEnabled && mc.player != null) if (sens.value) mc.gameSettings.mouseSensitivity = sensi * sensChange.value else mc.gameSettings.mouseSensitivity = sensi }
+        sensChange.settingListener = Setting.SettingListeners { if (isEnabled && mc.player != null) mc.gameSettings.mouseSensitivity = sensi * sensChange.value }
+        smoothCamera.settingListener = Setting.SettingListeners { if (isEnabled && mc.player != null) mc.gameSettings.smoothCamera = smoothCamera.value }
+    }
+
+    public override fun onEnable() { // - fix zoom - stashfinder
         if (mc.player == null) return
         fov = mc.gameSettings.fovSetting
         sensi = mc.gameSettings.mouseSensitivity
-        if (smoothCamera.value) mc.gameSettings.smoothCamera = true
+
+        mc.gameSettings.fovSetting = fovChange.value.toFloat()
+        if (sens.value) mc.gameSettings.mouseSensitivity = sensi * sensChange.value
+        mc.gameSettings.smoothCamera = smoothCamera.value
     }
 
     public override fun onDisable() {
         mc.gameSettings.fovSetting = fov
         mc.gameSettings.mouseSensitivity = sensi
-        if (smoothCamera.value) mc.gameSettings.smoothCamera = false
-    }
-
-    override fun onUpdate() {
-        if (mc.player == null) return
-        mc.gameSettings.fovSetting = fovChange.value.toFloat()
-        mc.gameSettings.smoothCamera = smoothCamera.value
-        if (sens.value) mc.gameSettings.mouseSensitivity = sensi * sensChange.value
+        mc.gameSettings.smoothCamera = false
     }
 }
