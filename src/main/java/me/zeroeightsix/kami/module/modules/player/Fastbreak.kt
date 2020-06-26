3405e529185543086f35b7b3e2b9abe6dc3f343a
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-06-26 15:16:15 -0400

    Adding packet mode to fastbreak, issue #796 (#985)
    Co-authored-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/module/modules/player/Fastbreak.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/Fastbreak.kt
@@ -1,16 +1,42 @@
 package me.zeroeightsix.kami.module.modules.player
 
+import me.zero.alpine.listener.EventHandler
+import me.zero.alpine.listener.EventHook
+import me.zero.alpine.listener.Listener
+import me.zeroeightsix.kami.event.events.PacketEvent
 import me.zeroeightsix.kami.module.Module
+import me.zeroeightsix.kami.setting.Settings
+import net.minecraft.network.play.client.CPacketPlayerDigging
 
 /**
  * @author 086
+ * Updated by Xiaro on 24/06/2020.
  */
 @Module.Info(
         name = "Fastbreak",
         category = Module.Category.PLAYER,
-        description = "Nullifies block hit delay"
+        description = "Breaks block faster and nullifies the break delay"
 )
 class Fastbreak : Module() {
+    private val packetMine = register(Settings.b("Packet Mine", true))
+    private val sneakTrigger = register(Settings.booleanBuilder("Sneak Trigger").withValue(true).withVisibility { packetMine.value }.build())
+
+    private var diggingPacket = CPacketPlayerDigging()
+
+    @EventHandler
+    private val sendListener = Listener(EventHook { event: PacketEvent.Send ->
+        if (event.packet !is CPacketPlayerDigging || !packetMine.value) return@EventHook
+        val packet = event.packet as CPacketPlayerDigging
+
+        if (packet.action == CPacketPlayerDigging.Action.START_DESTROY_BLOCK) {
+            diggingPacket = packet
+        } else if (packet.action == CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK && packet.position == diggingPacket.position && ((sneakTrigger.value && mc.player.isSneaking) || !sneakTrigger.value)) {
+            val stopDiggingPacket = CPacketPlayerDigging(CPacketPlayerDigging.Action.STOP_DESTROY_BLOCK, packet.position, packet.facing)
+            event.cancel() /* Cancels aborting packets */
+            mc.connection!!.sendPacket(stopDiggingPacket) /* Sends a stop digging packet so the blocks will actually be mined after the server side breaking animation */
+        }
+    })
+
     override fun onUpdate() {
         mc.playerController.blockHitDelay = 0
     }
