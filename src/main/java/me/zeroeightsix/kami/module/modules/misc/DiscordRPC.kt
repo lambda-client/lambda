698ee5c09fa4daa069ddc7fbd826160218d3f135
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-07-03 19:01:46 -0400

    Add hunger, speed, helditem, fps, tps options to DiscordRPC (#1011)

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/DiscordRPC.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/DiscordRPC.kt
@@ -3,15 +3,19 @@ package me.zeroeightsix.kami.module.modules.misc
 import me.zeroeightsix.kami.DiscordPresence
 import me.zeroeightsix.kami.KamiMod
 import me.zeroeightsix.kami.module.Module
+import me.zeroeightsix.kami.module.modules.client.InfoOverlay
 import me.zeroeightsix.kami.setting.Setting
 import me.zeroeightsix.kami.setting.Settings
-import me.zeroeightsix.kami.util.InfoCalculator
+import me.zeroeightsix.kami.util.InfoCalculator.*
 import me.zeroeightsix.kami.util.MessageSendHelper
+import net.minecraft.client.Minecraft
+import net.minecraft.util.EnumHand
 
 /**
  * @author dominikaaaa
  * Updated by dominikaaaa on 13/01/20
  * Updated (slightly) by Dewy on 3rd April 2020
+ * Updated by Xiaro on 03/07/20
  */
 @Module.Info(
         name = "DiscordRPC",
@@ -30,18 +34,23 @@ class DiscordRPC : Module() {
     var line4Setting: Setting<LineInfo> = register(Settings.e("Line2Right", LineInfo.HEALTH)) // state right
 
     enum class LineInfo {
-        VERSION, WORLD, DIMENSION, USERNAME, HEALTH, SERVER_IP, COORDS, NONE
+        VERSION, WORLD, DIMENSION, USERNAME, HEALTH, HUNGER, SERVER_IP, COORDS, SPEED, HELD_ITEM, FPS, TPS, NONE
     }
 
     fun getLine(line: LineInfo?): String {
         return when (line) {
             LineInfo.VERSION -> KamiMod.MODVERSMALL
             LineInfo.WORLD -> if (mc.isIntegratedServerRunning) "Singleplayer" else if (mc.getCurrentServerData() != null) "Multiplayer" else "Main Menu"
-            LineInfo.DIMENSION -> InfoCalculator.playerDimension(mc)
+            LineInfo.DIMENSION -> playerDimension(mc)
             LineInfo.USERNAME -> if (mc.player != null) mc.player.name else mc.getSession().username
             LineInfo.HEALTH -> if (mc.player != null) mc.player.health.toInt().toString() + " hp" else "No hp"
+            LineInfo.HUNGER -> if (mc.player != null) mc.player.getFoodStats().foodLevel.toString() + " hunger" else "No hunger"
             LineInfo.SERVER_IP -> if (mc.getCurrentServerData() != null) mc.getCurrentServerData()!!.serverIP else if (mc.isIntegratedServerRunning) "Offline" else "Main Menu"
             LineInfo.COORDS -> if (mc.player != null && coordsConfirm.value) "(" + mc.player.posX.toInt() + " " + mc.player.posY.toInt() + " " + mc.player.posZ.toInt() + ")" else "No coords"
+            LineInfo.SPEED -> if (mc.player != null) "Travling at " + KamiMod.MODULE_MANAGER.getModuleT(InfoOverlay::class.java).speed else "No speed"
+            LineInfo.HELD_ITEM -> "Holding " + (if (mc.player != null && !mc.player.getHeldItem(EnumHand.MAIN_HAND).isEmpty()) mc.player.getHeldItem(EnumHand.MAIN_HAND).displayName.toLowerCase() else "no item")
+            LineInfo.FPS -> Minecraft.debugFPS.toString() + " fps"
+            LineInfo.TPS -> if (mc.getCurrentServerData() != null) tps(2).toString() + " tps" else "No tps"
             else -> ""
         }
     }
