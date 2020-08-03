04a4f857e3597a6110ecdba4756780c720992c6f
dewy <37771542+iBuyMountainDew@users.noreply.github.com>
2020-08-03 08:01:37 +0100

    Add KamiMoji (#1099)
    Merge the emoji branch into master.

---
+++ src/main/java/me/zeroeightsix/kami/emoji/Emoji.kt
@@ -0,0 +1,8 @@
+/*
+ * This file is adapted from 2b2t-Utilities/emoji-api which is licensed under MIT.
+ * You can find a copy of the original license here: https://github.com/2b2t-Utilities/emoji-api/blob/35b0683/LICENSE
+ */
+
+package me.zeroeightsix.kami.emoji
+
+class Emoji(val name: String) 
