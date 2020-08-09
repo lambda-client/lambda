6c3ec7f7244ac8ae3ee77355f849857bcddc4220
robex <23484075+robex@users.noreply.github.com>
2020-08-10 01:02:05 +0200

    Updated AntiAFK to work on 2b2t
    * added squarewalk antiafk for 2b2t
    
    * Reformatted code, removed old code
    
    Co-authored-by: robex <robex@robex.ga>
    Co-authored-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/AntiAFK.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/AntiAFK.java
@@ -1,5 +1,7 @@
 package me.zeroeightsix.kami.module.modules.misc;
 
+import baritone.api.BaritoneAPI;
+import baritone.api.pathing.goals.GoalXZ;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import me.zeroeightsix.kami.event.events.PacketEvent;
@@ -20,7 +22,7 @@ import static me.zeroeightsix.kami.util.MessageSendHelper.sendServerMessage;
  * Created by 086 on 16/12/2017.
  * Updated by dominikaaaa on 21/04/20
  * TODO: Path finding to stay inside 1 chunk
- * TODO: Render which chunk is selected 
+ * TODO: Render which chunk is selected
  */
 @Module.Info(
         name = "AntiAFK",
@@ -29,32 +31,36 @@ import static me.zeroeightsix.kami.util.MessageSendHelper.sendServerMessage;
 )
 public class AntiAFK extends Module {
 
-    private Setting<Integer> frequency = register(Settings.integerBuilder("ActionFrequency").withMinimum(1).withMaximum(100).withValue(40).build());
+    private final Setting<Integer> frequency = register(Settings.integerBuilder("ActionFrequency").withMinimum(1).withMaximum(100).withValue(40).build());
     public Setting<Boolean> autoReply = register(Settings.b("AutoReply", true));
-    private Setting<Mode> mode = register(Settings.enumBuilder(Mode.class).withName("Mode").withValue(Mode.TRADITIONAL).withVisibility(v -> false).build());
-    private Setting<Boolean> swing = register(Settings.b("Swing", true));
-    private Setting<Boolean> jump = register(Settings.b("Jump", true));
-    private Setting<Boolean> turn = register(Settings.booleanBuilder("Turn").withValue(true).withVisibility(v -> mode.getValue().equals(Mode.TRADITIONAL)).build());
+    private final Setting<Boolean> swing = register(Settings.b("Swing", true));
+    private final Setting<Boolean> jump = register(Settings.b("Jump", true));
+    private final Setting<Boolean> squareWalk = register(Settings.b("SquareWalk", true));
+    private final Setting<Integer> radius = register(Settings.integerBuilder("Radius").withMinimum(1).withValue(64).build());
+    private final Setting<Boolean> turn = register(Settings.b("Turn", true));
 
-    private Random random = new Random();
-    private enum Mode { TRADITIONAL, CHUNK }
-//    private int[] pos = { 0, 0 };
-//
-//    public void onEnable() {
-//        if (mc.player == null) {
-//            return;
-//        }
-//
-//        if (mode.getValue().equals(Mode.CHUNK)) {
-//            pos[0] = (int) mc.player.posX;
-//            pos[1] = (int) mc.player.posZ;
-//            sendChatMessage(getChatName() + "Registered chunk: X: [" + pos[0] + "][" + (pos[0] + 16) + "] Z: [" + pos[1] + "][" + (pos[1] + 16) + "]");
-//        }
-//    }
-//
-//    private boolean insideChunk() {
-//        return (mc.player.posX > pos[0] && pos[0] + 16 > mc.player.posX) && (mc.player.posZ > pos[1] && pos[1] + 16 > mc.player.posZ);
-//    }
+    private final Random random = new Random();
+
+    private final int[] squareStartCoords = {0, 0};
+    private int squareStep = 0;
+
+    @Override
+    public void onEnable() {
+        if (mc.player == null)
+            return;
+
+        squareStartCoords[0] = (int) mc.player.posX;
+        squareStartCoords[1] = (int) mc.player.posZ;
+    }
+
+    @Override
+    public void onDisable() {
+        if (mc.player == null)
+            return;
+
+        if (isBaritoneActive())
+            BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().cancelEverything();
+    }
 
     @Override
     public void onUpdate() {
@@ -64,11 +70,34 @@ public class AntiAFK extends Module {
             Objects.requireNonNull(mc.getConnection()).sendPacket(new CPacketAnimation(EnumHand.MAIN_HAND));
         }
 
+        if (squareWalk.getValue() && mc.player.ticksExisted % getFrequency() == 0 && !isBaritoneActive()) {
+            int r = clamp(radius.getValue());
+            switch (squareStep) {
+                // +z
+                case 0:
+                    baritoneGotoXZ(squareStartCoords[0], squareStartCoords[1] + r);
+                    break;
+                // +x
+                case 1:
+                    baritoneGotoXZ(squareStartCoords[0] + r, squareStartCoords[1] + r);
+                    break;
+                // -z
+                case 2:
+                    baritoneGotoXZ(squareStartCoords[0] + r, squareStartCoords[1]);
+                    break;
+                // -x
+                case 3:
+                    baritoneGotoXZ(squareStartCoords[0], squareStartCoords[1]);
+                    break;
+            }
+            squareStep = (squareStep + 1) % 4;
+        }
+
         if (jump.getValue() && mc.player.ticksExisted % (2 * getFrequency()) == 0) {
             mc.player.jump();
         }
 
-        if (mode.getValue().equals(Mode.TRADITIONAL) && turn.getValue() && mc.player.ticksExisted % (0.375 * getFrequency()) == 0) {
+        if (turn.getValue() && mc.player.ticksExisted % (0.375 * getFrequency()) == 0) {
             mc.player.rotationYaw = random.nextInt(360) - makeNegRandom(180);
         }
     }
@@ -79,6 +108,20 @@ public class AntiAFK extends Module {
             sendServerMessage("/r I am currently AFK and using KAMI Blue!");
         }
     });
+
+    private boolean isBaritoneActive() {
+        return BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().isActive();
+    }
+
+    private void baritoneGotoXZ(int x, int z) {
+        BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoalAndPath(new GoalXZ(x, z));
+    }
+
+    private int clamp(int val) {
+        if (val < 0)
+            return 0;
+        return val;
+    }
 
     private float getFrequency() {
         return reverseNumber(frequency.getValue(), 1, 100);
