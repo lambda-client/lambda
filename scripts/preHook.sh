26a383bdc3e8e0ab6122cf0ffb22b0a8852dfa91
Dominika <sokolov.dominika@gmail.com>
2020-08-10 14:10:34 -0400

    Fix VER_FULL_BETA not being changed properly

--- scripts/preHook.sh
+++ scripts/preHook.sh
@@ -5,4 +5,4 @@ COMMIT_TRIM="$(git log --format=%h -1)"
 
 sed -i "s/modVersion=.*/modVersion=${CUR_VER:1}-$COMMIT_TRIM/" gradle.properties
 sed -i "s/\"version\": \".*\",/\"version\": \"${CUR_VER:1}-$COMMIT_TRIM\",/" src/main/resources/mcmod.info
-sed -i "s/MODVER = \".*\";/MODVER = \"$CUR_VER-$COMMIT_TRIM\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
+sed -i "s/VER_FULL_BETA = \".*\";/VER_FULL_BETA = \"$CUR_VER-$COMMIT_TRIM\";/" src/main/java/me/zeroeightsix/kami/KamiMod.java
