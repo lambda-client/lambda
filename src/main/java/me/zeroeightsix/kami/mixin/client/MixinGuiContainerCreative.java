1530cff49f461605564a3d9617efeb5bfbd24d0b
Dominika <sokolov.dominika@gmail.com>
2020-08-10 17:55:30 -0400

    Fix player model bugs caused by Mojang code
    Closes #1159

---
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinGuiContainerCreative.java
@@ -0,0 +1,21 @@
+package me.zeroeightsix.kami.mixin.client;
+
+import net.minecraft.client.gui.inventory.GuiContainerCreative;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.EntityLivingBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GuiContainerCreative.class)
+public class MixinGuiContainerCreative {
+    @Redirect(method = "drawGuiContainerBackgroundLayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/inventory/GuiInventory;drawEntityOnScreen(IIIFFLnet/minecraft/entity/EntityLivingBase;)V"))
+    private void drawEntityOnScreen(int posX, int posY, int scale, float mouseX, float mouseY, EntityLivingBase ent) {
+        GlStateManager.pushMatrix();
+        GlStateManager.enableDepth();
+        GuiInventory.drawEntityOnScreen(posX, posY, scale, mouseX, mouseY, ent);
+        GlStateManager.disableDepth();
+        GlStateManager.popMatrix();
+    }
+}
