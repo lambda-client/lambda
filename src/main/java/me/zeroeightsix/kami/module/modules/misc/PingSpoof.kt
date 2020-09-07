eca0f9d6652b2b3619021cdd2ab3fc1b66413de7
FriedrichM <friedrich.mueller@udo.edu>
2020-09-07 19:04:04 +0200

    added PingSpoof

---
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/PingSpoof.kt
@@ -0,0 +1,35 @@
+package me.zeroeightsix.kami.module.modules.misc
+
+import me.zero.alpine.listener.EventHandler
+import me.zero.alpine.listener.EventHook
+import me.zero.alpine.listener.Listener
+import me.zeroeightsix.kami.event.events.PacketEvent
+import me.zeroeightsix.kami.module.Module
+import me.zeroeightsix.kami.setting.Settings
+import net.minecraft.network.play.client.CPacketKeepAlive
+import net.minecraft.network.play.server.SPacketKeepAlive
+import java.util.*
+
+@Module.Info(
+        name = "PingSpoof",
+        category = Module.Category.MISC,
+        description = "Cancels or adds delay to your ping packets"
+)
+class PingSpoof : Module() {
+    private val cancel = register(Settings.b("Cancel", false))// most servers will kick/time you out for this
+    private val delay = register(Settings.integerBuilder("Delay").withValue(400).withMinimum(0).withMaximum(4000).withVisibility { !cancel.value }.build())
+
+    @EventHandler
+    private val receiveListener = Listener(EventHook { event: PacketEvent.Receive ->
+        if (event.packet is SPacketKeepAlive) {
+            event.cancel()
+            if (!cancel.value)
+                Timer().schedule(
+                        object : TimerTask() {
+                            override fun run() {
+                                mc.connection.let { it!!.sendPacket(CPacketKeepAlive((event.packet as SPacketKeepAlive).id)) }
+                            }
+                        }, delay.value.toLong())
+        }
+    })
+}
