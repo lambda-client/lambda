17ff298ecc9a5612508990c1f374f01e2b500bcb
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-09-21 15:39:08 -0400

    Rewrote NoEntityTrace options
    Added sneak trigger option
    Added pickaxe only option

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntityRenderer.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntityRenderer.java
@@ -65,7 +65,7 @@ public class MixinEntityRenderer {
 
     @Redirect(method = "getMouseOver", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;getEntitiesInAABBexcluding(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/AxisAlignedBB;Lcom/google/common/base/Predicate;)Ljava/util/List;"))
     public List<Entity> getEntitiesInAABBexcluding(WorldClient worldClient, Entity entityIn, AxisAlignedBB boundingBox, Predicate<? super Entity> predicate) {
-        if (NoEntityTrace.shouldBlock())
+        if (NoEntityTrace.INSTANCE.shouldIgnoreEntity())
             return new ArrayList<>();
         else
             return worldClient.getEntitiesInAABBexcluding(entityIn, boundingBox, predicate);
