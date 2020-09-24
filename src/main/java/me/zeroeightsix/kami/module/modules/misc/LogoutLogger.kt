d9ec75b32de8779f866b6e9bc316e8e5cc791899
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-09-23 22:37:44 -0400

    Rewrote LogoutLogger
    ...
    Thanks Bella :wheelchair:

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/LogoutLogger.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/LogoutLogger.kt
@@ -7,72 +7,46 @@ import me.zeroeightsix.kami.event.events.ConnectionEvent
 import me.zeroeightsix.kami.manager.mangers.WaypointManager
 import me.zeroeightsix.kami.module.Module
 import me.zeroeightsix.kami.setting.Settings
+import me.zeroeightsix.kami.util.TimerUtils
+import me.zeroeightsix.kami.util.math.CoordinateConverter.asString
+import me.zeroeightsix.kami.util.math.VectorUtils.toBlockPos
 import me.zeroeightsix.kami.util.text.MessageSendHelper
-import net.minecraft.client.network.NetworkPlayerInfo
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.math.BlockPos
 
-/**
- * @author dominikaaaa
- * Created by dominikaaaa on 30/07/20
- */
 @Module.Info(
         name = "LogoutLogger",
         category = Module.Category.MISC,
         description = "Logs when a player leaves the game"
 )
 object LogoutLogger : Module() {
-    private var saveToFile = register(Settings.b("SaveToFile", true))
-    private var print = register(Settings.b("PrintToChat", true))
+    private val saveToFile = register(Settings.b("SaveToFile", true))
+    private val print = register(Settings.b("PrintToChat", true))
 
-    private var loggedPlayers = HashMap<String, BlockPos>()
-    private var onlinePlayers = mutableListOf<NetworkPlayerInfo>()
-    private var ticks = 0
+    private val loggedPlayers = HashMap<String, BlockPos>()
+    private val timer = TimerUtils.TickTimer(TimerUtils.TimeUnit.SECONDS)
 
     @EventHandler
     private val disconnectListener = Listener(EventHook { event: ConnectionEvent.Disconnect ->
         loggedPlayers.clear()
-        onlinePlayers.clear()
     })
 
     override fun onUpdate() {
-        ticks++
-
-        for (player in mc.world.loadedEntityList.filterIsInstance<EntityPlayer>()) {
-            if (player.name == mc.player.name) continue
-            loggedPlayers[player.name] = BlockPos(player.posX.toInt(), player.posY.toInt(), player.posZ.toInt())
+        for (player in mc.world.loadedEntityList) {
+            if (player !is EntityPlayer) continue
+            if (player == mc.player) continue
+            loggedPlayers[player.name] = player.positionVector.toBlockPos()
         }
 
-        if (ticks >= 20) {
-            Thread {
-                updateOnlinePlayers()
-                loggedPlayers.forEach { loggedPlayer ->
-                    var found = false
-                    onlinePlayers.forEach { onlinePlayer ->
-                        if (onlinePlayer.gameProfile.name == loggedPlayer.key) {
-                            found = true
-                        }
-                    }
-
-                    if (!found) {
-                        val posString = "${loggedPlayer.value.x}, ${loggedPlayer.value.y}, ${loggedPlayer.value.z}"
-                        if (print.value) MessageSendHelper.sendChatMessage("${loggedPlayer.key} logged out at $posString")
-                        logCoordinates(loggedPlayer.value, "${loggedPlayer.key} Logout Spot")
-                        loggedPlayers.remove(loggedPlayer.key)
-                    }
-                }
-            }.start()
-            ticks = 0
+        if (timer.tick(1L)) {
+            val toRemove = ArrayList<String>()
+            for ((name, pos) in loggedPlayers) {
+                if (mc.connection!!.getPlayerInfo(name) != null) continue
+                if (print.value) MessageSendHelper.sendChatMessage("$name logged out at ${pos.asString()}")
+                if (saveToFile.value) WaypointManager.add(pos, "$name Logout Spot")
+                toRemove.add(name)
+            }
+            loggedPlayers.keys.removeAll(toRemove)
         }
-    }
-
-    private fun logCoordinates(coordinate: BlockPos, name: String): BlockPos {
-        return if (saveToFile.value) WaypointManager.add(coordinate, name).pos
-        else coordinate
-    }
-
-    private fun updateOnlinePlayers() {
-        if (mc.player == null) return
-        onlinePlayers = mc.player.connection.playerInfoMap.toMutableList()
     }
 }
