bfd809a98d923a82387910a711608763e0dbceb2
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-10-09 18:06:29 -0400

    [fix] Fix event bus crash caused by ConcurrentModificationException

--- src/main/java/me/zeroeightsix/kami/event/KamiEventBus.kt
+++ src/main/java/me/zeroeightsix/kami/event/KamiEventBus.kt
@@ -1,6 +1,5 @@
 package me.zeroeightsix.kami.event
 
-import me.zeroeightsix.kami.KamiMod
 import me.zeroeightsix.kami.util.event.EventBus
 
-object KamiEventBus : EventBus.SynchronizedEventBus(KamiMod.MAIN_THREAD)
+object KamiEventBus : EventBus.ConcurrentEventBus()
