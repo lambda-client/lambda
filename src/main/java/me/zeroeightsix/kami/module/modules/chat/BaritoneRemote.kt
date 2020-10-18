bbd03192ea8d4e7d78ba9d73fcc8c0a708debd72
living <sokolov.dominika@gmail.com>
2020-10-17 22:03:04 -0400

    [fix] BaritoneRemote typo

--- src/main/java/me/zeroeightsix/kami/module/modules/chat/BaritoneRemote.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/chat/BaritoneRemote.kt
@@ -32,9 +32,9 @@ object BaritoneRemote : Module() {
         allow.settingListener = Setting.SettingListeners {
             mc.player?.let {
                 if ((allow.value == Allow.CUSTOM || allow.value == Allow.FRIENDS_AND_CUSTOM) && custom.value == "unchanged") {
-                    MessageSendHelper.sendChatMessage("$chatName Use the &7" + Command.getCommandPrefix()
+                    MessageSendHelper.sendChatMessage("$chatName Use the &7" + Command.getCommandPrefix().value
                             + "set $name Custom names&f command to change the custom users list. Use , to separate players, for example &7"
-                            + Command.getCommandPrefix()
+                            + Command.getCommandPrefix().value
                             + "set $name Custom dominika,Dewy,086&f")
                 }
             }
