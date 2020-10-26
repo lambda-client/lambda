d2afc6654c4a2d20f85c8aec891cbd0c5a0170e1
FriedrichM <37214532+FriedrichM@users.noreply.github.com>
2020-10-26 19:48:05 +0100

    [fix] Bug with AntiWeather
    * made antiweather use mixin
    
    * added isthundering

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/AntiWeather.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/AntiWeather.kt
@@ -1,19 +1,10 @@
 package me.zeroeightsix.kami.module.modules.misc
 
-import me.zeroeightsix.kami.event.events.SafeTickEvent
 import me.zeroeightsix.kami.module.Module
-import me.zeroeightsix.kami.util.event.listener
 
 @Module.Info(
         name = "AntiWeather",
         description = "Removes rain from your world",
         category = Module.Category.MISC
 )
-object AntiWeather : Module() {
-    init {
-        listener<SafeTickEvent> {
-            if (mc.world.isRaining) mc.world.setRainStrength(0f)
-            if (mc.world.isThundering) mc.world.setThunderStrength(0f)
-        }
-    }
-}
+object AntiWeather : Module() {}
