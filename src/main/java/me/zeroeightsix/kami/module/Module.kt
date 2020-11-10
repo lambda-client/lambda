c19bb61f8b574e21e3e4bf6a92f466acc082bb48
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-10 17:13:58 -0500

    [fix] Fixed toggle message spam

--- src/main/java/me/zeroeightsix/kami/module/Module.kt
+++ src/main/java/me/zeroeightsix/kami/module/Module.kt
@@ -90,10 +90,11 @@ open class Module {
     }
 
     fun enable() {
+        if (!enabled.value) sendToggleMessage()
+
         enabled.value = true
         onEnable()
         onToggle()
-        sendToggleMessage()
         if (!alwaysListening) {
             KamiEventBus.subscribe(this)
         }
@@ -101,10 +102,11 @@ open class Module {
 
     fun disable() {
         if (annotation.alwaysEnabled) return
+        if (enabled.value) sendToggleMessage()
+
         enabled.value = false
         onDisable()
         onToggle()
-        sendToggleMessage()
         if (!alwaysListening) {
             KamiEventBus.unsubscribe(this)
         }
@@ -112,7 +114,7 @@ open class Module {
 
     private fun sendToggleMessage() {
         if (mc.currentScreen !is DisplayGuiScreen && this !is ClickGUI && CommandConfig.toggleMessages.value) {
-            MessageSendHelper.sendChatMessage(name.value.toString() + if (enabled.value) " &aenabled" else " &cdisabled")
+            MessageSendHelper.sendChatMessage(name.value.toString() + if (enabled.value) " &adisabled" else " &cenabled")
         }
     }
 
