1a8723e678aebc6885a29e15025b91af56a00d83
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-21 21:20:00 -0500

    Cleaned up HighwayToolsCommand

--- src/main/java/me/zeroeightsix/kami/command/commands/HighwayToolsCommand.kt
+++ src/main/java/me/zeroeightsix/kami/command/commands/HighwayToolsCommand.kt
@@ -12,9 +12,9 @@ import net.minecraft.block.Block
  * @since 01/09/2020
  */
 class HighwayToolsCommand : Command("highwaytools", ChunkBuilder()
-        .append("mode", true, EnumParser(arrayOf("material", "filler", "ignore", "reach", "settings")))
-        .append("value")
-        .build(), "ht") {
+    .append("mode", true, EnumParser(arrayOf("material", "filler", "ignore", "reach", "settings")))
+    .append("value")
+    .build(), "ht") {
 
     override fun call(args: Array<String?>) {
         when (getSubCommand(args)) {
@@ -23,97 +23,95 @@ class HighwayToolsCommand : Command("highwaytools", ChunkBuilder()
             }
 
             SubCommands.MATERIAL -> {
-                try {
-                    val block = Block.getBlockFromName(args[1].toString())!!
+                val block = Block.getBlockFromName(args[1].toString())
+
+                if (block != null) {
                     HighwayTools.material = block
                     MessageSendHelper.sendChatMessage("Set your building material to &7${block.localizedName}&r.")
-                } catch (e: Exception) {
+                } else {
                     MessageSendHelper.sendChatMessage("&7${args[1]}&r is not a valid block.")
                 }
             }
 
             SubCommands.FILLER -> {
-                try {
-                    val block = Block.getBlockFromName(args[1].toString())!!
+                val block = Block.getBlockFromName(args[1].toString())
+
+                if (block != null) {
                     HighwayTools.fillerMat = block
                     MessageSendHelper.sendChatMessage("Set your filling material to &7${block.localizedName}&r.")
-                } catch (e: Exception) {
+                } else {
                     MessageSendHelper.sendChatMessage("&7${args[1]}&r is not a valid block.")
                 }
             }
 
             SubCommands.IGNORE_ADD -> {
-                try {
-                    val block = Block.getBlockFromName(args[2].toString())!!
-                    if (block !in HighwayTools.ignoreBlocks) {
-                        HighwayTools.ignoreBlocks.add(block)
+                val block = Block.getBlockFromName(args[1].toString())
+
+                if (block != null) {
+                    val added = HighwayTools.ignoreBlocks.add(block)
+                    if (added) {
                         HighwayTools.printSettings()
                         MessageSendHelper.sendChatMessage("Added &7${block.localizedName}&r to ignore list.")
                     } else {
+
                         MessageSendHelper.sendChatMessage("&7${block.localizedName}&r is already ignored.")
                     }
-                } catch (e: Exception) {
+                } else {
                     MessageSendHelper.sendChatMessage("&7${args[1]}&r is not a valid block.")
                 }
             }
 
             SubCommands.IGNORE_DEL -> {
-                try {
-                    val block = Block.getBlockFromName(args[2].toString())!!
-                    if (block !in HighwayTools.ignoreBlocks) {
-                        HighwayTools.ignoreBlocks.remove(block)
+                val block = Block.getBlockFromName(args[1].toString())
+
+                if (block != null) {
+                    val removed = HighwayTools.ignoreBlocks.remove(block)
+                    if (removed) {
                         HighwayTools.printSettings()
                         MessageSendHelper.sendChatMessage("Removed &7${block.localizedName}&r from ignore list.")
                     } else {
                         MessageSendHelper.sendChatMessage("&7${block.localizedName}&r is not yet ignored.")
                     }
-                } catch (e: Exception) {
+                } else {
                     MessageSendHelper.sendChatMessage("&7${args[1]}&r is not a valid block.")
                 }
             }
 
-            SubCommands.MAX_REACH -> {
-                try {
-                    HighwayTools.maxReach.value = args[1]!!.toFloatOrNull()
-                    MessageSendHelper.sendChatMessage("Set your max reach to &7${HighwayTools.maxReach.value}&r.")
-                } catch (e: Exception) {
-                    MessageSendHelper.sendChatMessage("&7${args[1]}&r is not a valid number (eg: 5.5).")
-                }
-            }
-
-            SubCommands.NULL -> {
+            else -> {
                 val commands = args.joinToString(separator = " ")
                 MessageSendHelper.sendChatMessage("Invalid command &7${commandPrefix.value}${label} $commands&f!")
             }
         }
     }
 
-    private fun getSubCommand(args: Array<String?>): SubCommands {
+    private fun getSubCommand(args: Array<String?>): SubCommands? {
         return when {
             args[0].isNullOrBlank() -> SubCommands.SETTINGS
 
-            args[1].isNullOrBlank() -> SubCommands.NULL
-
             args[0].equals("settings", ignoreCase = true) -> SubCommands.SETTINGS
+
+            args[1].isNullOrBlank() -> null
 
             args[0].equals("material", ignoreCase = true) -> SubCommands.MATERIAL
 
             args[0].equals("filler", ignoreCase = true) -> SubCommands.FILLER
 
-            args[0].equals("reach", ignoreCase = true) -> SubCommands.MAX_REACH
+            args[0].equals("ignore", ignoreCase = true) -> {
+                when {
+                    args[1].equals("add", ignoreCase = true) -> SubCommands.IGNORE_ADD
 
-            args[0].equals("ignore", ignoreCase = true) && args[2].isNullOrBlank() -> SubCommands.IGNORE_ADD
+                    args[1].equals("del", ignoreCase = true) -> SubCommands.IGNORE_DEL
 
-            args[0].equals("ignore", ignoreCase = true) && args[1].equals("add", ignoreCase = true) -> SubCommands.IGNORE_ADD
+                    else -> null
+                }
+            }
 
-            args[0].equals("ignore", ignoreCase = true) && args[1].equals("del", ignoreCase = true) -> SubCommands.IGNORE_DEL
-
-            else -> SubCommands.NULL
+            else -> null
         }
     }
 
     private enum class SubCommands {
-        MATERIAL, FILLER, IGNORE_ADD, IGNORE_DEL, MAX_REACH, SETTINGS, NULL
+        MATERIAL, FILLER, IGNORE_ADD, IGNORE_DEL, SETTINGS
     }
 
     init {
