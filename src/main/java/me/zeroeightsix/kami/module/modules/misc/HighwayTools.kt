53007af54f34d9b0fbe4ede0ece399a2e31ac27d
theredstoner <huddy987@yahoo.com>
2020-12-16 22:00:51 -0700

    Add swing for breaking fire blocks

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/HighwayTools.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/HighwayTools.kt
@@ -660,6 +660,7 @@ object HighwayTools : Module() {
         if (blockTask.block == Blocks.FIRE) {
             mc.connection!!.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, blockTask.blockPos, EnumFacing.DOWN))
             mc.connection!!.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.STOP_DESTROY_BLOCK, blockTask.blockPos, EnumFacing.DOWN))
+            mc.player.swingArm(EnumHand.MAIN_HAND)
             updateTask(blockTask, TaskState.BREAKING)
             return
         }
