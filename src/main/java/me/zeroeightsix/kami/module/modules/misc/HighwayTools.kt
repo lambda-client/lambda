0290c8872028af98ec7b032fcb5d34a98b9559b3
Natan <66911017+natan515@users.noreply.github.com>
2020-12-28 08:07:17 +0200

    Wait sorry lol. this is the last edit.

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/HighwayTools.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/HighwayTools.kt
@@ -912,7 +912,7 @@ object HighwayTools : Module() {
                         append("\n    §9> §7Coordinate: §a${startingBlockPos.z}§r")
                     }
                 }
-                if (startingBlockPos.y >= 117 && startingBlockPos.y < 120) append("\n    §9> §7You should move to Y 120 to build proper highways")
+                if (startingBlockPos.y in 117..119) append("\n    §9> §7You should move to Y 120 to build proper highways")
                 sendChatMessage(toString())
             }
         }
