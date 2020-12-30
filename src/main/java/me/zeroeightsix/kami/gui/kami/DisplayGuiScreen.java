e2e26c98682b5f8046103a660f559bbf6245f473
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-30 13:52:35 -0500

    [fix] ClickGUI pauses game in single player

--- src/main/java/me/zeroeightsix/kami/gui/kami/DisplayGuiScreen.java
+++ src/main/java/me/zeroeightsix/kami/gui/kami/DisplayGuiScreen.java
@@ -49,6 +49,11 @@ public class DisplayGuiScreen extends GuiScreen {
     }
 
     @Override
+    public boolean doesGuiPauseGame() {
+        return false;
+    }
+
+    @Override
     public void onGuiClosed() {
         KamiGUI gui = KamiMod.INSTANCE.getGuiManager();
 
