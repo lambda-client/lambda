a15730ae696093874007a1af60bd7435d7c6f299
Dominika <sokolov.dominika@gmail.com>
2020-12-30 22:30:14 -0500

    [feature] Set maxCacheSize in UUIDManager to 1000
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/manager/managers/UUIDManager.kt
+++ src/main/java/me/zeroeightsix/kami/manager/managers/UUIDManager.kt
@@ -7,7 +7,7 @@ import org.kamiblue.capeapi.AbstractUUIDManager
 import org.kamiblue.capeapi.PlayerProfile
 import org.kamiblue.capeapi.UUIDUtils
 
-object UUIDManager : AbstractUUIDManager(KamiMod.DIRECTORY + "UUIDCache.json", KamiMod.LOG), Manager {
+object UUIDManager : AbstractUUIDManager(KamiMod.DIRECTORY + "UUIDCache.json", KamiMod.LOG, maxCacheSize = 1000), Manager {
 
     override fun getOrRequest(nameOrUUID: String): PlayerProfile? {
         return Wrapper.minecraft.connection?.playerInfoMap?.let { playerInfoMap ->
@@ -23,5 +23,4 @@ object UUIDManager : AbstractUUIDManager(KamiMod.DIRECTORY + "UUIDCache.json", K
             }
         } ?: super.getOrRequest(nameOrUUID)
     }
-
 }
