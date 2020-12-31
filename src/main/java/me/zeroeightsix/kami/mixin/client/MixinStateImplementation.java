131f5ac9f5c48956e0e2f7d53544d28dff4ba423
Dominika <sokolov.dominika@gmail.com>
2020-12-30 20:56:13 -0500

    [cleanup] Suppress useless warning in MixinStateImplementation
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinStateImplementation.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinStateImplementation.java
@@ -28,6 +28,7 @@ public class MixinStateImplementation {
     @Final
     private Block block;
 
+    @SuppressWarnings("deprecation")
     @Redirect(method = "addCollisionBoxToList", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;addCollisionBoxToList(Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/AxisAlignedBB;Ljava/util/List;Lnet/minecraft/entity/Entity;Z)V"))
     public void addCollisionBoxToList(Block b, IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean isActualState) {
         AddCollisionBoxToListEvent event = new AddCollisionBoxToListEvent(b, state, worldIn, pos, entityBox, collidingBoxes, entityIn, isActualState);
