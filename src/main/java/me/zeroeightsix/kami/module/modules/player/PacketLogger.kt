910f07f6ad8967125cc5a5396ee120838138b087
Christian <teufelsrochen@go4more.de>
2021-01-02 01:40:30 +0100

    Help to understand packet behavior with PacketLogger
    more packets coming soon

--- src/main/java/me/zeroeightsix/kami/module/modules/player/PacketLogger.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/PacketLogger.kt
@@ -8,6 +8,9 @@ import me.zeroeightsix.kami.setting.Setting
 import me.zeroeightsix.kami.setting.Settings
 import org.kamiblue.event.listener.listener
 import me.zeroeightsix.kami.util.text.MessageSendHelper
+import net.minecraft.network.play.client.CPacketPlayer
+import net.minecraft.network.play.client.CPacketPlayerDigging
+import net.minecraft.util.math.Vec3d
 import java.io.*
 import java.nio.charset.StandardCharsets
 import java.text.SimpleDateFormat
@@ -41,7 +44,17 @@ object PacketLogger : Module() {
                 return@listener
             }
 
-            lines.add("${sdf.format(Date())}\n${it.packet.javaClass.simpleName}\n${it.packet.javaClass}\n${it.packet}\n\n")
+            lines.add("${sdf.format(Date())}\n${it.packet.javaClass.simpleName}\n${it.packet.javaClass}\n${it.packet}")
+            when (it.packet) {
+                is CPacketPlayerDigging -> {
+                    lines.add("\nMining - ${it.packet.position}@${it.packet.facing} - ${it.packet.action}")
+                }
+                is CPacketPlayer.Rotation -> {
+                    val vec = Vec3d(it.packet.getX(0.0), it.packet.getY(0.0), it.packet.getZ(0.0))
+                    lines.add("\nRotation - Pitch: ${it.packet.getPitch(0.0F)} Yaw: ${it.packet.getYaw(0.0F)}")
+                }
+            }
+            lines.add("\n\n")
         }
 
         listener<SafeTickEvent> {
