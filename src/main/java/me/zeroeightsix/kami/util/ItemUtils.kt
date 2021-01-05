64cde106b06ee1d09da8706d161c0caebc254fa2
Dominika <sokolov.dominika@gmail.com>
2021-01-04 21:36:20 -0500

    [feature] Added a sign option to `;dupebook`
    Closes #956
    
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/util/ItemUtils.kt
+++ src/main/java/me/zeroeightsix/kami/util/ItemUtils.kt
@@ -1,7 +1,11 @@
 package me.zeroeightsix.kami.util
 
+import io.netty.buffer.Unpooled
+import me.zeroeightsix.kami.event.SafeClientEvent
 import net.minecraft.block.Block
 import net.minecraft.item.*
+import net.minecraft.network.PacketBuffer
+import net.minecraft.network.play.client.CPacketCustomPayload
 
 val ItemStack.originalName: String get() = item.getItemStackDisplayName(this)
 
@@ -14,3 +18,9 @@ val Item.isWeapon get() = this is ItemSword || this is ItemAxe
 val ItemFood.foodValue get() = this.getHealAmount(ItemStack.EMPTY)
 
 val ItemFood.saturation get() = foodValue * this.getSaturationModifier(ItemStack.EMPTY) * 2f
+
+fun SafeClientEvent.itemPayload(item: ItemStack, channelIn: String) {
+    val buffer = PacketBuffer(Unpooled.buffer())
+    buffer.writeItemStack(item)
+    player.connection.sendPacket(CPacketCustomPayload(channelIn, buffer))
+}
