2f122a0d26043c7a11f25b76c60c4eeab9e78e8b
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-01-05 19:19:53 -0500

    [new] Added ItemModel
    closes #1089

--- src/main/java/me/zeroeightsix/kami/mixin/client/render/MixinItemRenderer.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/render/MixinItemRenderer.java
@@ -1,7 +1,13 @@
 package me.zeroeightsix.kami.mixin.client.render;
 
 import me.zeroeightsix.kami.module.modules.player.Freecam;
+import me.zeroeightsix.kami.module.modules.render.ItemModel;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumHandSide;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,6 +19,30 @@ public class MixinItemRenderer {
     private void rotateArm(float partialTicks, CallbackInfo ci) {
         if (Freecam.INSTANCE.isEnabled() && Freecam.INSTANCE.getCameraGuy() != null) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "renderItemInFirstPerson(Lnet/minecraft/client/entity/AbstractClientPlayer;FFLnet/minecraft/util/EnumHand;FLnet/minecraft/item/ItemStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;pushMatrix()V", shift = At.Shift.AFTER))
+    private void transformSideFirstPerson$pushMatrix(AbstractClientPlayer player, float p_187457_2_, float p_187457_3_, EnumHand hand, float p_187457_5_, ItemStack stack, float p_187457_7_, CallbackInfo ci) {
+        if (ItemModel.INSTANCE.isEnabled()) {
+            EnumHandSide enumhandside = hand == EnumHand.MAIN_HAND ? player.getPrimaryHand() : player.getPrimaryHand().opposite();
+            float sideMultiplier = enumhandside == EnumHandSide.RIGHT ? 1.0f : -1.0f;
+
+            GlStateManager.translate(ItemModel.INSTANCE.getPosX() * sideMultiplier, ItemModel.INSTANCE.getPosY(), ItemModel.INSTANCE.getPosZ());
+        }
+    }
+
+    @Inject(method = "renderItemInFirstPerson(Lnet/minecraft/client/entity/AbstractClientPlayer;FFLnet/minecraft/util/EnumHand;FLnet/minecraft/item/ItemStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemRenderer;renderItemSide(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/renderer/block/model/ItemCameraTransforms$TransformType;Z)V"))
+    private void transformSideFirstPerson$renderItemSide(AbstractClientPlayer player, float p_187457_2_, float p_187457_3_, EnumHand hand, float p_187457_5_, ItemStack stack, float p_187457_7_, CallbackInfo ci) {
+        if (ItemModel.INSTANCE.isEnabled()) {
+            EnumHandSide enumhandside = hand == EnumHand.MAIN_HAND ? player.getPrimaryHand() : player.getPrimaryHand().opposite();
+            float sideMultiplier = enumhandside == EnumHandSide.RIGHT ? 1.0f : -1.0f;
+            float scale = ItemModel.INSTANCE.getScale();
+
+            GlStateManager.rotate(ItemModel.INSTANCE.getRotateX(), 1.0f, 0.0f, 0.0f);
+            GlStateManager.rotate(ItemModel.INSTANCE.getRotateY() * sideMultiplier, 0.0f, 1.0f, 0.0f);
+            GlStateManager.rotate(ItemModel.INSTANCE.getRotateZ() * sideMultiplier, 0.0f, 0.0f, 1.0f);
+            GlStateManager.scale(scale, scale, scale);
         }
     }
 }
