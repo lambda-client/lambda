616cc699020f6540ce16ec88c017cef2f75bb08e
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-01-05 23:29:29 -0500

    [fix] Fixed build warnings

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/LogoutLogger.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/LogoutLogger.kt
@@ -5,11 +5,12 @@ import me.zeroeightsix.kami.event.events.ConnectionEvent
 import me.zeroeightsix.kami.manager.managers.WaypointManager
 import me.zeroeightsix.kami.module.Module
 import me.zeroeightsix.kami.setting.ModuleConfig.setting
+import me.zeroeightsix.kami.util.EntityUtils.flooredPosition
 import me.zeroeightsix.kami.util.TickTimer
 import me.zeroeightsix.kami.util.TimeUnit
 import me.zeroeightsix.kami.util.math.CoordinateConverter.asString
-import me.zeroeightsix.kami.util.math.VectorUtils.toBlockPos
 import me.zeroeightsix.kami.util.text.MessageSendHelper
+import me.zeroeightsix.kami.util.threads.onMainThread
 import me.zeroeightsix.kami.util.threads.safeListener
 import net.minecraft.client.entity.EntityOtherPlayerMP
 import net.minecraft.util.math.BlockPos
@@ -30,25 +31,28 @@ object LogoutLogger : Module() {
 
     init {
         listener<ConnectionEvent.Disconnect> {
-            loggedPlayers.clear()
+            onMainThread {
+                loggedPlayers.clear()
+            }
         }
 
         safeListener<TickEvent.ClientTickEvent> {
-            for (loadedPlayer in world.loadedEntityList) {
+            for (loadedPlayer in world.playerEntities) {
                 if (loadedPlayer !is EntityOtherPlayerMP) continue
-                connection.getPlayerInfo(loadedPlayer.gameProfile.id).let {
-                    loggedPlayers[it.gameProfile] = loadedPlayer.positionVector.toBlockPos()
-                }
+
+                val info = connection.getPlayerInfo(loadedPlayer.gameProfile.id)
+                loggedPlayers[info.gameProfile] = loadedPlayer.flooredPosition
             }
 
             if (timer.tick(1L)) {
                 val toRemove = ArrayList<GameProfile>()
+
                 for ((profile, pos) in loggedPlayers) {
-                    if (connection.getPlayerInfo(profile.id) != null) continue
                     if (print.value) MessageSendHelper.sendChatMessage("${profile.name} logged out at ${pos.asString()}")
                     if (saveToFile.value) WaypointManager.add(pos, "${profile.name} Logout Spot")
                     toRemove.add(profile)
                 }
+
                 loggedPlayers.keys.removeAll(toRemove)
             }
         }
