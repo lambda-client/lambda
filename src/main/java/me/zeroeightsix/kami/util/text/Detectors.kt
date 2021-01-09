03e770b5c40cafdbd0d1356bee61dae41c142587
Dominika <sokolov.dominika@gmail.com>
2021-01-09 14:28:22 -0500

    [feature] Added RemoteCommand, fixed regex detection
    Closes #1291
    
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/util/text/Detectors.kt
+++ src/main/java/me/zeroeightsix/kami/util/text/Detectors.kt
@@ -1,5 +1,7 @@
 package me.zeroeightsix.kami.util.text
 
+import me.zeroeightsix.kami.KamiMod
+
 interface Detector {
     infix fun detect(input: CharSequence): Boolean
 
@@ -21,17 +23,23 @@ interface PrefixDetector : Detector, RemovableDetector {
 
     override fun removedOrNull(input: CharSequence) = prefixes.firstOrNull(input::startsWith)?.let {
         input.removePrefix(it)
+    } ?: run {
+        KamiMod.LOG.debug("Could not replace type ${this.javaClass.simpleName} for message \"$input\"")
+        null
     }
 }
 
 interface RegexDetector : Detector, RemovableDetector {
     val regexes: Array<out Regex>
 
-    override infix fun detect(input: CharSequence) = regexes.any { it.matches(input) }
+    override infix fun detect(input: CharSequence) = regexes.any { it.containsMatchIn(input) }
 
-    fun matchedRegex(input: CharSequence) = regexes.find { it.matches(input) }
+    fun matchedRegex(input: CharSequence) = regexes.find { it.containsMatchIn(input) }
 
     override fun removedOrNull(input: CharSequence): CharSequence? = matchedRegex(input)?.let { regex ->
         input.replace(regex, "").takeIf { it.isNotBlank() }
+    } ?: run {
+        KamiMod.LOG.debug("Could not replace type ${this.javaClass.simpleName} for message \"$input\"")
+        null
     }
 }
