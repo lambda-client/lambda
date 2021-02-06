c624e5ca7a3d1021f3eed7c42a98c008abf99a5c
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-06 03:29:32 -0500

    [cleanup] Removed game broken setting, cleaned up PingSpoof code

--- src/main/kotlin/org/kamiblue/client/module/modules/misc/PingSpoof.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/misc/PingSpoof.kt
@@ -1,31 +1,33 @@
 package org.kamiblue.client.module.modules.misc
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import net.minecraft.network.play.client.CPacketKeepAlive
+import net.minecraft.network.play.server.SPacketKeepAlive
 import org.kamiblue.client.event.events.PacketEvent
 import org.kamiblue.client.module.Category
 import org.kamiblue.client.module.Module
-import net.minecraft.network.play.client.CPacketKeepAlive
-import net.minecraft.network.play.server.SPacketKeepAlive
+import org.kamiblue.client.util.threads.defaultScope
+import org.kamiblue.client.util.threads.onMainThreadSafe
 import org.kamiblue.event.listener.listener
-import java.util.*
 
 internal object PingSpoof : Module(
     name = "PingSpoof",
     category = Category.MISC,
     description = "Cancels or adds delay to your ping packets"
 ) {
-    private val cancel = setting("Cancel", false) // most servers will kick/time you out for this
-    private val delay = setting("Delay", 100, 0..2000, 25, { !cancel.value })
+    private val delay = setting("Delay", 100, 0..2000, 25)
 
     init {
         listener<PacketEvent.Receive> {
-            if (it.packet !is SPacketKeepAlive || mc.player == null) return@listener
-            it.cancel()
-            if (!cancel.value) {
-                Timer().schedule(object : TimerTask() {
-                    override fun run() {
-                        mc.connection?.sendPacket(CPacketKeepAlive(it.packet.id))
+            if (it.packet is SPacketKeepAlive) {
+                it.cancel()
+                defaultScope.launch {
+                    delay(delay.value.toLong())
+                    onMainThreadSafe {
+                        connection.sendPacket(CPacketKeepAlive(it.packet.id))
                     }
-                }, delay.value.toLong())
+                }
             }
         }
     }
