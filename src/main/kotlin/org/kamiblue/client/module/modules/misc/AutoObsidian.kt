4ae8917a8038a1f5a62cf9e71439dba2727f15e5
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-08 03:10:46 -0500

    [enhancement] Added instant mining to AutoObsidian

--- src/main/kotlin/org/kamiblue/client/module/modules/misc/AutoObsidian.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/misc/AutoObsidian.kt
@@ -19,6 +19,7 @@ import net.minecraft.item.ItemShulkerBox
 import net.minecraft.network.play.client.CPacketEntityAction
 import net.minecraft.network.play.client.CPacketPlayerDigging
 import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock
+import net.minecraft.network.play.server.SPacketBlockChange
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
@@ -26,6 +27,7 @@ import net.minecraft.util.math.RayTraceResult
 import net.minecraft.util.math.Vec3d
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import org.kamiblue.client.event.SafeClientEvent
+import org.kamiblue.client.event.events.PacketEvent
 import org.kamiblue.client.event.events.RenderWorldEvent
 import org.kamiblue.client.manager.managers.PlayerPacketManager
 import org.kamiblue.client.module.Category
@@ -46,8 +48,10 @@ import org.kamiblue.client.util.math.VectorUtils.toVec3dCenter
 import org.kamiblue.client.util.text.MessageSendHelper
 import org.kamiblue.client.util.threads.defaultScope
 import org.kamiblue.client.util.threads.onMainThreadSafe
+import org.kamiblue.client.util.threads.safeAsyncListener
 import org.kamiblue.client.util.threads.safeListener
 import org.kamiblue.commons.interfaces.DisplayEnum
+import org.kamiblue.event.listener.asyncListener
 import org.kamiblue.event.listener.listener
 import kotlin.math.ceil
 
@@ -61,9 +65,11 @@ internal object AutoObsidian : Module(
     private val searchShulker by setting("Search Shulker", false)
     private val leaveEmptyShulkers by setting("Leave Empty Shulkers", true, { searchShulker })
     private val autoRefill by setting("Auto Refill", false, { fillMode != FillMode.INFINITE })
-    private val threshold by setting("Refill Threshold", 8, 1..63, 1, { autoRefill && fillMode != FillMode.INFINITE })
+    private val instantMining by setting("Instant Mining", true)
+    private val instantMiningDelay by setting("Instant Mining Delay", 10, 1..20, 1, { instantMining })
+    private val threshold by setting("Refill Threshold", 32, 1..64, 1, { autoRefill && fillMode != FillMode.INFINITE })
     private val targetStacks by setting("Target Stacks", 1, 1..20, 1, { fillMode == FillMode.TARGET_STACKS })
-    private val delayTicks by setting("Delay Ticks", 5, 0..10, 1)
+    private val delayTicks by setting("Delay Ticks", 5, 1..10, 1)
     private val rotationMode by setting("Rotation Mode", RotationMode.SPOOF)
     private val maxReach by setting("Max Reach", 4.5f, 2.0f..6.0f, 0.1f)
 
@@ -104,10 +110,15 @@ internal object AutoObsidian : Module(
     private var placingPos = BlockPos(0, -1, 0)
     private var shulkerID = 0
     private var lastHitVec: Vec3d? = null
+    private var lastMiningSide = EnumFacing.UP
+    private var canInstantMine = false
 
     private val tickTimer = TickTimer(TimeUnit.TICKS)
     private val rotateTimer = TickTimer(TimeUnit.TICKS)
     private val shulkerOpenTimer = TickTimer(TimeUnit.TICKS)
+    private val miningTimer = TickTimer(TimeUnit.TICKS)
+    private val miningTimeoutTimer = TickTimer(TimeUnit.SECONDS)
+
     private val renderer = ESPRenderer().apply { aFilled = 33; aOutline = 233 }
 
     override fun isActive(): Boolean {
@@ -123,15 +134,39 @@ internal object AutoObsidian : Module(
             reset()
         }
 
+        asyncListener<PacketEvent.PostSend> {
+            if (!instantMining || it.packet !is CPacketPlayerDigging) return@asyncListener
+
+            if (it.packet.position != placingPos || it.packet.facing != lastMiningSide) {
+                canInstantMine = false
+            }
+        }
+
+        safeAsyncListener<PacketEvent.Receive> {
+            if (!instantMining || it.packet !is SPacketBlockChange) return@safeAsyncListener
+            if (it.packet.blockPosition != placingPos) return@safeAsyncListener
+
+            val prevBlock = world.getBlockState(it.packet.blockPosition).block
+            val newBlock = it.packet.blockState.block
+
+            if (prevBlock != newBlock) {
+                if (prevBlock != Blocks.AIR && newBlock == Blocks.AIR) {
+                    canInstantMine = true
+                }
+                miningTimer.reset()
+                miningTimeoutTimer.reset()
+            }
+        }
+
+        listener<RenderWorldEvent> {
+            if (state != State.DONE) renderer.render(clear = false, cull = true)
+        }
+
         safeListener<TickEvent.ClientTickEvent>(69) {
             if (it.phase != TickEvent.Phase.START) return@safeListener
 
             runAutoObby()
             doRotation()
-        }
-
-        listener<RenderWorldEvent> {
-            if (state != State.DONE) renderer.render(clear = false, cull = true)
         }
     }
 
@@ -219,9 +254,13 @@ internal object AutoObsidian : Module(
             ?: posList.find { isPositionValid(it.first, it.second, eyePos) }
 
         if (pair != null) {
-            placingPos = pair.first
-            renderer.clear()
-            renderer.add(pair.first, ColorHolder(64, 255, 64))
+            if (pair.first != placingPos) {
+                placingPos = pair.first
+                canInstantMine = false
+
+                renderer.clear()
+                renderer.add(pair.first, ColorHolder(64, 255, 64))
+            }
         } else {
             MessageSendHelper.sendChatMessage("$chatName No valid position for placing shulker box / ender chest nearby, disabling.")
             mc.soundHandler.playSound(PositionedSoundRecord.getRecord(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f))
@@ -494,10 +533,20 @@ internal object AutoObsidian : Module(
         val center = pos.toVec3dCenter()
         val diff = player.getPositionEyes(1.0f).subtract(center)
         val normalizedVec = diff.normalize()
-        val side = EnumFacing.getFacingFromVector(normalizedVec.x.toFloat(), normalizedVec.y.toFloat(), normalizedVec.z.toFloat())
+        var side = EnumFacing.getFacingFromVector(normalizedVec.x.toFloat(), normalizedVec.y.toFloat(), normalizedVec.z.toFloat())
 
         lastHitVec = center
         rotateTimer.reset()
+
+        if (instantMining && canInstantMine) {
+            if (!miningTimer.tick(instantMiningDelay.toLong(), false)) return
+
+            if (!miningTimeoutTimer.tick(2L, false)) {
+                side = side.opposite
+            } else {
+                canInstantMine = false
+            }
+        }
 
         defaultScope.launch {
             delay(20L)
@@ -505,6 +554,7 @@ internal object AutoObsidian : Module(
                 connection.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, pos, side))
                 connection.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.STOP_DESTROY_BLOCK, pos, side))
                 player.swingArm(EnumHand.MAIN_HAND)
+                lastMiningSide = side
             }
         }
     }
@@ -558,5 +608,7 @@ internal object AutoObsidian : Module(
         searchingState = SearchingState.PLACING
         placingPos = BlockPos(0, -1, 0)
         lastHitVec = null
+        lastMiningSide = EnumFacing.UP
+        canInstantMine = false
     }
 }
