e2978032e04a8f2ae7e6488eef51de972834ed98
Christian <fractalminds@protonmail.com>
2021-02-09 23:34:58 +0100

    Attempt to fix random stucks

--- src/main/kotlin/org/kamiblue/client/module/modules/misc/HighwayTools.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/misc/HighwayTools.kt
@@ -636,7 +636,7 @@ internal object HighwayTools : Module(
 
     private fun checkTasks(pos: BlockPos): Boolean {
         return pendingTasks.values.all {
-            it.taskState == TaskState.DONE || pos.distanceTo(it.blockPos) < maxReach - 1.0
+            it.taskState == TaskState.DONE || pos.distanceTo(it.blockPos) < maxReach - 0.7
         }
     }
 
