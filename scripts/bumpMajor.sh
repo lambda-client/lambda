41f52ab19bac708b7e79605b6743ec1e50c2dfeb
lv <~@l1v.in>
2021-02-14 15:08:57 -0500

    [fix] Broken references to KamiMod
    Signed-off-by: lv <~@l1v.in>

--- scripts/bumpMajor.sh
+++ scripts/bumpMajor.sh
@@ -18,9 +18,9 @@ VERSION="$CUR_R$CUR_M.01"
 VERSION_DEV="$CUR_R$CUR_M.xx-dev"
 
 sed -i "s/modVersion=.*/modVersion=$VERSION_DEV/" gradle.properties
-sed -i "s/VERSION = \".*\";/VERSION = \"$VERSION_DEV\";/" src/main/java/org/kamiblue/client/KamiMod.java
-sed -i "s/VERSION_SIMPLE = \".*\";/VERSION_SIMPLE = \"$VERSION_DEV\";/" src/main/java/org/kamiblue/client/KamiMod.java
-sed -i "s/VERSION_MAJOR = \".*\";/VERSION_MAJOR = \"$VERSION\";/" src/main/java/org/kamiblue/client/KamiMod.java
+sed -i "s/VERSION = \".*\";/VERSION = \"$VERSION_DEV\";/" src/main/kotlin/org/kamiblue/client/KamiMod.kt
+sed -i "s/VERSION_SIMPLE = \".*\";/VERSION_SIMPLE = \"$VERSION_DEV\";/" src/main/kotlin/org/kamiblue/client/KamiMod.kt
+sed -i "s/VERSION_MAJOR = \".*\";/VERSION_MAJOR = \"$VERSION\";/" src/main/kotlin/org/kamiblue/client/KamiMod.kt
 git commit -am "[bump] Release Major $VERSION"
 
 echo "[bumpMajor] Created commit for version '$VERSION', remember to push!"
