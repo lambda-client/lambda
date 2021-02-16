0147669b0b644adf0b904541da68302b0c4767ca
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-16 15:59:21 -0500

    [fix] Baritone PauseProcess not pausing AutoObsidian

--- src/main/kotlin/org/kamiblue/client/module/modules/misc/AutoObsidian.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/misc/AutoObsidian.kt
@@ -33,6 +33,7 @@ import org.kamiblue.client.manager.managers.PlayerPacketManager
 import org.kamiblue.client.module.Category
 import org.kamiblue.client.module.Module
 import org.kamiblue.client.process.AutoObsidianProcess
+import org.kamiblue.client.process.PauseProcess
 import org.kamiblue.client.util.*
 import org.kamiblue.client.util.EntityUtils.getDroppedItem
 import org.kamiblue.client.util.EntityUtils.getDroppedItems
@@ -163,7 +164,7 @@ internal object AutoObsidian : Module(
         }
 
         safeListener<TickEvent.ClientTickEvent>(69) {
-            if (it.phase != TickEvent.Phase.START) return@safeListener
+            if (it.phase != TickEvent.Phase.START || PauseProcess.isActive) return@safeListener
 
             runAutoObby()
             doRotation()
