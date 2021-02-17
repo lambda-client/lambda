97a62ce0a3e165f445e46bc6ea0823020d1b14ae
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-17 00:34:16 -0500

    [new] Added MultiTask
    closes #1049
    100% compatible with Phobos, also fixed bug with placing blocks with crystal aura enabled.

---
+++ src/main/java/org/kamiblue/client/mixin/client/accessor/player/AccessorEntityPlayerSP.java
@@ -0,0 +1,13 @@
+package org.kamiblue.client.mixin.client.accessor.player;
+
+import net.minecraft.client.entity.EntityPlayerSP;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EntityPlayerSP.class)
+public interface AccessorEntityPlayerSP {
+
+    @Accessor("handActive")
+    void kbSetHandActive(boolean value);
+
+}
