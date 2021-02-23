4ce5a74f10afe38723c59b0e0ce87fcfd06b23cd
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-23 17:45:40 -0500

    [enhancement] Rewrote Criticals

--- src/main/kotlin/org/kamiblue/client/module/modules/combat/Criticals.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/combat/Criticals.kt
@@ -1,113 +1,138 @@
 package org.kamiblue.client.module.modules.combat
 
+import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.MobEffects
 import net.minecraft.network.play.client.CPacketAnimation
 import net.minecraft.network.play.client.CPacketPlayer
 import net.minecraft.network.play.client.CPacketUseEntity
+import net.minecraft.util.EnumHand
+import net.minecraft.world.GameType
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import org.kamiblue.client.event.SafeClientEvent
 import org.kamiblue.client.event.events.PacketEvent
+import org.kamiblue.client.event.events.PlayerAttackEvent
+import org.kamiblue.client.mixin.extension.isInWeb
 import org.kamiblue.client.module.Category
 import org.kamiblue.client.module.Module
-import org.kamiblue.client.util.MovementUtils.setSpeed
-import org.kamiblue.client.util.MovementUtils.speed
+import org.kamiblue.client.util.EntityUtils.isFakeOrSelf
+import org.kamiblue.client.util.EntityUtils.isInOrAboveLiquid
 import org.kamiblue.client.util.threads.safeListener
+import org.kamiblue.commons.interfaces.DisplayEnum
+import org.kamiblue.event.listener.listener
 
 internal object Criticals : Module(
     name = "Criticals",
     category = Category.COMBAT,
     description = "Always do critical attacks"
 ) {
-    private val mode = setting("Mode", CriticalMode.PACKET)
-    private val miniJump = setting("Mini Jump", true, { mode.value == CriticalMode.DELAY })
+    private val mode by setting("Mode", Mode.PACKET)
+    private val jumpMotion by setting("Jump Motion", 0.25, 0.1..0.5, 0.01, { mode == Mode.MINI_JUMP }, fineStep = 0.001)
+    private val attackFallDistance by setting("Attack Fall Distance", 0.5, 0.05..1.0, 0.05, { mode != Mode.PACKET })
 
-    private enum class CriticalMode {
-        PACKET, DELAY
+    private enum class Mode(override val displayName: String) : DisplayEnum {
+        PACKET("Packet"),
+        JUMP("Jump"),
+        MINI_JUMP("Mini Jump")
     }
 
-    private var delayTick = 0
-    private var sendingPacket = false
-    private var attackPacket = CPacketUseEntity()
-    private var swingPacket = CPacketAnimation()
+    private var delayTick = -1
+    private var target: Entity? = null
+    private var attacking = false
+
+    override fun isActive(): Boolean {
+        return isEnabled && !delaying()
+    }
+
+    override fun getHudInfo(): String {
+        return mode.displayName
+    }
 
     init {
         onDisable {
-            delayTick = 0
+            reset()
         }
 
-        safeListener<PacketEvent.Send> {
-            if (it.packet !is CPacketAnimation && it.packet !is CPacketUseEntity) return@safeListener
-
-            if (player.isInWater || player.isInLava || !player.onGround) return@safeListener /* Don't run if player is sprinting or weapon is still in cooldown */
-
-            if (it.packet is CPacketUseEntity && it.packet.action == CPacketUseEntity.Action.ATTACK) {
-                val target = it.packet.getEntityFromWorld(world)
-                if (target == null || target !is EntityLivingBase) return@safeListener
-                player.isSprinting = false
-                if (player.speed > 0.2) setSpeed(0.2)
-                if (mode.value == CriticalMode.PACKET) {
-                    packetMode()
-                } else {
-                    delayModeAttack(it)
-                }
-            } else if (it.packet is CPacketAnimation && mode.value == CriticalMode.DELAY) {
-                delayModeSwing(it)
+        listener<PacketEvent.Send> {
+            if (it.packet is CPacketAnimation && mode != Mode.PACKET && delayTick > -1) {
+                it.cancel()
             }
         }
 
-        safeListener<TickEvent.ClientTickEvent> {
-            /* Sends attack packet and swing packet when falling */
-            if (mode.value == CriticalMode.DELAY && delayTick != 0) {
-                player.isSprinting = false
-                if (player.speed > 0.2) setSpeed(0.2)
-                if (player.motionY < -0.1 && delayTick in 1..15) {
-                    sendingPacket = true
-                    connection.sendPacket(attackPacket)
-                    connection.sendPacket(swingPacket)
-                    delayTick = 16
+        safeListener<PlayerAttackEvent>(0) {
+            if (it.cancelled || attacking || !checkTarget(it.entity) || !canDoCriticals(true)) return@safeListener
+
+            val cooldownReady = player.onGround && player.getCooledAttackStrength(0.5f) > 0.9f
+
+            when (mode) {
+                Mode.PACKET -> {
+                    if (cooldownReady) {
+                        connection.sendPacket(CPacketPlayer.Position(player.posX, player.posY + 0.1, player.posZ, false))
+                        connection.sendPacket(CPacketPlayer.Position(player.posX, player.posY, player.posZ, false))
+                    }
                 }
-                if (delayTick in 1..19) {
-                    delayTick++
-                } else if (delayTick > 19) { /* Resets after 1 second if no packets sent */
-                    delayTick = 0
+                Mode.JUMP -> {
+                    jumpAndCancel(it, cooldownReady, null)
+                }
+                Mode.MINI_JUMP -> {
+                    jumpAndCancel(it, cooldownReady, jumpMotion)
                 }
             }
         }
-    }
 
-    private fun SafeClientEvent.packetMode() {
-        /* lol Minecraft checks for criticals if you're not on a block so just say you're not */
-        connection.sendPacket(CPacketPlayer.Position(player.posX, player.posY + 0.1f, player.posZ, false))
-        connection.sendPacket(CPacketPlayer.Position(player.posX, player.posY, player.posZ, false))
-    }
+        safeListener<TickEvent.ClientTickEvent> { event ->
+            if (event.phase != TickEvent.Phase.END || mode == Mode.PACKET || !delaying()) return@safeListener
 
-    private fun delayModeAttack(event: PacketEvent) {
-        /* Cancels attack packet and jump when you hit an entity */
-        val packet = event.packet as CPacketUseEntity
-        if (delayTick == 0) {
-            attackPacket = packet
-            event.cancel()
-            delayTick = 1
-            mc.player.jump()
-            if (miniJump.value) {
-                mc.player.motionY = 0.25
-            }
-        } else if (!sendingPacket) {
-            event.cancel()
-        }
-    }
+            delayTick--
 
-    private fun delayModeSwing(event: PacketEvent) {
-        /* Cancels swing packet after attack packet was cancelled */
-        if (delayTick != 0) {
-            if (delayTick == 1) {
-                swingPacket = event.packet as CPacketAnimation
-                event.cancel()
-            } else if (!sendingPacket) {
-                event.cancel()
-            } else {
-                sendingPacket = false
+            if (player.fallDistance >= attackFallDistance && canDoCriticals(!player.onGround)) {
+                val target = target
+                reset()
+
+                if (target != null) {
+                    attacking = true
+                    connection.sendPacket(CPacketUseEntity(target))
+                    connection.sendPacket(CPacketAnimation(EnumHand.MAIN_HAND))
+                    attacking = false
+                }
             }
         }
     }
+
+    private fun reset() {
+        delayTick = -1
+        target = null
+    }
+
+    private fun SafeClientEvent.jumpAndCancel(event: PlayerAttackEvent, cooldownReady: Boolean, motion: Double?) {
+        if (cooldownReady && !delaying()) {
+            player.jump()
+            if (motion != null) player.motionY = motion
+            target = event.entity
+
+            if (playerController.currentGameType != GameType.SPECTATOR) {
+                player.attackTargetEntityWithCurrentItem(event.entity)
+                player.resetCooldown()
+            }
+        }
+
+        delayTick = 10
+        event.cancel()
+    }
+
+    private fun delaying() =
+        mode != Mode.PACKET && delayTick > -1 && target != null
+
+    private fun checkTarget(entity: Entity) =
+        entity is EntityLivingBase
+            && (entity !is EntityPlayer || !entity.isFakeOrSelf)
+
+    private fun SafeClientEvent.canDoCriticals(onGround: Boolean) =
+        onGround
+            && !player.isInWeb
+            && !player.isOnLadder
+            && !player.isRiding
+            && !player.isPotionActive(MobEffects.BLINDNESS)
+            && !player.isInOrAboveLiquid
 }
