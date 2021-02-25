ed0e3466b6b4c34ffaed5f1df05eae4609f9b29c
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-24 23:37:10 -0500

    [enhancement] Added separate hand mode to ItemModel

--- src/main/java/org/kamiblue/client/mixin/client/render/MixinItemRenderer.java
+++ src/main/java/org/kamiblue/client/mixin/client/render/MixinItemRenderer.java
@@ -1,13 +1,11 @@
 package org.kamiblue.client.mixin.client.render;
 
 import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import org.kamiblue.client.module.modules.player.Freecam;
 import org.kamiblue.client.module.modules.render.ItemModel;
-import org.kamiblue.client.util.math.Vec3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,26 +21,12 @@ public class MixinItemRenderer {
     }
 
     @Inject(method = "renderItemInFirstPerson(Lnet/minecraft/client/entity/AbstractClientPlayer;FFLnet/minecraft/util/EnumHand;FLnet/minecraft/item/ItemStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;pushMatrix()V", shift = At.Shift.AFTER))
-    private void transformSideFirstPerson$pushMatrix(AbstractClientPlayer player, float partialTicks, float pitch, EnumHand hand, float swingProgress, ItemStack stack, float equippedProgress, CallbackInfo ci) {
-        if (ItemModel.INSTANCE.isEnabled()) {
-            Vec3f vec = ItemModel.getTranslation(stack, hand, player);
-            if (vec != null) {
-                GlStateManager.translate(vec.getX(), vec.getY(), vec.getZ());
-            }
-        }
+    private void transformSideFirstPersonInvokePushMatrix(AbstractClientPlayer player, float partialTicks, float pitch, EnumHand hand, float swingProgress, ItemStack stack, float equippedProgress, CallbackInfo ci) {
+        ItemModel.translate(stack, hand, player);
     }
 
     @Inject(method = "renderItemInFirstPerson(Lnet/minecraft/client/entity/AbstractClientPlayer;FFLnet/minecraft/util/EnumHand;FLnet/minecraft/item/ItemStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemRenderer;renderItemSide(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/renderer/block/model/ItemCameraTransforms$TransformType;Z)V"))
-    private void transformSideFirstPerson$renderItemSide(AbstractClientPlayer player, float partialTicks, float pitch, EnumHand hand, float swingProgress, ItemStack stack, float equippedProgress, CallbackInfo ci) {
-        if (ItemModel.INSTANCE.isEnabled()) {
-            Vec3f vec = ItemModel.getRotation(stack, hand, player);
-            if (vec != null) {
-                float scale = ItemModel.INSTANCE.getScale();
-                GlStateManager.rotate(vec.getX(), 1.0f, 0.0f, 0.0f);
-                GlStateManager.rotate(vec.getY(), 0.0f, 1.0f, 0.0f);
-                GlStateManager.rotate(vec.getZ(), 0.0f, 0.0f, 1.0f);
-                GlStateManager.scale(scale, scale, scale);
-            }
-        }
+    private void transformSideFirstPersonInvokeRenderItemSide(AbstractClientPlayer player, float partialTicks, float pitch, EnumHand hand, float swingProgress, ItemStack stack, float equippedProgress, CallbackInfo ci) {
+        ItemModel.rotateAndScale(stack, hand, player);
     }
 }
--- src/main/kotlin/org/kamiblue/client/module/modules/render/ItemModel.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/render/ItemModel.kt
@@ -1,45 +1,99 @@
 package org.kamiblue.client.module.modules.render
 
 import net.minecraft.client.entity.AbstractClientPlayer
+import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumHand
 import net.minecraft.util.EnumHandSide
 import org.kamiblue.client.module.Category
 import org.kamiblue.client.module.Module
-import org.kamiblue.client.util.math.Vec3f
 
 internal object ItemModel : Module(
     name = "ItemModel",
-    alias = arrayOf("SmallShield"),
+    alias = arrayOf("ViewModel", "SmallShield", "LowerOffhand"),
     description = "Modify hand item rendering in first person",
     category = Category.RENDER
 ) {
-    private val posX by setting("Pos X", 0.0f, -5.0f..5.0f, 0.025f)
-    private val posY by setting("Pos Y", 0.0f, -5.0f..5.0f, 0.025f)
-    private val posZ by setting("Pos Z", 0.0f, -5.0f..5.0f, 0.025f)
-    private val rotateX by setting("Rotate X", 0.0f, -180.0f..180.0f, 1.0f)
-    private val rotateY by setting("Rotate Y", 0.0f, -180.0f..180.0f, 1.0f)
-    private val rotateZ by setting("Rotate Z", 0.0f, -180.0f..180.0f, 1.0f)
-    val scale by setting("Scale", 1.0f, 0.1f..3.0f, 0.025f)
+    private val mode by setting("Mode", Mode.BOTH)
+    private val page by setting("Page", Page.POSITION)
+
+    private val posX by setting("Pos X", 0.0f, -5.0f..5.0f, 0.025f, { page == Page.POSITION })
+    private val posY by setting("Pos Y", 0.0f, -5.0f..5.0f, 0.025f, { page == Page.POSITION })
+    private val posZ by setting("Pos Z", 0.0f, -5.0f..5.0f, 0.025f, { page == Page.POSITION })
+    private val posXR by setting("Pos X Right", 0.0f, -5.0f..5.0f, 0.025f, { page == Page.POSITION && mode == Mode.SEPARATE })
+    private val posYR by setting("Pos Y Right", 0.0f, -5.0f..5.0f, 0.025f, { page == Page.POSITION && mode == Mode.SEPARATE })
+    private val posZR by setting("Pos Z Right", 0.0f, -5.0f..5.0f, 0.025f, { page == Page.POSITION && mode == Mode.SEPARATE })
+
+    private val rotateX by setting("Rotate X", 0.0f, -180.0f..180.0f, 1.0f, { page == Page.ROTATION })
+    private val rotateY by setting("Rotate Y", 0.0f, -180.0f..180.0f, 1.0f, { page == Page.ROTATION })
+    private val rotateZ by setting("Rotate Z", 0.0f, -180.0f..180.0f, 1.0f, { page == Page.ROTATION })
+    private val rotateXR by setting("Rotate X Right", 0.0f, -180.0f..180.0f, 1.0f, { page == Page.ROTATION && mode == Mode.SEPARATE })
+    private val rotateYR by setting("Rotate Y Right", 0.0f, -180.0f..180.0f, 1.0f, { page == Page.ROTATION && mode == Mode.SEPARATE })
+    private val rotateZR by setting("Rotate Z Right", 0.0f, -180.0f..180.0f, 1.0f, { page == Page.ROTATION && mode == Mode.SEPARATE })
+
+    private val scale by setting("Scale", 1.0f, 0.1f..3.0f, 0.025f, { page == Page.SCALE })
+    private val scaleR by setting("Scale Right", 1.0f, 0.1f..3.0f, 0.025f, { page == Page.SCALE && mode == Mode.SEPARATE })
+
     private val modifyHand by setting("Modify Hand", false)
 
-    @JvmStatic
-    fun getTranslation(stack: ItemStack, hand: EnumHand, player: AbstractClientPlayer): Vec3f? {
-        if (!modifyHand && stack.isEmpty) return null
+    private enum class Mode {
+        BOTH, SEPARATE
+    }
 
-        val enumHandSide: EnumHandSide = if (hand == EnumHand.MAIN_HAND) player.primaryHand else player.primaryHand.opposite()
-        val sideMultiplier = if (enumHandSide == EnumHandSide.RIGHT) 1.0f else -1.0f
-
-        return Vec3f(posX * sideMultiplier, posY, posZ)
+    private enum class Page {
+        POSITION, ROTATION, SCALE
     }
 
     @JvmStatic
-    fun getRotation(stack: ItemStack, hand: EnumHand, player: AbstractClientPlayer): Vec3f? {
-        if (!modifyHand && stack.isEmpty) return null
+    fun translate(stack: ItemStack, hand: EnumHand, player: AbstractClientPlayer) {
+        if (isDisabled || !modifyHand && stack.isEmpty) return
 
-        val enumHandSide: EnumHandSide = if (hand == EnumHand.MAIN_HAND) player.primaryHand else player.primaryHand.opposite()
-        val sideMultiplier = if (enumHandSide == EnumHandSide.RIGHT) 1.0f else -1.0f
+        val enumHandSide = getEnumHandSide(player, hand)
 
-        return Vec3f(rotateX, rotateY * sideMultiplier, rotateZ * sideMultiplier)
+        if (mode == Mode.BOTH) {
+            translate(posX, posY, posZ, getSideMultiplier(enumHandSide))
+        } else {
+            if (enumHandSide == EnumHandSide.LEFT) {
+                translate(posX, posY, posZ, -1.0f)
+            } else {
+                translate(posXR, posYR, posZR, 1.0f)
+            }
+        }
     }
+
+    private fun translate(x: Float, y: Float, z: Float, sideMultiplier: Float) {
+        GlStateManager.translate(x * sideMultiplier, y, -z)
+    }
+
+    @JvmStatic
+    fun rotateAndScale(stack: ItemStack, hand: EnumHand, player: AbstractClientPlayer) {
+        if (isDisabled || !modifyHand && stack.isEmpty) return
+
+        val enumHandSide = getEnumHandSide(player, hand)
+
+        if (mode == Mode.BOTH) {
+            rotate(rotateX, rotateY, rotateZ, getSideMultiplier(enumHandSide))
+            GlStateManager.scale(scale, scale, scale)
+        } else {
+            if (enumHandSide == EnumHandSide.LEFT) {
+                rotate(rotateX, rotateY, rotateZ, -1.0f)
+                GlStateManager.scale(scale, scale, scale)
+            } else {
+                rotate(rotateXR, rotateYR, rotateZR, 1.0f)
+                GlStateManager.scale(scaleR, scaleR, scaleR)
+            }
+        }
+    }
+
+    private fun rotate(x: Float, y: Float, z: Float, sideMultiplier: Float) {
+        GlStateManager.rotate(x, 1.0f, 0.0f, 0.0f)
+        GlStateManager.rotate(y * sideMultiplier, 0.0f, 1.0f, 0.0f)
+        GlStateManager.rotate(z * sideMultiplier, 0.0f, 0.0f, 1.0f)
+    }
+
+    private fun getEnumHandSide(player: AbstractClientPlayer, hand: EnumHand): EnumHandSide =
+        if (hand == EnumHand.MAIN_HAND) player.primaryHand else player.primaryHand.opposite()
+
+    private fun getSideMultiplier(side: EnumHandSide) =
+        if (side == EnumHandSide.LEFT) -1.0f else 1.0f
 }
