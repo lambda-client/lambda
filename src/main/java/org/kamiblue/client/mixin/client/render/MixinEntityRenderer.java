57759bd66b6b9f7772b15e98045d287233dda77b
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-25 01:09:46 -0500

    [cleanup] Rewrote AntiFog
    why tf was this so over complicated?

--- src/main/java/org/kamiblue/client/mixin/client/render/MixinEntityRenderer.java
+++ src/main/java/org/kamiblue/client/mixin/client/render/MixinEntityRenderer.java
@@ -1,29 +1,22 @@
 package org.kamiblue.client.mixin.client.render;
 
 import com.google.common.base.Predicate;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.multiplayer.WorldClient;
-import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 import org.kamiblue.client.event.KamiEventBus;
 import org.kamiblue.client.event.events.RenderOverlayEvent;
 import org.kamiblue.client.module.modules.movement.ElytraFlight;
-import org.kamiblue.client.module.modules.player.Freecam;
 import org.kamiblue.client.module.modules.player.NoEntityTrace;
-import org.kamiblue.client.module.modules.player.ViewLock;
 import org.kamiblue.client.module.modules.render.AntiFog;
 import org.kamiblue.client.module.modules.render.AntiOverlay;
 import org.kamiblue.client.module.modules.render.CameraClip;
 import org.kamiblue.client.module.modules.render.NoHurtCam;
-import org.kamiblue.client.util.math.Vec2f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -54,19 +47,10 @@ public class MixinEntityRenderer {
         }
     }
 
-    @Inject(method = "setupFog", at = @At(value = "HEAD"), cancellable = true)
+    @Inject(method = "setupFog", at = @At(value = "RETURN"), cancellable = true)
     public void setupFog(int startCoords, float partialTicks, CallbackInfo callbackInfo) {
-        if (AntiFog.INSTANCE.getShouldNoFog()) {
-            callbackInfo.cancel();
-        }
-    }
-
-    @Redirect(method = "setupFog", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ActiveRenderInfo;getBlockStateAtEntityViewpoint(Lnet/minecraft/world/World;Lnet/minecraft/entity/Entity;F)Lnet/minecraft/block/state/IBlockState;"))
-    public IBlockState getBlockStateAtEntityViewpoint(World worldIn, Entity entityIn, float p_186703_2_) {
-        if (AntiFog.INSTANCE.getShouldAir()) {
-            return Blocks.AIR.getDefaultState();
-        } else {
-            return ActiveRenderInfo.getBlockStateAtEntityViewpoint(worldIn, entityIn, p_186703_2_);
+        if (AntiFog.INSTANCE.isEnabled()) {
+            GlStateManager.disableFog();
         }
     }
 
