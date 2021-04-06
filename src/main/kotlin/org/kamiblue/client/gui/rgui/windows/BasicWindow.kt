daa82f3c8f8fd2e17c3c4f75a953ba53e87c488a
lv <~@l1v.in>
2021-04-06 17:37:40 -0400

    enh: Allow BasicWindow radius to be customizable
    Signed-off-by: lv <~@l1v.in>

--- src/main/kotlin/org/kamiblue/client/gui/rgui/windows/BasicWindow.kt
+++ src/main/kotlin/org/kamiblue/client/gui/rgui/windows/BasicWindow.kt
@@ -1,5 +1,6 @@
 package org.kamiblue.client.gui.rgui.windows
 
+import org.kamiblue.client.module.modules.client.ClickGUI
 import org.kamiblue.client.module.modules.client.GuiColors
 import org.kamiblue.client.setting.GuiConfig
 import org.kamiblue.client.setting.configs.AbstractConfig
@@ -24,8 +25,8 @@ open class BasicWindow(
 
     override fun onRender(vertexHelper: VertexHelper, absolutePos: Vec2f) {
         super.onRender(vertexHelper, absolutePos)
-        RenderUtils2D.drawRoundedRectFilled(vertexHelper, Vec2d(0.0, 0.0), Vec2f(renderWidth, renderHeight).toVec2d(), 4.0, color = GuiColors.backGround)
-        RenderUtils2D.drawRoundedRectOutline(vertexHelper, Vec2d(0.0, 0.0), Vec2f(renderWidth, renderHeight).toVec2d(), 4.0, lineWidth = 2.5f, color = GuiColors.primary)
+        RenderUtils2D.drawRoundedRectFilled(vertexHelper, Vec2d(0.0, 0.0), Vec2f(renderWidth, renderHeight).toVec2d(), ClickGUI.radius, color = GuiColors.backGround)
+        RenderUtils2D.drawRoundedRectOutline(vertexHelper, Vec2d(0.0, 0.0), Vec2f(renderWidth, renderHeight).toVec2d(), ClickGUI.radius, lineWidth = 2.5f, color = GuiColors.primary)
     }
 
 }
