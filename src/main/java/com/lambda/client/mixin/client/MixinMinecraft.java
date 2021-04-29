c59e6deed1c65d148496410dbeb0513c3f940923
Aven <jacobherd229543@gmail.com>
2021-04-29 10:51:24 -0400

    Fix import again
    I can't test shit because my schools network filter blocks Forge maven so I can't test builds properly without CI

--- src/main/java/com/lambda/client/mixin/client/MixinMinecraft.java
+++ src/main/java/com/lambda/client/mixin/client/MixinMinecraft.java
@@ -29,6 +29,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
