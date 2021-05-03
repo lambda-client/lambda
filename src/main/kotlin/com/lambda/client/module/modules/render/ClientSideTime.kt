f0917c416aa43689b17d9c1bf7002c0155c4d600
Aven <jacobherd229543@gmail.com>
2021-05-02 21:05:06 -0400

    Add ClientSideTime
    IDK why I never thought of cancelling the time packet, I have brain damage

---
+++ src/main/kotlin/com/lambda/client/module/modules/render/ClientSideTime.kt
@@ -0,0 +1,29 @@
+package com.lambda.client.module.modules.render
+
+import com.lambda.client.event.events.PacketEvent
+import com.lambda.client.module.Category
+import com.lambda.client.module.Module
+import com.lambda.client.util.threads.safeListener
+import com.lambda.event.listener.listener
+import net.minecraft.network.play.server.SPacketTimeUpdate
+import net.minecraftforge.fml.common.gameevent.TickEvent
+
+internal object ClientSideTime : Module(
+    name = "ClientSideTime",
+    category = Category.RENDER,
+    description = "Change time of day client side",
+    showOnArray = false
+) {
+    private val time by setting("Time", 6000, 0..24000, 20)
+
+    init {
+
+        listener<PacketEvent.Receive> {
+            if (it.packet is SPacketTimeUpdate) it.cancel()
+        }
+
+        safeListener<TickEvent.ClientTickEvent> {
+            mc.world.worldTime = time.toLong()
+        }
+    }
+}
