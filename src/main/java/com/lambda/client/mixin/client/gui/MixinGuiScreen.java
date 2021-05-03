60d2b6da5980643dec3b6f2edb2d89f345d20275
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-05-03 14:25:53 -0400

    Cleaned up ContainerPreview

--- src/main/java/com/lambda/client/mixin/client/gui/MixinGuiScreen.java
+++ src/main/java/com/lambda/client/mixin/client/gui/MixinGuiScreen.java
@@ -1,20 +1,12 @@
 package com.lambda.client.mixin.client.gui;
 
 import com.lambda.client.module.modules.render.CleanGUI;
-import com.lambda.client.module.modules.render.MapPreview;
 import com.lambda.client.module.modules.render.ContainerPreview;
+import com.lambda.client.module.modules.render.MapPreview;
 import com.lambda.client.util.Wrapper;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockEnderChest;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.ItemMap;
-import net.minecraft.item.ItemShulkerBox;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.NonNullList;
 import net.minecraft.world.storage.MapData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,38 +17,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinGuiScreen {
 
     @Inject(method = "renderToolTip", at = @At("HEAD"), cancellable = true)
-    public void renderToolTip(ItemStack stack, int x, int y, CallbackInfo info) {
-        if (ContainerPreview.INSTANCE.isEnabled() && (stack.getItem() instanceof ItemShulkerBox || Block.getBlockFromItem(stack.getItem()) instanceof BlockEnderChest)) {
-            NonNullList<ItemStack> items = NonNullList.withSize(27, ItemStack.EMPTY);
-
-            if (stack.getItem() instanceof ItemShulkerBox) {
-                NBTTagCompound tagCompound = ContainerPreview.INSTANCE.getShulkerData(stack);
-                if (tagCompound != null) {
-                    ItemStackHelper.loadAllItems(tagCompound, items);
-                } else {
-                    items = null;
-                }
-            } else {
-                // Item is an Ender Chest
-                IInventory inventory = ContainerPreview.INSTANCE.getEnderChestItems();
-                if (inventory != null) {
-                    for (int i = 0; i < items.size(); i++) {
-                        items.set(i, inventory.getStackInSlot(i));
-                    }
-                }
-            }
-
-            if (items != null) {
-                info.cancel();
-                ContainerPreview.INSTANCE.renderContainerAndItems(stack, x, y, items);
-            }
-        } else if (MapPreview.INSTANCE.isEnabled() && stack.getItem() instanceof ItemMap) {
+    public void renderToolTip(ItemStack stack, int x, int y, CallbackInfo ci) {
+        if (MapPreview.INSTANCE.isEnabled() && stack.getItem() instanceof ItemMap) {
             MapData mapData = MapPreview.getMapData(stack);
 
             if (mapData != null) {
-                info.cancel();
+                ci.cancel();
                 MapPreview.drawMap(stack, mapData, x, y);
             }
+        } else if (ContainerPreview.INSTANCE.isEnabled()) {
+            ContainerPreview.INSTANCE.renderTooltips(stack, x, y, ci);
         }
     }
 
