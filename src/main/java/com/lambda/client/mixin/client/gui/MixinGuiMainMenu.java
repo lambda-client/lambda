e6ac571ea8cfd0bc0996eb8bbe055d10757981ac
Christian <fractalminds@protonmail.com>
2021-05-04 23:27:48 +0200

    MainMenu Option

--- src/main/java/com/lambda/client/mixin/client/gui/MixinGuiMainMenu.java
+++ src/main/java/com/lambda/client/mixin/client/gui/MixinGuiMainMenu.java
@@ -1,36 +1,46 @@
 package com.lambda.client.mixin.client.gui;
 
+import com.lambda.client.LambdaMod;
 import com.lambda.client.gui.mc.LambdaGuiMenuButton;
+import com.lambda.client.gui.mc.LambdaGuiPluginError;
+import com.lambda.client.gui.mc.LambdaGuiPluginManager;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.util.text.TextFormatting;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
 
+import java.io.IOException;
+
 @Mixin(GuiMainMenu.class)
 public class MixinGuiMainMenu extends GuiScreen {
 
-    @Inject(method = "initGui", at = @At("HEAD"))
+    @Shadow private GuiButton realmsButton;
+
+    @Inject(method = "initGui", at = @At("TAIL"), cancellable = true)
     public void initGui(CallbackInfo ci) {
-            buttonList.add(new LambdaGuiMenuButton(width / 2 - 124, height / 4 + 48 + 72 + 12 - 36));
+        buttonList.removeIf(button -> button.id == 14);
+        realmsButton = addButton(new GuiButton(9001, width / 2 + 2, height / 4 + 48 + 24 * 2, 98, 20, "Lambda"));
     }
 
-//    @Inject(method = "drawScreen", at = @At("TAIL"), cancellable = true)
-//    public void drawText(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
-//        FontRenderer fr = mc.fontRenderer;
-//        fr.drawStringWithShadow(LambdaMod.NAME + TextFormatting.WHITE + " Version " + TextFormatting.GRAY + LambdaMod.VERSION, 2, 2, 0xffffff);
-//        fr.drawStringWithShadow(TextFormatting.BLUE + LambdaMod.WEBSITE_LINK,  2, 12, 0xffffff);
-//    }
+    @Inject(method = "drawScreen", at = @At("TAIL"), cancellable = true)
+    public void drawText(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
+        FontRenderer fr = mc.fontRenderer;
+        fr.drawStringWithShadow(TextFormatting.WHITE + LambdaMod.NAME + " " + TextFormatting.GRAY + LambdaMod.VERSION, 2, 2, 0x808080);
+        fr.drawString(TextFormatting.GRAY + LambdaMod.WEBSITE_LINK,  2, 12, 0xffffff);
+    }
 
-    /*
-    @Override
-    protected void actionPerformed(GuiButton button) throws IOException {
-        if (button.id == 3252) {
-            mc.displayGuiScreen(new LambdaAltMenu());
+    @Inject(method = "actionPerformed", at = @At("TAIL"), cancellable = true)
+    protected void actionPerformed(GuiButton button, CallbackInfo ci) throws IOException {
+        if (button.id == 9001) {
+            mc.displayGuiScreen(new LambdaGuiPluginManager(this));
         } else {
             super.actionPerformed(button);
         }
     }
-     */
 }
--- src/main/kotlin/com/lambda/client/gui/mc/LambdaGuiPluginManager.kt
+++ src/main/kotlin/com/lambda/client/gui/mc/LambdaGuiPluginManager.kt
@@ -1,4 +1,40 @@
 package com.lambda.client.gui.mc
 
-class LambdaGuiPluginManager {
+import com.lambda.client.plugin.PluginManager
+import net.minecraft.client.gui.GuiButton
+import net.minecraft.client.gui.GuiScreen
+import net.minecraft.client.renderer.GlStateManager
+import java.util.*
+
+class LambdaGuiPluginManager(private val previousScreen: GuiScreen): GuiScreen() {
+
+    override fun initGui() {
+        super.initGui()
+        buttonList.add(GuiButton(0, width / 2 - 100, height - 50, "Back"))
+    }
+
+    override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
+        drawBackground(0)
+//        drawCenteredString(fontRenderer, "Plugin Manager", width / 2, 80, 0x9B90FF)
+        val pluginList = TreeSet<String>()
+        PluginManager.loadedPlugins.forEach {
+            pluginList.add(it.config.name)
+        }
+        drawList("Loaded Plugins:", pluginList)
+        super.drawScreen(mouseX, mouseY, partialTicks)
+    }
+
+    override fun actionPerformed(button: GuiButton) {
+        when (button.id) {
+            0 -> mc.displayGuiScreen(previousScreen)
+        }
+    }
+
+    private fun drawList(title: String, list: Set<String>) {
+        drawCenteredString(fontRenderer, title, width / 2, 50, 0xFFFFFF)
+
+        list.forEachIndexed { index, s ->
+            drawCenteredString(fontRenderer, s, width / 2, 50 + (fontRenderer.FONT_HEIGHT + 2) * (index + 1), 0x808080)
+        }
+    }
 }
