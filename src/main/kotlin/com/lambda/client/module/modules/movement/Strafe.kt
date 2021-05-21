d9f482b6dfb9f13cdfffd7aadcb1dd48409be206
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-05-21 16:24:50 -0400

    Fixed slow Strafe
    Now it is faster than manually sprinting and jumping

--- src/main/kotlin/com/lambda/client/module/modules/movement/Strafe.kt
+++ src/main/kotlin/com/lambda/client/module/modules/movement/Strafe.kt
@@ -1,6 +1,7 @@
 package com.lambda.client.module.modules.movement
 
 import com.lambda.client.event.SafeClientEvent
+import com.lambda.client.event.events.PlayerMoveEvent
 import com.lambda.client.event.events.PlayerTravelEvent
 import com.lambda.client.manager.managers.TimerManager.modifyTimer
 import com.lambda.client.manager.managers.TimerManager.resetTimer
@@ -19,6 +20,7 @@ import com.lambda.client.util.TimeUnit
 import com.lambda.client.util.threads.safeListener
 import net.minecraft.client.settings.KeyBinding
 import kotlin.math.cos
+import kotlin.math.max
 import kotlin.math.sin
 
 internal object Strafe : Module(
@@ -27,31 +29,11 @@ internal object Strafe : Module(
     description = "Improves control in air",
     modulePriority = 100
 ) {
-    private val mode by setting("Mode", SpeedBoost.NCP)
-    private val page by setting("Page", Page.GENERIC_SETTINGS)
-
-    /* Generic Settings */
-    private val airSpeedBoost by setting("Air Speed Boost", true, { page == Page.GENERIC_SETTINGS })
-    private val groundSpeedBoost by setting("Ground Speed Boost", true, { page == Page.GENERIC_SETTINGS })
-    private val timerBoost by setting("Timer Boost", true, { page == Page.GENERIC_SETTINGS })
-    private val autoJump by setting("Auto Jump", true, { page == Page.GENERIC_SETTINGS })
-    private val onHoldingSprint by setting("On Holding Sprint", false, { page == Page.GENERIC_SETTINGS })
-    private val cancelInertia by setting("Cancel Inertia", false, { page == Page.GENERIC_SETTINGS })
-
-    /* NCP Mode */
-    private val ncpStrict by setting("NCP Strict", false, { mode == SpeedBoost.NCP && page == Page.MODE_SETTINGS })
-
-    /* Custom Mode */
-    private val settingSpeed by setting("Speed", 0.28, 0.0..1.0, 0.01, { mode == SpeedBoost.CUSTOM && page == Page.MODE_SETTINGS })
-    private val constantSpeed by setting("Constant Speed", false, { mode == SpeedBoost.CUSTOM && page == Page.MODE_SETTINGS })
-
-    private enum class SpeedBoost {
-        NCP, CUSTOM
-    }
-
-    private enum class Page {
-        GENERIC_SETTINGS, MODE_SETTINGS
-    }
+    private val airSpeedBoost by setting("Air Speed Boost", 0.029f, 0.01f..0.04f, 0.001f)
+    private val timerBoost by setting("Timer Boost", true)
+    private val autoJump by setting("Auto Jump", true)
+    private val onHoldingSprint by setting("On Holding Sprint", false)
+    private val cancelInertia by setting("Cancel Inertia", false)
 
     private var jumpTicks = 0
     private val strafeTimer = TickTimer(TimeUnit.TICKS)
@@ -62,24 +44,27 @@ internal object Strafe : Module(
         }
 
         safeListener<PlayerTravelEvent> {
-            if (!shouldStrafe) {
+            if (!shouldStrafe()) return@safeListener
+
+            player.jumpMovementFactor = airSpeedBoost
+            if (timerBoost) modifyTimer(45.87155914306640625f)
+            if (Step.isDisabled || !player.collidedHorizontally) {
+                if (autoJump) jump()
+            }
+
+            strafeTimer.reset()
+        }
+
+        safeListener<PlayerMoveEvent> {
+            if (!shouldStrafe()) {
                 reset()
                 if (cancelInertia && !strafeTimer.tick(2L, false)) {
                     player.motionX = 0.0
                     player.motionZ = 0.0
                 }
-                return@safeListener
+            } else {
+                setSpeed(max(player.speed, applySpeedPotionEffects(0.2873)))
             }
-
-
-            if (airSpeedBoost) player.jumpMovementFactor = 0.029f
-            if (timerBoost) modifyTimer(45.87155914306640625f)
-            if (!player.collidedHorizontally) {
-                if (autoJump) jump()
-                setSpeed(getSpeed())
-            }
-
-            strafeTimer.reset()
         }
     }
 
@@ -89,14 +74,15 @@ internal object Strafe : Module(
         jumpTicks = 0
     }
 
-    private val SafeClientEvent.shouldStrafe: Boolean
-        get() = !player.capabilities.isFlying
+    private fun SafeClientEvent.shouldStrafe(): Boolean {
+        return (!player.capabilities.isFlying
             && !player.isElytraFlying
             && !mc.gameSettings.keyBindSneak.isKeyDown
             && (!onHoldingSprint || mc.gameSettings.keyBindSprint.isKeyDown)
             && !BaritoneUtils.isPathing
             && MovementUtils.isInputting
-            && !(player.isInOrAboveLiquid || player.isInWeb)
+            && !(player.isInOrAboveLiquid || player.isInWeb))
+    }
 
     private fun SafeClientEvent.jump() {
         if (player.onGround && jumpTicks <= 0) {
@@ -107,38 +93,11 @@ internal object Strafe : Module(
             }
 
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindJump.keyCode, false)
-            player.motionY = 0.41
+            player.motionY = 0.4
             player.isAirBorne = true
             jumpTicks = 5
         }
 
         jumpTicks--
     }
-
-    private fun SafeClientEvent.getSpeed() = when (mode) {
-        SpeedBoost.NCP -> {
-            if (shouldBoostGroundSpeed) {
-                val speed = if (ncpStrict) 0.26 else 0.28
-                applySpeedPotionEffects(speed)
-            } else {
-                player.speed
-            }
-        }
-        SpeedBoost.CUSTOM -> {
-            when {
-                constantSpeed -> {
-                    settingSpeed
-                }
-                shouldBoostGroundSpeed -> {
-                    applySpeedPotionEffects(settingSpeed)
-                }
-                else -> {
-                    player.speed
-                }
-            }
-        }
-    }
-
-    private val SafeClientEvent.shouldBoostGroundSpeed
-        get() = groundSpeedBoost && player.onGround
 }
