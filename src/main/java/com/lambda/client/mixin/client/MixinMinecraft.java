e77233055117660e6ef5ff0130de8d4176080013
Christian <fractalminds@protonmail.com>
2021-06-02 03:17:17 +0200

    Change profiler name

--- src/main/java/com/lambda/client/mixin/client/MixinMinecraft.java
+++ src/main/java/com/lambda/client/mixin/client/MixinMinecraft.java
@@ -57,28 +57,28 @@ public abstract class MixinMinecraft {
 
     @Inject(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Timer;updateTimer()V", shift = At.Shift.BEFORE))
     public void runGameLoopStart(CallbackInfo ci) {
-        Wrapper.getMinecraft().profiler.startSection("kbRunGameLoop");
+        Wrapper.getMinecraft().profiler.startSection("lambda");
         LambdaEventBus.INSTANCE.post(new RunGameLoopEvent.Start());
         Wrapper.getMinecraft().profiler.endSection();
     }
 
     @Inject(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/profiler/Profiler;endSection()V", ordinal = 0, shift = At.Shift.BEFORE))
     public void runGameLoopTick(CallbackInfo ci) {
-        Wrapper.getMinecraft().profiler.endStartSection("kbRunGameLoop");
+        Wrapper.getMinecraft().profiler.endStartSection("lambda");
         LambdaEventBus.INSTANCE.post(new RunGameLoopEvent.Tick());
         Wrapper.getMinecraft().profiler.endStartSection("scheduledExecutables");
     }
 
     @Inject(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/profiler/Profiler;startSection(Ljava/lang/String;)V", ordinal = 2, shift = At.Shift.BEFORE))
     public void runGameLoopRender(CallbackInfo ci) {
-        Wrapper.getMinecraft().profiler.startSection("kbRunGameLoop");
+        Wrapper.getMinecraft().profiler.startSection("lambda");
         LambdaEventBus.INSTANCE.post(new RunGameLoopEvent.Render());
         Wrapper.getMinecraft().profiler.endSection();
     }
 
     @Inject(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;isFramerateLimitBelowMax()Z", shift = At.Shift.BEFORE))
     public void runGameLoopEnd(CallbackInfo ci) {
-        Wrapper.getMinecraft().profiler.startSection("kbRunGameLoop");
+        Wrapper.getMinecraft().profiler.startSection("lambda");
         LambdaEventBus.INSTANCE.post(new RunGameLoopEvent.End());
         Wrapper.getMinecraft().profiler.endSection();
     }
