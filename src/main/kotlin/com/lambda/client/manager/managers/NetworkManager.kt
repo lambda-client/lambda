5028a69f9c616bab1523f0b3d8b1aa042aa7280a
Christian <fractalminds@protonmail.com>
2021-06-22 18:43:37 +0200

    Fix online check thread blocking

--- src/main/kotlin/com/lambda/client/manager/managers/NetworkManager.kt
+++ src/main/kotlin/com/lambda/client/manager/managers/NetworkManager.kt
@@ -2,7 +2,11 @@ package com.lambda.client.manager.managers
 
 import com.lambda.client.manager.Manager
 import com.lambda.client.util.TickTimer
+import com.lambda.client.util.threads.defaultScope
 import com.lambda.event.listener.listener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import java.net.InetSocketAddress
 import java.net.Socket
@@ -16,13 +20,15 @@ object NetworkManager : Manager {
     init {
         listener<TickEvent.ClientTickEvent> {
             if (lastUpdateTimer.tick(1500L)) {
-                isOffline = try {
-                    Socket().use { socket ->
-                        socket.connect(InetSocketAddress("1.1.1.1", 80), 100)
-                        false
+                defaultScope.launch(Dispatchers.IO) {
+                    isOffline = try {
+                        Socket().use { socket ->
+                            socket.connect(InetSocketAddress("1.1.1.1", 80), 100)
+                            false
+                        }
+                    } catch (e: Exception) {
+                        true // Either timeout or unreachable or failed DNS lookup.
                     }
-                } catch (e: Exception) {
-                    true // Either timeout or unreachable or failed DNS lookup.
                 }
             }
         }
