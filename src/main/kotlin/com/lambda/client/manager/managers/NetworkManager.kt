438d28b10c04c3d4689fc356456519242ae54fa2
Christian <fractalminds@protonmail.com>
2021-06-23 21:28:36 +0200

    Fix BlockingMethodInNonBlockingContext false positive

--- src/main/kotlin/com/lambda/client/manager/managers/NetworkManager.kt
+++ src/main/kotlin/com/lambda/client/manager/managers/NetworkManager.kt
@@ -2,9 +2,6 @@ package com.lambda.client.manager.managers
 
 import com.lambda.client.manager.Manager
 import com.lambda.client.util.threads.BackgroundScope
-import com.lambda.client.util.threads.defaultScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.net.InetSocketAddress
 import java.net.Socket
 
@@ -14,15 +11,14 @@ object NetworkManager : Manager {
 
     init {
         BackgroundScope.launchLooping("offline", 1500L) {
-            defaultScope.launch(Dispatchers.IO) {
-                isOffline = try {
-                    Socket().use { socket ->
-                        socket.connect(InetSocketAddress("1.1.1.1", 80), 100)
-                        false
-                    }
-                } catch (e: Exception) {
-                    true // Either timeout or unreachable or failed DNS lookup.
+            @Suppress("BlockingMethodInNonBlockingContext")
+            isOffline = try {
+                Socket().use { socket ->
+                    socket.connect(InetSocketAddress("1.1.1.1", 80), 100)
+                    false
                 }
+            } catch (e: Exception) {
+                true // Either timeout or unreachable or failed DNS lookup.
             }
         }
     }
