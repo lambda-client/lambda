9605c45334b4293f68a189d8f01ed3daa1949c65
Constructor <fractalminds@protonmail.com>
2021-09-06 05:14:52 +0200

    Bump Lambda to 2.09 and Kotlin to 1.5.30 and fix plugin loading

--- src/main/kotlin/com/lambda/client/LambdaMod.kt
+++ src/main/kotlin/com/lambda/client/LambdaMod.kt
@@ -25,9 +25,9 @@ class LambdaMod {
         const val ID = "lambda"
         const val DIRECTORY = "lambda/"
 
-        const val VERSION = "2.07.xx-dev" // Used for debugging. R.MM.DD-hash format.
-        const val VERSION_SIMPLE = "2.07.xx-dev" // Shown to the user. R.MM.DD[-beta] format.
-        const val VERSION_MAJOR = "2.07.01" // Used for update checking. RR.MM.01 format.
+        const val VERSION = "2.09.xx-dev" // Used for debugging. R.MM.DD-hash format.
+        const val VERSION_SIMPLE = "2.09.xx-dev" // Shown to the user. R.MM.DD[-beta] format.
+        const val VERSION_MAJOR = "2.09.01" // Used for update checking. RR.MM.01 format.
 
         const val APP_ID = "835368493150502923"
 
--- src/main/kotlin/com/lambda/client/setting/configs/PluginConfig.kt
+++ src/main/kotlin/com/lambda/client/setting/configs/PluginConfig.kt
@@ -5,6 +5,16 @@ import com.lambda.client.plugin.api.IPluginClass
 import com.lambda.client.plugin.api.PluginHudElement
 import com.lambda.client.plugin.api.PluginModule
 import com.lambda.client.setting.settings.AbstractSetting
+import com.lambda.client.setting.settings.impl.number.DoubleSetting
+import com.lambda.client.setting.settings.impl.number.FloatSetting
+import com.lambda.client.setting.settings.impl.number.IntegerSetting
+import com.lambda.client.setting.settings.impl.other.BindSetting
+import com.lambda.client.setting.settings.impl.other.ColorSetting
+import com.lambda.client.setting.settings.impl.primitive.BooleanSetting
+import com.lambda.client.setting.settings.impl.primitive.EnumSetting
+import com.lambda.client.setting.settings.impl.primitive.StringSetting
+import com.lambda.client.util.Bind
+import com.lambda.client.util.color.ColorHolder
 import com.lambda.commons.interfaces.Nameable
 import java.io.File
 
@@ -40,5 +50,37 @@ class PluginConfig(pluginName: String) : NameableConfig<IPluginClass>(
                 getGroup("misc")?.getGroup(nameable.name)?.getSettings()
             }
         } ?: emptyList()
+    }
+
+    override fun <E : Enum<E>> IPluginClass.setting(name: String, value: E, visibility: () -> Boolean, consumer: (prev: E, input: E) -> E, description: String): EnumSetting<E> {
+        return setting(EnumSetting(name, value, visibility, consumer, description))
+    }
+
+    override fun IPluginClass.setting(name: String, value: Boolean, visibility: () -> Boolean, consumer: (prev: Boolean, input: Boolean) -> Boolean, description: String): BooleanSetting {
+        return setting(BooleanSetting(name, value, visibility, consumer, description))
+    }
+
+    override fun IPluginClass.setting(name: String, value: ColorHolder, hasAlpha: Boolean, visibility: () -> Boolean, description: String): ColorSetting {
+        return setting(ColorSetting(name, value, hasAlpha, visibility, description))
+    }
+
+    override fun IPluginClass.setting(name: String, value: String, visibility: () -> Boolean, consumer: (prev: String, input: String) -> String, description: String): StringSetting {
+        return setting(StringSetting(name, value, visibility, consumer, description))
+    }
+
+    override fun IPluginClass.setting(name: String, value: Double, range: ClosedFloatingPointRange<Double>, step: Double, visibility: () -> Boolean, consumer: (prev: Double, input: Double) -> Double, description: String, fineStep: Double): DoubleSetting {
+        return setting(DoubleSetting(name, value, range, step, visibility, consumer, description))
+    }
+
+    override fun IPluginClass.setting(name: String, value: Float, range: ClosedFloatingPointRange<Float>, step: Float, visibility: () -> Boolean, consumer: (prev: Float, input: Float) -> Float, description: String, fineStep: Float): FloatSetting {
+        return setting(FloatSetting(name, value, range, step, visibility, consumer, description, fineStep))
+    }
+
+    override fun IPluginClass.setting(name: String, value: Int, range: IntRange, step: Int, visibility: () -> Boolean, consumer: (prev: Int, input: Int) -> Int, description: String, fineStep: Int): IntegerSetting {
+        return setting(IntegerSetting(name, value, range, step, visibility, consumer, description, fineStep))
+    }
+
+    override fun IPluginClass.setting(name: String, value: Bind, visibility: () -> Boolean, description: String): BindSetting {
+        return setting(BindSetting(name, value, visibility, description))
     }
 }
