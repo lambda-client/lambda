5d35d603917e13c2c36e110d20164d942e3e768c
Constructor <fractalminds@protonmail.com>
2021-10-16 06:36:06 +0200

    Clean up

--- src/main/kotlin/com/lambda/client/module/modules/movement/Speed.kt
+++ src/main/kotlin/com/lambda/client/module/modules/movement/Speed.kt
@@ -6,9 +6,11 @@ import com.lambda.client.event.events.PlayerTravelEvent
 import com.lambda.client.manager.managers.PlayerPacketManager.sendPlayerPacket
 import com.lambda.client.manager.managers.TimerManager.modifyTimer
 import com.lambda.client.manager.managers.TimerManager.resetTimer
+import com.lambda.client.mixin.extension.isInWeb
 import com.lambda.client.mixin.extension.tickLength
 import com.lambda.client.mixin.extension.timer
 import com.lambda.client.module.Category
+import com.lambda.client.module.Module
 import com.lambda.client.util.BaritoneUtils
 import com.lambda.client.util.EntityUtils.flooredPosition
 import com.lambda.client.util.EntityUtils.isInOrAboveLiquid
@@ -21,9 +23,6 @@ import com.lambda.client.util.MovementUtils.speed
 import com.lambda.client.util.TickTimer
 import com.lambda.client.util.TimeUnit
 import com.lambda.client.util.threads.runSafe
-import com.lambda.client.mixin.extension.isInWeb
-import com.lambda.client.module.Module
-
 import com.lambda.client.util.threads.safeListener
 import net.minecraft.client.settings.KeyBinding
 import net.minecraftforge.fml.common.gameevent.TickEvent
@@ -31,69 +30,71 @@ import java.lang.Double.max
 import kotlin.math.cos
 import kotlin.math.sin
 
-object Speed: Module(
+object Speed : Module(
     name = "Speed",
     description = "vrooommm",
     category = Category.MOVEMENT,
 ) {
     //General settings
-    val mode = setting("Mode", SpeedMode.ONGROUND)
+    val mode by setting("Mode", SpeedMode.ONGROUND)
 
     //onGround speed settings
-    private val onGroundTimer by setting("Timer", true, { mode.value == SpeedMode.ONGROUND })
-    private val onGroundTimerSpeed by setting("Timer Speed", 1.29f, 0.0f..2.0f, 0.01f, { mode.value == SpeedMode.ONGROUND && onGroundTimer })
-    private val onGroundSpeed by setting("Speed", 1.31f, 1.0f..2.0f, 0.01f, { mode.value == SpeedMode.ONGROUND })
-    private val onGroundSprint by setting("Sprint", true, { mode.value == SpeedMode.ONGROUND })
-    private val onGroundCheckAbove by setting("Smart Mode", true, { mode.value == SpeedMode.ONGROUND })
+    private val onGroundTimer by setting("Timer", true, { mode == SpeedMode.ONGROUND })
+    private val onGroundTimerSpeed by setting("Timer Speed", 1.29f, 0.0f..2.0f, 0.01f, { mode == SpeedMode.ONGROUND && onGroundTimer })
+    private val onGroundSpeed by setting("Speed", 1.31f, 1.0f..2.0f, 0.01f, { mode == SpeedMode.ONGROUND })
+    private val onGroundSprint by setting("Sprint", true, { mode == SpeedMode.ONGROUND })
+    private val onGroundCheckAbove by setting("Smart Mode", true, { mode == SpeedMode.ONGROUND })
 
-    private val strafeAirSpeedBoost by setting("Air Speed Boost", 0.029f, 0.01f..0.04f, 0.001f, { mode.value == SpeedMode.STRAFE })
-    private val strafeTimerBoost by setting("Timer Boost", true, { mode.value == SpeedMode.STRAFE })
-    private val strafeAutoJump by setting("Auto Jump", true, { mode.value == SpeedMode.STRAFE })
-    private val strafeOnHoldingSprint by setting("On Holding Sprint", false, { mode.value == SpeedMode.STRAFE })
-    private val strafeCancelInertia by setting("Cancel Inertia", false, { mode.value == SpeedMode.STRAFE })
+    private val strafeAirSpeedBoost by setting("Air Speed Boost", 0.029f, 0.01f..0.04f, 0.001f, { mode == SpeedMode.STRAFE })
+    private val strafeTimerBoost by setting("Timer Boost", true, { mode == SpeedMode.STRAFE })
+    private val strafeAutoJump by setting("Auto Jump", true, { mode == SpeedMode.STRAFE })
+    private val strafeOnHoldingSprint by setting("On Holding Sprint", false, { mode == SpeedMode.STRAFE })
+    private val strafeCancelInertia by setting("Cancel Inertia", false, { mode == SpeedMode.STRAFE })
 
-    //Variables for onGround Mode
+    // onGround Mode
     private var onGroundOffset = false
     private var wasSprintEnabled = Sprint.isEnabled
 
-    //Variables for Strafe Mode
+    // Strafe Mode
     private var jumpTicks = 0
     private val strafeTimer = TickTimer(TimeUnit.TICKS)
 
-    private var currentMode = mode.value
+    private var currentMode = mode
+
+    enum class SpeedMode {
+        ONGROUND, STRAFE
+    }
 
     init {
         safeListener<TickEvent.ClientTickEvent> {
             if (isDisabled) return@safeListener
 
-            if(mode.value != currentMode) {
-                currentMode = mode.value
+            if (mode != currentMode) {
+                currentMode = mode
                 reset()
             }
 
-            if(mode.value == SpeedMode.ONGROUND && Sprint.isDisabled && onGroundSprint) Sprint.enable()
+            if (mode == SpeedMode.ONGROUND && Sprint.isDisabled && onGroundSprint) Sprint.enable()
         }
 
         safeListener<PlayerTravelEvent> {
-            when (mode.value) {
+            when (mode) {
                 SpeedMode.STRAFE -> {
-                    if(shouldStrafe())
-                        strafe()
+                    if (shouldStrafe()) strafe()
                 }
-
-                SpeedMode.ONGROUND -> {}
+                SpeedMode.ONGROUND -> {
+                }
             }
         }
 
         safeListener<PlayerMoveEvent> {
             if (isDisabled) return@safeListener
 
-            when (mode.value) {
+            when (mode) {
                 SpeedMode.ONGROUND -> {
                     if (shouldOnGround()) onGround()
                     else mc.timer.tickLength = 50.0f
                 }
-
                 SpeedMode.STRAFE -> {
                     if (shouldStrafe()) setSpeed(max(player.speed, applySpeedPotionEffects(0.2873)))
                     else {
@@ -111,7 +112,7 @@ object Speed: Module(
             }
 
             onDisable {
-                if(!wasSprintEnabled && mode.value == SpeedMode.ONGROUND) Sprint.disable()
+                if (!wasSprintEnabled && mode == SpeedMode.ONGROUND) Sprint.disable()
                 runSafe {
                     reset()
                 }
@@ -121,7 +122,7 @@ object Speed: Module(
 
     private fun SafeClientEvent.onGround() {
         sendPlayerPacket {
-            if(onGroundOffset) move(player.positionVector.add(0.0, 0.4, 0.0))
+            if (onGroundOffset) move(player.positionVector.add(0.0, 0.4, 0.0))
         }
 
         if (onGroundTimer) mc.timer.tickLength = 50.0f / onGroundTimerSpeed
@@ -129,23 +130,21 @@ object Speed: Module(
 
         player.motionX *= onGroundSpeed
         player.motionZ *= onGroundSpeed
-        if(onGroundOffset) player.motionY += 0.4
+        if (onGroundOffset) player.motionY += 0.4
 
         onGroundOffset.not()
     }
 
-    private fun SafeClientEvent.strafe(){
+    private fun SafeClientEvent.strafe() {
         player.jumpMovementFactor = strafeAirSpeedBoost
         if (strafeTimerBoost) modifyTimer(45.87155914306640625f)
-        if (Step.isDisabled || !player.collidedHorizontally) {
-            if (strafeAutoJump) jump()
-        }
+        if ((Step.isDisabled || !player.collidedHorizontally) && strafeAutoJump) jump()
 
         strafeTimer.reset()
     }
 
-    private fun SafeClientEvent.shouldOnGround(): Boolean{
-        return (world.getBlockState(player.flooredPosition.add(0.0,2.0,0.0)).material.isSolid || !onGroundCheckAbove)
+    private fun SafeClientEvent.shouldOnGround(): Boolean =
+        (world.getBlockState(player.flooredPosition.add(0.0, 2.0, 0.0)).material.isSolid || !onGroundCheckAbove)
             && player.isMoving
             && MovementUtils.isInputting
             && !player.movementInput.sneak
@@ -154,17 +153,15 @@ object Speed: Module(
             && !player.capabilities.isFlying
             && !player.isElytraFlying
             && !mc.gameSettings.keyBindSneak.isKeyDown
-    }
 
-    private fun SafeClientEvent.shouldStrafe(): Boolean {
-        return (!player.capabilities.isFlying
+    private fun SafeClientEvent.shouldStrafe(): Boolean =
+        (!player.capabilities.isFlying
             && !player.isElytraFlying
             && !mc.gameSettings.keyBindSneak.isKeyDown
             && (!strafeOnHoldingSprint || mc.gameSettings.keyBindSprint.isKeyDown)
             && !BaritoneUtils.isPathing
             && MovementUtils.isInputting
             && !(player.isInOrAboveLiquid || player.isInWeb))
-    }
 
     private fun SafeClientEvent.reset() {
         player.jumpMovementFactor = 0.02f
@@ -187,9 +184,5 @@ object Speed: Module(
         }
 
         jumpTicks--
-    }
-
-    enum class SpeedMode {
-        ONGROUND, STRAFE
     }
 }
