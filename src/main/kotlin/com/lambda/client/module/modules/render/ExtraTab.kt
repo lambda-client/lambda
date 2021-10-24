43a233537c073d9e657bea954f5bd021b7400594
Constructor <fractalminds@protonmail.com>
2021-10-25 01:00:39 +0200

    Merge TabFriends into ExtraTab

--- src/main/kotlin/com/lambda/client/module/modules/render/ExtraTab.kt
+++ src/main/kotlin/com/lambda/client/module/modules/render/ExtraTab.kt
@@ -1,8 +1,12 @@
 package com.lambda.client.module.modules.render
 
+import com.lambda.client.manager.managers.FriendManager
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
+import com.lambda.client.util.color.EnumTextColor
+import com.lambda.client.util.text.format
 import net.minecraft.client.network.NetworkPlayerInfo
+import net.minecraft.scoreboard.ScorePlayerTeam
 
 object ExtraTab : Module(
     name = "ExtraTab",
@@ -10,6 +14,20 @@ object ExtraTab : Module(
     category = Category.RENDER
 ) {
     private val tabSize by setting("Max Players", 265, 80..400, 5)
+    val highlightFriends by setting("Highlight Friends", true)
+    private val color by setting("Color", EnumTextColor.GREEN, { highlightFriends })
+
+    @JvmStatic
+    fun getPlayerName(info: NetworkPlayerInfo): String {
+        val name = info.displayName?.formattedText
+            ?: ScorePlayerTeam.formatPlayerName(info.playerTeam, info.gameProfile.name)
+
+        return if (FriendManager.isFriend(name)) {
+            color format name
+        } else {
+            name
+        }
+    }
 
     @JvmStatic
     fun subList(list: List<NetworkPlayerInfo>, newList: List<NetworkPlayerInfo>): List<NetworkPlayerInfo> {
