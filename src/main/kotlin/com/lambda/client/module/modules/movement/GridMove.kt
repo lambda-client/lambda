ca339c10748b6e8af57d09a457445161afa03a49
ItCameFr0mMars <48456413+itcamefr0mmars@users.noreply.github.com>
2021-11-10 20:48:37 +0000

    make it work

--- src/main/kotlin/com/lambda/client/module/modules/movement/GridMove.kt
+++ src/main/kotlin/com/lambda/client/module/modules/movement/GridMove.kt
@@ -1,37 +1,41 @@
 package com.lambda.client.module.modules.movement
 
-import baritone.api.pathing.goals.GoalXZ
-import com.lambda.client.event.SafeClientEvent
-import com.lambda.client.event.events.BaritoneCommandEvent
 import com.lambda.client.event.events.ConnectionEvent
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
 import com.lambda.client.module.modules.player.LagNotifier
-import com.lambda.client.util.BaritoneUtils
-import com.lambda.client.util.TickTimer
-import com.lambda.client.util.TimeUnit
-import com.lambda.client.util.math.Direction
-import com.lambda.client.util.text.MessageSendHelper
-import com.lambda.client.util.threads.runSafe
-import com.lambda.client.util.threads.safeListener
-import com.lambda.commons.extension.floorToInt
 import com.lambda.commons.interfaces.DisplayEnum
 import com.lambda.event.listener.listener
 import net.minecraft.util.MovementInputFromOptions
 import net.minecraftforge.client.event.InputUpdateEvent
-import net.minecraftforge.fml.common.gameevent.TickEvent
 
-object Gridmove : Module(
+object GridMove : Module(
     name = "GridMove",
     category = Category.MOVEMENT,
     description = "Moves in a grid. Useful for stash hunting"
 ) {
+    private val disableOnDisconnect by setting("Disable On Disconnect", true)
+    enum class GridMoveMode(override val displayName: String) : DisplayEnum {
+        FORWARD("Forward"),
+        BACKWARD("Backward")
+    }
+    var foo = GridMoveMode.FORWARD
     init {
+        listener<ConnectionEvent.Disconnect> {
+            if (disableOnDisconnect) disable()
+        }
         listener<InputUpdateEvent>(6969) {
             if (LagNotifier.paused && LagNotifier.pauseAutoWalk) return@listener
 
             if (it.movementInput !is MovementInputFromOptions) return@listener
-            it.movementInput.moveForward = 1.0f
-        }        
+            when (foo) {
+                GridMoveMode.FORWARD -> {
+                    it.movementInput.moveForward = 1.0f
+                }
+                GridMoveMode.BACKWARD -> {
+                    it.movementInput.moveForward = -1.0f
+                }
+            }
+        }
     }
 }
