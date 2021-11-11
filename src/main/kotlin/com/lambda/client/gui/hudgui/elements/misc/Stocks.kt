68328d9639f5121c4d2e0022364013b4e06eab67
ItCameFr0mMars <ryanmertz06@gmail.com>
2021-11-11 18:09:10 +0000

    update

--- src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
+++ src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
@@ -2,6 +2,10 @@ package com.lambda.client.gui.hudgui.elements.misc
 
 import com.lambda.client.event.SafeClientEvent
 import com.lambda.client.gui.hudgui.LabelHud
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 
 internal object Stocks : LabelHud(
     name = "Stocks",
@@ -11,7 +15,14 @@ internal object Stocks : LabelHud(
 
     private val symbol = setting("Symbol", "TSLA")
     private val delay by setting("Delay", 100, 5..60, 1)
+
     override fun SafeClientEvent.updateText() {
+        val client = HttpClient(CIO)
+        if (timer.tick(delay.toLong()) {
+                //get a new request from the API
+                //parse the response
+                //display the update
         displayText.add("Current Price of $symbol is [price] $delay")
         }
     }
+}
