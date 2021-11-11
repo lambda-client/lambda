1c5ba92e59af9fa77c1026e177b0339f96018756
ItCameFr0mMars <ryanmertz06@gmail.com>
2021-11-11 19:48:37 +0000

    stocks may work now!

--- src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
+++ src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
@@ -12,17 +12,19 @@ internal object Stocks : LabelHud(
     category = Category.MISC,
     description = "Info About a stock"
 ) {
-
-    private val symbol = setting("Symbol", "TSLA")
+    init {
+        ApiClient.apiKey["token"] = "c5resoqad3ifnpn51ou0"
+    }
+    private val symbol by setting("Symbol", "TSLA")
     private val delay by setting("Delay", 100, 5..60, 1)
-    val timer = TickTimer(TimeUnit.SECONDS)
-    var url = "https://finnhub.io/api/v1/quote?symbol="+symbol+"&token=c5resoqad3ifnpn51ou0"
+    private val timer = TickTimer(TimeUnit.SECONDS)
+    private var price = "test"
+    private val apiClient = DefaultApi()
     override fun SafeClientEvent.updateText() {
         if (timer.tick(delay.toLong())){
-                //get a new request from the API
-                //parse the response
-                //display the update
-        displayText.add("Current Price of $symbol is [price] $delay")
+            //ApiClient.apiKey["token"] = "c5resoqad3ifnpn51ou0"
+            price = apiClient.quote("AAPL").toString()
+            displayText.add("Current Price of $symbol is $price $delay")
         }
     }
 }
