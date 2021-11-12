f1f46ec1ecb4b9ec24ea83b793b14230dd967d9f
ItCameFr0mMars <ryanmertz06@Gmail.com>
2021-11-12 15:42:16 +0000

    LETS GOOOOO

--- src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
+++ src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
@@ -18,11 +18,11 @@ internal object Stocks : LabelHud(
     private val symbol by setting("Symbol", "TSLA")
     private val delay by setting("Delay", 100, 1..60, 1)
     private val timer = TickTimer(TimeUnit.SECONDS)
-    //private val apiClient = DefaultApi()
+    private val apiClient = DefaultApi()
     override fun SafeClientEvent.updateText() {
         if (! timer.tick(delay.toLong())){
-            //displayText.add("Current Price of $symbol is ${apiClient.quote(symbol)} $delay")
-            displayText.add("THIS IS A LOT OF TEXT MARS ON TOP EZZZ")
+            displayText.add("Current Price of $symbol is ${apiClient.quote(symbol)} $delay")
+
         }
     }
 }
