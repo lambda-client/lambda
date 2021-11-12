3767d18309b8074eee8cf869e6fb8b209f21b1df
ItCameFr0mMars <ryanmertz06@Gmail.com>
2021-11-12 18:33:33 +0000

    add a second timer

--- src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
+++ src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
@@ -16,13 +16,17 @@ internal object Stocks : LabelHud(
         ApiClient.apiKey["token"] = "c5resoqad3ifnpn51ou0"
     }
     private val symbol by setting("Symbol", "TSLA")
-    private val delay by setting("Delay", 10, 5..60, 1)
-    private val ticktimer = TickTimer(TimeUnit.MILLISECONDS)
+    private val tickdelay by setting("Delay", 10, 5..60, 1)
+    private val delay = 1
+    private val ticktimer = TickTimer(TimeUnit.SECONDS)
+    private val timer = TickTimer(TimeUnit.SECONDS)
     private val apiClient = DefaultApi()
     override fun SafeClientEvent.updateText() {
-        if (!ticktimer.tick(delay.toLong())){
-            displayText.add("Current Price of $symbol is ", primaryColor)
-            displayText.add(" ${ apiClient.quote(symbol)}", secondaryColor)
+        if (!ticktimer.tick(tickdelay.toLong())){
+            if (!timer.tick(delay.toLong())) {
+                displayText.add("Current Price of $symbol is ", primaryColor)
+                displayText.add(" ${apiClient.quote(symbol)}", secondaryColor)
+            }
         }
     }
 }
