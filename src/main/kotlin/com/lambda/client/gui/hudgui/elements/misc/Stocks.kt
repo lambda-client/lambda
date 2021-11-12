1d4895b8b5ce6b01fb2bced65bb4ec54ffd5a983
ItCameFr0mMars <ryanmertz06@Gmail.com>
2021-11-12 16:26:39 +0000

    h

--- src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
+++ src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
@@ -16,11 +16,11 @@ internal object Stocks : LabelHud(
         ApiClient.apiKey["token"] = "c5resoqad3ifnpn51ou0"
     }
     private val symbol by setting("Symbol", "TSLA")
-    private val delay by setting("Delay", 100, 1..60, 1)
+    private val delay by setting("Delay", 10, 5..60, 1)
     private val timer = TickTimer(TimeUnit.SECONDS)
-    private val apiClient = DefaultApi()
+    val apiClient = DefaultApi()
     override fun SafeClientEvent.updateText() {
-        if (! timer.tick(delay.toLong())){
+        if (!timer.tick(delay.toLong())){
             displayText.add("Current Price of $symbol is ", primaryColor)
             displayText.add(" ${ apiClient.quote(symbol)}", secondaryColor)
         }
