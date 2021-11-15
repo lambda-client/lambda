050acdcf69d485575c02e4307ee71d5b1efff49e
ItCameFr0mMars <ryanmertz06@gmail.com>
2021-11-15 20:02:21 +0000

    thx constructor

--- src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
+++ src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/Stocks.kt
@@ -4,6 +4,7 @@ import com.lambda.client.event.SafeClientEvent
 import com.lambda.client.gui.hudgui.LabelHud
 import com.lambda.client.util.TickTimer
 import com.lambda.client.util.TimeUnit
+import com.lambda.client.util.text.MessageSendHelper
 import io.finnhub.api.apis.DefaultApi
 import io.finnhub.api.infrastructure.ApiClient
 
@@ -15,16 +16,18 @@ internal object Stocks : LabelHud(
     init {
         ApiClient.apiKey["token"] = "c5resoqad3ifnpn51ou0"
     }
+
     private val symbol by setting("Symbol", "TSLA")
     private val tickdelay by setting("Delay", 10, 5..60, 1)
     private val ticktimer = TickTimer(TimeUnit.SECONDS)
     private val apiClient = DefaultApi()
     override fun SafeClientEvent.updateText() {
-        if (!ticktimer.tick(tickdelay.toLong())){
-            displayText.run {
-                add("Current Price of $symbol is ", primaryColor)
-                add("${apiClient.quote(symbol)}", secondaryColor)
+            if (ticktimer.tick(tickdelay)) {
+                displayText.run {
+                    add("Current Price of $symbol is ", primaryColor)
+                    add("${apiClient.quote(symbol)}", secondaryColor)
+                }
+                MessageSendHelper.sendChatMessage("Stonk")
             }
-        }
         }
     }
