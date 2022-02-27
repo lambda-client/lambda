a86d4a8f6b022a493371bd99ca370f1e1dbefb80
mmvanheusden <mmvanheusden@gmail.com>
2022-02-27 17:55:17 +0100

    Make the script generate the IntelliJ runs and print the duration afterwards.

--- setupWorkspace.sh
+++ setupWorkspace.sh
@@ -4,20 +4,28 @@
 #
 # Usage: "./setupWorkspace.sh"
 
-#
+SECONDS=0
 
 # To allow use from outside the lambda directory
 cd "$(dirname "$0")" || exit
 
-echo "[$(date +"%H:%M:%S")] Running gradlew classes without daemon..."
+echo "Running gradlew classes without daemon..."
 ./gradlew --no-daemon classes || {
-  echo "[$(date +"%H:%M:%S")] ERROR: Running gradlew build failed! Run './gradlew --no-daemon classes' manually"
+  echo "ERROR: Running gradlew build failed! Run './gradlew --no-daemon classes' manually"
   exit 1
 }
 
+# Generates InteliJ runs so the user doesn't have to do that manually
+echo "Generating InteliJ runs"
+./gradlew --no-daemon genIntellijRuns || {
+  echo "ERROR: Failure generating InteliJ runs! try generating them manually from within IntelliJ"
+  exit 1
+}
+
+clear
 cat logo_ascii.txt 2>/dev/null
 echo "=========================================================================="
 echo ""
-echo "[$(date +"%H:%M:%S")] Build succeeded! All checks passed, you can build normally now! Welcome to Lambda."
+echo "Build succeeded in $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds! All checks passed, you can build normally now! Welcome to Lambda."
 echo ""
 echo "=========================================================================="
