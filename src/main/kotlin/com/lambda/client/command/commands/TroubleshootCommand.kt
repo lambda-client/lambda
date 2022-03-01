4fb743e780efce0ecabd48f4f7355599d784d235
mmvanheusden <mmvanheusden@gmail.com>
2022-03-01 17:24:18 +0100

    Add more information to the troubleshoot command

--- src/main/kotlin/com/lambda/client/command/commands/TroubleshootCommand.kt
+++ src/main/kotlin/com/lambda/client/command/commands/TroubleshootCommand.kt
@@ -6,8 +6,8 @@ import com.lambda.client.module.ModuleManager
 import com.lambda.client.util.text.MessageSendHelper
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.client.renderer.OpenGlHelper
-import net.minecraftforge.common.ForgeVersion
 import org.lwjgl.opengl.GL11
+import java.util.*
 
 object TroubleshootCommand : ClientCommand(
     name = "troubleshoot",
@@ -18,9 +18,11 @@ object TroubleshootCommand : ClientCommand(
         execute("Print troubleshooting information") {
             MessageSendHelper.sendErrorMessage("&l&cSend a screenshot of all information below this line!")
             MessageSendHelper.sendChatMessage("Enabled Modules:\n" + ModuleManager.modules.filter { it.isEnabled }.joinToString { it.name })
-            MessageSendHelper.sendChatMessage("Forge ${ForgeVersion.getMajorVersion()}.${ForgeVersion.getMinorVersion()}.${ForgeVersion.getRevisionVersion()}.${ForgeVersion.getBuildVersion()}")
             MessageSendHelper.sendChatMessage("${LambdaMod.NAME} ${LambdaMod.LAMBDA} ${LambdaMod.VERSION}")
-            MessageSendHelper.sendChatMessage("CPU: ${OpenGlHelper.getCpu()} GPU: ${GlStateManager.glGetString(GL11.GL_VENDOR)}")
+            MessageSendHelper.sendChatMessage("Operating System: ${System.getProperty("os.name").lowercase(Locale.getDefault()).replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }} ${System.getProperty("os.version")} ")
+            MessageSendHelper.sendChatMessage("JVM: ${System.getProperty("java.version")} ${System.getProperty("java.vendor")}")
+            MessageSendHelper.sendChatMessage("GPU: ${GlStateManager.glGetString(GL11.GL_VENDOR)}")
+            MessageSendHelper.sendChatMessage("CPU: ${System.getProperty("os.arch")} ${OpenGlHelper.getCpu()}")
             MessageSendHelper.sendErrorMessage("&l&cPlease send a screenshot of the full output to the developer or moderator who's helping you!")
         }
     }
