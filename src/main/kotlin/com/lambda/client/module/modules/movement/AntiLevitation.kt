b693bccc0a1f284218b0b3be83f9b60de22dd094
StorageESP <mmvanheusden@gmail.com>
2022-03-04 13:36:54 +0100

    Cleanup

--- src/main/kotlin/com/lambda/client/module/modules/movement/AntiLevitation.kt
+++ src/main/kotlin/com/lambda/client/module/modules/movement/AntiLevitation.kt
@@ -8,11 +8,11 @@ import com.lambda.client.module.Module
 import com.lambda.client.util.MovementUtils
 import com.lambda.client.util.MovementUtils.calcMoveYaw
 import com.lambda.client.util.MovementUtils.isMoving
+import com.lambda.client.util.MovementUtils.setSpeed
 import com.lambda.client.util.text.MessageSendHelper
 import com.lambda.client.util.threads.safeListener
 import net.minecraft.init.MobEffects
 import net.minecraftforge.fml.common.gameevent.TickEvent
-import org.lwjgl.input.Keyboard
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -25,7 +25,7 @@ object AntiLevitation : Module(
 
     /* Flight mode */
     private val vertical by setting("Only Vertical", false, { mode == Mode.FLIGHT }, description = "doesn't apply extra speed when enabled")
-    private val YMotion by setting("Motion UP", 0.002f, 0.0f..0.02f, 0.001f, { mode == Mode.FLIGHT }, description = "The Y Motion that is applied when moving to bypass the anticheat")
+    private val YMotion by setting("Y Motion", 0.002f, 0.0f..0.02f, 0.001f, { mode == Mode.FLIGHT }, description = "The Y Motion that is applied when moving to bypass the anticheat")
     private val speed by setting("Speed",  0.28f, 0.15f..0.3f, 0.005f, { !vertical && mode == Mode.FLIGHT }, description = "The speed you fly at")
     private val timer by setting("Timer Boost",  true, { !vertical && mode == Mode.FLIGHT }, description = "Use timer for a slight speed boost")
     private val timerSpeed by setting("Timer Speed",  1.15f, 1.1f..1.2f, 0.01f, { timer && !vertical && mode == Mode.FLIGHT }, description = "The timer modifier")
@@ -33,6 +33,9 @@ object AntiLevitation : Module(
     /* Legit mode */
     private val LegitYMotion by setting("Motion UP", 0.018f, 0.001f..0.1f, 0.001f, { mode == Mode.LEGIT }, description = "The Y Motion that is applied when moving to bypass the anticheat")
     private val boost by setting("Sprint Boost", true, { mode == Mode.LEGIT }, description = "Gives you extra motion when control is pressed")
+
+    /* Jump motion (used by flight mode and legit mode) */
+    private val jumpMotion by setting("Jump Motion", 0.099f, 0.090f..0.10f, 0.001f, { mode == Mode.FLIGHT || mode == Mode.LEGIT}, description = "The Y Motion that is applied when you press space")
 
     private var ready = false
     private var legitReady = false
@@ -44,75 +47,63 @@ object AntiLevitation : Module(
     init {
         onDisable {
             ready = false
-            legitReady = false
             resetTimer()
         }
 
         safeListener<TickEvent.ClientTickEvent> {
             if (player.isPotionActive(MobEffects.LEVITATION)) {
-                if (mode == Mode.FLIGHT) {
-                    if (!ready) MessageSendHelper.sendChatMessage("You can now fly.")
+                if (mode != Mode.REMOVE && !ready) {
                     ready = true
-                } else if (mode == Mode.LEGIT){
-                    legitReady = true
-                } else {
+                    MessageSendHelper.sendChatMessage("You can now fly.")
+                }
+                if (mode == Mode.REMOVE) {
                     player.removeActivePotionEffect(MobEffects.LEVITATION)
                     MessageSendHelper.sendChatMessage("Removed levitation effect.")
                 }
             } else {
-                if (mode == Mode.FLIGHT && ready || mode == Mode.LEGIT && legitReady) {
+                if (ready) {
                     resetTimer()
+                    MessageSendHelper.sendWarningMessage("Levitation ran out. Brace for impact....")
+                    mc.player.setVelocity(0.0,0.0,0.0)
                     ready = false
-                    legitReady = false
-                    MessageSendHelper.sendChatMessage("Levitation ran out. Brace for impact....")
-                    mc.player.setVelocity(0.0,0.0,0.0) //Reset velocity to make anticheat glitch out less
                 }
             }
         }
 
         safeListener<PlayerTravelEvent> {
             if (ready) {
-                if (MovementUtils.isInputting && !vertical) {
-                    /* Makes the player move when they press a movement key */
-                    val yaw = calcMoveYaw()
-                    player.isSprinting = false //disables sprinting so you can't go too fast
-                    player.motionX = -sin(yaw) * speed
-                    player.motionZ = cos(yaw) * speed
-                    if (timer && !vertical) modifyTimer(50.0f / timerSpeed)
+                if (mode == Mode.FLIGHT) {
+                    if (MovementUtils.isInputting && !vertical) {
+                        player.isSprinting = false //disables sprinting so you can't go too fast
+                        setSpeed(speed.toDouble())
+                        if (timer && !vertical) modifyTimer(50.0f / timerSpeed)
+                    } else {
+                        player.motionY = 0.0
+                        /* Make the motion slowly become 0 so it flattens out smooth */
+                        player.motionX *= 0.8
+                        player.motionZ *= 0.8
+                    }
 
-                } else {
-                    player.motionY = 0.0 // Makes the Y motion always be 0.0 when not pressing any key so you can stand still in the air
+                    if (MovementUtils.isInputting || player.isMoving) {
+                        player.motionY = YMotion.toDouble()
+                    }
 
-                    /* Make the motion slowly become 0 so it flattens out smooth */
-                    player.motionX *= 0.8 // Possible memory leak? cuz it keeps making the number smaller i guess
-                    player.motionZ *= 0.8
+                    if (mc.gameSettings.keyBindJump.isKeyDown) player.motionY = jumpMotion.toDouble()
+                    if (mc.gameSettings.keyBindSneak.isKeyDown) player.motionY = -0.49
+                } else if (mode == Mode.LEGIT) {
+                    /* Override vanilla motion with our own motion */
+                    player.motionY = LegitYMotion.toDouble()
+
+                    if (mc.gameSettings.keyBindJump.isKeyDown) player.motionY = jumpMotion.toDouble()
+                    if (mc.gameSettings.keyBindSneak.isKeyDown) player.motionY = 0.005
+
+                    if (mc.gameSettings.keyBindSprint.isKeyDown && player.isSprinting && boost) { //player must be sprinting so you can only boost when you press W
+                        val yaw = calcMoveYaw()
+                        player.motionX = -sin(yaw) * 0.26
+                        player.motionZ = cos(yaw) * 0.26
+                    }
                 }
-
-                /* Apply tiny Y motion when the player wants to move to trick the anticheat */
-                if (MovementUtils.isInputting || player.isMoving) {
-                    player.motionY = YMotion.toDouble()
-                }
-
-                /* Vertical movement */
-                if (mc.gameSettings.keyBindJump.isKeyDown) player.motionY = 0.0925
-                if (mc.gameSettings.keyBindSneak.isKeyDown) player.motionY = -0.49 // You can go down way faster then going up for some reason
-            } else if (legitReady) {
-                /* Override vanilla motion with our own motion */
-                player.motionY = LegitYMotion.toDouble()
-
-                /* Jump boost */
-                if (mc.gameSettings.keyBindJump.isKeyDown) player.motionY = 0.0875
-
-                /* Shift slow down */
-                if (mc.gameSettings.keyBindSneak.isKeyDown) player.motionY = 0.005
-
-                /* Slight boost when control is pressed */
-                if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) && player.isSprinting && boost) { //player also must be sprinting so you can only boost when you press W
-                    val yaw = calcMoveYaw()
-                    player.motionX = -sin(yaw) * 0.26
-                    player.motionZ = cos(yaw) * 0.26
-                }
-            } else return@safeListener
+            }
         }
     }
 }
