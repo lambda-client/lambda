ac428c73b672bbfb67fd7d23b6386edb7749a21e
urFate <georgiylakidon@gmail.com>
2022-04-02 18:11:00 +0300

    Add active potion effects HUD

---
+++ src/main/kotlin/com/lambda/client/gui/hudgui/elements/player/Effects.kt
@@ -0,0 +1,31 @@
+package com.lambda.client.gui.hudgui.elements.player
+
+import com.lambda.client.event.SafeClientEvent
+import com.lambda.client.gui.hudgui.LabelHud
+import com.lambda.client.util.color.ColorConverter
+import com.lambda.client.util.text.RomanNumerals
+import net.minecraft.potion.*
+import net.minecraft.client.resources.I18n
+
+internal object Effects : LabelHud(
+    name = "Effects",
+    category = Category.PLAYER,
+    description = "Displays active effects"
+) {
+
+    private val romanNumerals by setting("Roman Numerals", true)
+    private val coloredEffectNames by setting("Colored Effect Names", true)
+
+    override fun SafeClientEvent.updateText() {
+        player.activePotionEffects.forEach{
+            val amplifier = if (romanNumerals) RomanNumerals.numberToRoman(it.amplifier + 1) else (it.amplifier + 1).toString()
+            val duration = Potion.getPotionDurationString(it, 1f)
+            val color = if (coloredEffectNames) ColorConverter.hexToRgb(it.potion.liquidColor) else secondaryColor
+            val name = I18n.format(it.potion.name)
+
+            displayText.add(name, color)
+            displayText.add(amplifier, primaryColor)
+            displayText.addLine("(${duration})", primaryColor)
+        }
+    }
+}
