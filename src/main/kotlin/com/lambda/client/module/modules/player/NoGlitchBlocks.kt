02e6f0ec2d74c66715098e13f6fe29107b491ce2
Constructor <fractalminds@protonmail.com>
2022-04-30 03:14:17 +0200

    Added NoGlitchBlocks

---
+++ src/main/kotlin/com/lambda/client/module/modules/player/NoGlitchBlocks.kt
@@ -0,0 +1,11 @@
+package com.lambda.client.module.modules.player
+
+import com.lambda.client.module.Category
+import com.lambda.client.module.Module
+
+object NoGlitchBlocks : Module(
+    name = "NoGlitchBlocks",
+    description = "Syncs block interactions for strict environments",
+    category = Category.PLAYER
+) {
+}
