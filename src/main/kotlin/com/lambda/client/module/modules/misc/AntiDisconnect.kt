57fd398b17fc5ef72b0402bdffcdedbd19e57a1b
CozyMeli-gitv2 <110065125+CozyMeli-gitv2@users.noreply.github.com>
2022-08-03 20:51:44 -0400

    Improved description
    Changed description to describe the function of the module and removed punctuation to fit the overall description style.

--- src/main/kotlin/com/lambda/client/module/modules/misc/AntiDisconnect.kt
+++ src/main/kotlin/com/lambda/client/module/modules/misc/AntiDisconnect.kt
@@ -5,7 +5,7 @@ import com.lambda.client.module.Module
 
 object AntiDisconnect : Module(
     name = "AntiDisconnect",
-    description = "Are you sure you want to disconnect?",
+    description = "Prevents you from accidently disconnecting",
     category = Category.MISC
 ) {
     val presses by setting("Button Presses", 3, 1..20, 1)
