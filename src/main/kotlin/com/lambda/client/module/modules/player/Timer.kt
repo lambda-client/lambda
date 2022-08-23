6f122b092aed7ce03eb7fc0c173e356a44103e87
mmvanheusden <50550545+mmvanheusden@users.noreply.github.com>
2022-08-23 10:11:43 +0200

    Add a timer "only when inputting option"

--- src/main/kotlin/com/lambda/client/module/modules/player/Timer.kt
+++ src/main/kotlin/com/lambda/client/module/modules/player/Timer.kt
@@ -5,6 +5,7 @@ import com.lambda.client.manager.managers.TimerManager.modifyTimer
 import com.lambda.client.manager.managers.TimerManager.resetTimer
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
+import com.lambda.client.util.MovementUtils
 import net.minecraftforge.fml.common.gameevent.TickEvent
 
 object Timer : Module(
@@ -13,6 +14,7 @@ object Timer : Module(
     category = Category.PLAYER,
     modulePriority = 500
 ) {
+    private val onlyWhenInputting by setting("Only when inputting", false)
     private val slow by setting("Slow Mode", false)
     private val tickNormal by setting("Tick N", 2.0f, 1f..10f, 0.1f, { !slow })
     private val tickSlow by setting("Tick S", 8f, 1f..10f, 0.1f, { slow })
@@ -26,7 +28,9 @@ object Timer : Module(
             if (it.phase != TickEvent.Phase.END) return@listener
 
             val multiplier = if (!slow) tickNormal else tickSlow / 10.0f
-            modifyTimer(50.0f / multiplier)
+            if (onlyWhenInputting) {
+                if (MovementUtils.isInputting) modifyTimer(50.0f / multiplier)
+            } else modifyTimer(50.0f / multiplier)
         }
     }
 }
