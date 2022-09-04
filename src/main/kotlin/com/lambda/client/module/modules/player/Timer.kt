862ef05f7f4ba76729878dfafb2e02640d9bf8a1
Maarten <50550545+mmvanheusden@users.noreply.github.com>
2022-09-05 01:03:01 +0200

    Add a timer "only when inputting" option (#370)
    * Add a timer "only when inputting option"
    
    * Fix setting name convention
    
    * Make timer pause in ClickGUI
    
    Co-authored-by: Constructor <fractalminds@protonmail.com>

--- src/main/kotlin/com/lambda/client/module/modules/player/Timer.kt
+++ src/main/kotlin/com/lambda/client/module/modules/player/Timer.kt
@@ -5,6 +5,8 @@ import com.lambda.client.manager.managers.TimerManager.modifyTimer
 import com.lambda.client.manager.managers.TimerManager.resetTimer
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
+import com.lambda.client.module.modules.client.ClickGUI
+import com.lambda.client.util.MovementUtils
 import net.minecraftforge.fml.common.gameevent.TickEvent
 
 object Timer : Module(
@@ -13,6 +15,7 @@ object Timer : Module(
     category = Category.PLAYER,
     modulePriority = 500
 ) {
+    private val onlyWhenInputting by setting("Only When Inputting", false)
     private val slow by setting("Slow Mode", false)
     private val tickNormal by setting("Tick N", 2.0f, 1f..10f, 0.1f, { !slow })
     private val tickSlow by setting("Tick S", 8f, 1f..10f, 0.1f, { slow })
@@ -25,8 +28,15 @@ object Timer : Module(
         listener<TickEvent.ClientTickEvent> {
             if (it.phase != TickEvent.Phase.END) return@listener
 
+            if (ClickGUI.isEnabled) {
+                resetTimer()
+                return@listener
+            }
+
             val multiplier = if (!slow) tickNormal else tickSlow / 10.0f
-            modifyTimer(50.0f / multiplier)
+            if (onlyWhenInputting) {
+                if (MovementUtils.isInputting) modifyTimer(50.0f / multiplier)
+            } else modifyTimer(50.0f / multiplier)
         }
     }
 }
