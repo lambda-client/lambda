b95badf4a6fd2899b5dcb67ccf3addf467683796
Constructor <fractalminds@protonmail.com>
2022-12-15 22:14:20 +0100

    Inventory stuff

--- src/main/kotlin/com/lambda/client/module/modules/misc/TestActivityManager.kt
+++ src/main/kotlin/com/lambda/client/module/modules/misc/TestActivityManager.kt
@@ -3,22 +3,48 @@ package com.lambda.client.module.modules.misc
 import com.lambda.client.manager.managers.ActivityManager
 import com.lambda.client.manager.managers.activity.activities.example.SayAnnoyinglyActivity
 import com.lambda.client.manager.managers.activity.activities.inventory.DumpInventoryActivity
-import com.lambda.client.manager.managers.activity.activities.inventory.SwapOrMoveToAnyBlockActivity
+import com.lambda.client.manager.managers.activity.activities.inventory.SwapOrMoveToItemActivity
 import com.lambda.client.module.Category
 import com.lambda.client.module.Module
+import com.lambda.client.util.items.block
+import net.minecraft.block.BlockShulkerBox
+import net.minecraft.enchantment.EnchantmentHelper
+import net.minecraft.init.Enchantments
+import net.minecraft.init.Items
 
 object TestActivityManager : Module(
     name = "TestActivityManager",
     description = "",
     category = Category.MISC
 ) {
-    private val getBlock by setting("Get Block", false, consumer = { _, _->
-        ActivityManager.addActivity(SwapOrMoveToAnyBlockActivity())
+    private val a by setting("Get Dia Pickaxe", false, consumer = { _, _->
+        ActivityManager.addActivity(SwapOrMoveToItemActivity(Items.DIAMOND_PICKAXE))
+        false
+    })
+
+    private val b by setting("Get Dia Pickaxe with silktouch", false, consumer = { _, _->
+        ActivityManager.addActivity(
+            SwapOrMoveToItemActivity(
+                Items.DIAMOND_PICKAXE,
+                predicateItem = {
+                    EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, it) == 1
+                },
+                predicateSlot = {
+                    val item = it.item
+                    item != Items.DIAMOND_PICKAXE && item.block !is BlockShulkerBox
+                }
+            )
+        )
         false
     })
 
     private val dumpInventoryActivity by setting("Dump Inventory", false, consumer = { _, _->
         ActivityManager.addActivity(DumpInventoryActivity())
+        false
+    })
+
+    private val onlyOne by setting("Dump only one Inventory", false, consumer = { _, _->
+        ActivityManager.addActivity(DumpInventoryActivity(1))
         false
     })
 
