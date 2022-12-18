273fbb9ff1c9959e20fba00bb4d24eb2ee506ad6
Constructor <fractalminds@protonmail.com>
2022-12-18 03:55:27 +0100

    Added CustomGoalActivity

---
+++ src/main/kotlin/com/lambda/client/manager/managers/activity/activities/travel/CustomGoalActivity.kt
@@ -0,0 +1,29 @@
+package com.lambda.client.manager.managers.activity.activities.travel
+
+import baritone.api.pathing.goals.Goal
+import com.lambda.client.manager.managers.activity.Activity
+import com.lambda.client.manager.managers.activity.activities.TimeoutActivity
+import com.lambda.client.util.BaritoneUtils
+import com.lambda.client.util.EntityUtils.flooredPosition
+import com.lambda.client.util.threads.safeListener
+import net.minecraftforge.fml.common.gameevent.TickEvent
+
+class CustomGoalActivity(
+    private val goal: Goal,
+    override val timeout: Long = 100000L,
+    override var creationTime: Long = 0L
+) : TimeoutActivity, Activity() {
+    init {
+        safeListener<TickEvent.ClientTickEvent> { event ->
+            if (event.phase != TickEvent.Phase.START) return@safeListener
+
+            BaritoneUtils.primary?.customGoalProcess?.setGoalAndPath(goal)
+
+            if (BaritoneUtils.primary?.customGoalProcess?.goal == null) {
+                activityStatus = ActivityStatus.FAILURE
+            }
+
+            if (goal.isInGoal(player.flooredPosition)) activityStatus = ActivityStatus.SUCCESS
+        }
+    }
+}
