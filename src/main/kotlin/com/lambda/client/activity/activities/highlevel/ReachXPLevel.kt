4565b68846ba11606e236db27cf3f62b0d579f80
minecraft-simon <minecraft_simon@gmx.net>
2022-12-30 18:05:36 +0100

    proposal for activity events

--- src/main/kotlin/com/lambda/client/activity/activities/highlevel/ReachXPLevel.kt
+++ src/main/kotlin/com/lambda/client/activity/activities/highlevel/ReachXPLevel.kt
@@ -3,25 +3,40 @@ package com.lambda.client.activity.activities.highlevel
 import com.lambda.client.activity.Activity
 import com.lambda.client.activity.activities.LoopingUntilActivity
 import com.lambda.client.activity.activities.interaction.UseThrowableOnEntity
+import com.lambda.client.activity.activities.inventory.DumpInventory
 import com.lambda.client.activity.activities.inventory.SwapOrMoveToItem
 import com.lambda.client.activity.activities.inventory.TakeOffArmor
 import com.lambda.client.event.SafeClientEvent
+import com.lambda.client.manager.managers.ActivityManager.onSuccess
 import net.minecraft.init.Items
 import net.minecraft.util.math.BlockPos
 
 class ReachXPLevel(
     private val desiredLevel: Int,
 //    private val xpSupply: BlockPos, should be replaced with using ecosystem for stashes
-    override val loopUntil: SafeClientEvent.() -> Boolean = {
-        player.experienceLevel >= desiredLevel
-    },
-    override var currentLoops: Int = 0
-) : LoopingUntilActivity, Activity() {
+    //override val loopUntil: SafeClientEvent.() -> Boolean = {
+    //    player.experienceLevel >= desiredLevel
+    //},
+    //override var currentLoops: Int = 0
+) : Activity() {
     override fun SafeClientEvent.onInitialize() {
+        val useThrowableOnEntity = UseThrowableOnEntity(player)
+        useThrowableOnEntity.executeOnSuccess = {
+            println("Successfully used throwable on entity")
+        }
+        useThrowableOnEntity.executeOnFailure = {
+            println("Error: ${it.message}")
+        }
+        useThrowableOnEntity.executeOnFinalize = {
+            println("Finalized throwable on entity")
+            addSubActivities(
+                DumpInventory()
+            )
+        }
         addSubActivities(
             TakeOffArmor(),
             SwapOrMoveToItem(Items.EXPERIENCE_BOTTLE),
-            UseThrowableOnEntity(player)
+            useThrowableOnEntity
         )
     }
 }
