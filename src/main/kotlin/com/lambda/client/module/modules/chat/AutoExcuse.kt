8d1c75aaa6e5de298d56721aee5b645d7ccd5ca9
Constructor <fractalminds@protonmail.com>
2023-01-08 20:33:57 +0100

    Brought back default excuses :)

--- src/main/kotlin/com/lambda/client/module/modules/chat/AutoExcuse.kt
+++ src/main/kotlin/com/lambda/client/module/modules/chat/AutoExcuse.kt
@@ -1,6 +1,5 @@
 package com.lambda.client.module.modules.chat
 
-import com.lambda.client.LambdaMod
 import com.lambda.client.commons.extension.synchronized
 import com.lambda.client.event.SafeClientEvent
 import com.lambda.client.event.events.PacketEvent
@@ -28,6 +27,7 @@ object AutoExcuse : Module(
     modulePriority = 500
 ) {
     private val modeSetting by setting("Order", Mode.RANDOM_ORDER)
+    private val modeSource by setting("Source", SourceMode.INTERNAL)
 
     private val file = File(FolderUtils.lambdaFolder + "excuses.txt")
     private val loadedExcuses = ArrayList<String>().synchronized()
@@ -39,32 +39,80 @@ object AutoExcuse : Module(
         IN_ORDER, RANDOM_ORDER
     }
 
+    private enum class SourceMode {
+        INTERNAL, EXTERNAL
+    }
+
+    private const val CLIENT_NAME = "%CLIENT%"
+
+    private val defaultExcuses = arrayOf(
+        "Sorry, im using $CLIENT_NAME",
+        "My ping is so bad",
+        "I was changing my config :(",
+        "Why did my AutoTotem break",
+        "I was desynced",
+        "Stupid hackers killed me",
+        "Wow, so many try hards",
+        "Lagggg",
+        "I wasn't trying",
+        "I'm not using $CLIENT_NAME",
+        "Thers to much lag",
+        "My dog ate my pc",
+        "Sorry, $CLIENT_NAME is really bad",
+        "I was lagging",
+        "He was cheating!",
+        "Your hacking!",
+        "Lol imagine actully trying",
+        "I didn't move my mouse",
+        "I was playing on easy mode(;",
+        "My wifi went down",
+        "I'm playing vanila",
+        "My optifine didn't work",
+        "The CPU cheated!",
+        "I am using a cracked client",
+        "My brother was playing."
+    )
+
+    private val clients = arrayOf(
+        "Future Client",
+        "Salhack",
+        "Pyro",
+        "Impact"
+    )
+
     init {
         safeListener<PacketEvent.Receive> {
             if (loadedExcuses.isEmpty() || it.packet !is SPacketUpdateHealth) return@safeListener
             if (it.packet.health <= 0.0f && !isHoldingTotem && timer.tick(3L)) {
                 val message = if (modeSetting == Mode.IN_ORDER) getOrdered() else getRandom()
-                sendServerMessage(message)
+                sendServerMessage(message.replace("%CLIENT%", clients.random()))
             }
         }
 
         onEnable {
             loadedExcuses.clear()
+            currentLine = 0
 
-            defaultScope.launch(Dispatchers.IO) {
-                if (file.exists()) {
-                    try {
-                        file.forEachLine { if (it.isNotBlank()) loadedExcuses.add(it.trim()) }
-                        MessageSendHelper.sendChatMessage("$chatName Loaded excuse messages!")
-                    } catch (e: Exception) {
-                        MessageSendHelper.sendErrorMessage("$chatName Failed loading excuses, $e")
-                        disable()
+            when (modeSource) {
+                SourceMode.INTERNAL -> loadedExcuses.addAll(defaultExcuses)
+                SourceMode.EXTERNAL -> {
+                    defaultScope.launch(Dispatchers.IO) {
+                        if (!file.exists()) {
+                            file.createNewFile()
+                            MessageSendHelper.sendErrorMessage("$chatName Excuses file is empty!" +
+                                ", please add them in the &7excuses.txt&f under the &7.minecraft/lambda&f directory.")
+                            disable()
+                            return@launch
+                        }
+
+                        try {
+                            file.forEachLine { if (it.isNotBlank()) loadedExcuses.add(it.trim()) }
+                            MessageSendHelper.sendChatMessage("$chatName Loaded excuse messages!")
+                        } catch (e: Exception) {
+                            MessageSendHelper.sendErrorMessage("$chatName Failed loading excuses, $e")
+                            disable()
+                        }
                     }
-                } else {
-                    file.createNewFile()
-                    MessageSendHelper.sendErrorMessage("$chatName Excuses file is empty!" +
-                        ", please add them in the &7excuses.txt&f under the &7.minecraft/lambda&f directory.")
-                    disable()
                 }
             }
         }
@@ -77,6 +125,7 @@ object AutoExcuse : Module(
         currentLine %= loadedExcuses.size
         return loadedExcuses[currentLine++]
     }
+
     private fun getRandom(): String {
         val prevLine = currentLine
         // Avoids sending the same message
