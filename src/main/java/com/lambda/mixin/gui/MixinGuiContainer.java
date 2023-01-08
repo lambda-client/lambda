7f630bcc9b304da6870ada4699367eb7fdb60cfb
Constructor <fractalminds@protonmail.com>
2023-01-08 17:43:13 +0100

    Reduce nesting and this calls

--- src/main/java/com/lambda/mixin/gui/MixinGuiContainer.java
+++ src/main/java/com/lambda/mixin/gui/MixinGuiContainer.java
@@ -18,37 +18,39 @@ public class MixinGuiContainer extends GuiScreen {
     @Shadow protected int guiLeft;
     @Shadow protected int guiTop;
     @Shadow protected int xSize;
-    private final GuiButton stealButton = new LambdaGuiStealButton(this.guiLeft + this.xSize + 2, this.guiTop + 2);
-    private final GuiButton storeButton = new LambdaGuiStoreButton(this.guiLeft + this.xSize + 2, this.guiTop + 4 + stealButton.height);
+    private final GuiButton stealButton = new LambdaGuiStealButton(guiLeft + xSize + 2, guiTop + 2);
+    private final GuiButton storeButton = new LambdaGuiStoreButton(guiLeft + xSize + 2, guiTop + 4 + stealButton.height);
 
     @Inject(method = "mouseClicked", at = @At("TAIL"))
     public void mouseClicked(int x, int y, int button, CallbackInfo ci) {
-        if (button == 0) {
-            if (storeButton.mousePressed(mc, x, y)) {
-                ChestStealer.INSTANCE.setStoring(!ChestStealer.INSTANCE.getStoring());
-            } else if (stealButton.mousePressed(mc, x, y)) {
-                ChestStealer.INSTANCE.setStealing(!ChestStealer.INSTANCE.getStealing());
-            }
+        if (button != 0) return;
+
+        if (storeButton.mousePressed(mc, x, y)) {
+            ChestStealer.INSTANCE.setStoring(!ChestStealer.INSTANCE.getStoring());
+        } else if (stealButton.mousePressed(mc, x, y)) {
+            ChestStealer.INSTANCE.setStealing(!ChestStealer.INSTANCE.getStealing());
         }
     }
 
     @Inject(method = "updateScreen", at = @At("TAIL"))
     public void updateScreen(CallbackInfo ci) {
-        if (ChestStealer.INSTANCE.isValidGui()) {
-            if (ChestStealer.INSTANCE.isEnabled()) {
-                if (!this.buttonList.contains(stealButton)) {
-                    this.buttonList.add(stealButton);
-                }
-                if (!this.buttonList.contains(storeButton)) {
-                    this.buttonList.add(storeButton);
-                }
-                ChestStealer.updateButton(stealButton, this.guiLeft, this.xSize, this.guiTop);
-                ChestStealer.updateButton(storeButton, this.guiLeft, this.xSize, this.guiTop);
-            } else {
-                this.buttonList.remove(storeButton);
-                this.buttonList.remove(storeButton);
-            }
+        if (!ChestStealer.INSTANCE.isValidGui()) return;
+
+        if (!ChestStealer.INSTANCE.isEnabled()) {
+            buttonList.remove(storeButton);
+            buttonList.remove(storeButton);
+            return;
         }
+
+        if (!buttonList.contains(stealButton)) {
+            buttonList.add(stealButton);
+        }
+        if (!buttonList.contains(storeButton)) {
+            buttonList.add(storeButton);
+        }
+
+        ChestStealer.updateButton(stealButton, guiLeft, xSize, guiTop);
+        ChestStealer.updateButton(storeButton, guiLeft, xSize, guiTop);
     }
 
 }
