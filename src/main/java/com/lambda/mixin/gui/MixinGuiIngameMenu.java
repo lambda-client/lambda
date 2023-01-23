d8c3fa77ef637eff12ea6c13e719c3d6701762cb
Baitinq <manuelpalenzuelamerino@gmail.com>
2023-01-23 14:51:17 +0100

    Add "Server List" button in the Minecraft pause GUI
    This patch adds a new "Server List" button replacing the disabled
    "Open To Lan" button in the multiplayer pause menu :)

--- src/main/java/com/lambda/mixin/gui/MixinGuiIngameMenu.java
+++ src/main/java/com/lambda/mixin/gui/MixinGuiIngameMenu.java
@@ -3,9 +3,7 @@ package com.lambda.mixin.gui;
 import com.lambda.client.gui.mc.LambdaGuiAntiDisconnect;
 import com.lambda.client.module.modules.misc.AntiDisconnect;
 import com.lambda.client.util.Wrapper;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiIngameMenu;
-import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,11 +13,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinGuiIngameMenu extends GuiScreen {
     @Inject(method = "actionPerformed", at = @At("HEAD"), cancellable = true)
     public void actionPerformed(GuiButton button, CallbackInfo callbackInfo) {
-        if (button.id == 1) {
-            if (AntiDisconnect.INSTANCE.isEnabled()) {
-                Wrapper.getMinecraft().displayGuiScreen(new LambdaGuiAntiDisconnect());
-                callbackInfo.cancel();
-            }
+        switch (button.id) {
+            case 1:
+                if (AntiDisconnect.INSTANCE.isEnabled()) {
+                    Wrapper.getMinecraft().displayGuiScreen(new LambdaGuiAntiDisconnect());
+                    callbackInfo.cancel();
+                }
+                break;
+            case Integer.MIN_VALUE:
+                Wrapper.getMinecraft().displayGuiScreen(new GuiMultiplayer(this));
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Inject(method = "initGui", at = @At("RETURN"))
+    public void initGui(CallbackInfo ci) {
+        if (!mc.isSingleplayer()) {
+            GuiButton openToLanButton = buttonList.remove(4);
+            buttonList.add(new GuiButton(Integer.MIN_VALUE, openToLanButton.x, openToLanButton.y, openToLanButton.width, openToLanButton.height, "Server List"));
         }
     }
 }
