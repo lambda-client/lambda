bdb218fd69fb92a5fd739bff124c2d3713c9017f
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2023-03-19 22:39:31 -0400

    Update CrystalManager.kt

--- src/main/kotlin/com/lambda/client/manager/managers/CrystalManager.kt
+++ src/main/kotlin/com/lambda/client/manager/managers/CrystalManager.kt
@@ -1,62 +1,44 @@
 package com.lambda.client.manager.managers
 
+import com.google.common.collect.ConcurrentHashMultiset
 import com.lambda.client.event.events.ConnectionEvent
 import com.lambda.client.event.events.WorldEvent
-import com.lambda.client.event.listener.listener
 import com.lambda.client.manager.Manager
 import com.lambda.client.module.modules.combat.CrystalAura
-import com.lambda.client.util.combat.CombatUtils
-import com.lambda.client.util.combat.CombatUtils.getExplosionAffectedEntities
 import com.lambda.client.util.combat.CrystalUtils.calcCrystalDamage
 import com.lambda.client.util.combat.CrystalUtils.getBestPlace
 import com.lambda.client.util.math.VectorUtils.distanceTo
-import com.lambda.client.util.threads.onMainThreadSafe
 import com.lambda.client.util.threads.safeListener
-import io.netty.util.internal.ConcurrentSet
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.item.EntityEnderCrystal
 import net.minecraft.util.math.Vec3d
-import net.minecraftforge.event.world.ExplosionEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import java.util.concurrent.ConcurrentHashMap
 
 object CrystalManager : Manager {
-    val placedCrystals = ConcurrentSet<Crystal>()
+    val placedCrystals: ConcurrentHashMultiset<Crystal> = ConcurrentHashMultiset.create()
     val toPlaceList = ConcurrentHashMap<EntityLivingBase, CrystalPlaceInfo>()
 
     init {
-        safeListener<net.minecraftforge.event.world.WorldEvent.Load> {
-            world.loadedEntityList.filterIsInstance<EntityEnderCrystal>().forEach { crystal ->
-                CombatManager.target
-            }
-        }
-
         safeListener<WorldEvent.EntityCreate> { update ->
-            if (update.entity !is EntityEnderCrystal) return@safeListener
-            val crystal = Crystal(update.entity, CrystalPlaceInfo(update.entity.positionVector, calcCrystalDamage(update.entity, CombatManager.target)))
-            placedCrystals.add(crystal)
+            if (update.entity !is EntityEnderCrystal || update.entity.distanceTo(player.positionVector) > 12) return@safeListener
+            CombatManager.target?.let { target ->
+                placedCrystals.add(Crystal(update.entity, CrystalPlaceInfo(update.entity.positionVector, calcCrystalDamage(update.entity, target))))
+            }
         }
 
         safeListener<TickEvent.ClientTickEvent> {
             CombatManager.target?.let { entity ->
                 toPlaceList[entity] = getBestPlace(entity, CrystalAura.placeDistance) ?: return@safeListener
-                placedCrystals.forEach { it.info.damage = calcCrystalDamage(entity.positionVector, CombatManager.target) }
-                toPlaceList.forEach { (entity, crystal) ->
-                    if (
-                        !world.loadedEntityList.contains(entity)
-                        || entity.distanceTo(player.positionVector) > 10
-                        || placedCrystals.any { placed -> placed.info == crystal }
-                    ) toPlaceList.remove(entity)
-                    else crystal.damage = calcCrystalDamage(crystal.position, entity)
+                placedCrystals.forEach { crystal ->
+                    crystal.info.damage = calcCrystalDamage(crystal.entity, entity)
                 }
-            }
-        }
+                toPlaceList.forEach { (entity, crystal) ->
+                    crystal.damage = calcCrystalDamage(crystal.position, entity)
+                }
+            } ?: toPlaceList.clear()
 
-        safeListener<ExplosionEvent.Detonate> {
-            // For some reason, even if one of the condition is true, it will NOT be removed from the list
-            /*val affectedEntities = getExplosionAffectedEntities(EntityEnderCrystal::class.java, it.explosion.position, CombatUtils.ExplosionStrength.EndCrystal)
-            placedCrystals.removeIf { crystal -> affectedEntities.contains(crystal.entity) || crystal.entity.isDead }*/
-            placedCrystals.clear()
+            placedCrystals.removeAll { it.entity.isDead }
         }
 
         safeListener<ConnectionEvent.Disconnect> {
