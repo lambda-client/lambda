077e869150ad52f4c7ca577a1e451f386dbd3ebf
Constructor <fractalminds@protonmail.com>
2023-05-05 22:04:24 +0200

    Fix Graffiti logic

--- src/main/kotlin/com/lambda/client/activity/activities/construction/Graffiti.kt
+++ src/main/kotlin/com/lambda/client/activity/activities/construction/Graffiti.kt
@@ -3,28 +3,48 @@ package com.lambda.client.activity.activities.construction
 import com.lambda.client.activity.Activity
 import com.lambda.client.activity.activities.interaction.AttachItemFrame
 import com.lambda.client.activity.activities.interaction.AttachMap
+import com.lambda.client.activity.types.LoopWhileActivity
+import com.lambda.client.activity.types.RepeatingActivity
 import com.lambda.client.event.SafeClientEvent
+import com.lambda.client.module.modules.misc.Graffiti
 import com.lambda.client.util.math.RotationUtils.getRotationTo
 import com.lambda.client.util.math.VectorUtils
+import com.lambda.client.util.math.VectorUtils.distanceTo
+import com.lambda.client.util.world.getNeighbour
 import com.lambda.client.util.world.isReplaceable
 import net.minecraft.entity.item.EntityItemFrame
 import net.minecraft.util.EnumFacing
 
-class Graffiti : Activity() {
+class Graffiti(
+    private val mapID: Int,
+    override val loopWhile: SafeClientEvent.() -> Boolean = { Graffiti.isEnabled },
+    override var currentLoops: Int = 0
+) : LoopWhileActivity, Activity() {
     override fun SafeClientEvent.onInitialize() {
-        VectorUtils.getBlockPosInSphere(player.positionVector, 5.0f)
-            .filter { world.getBlockState(it).isReplaceable }
-            .forEach {
-                addSubActivities(
-                    AttachItemFrame(player.heldItemMainhand, it, player.horizontalFacing.opposite)
-                )
-            }
+        val range = 4.95f
 
+        // 1. Check for missing maps in item frames
         world.loadedEntityList
             .filterIsInstance<EntityItemFrame>()
-            .forEach {
+            .minByOrNull { player.distanceTo(it.positionVector) }
+            ?.let {
+                if (player.distanceTo(it.positionVector) > range) return
+
                 addSubActivities(
-                    AttachMap(it, player.heldItemMainhand)
+                    AttachMap(it, mapID)
+                )
+                return
+            }
+
+        // 2. Check for surface to place item frames on and place it
+        VectorUtils.getBlockPosInSphere(player.positionVector, range)
+            .mapNotNull { getNeighbour(it, 1, range, true, EnumFacing.HORIZONTALS) }
+            .minByOrNull { player.distanceTo(it.hitVec) }
+            ?.let {
+                if (player.distanceTo(it.hitVec) > range) return
+
+                addSubActivities(
+                    AttachItemFrame(it.pos, it.side)
                 )
             }
     }
--- src/main/kotlin/com/lambda/client/activity/activities/interaction/AttachMap.kt
+++ src/main/kotlin/com/lambda/client/activity/activities/interaction/AttachMap.kt
@@ -1,11 +1,13 @@
 package com.lambda.client.activity.activities.interaction
 
 import com.lambda.client.activity.Activity
+import com.lambda.client.activity.activities.inventory.AcquireItemInActiveHand
 import com.lambda.client.activity.activities.inventory.core.SwapOrSwitchToSlot
 import com.lambda.client.activity.types.RotatingActivity
 import com.lambda.client.activity.types.TimeoutActivity
 import com.lambda.client.event.SafeClientEvent
 import com.lambda.client.event.events.PacketEvent
+import com.lambda.client.util.items.HotbarSlot
 import com.lambda.client.util.items.inventorySlots
 import com.lambda.client.util.math.RotationUtils.getRotationToEntity
 import com.lambda.client.util.math.Vec2f
@@ -14,6 +16,7 @@ import com.lambda.client.util.threads.onMainThreadSafe
 import com.lambda.client.util.threads.safeListener
 import kotlinx.coroutines.launch
 import net.minecraft.entity.item.EntityItemFrame
+import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
 import net.minecraft.network.play.client.CPacketUseEntity
 import net.minecraft.network.play.server.SPacketSpawnObject
@@ -21,20 +24,16 @@ import net.minecraft.util.EnumHand
 
 class AttachMap(
     private val itemFrame: EntityItemFrame,
-    private val map: ItemStack,
+    private val mapID: Int,
     override var rotation: Vec2f? = null,
     override var timeout: Long = 1000L
 ) : RotatingActivity, TimeoutActivity, Activity() {
     override fun SafeClientEvent.onInitialize() {
         rotation = getRotationToEntity(itemFrame)
 
-        player.inventorySlots.firstOrNull {
-            ItemStack.areItemStacksEqual(it.stack, map)
-        }?.let {
-            addSubActivities(
-                SwapOrSwitchToSlot(it)
-            )
-        }
+        addSubActivities(
+            AcquireItemInActiveHand(Items.MAP, predicateStack = { it.itemDamage == mapID }),
+        )
     }
 
     init {
