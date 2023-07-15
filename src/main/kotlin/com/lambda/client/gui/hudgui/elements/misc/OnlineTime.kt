30ced5d045f7c43f6f9c76356a6d64c979182a63
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-14 18:11:36 -0700

    onlinetime formatting

--- src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/OnlineTime.kt
+++ src/main/kotlin/com/lambda/client/gui/hudgui/elements/misc/OnlineTime.kt
@@ -1,8 +1,10 @@
 package com.lambda.client.gui.hudgui.elements.misc
 
+import com.lambda.client.commons.utils.grammar
 import com.lambda.client.event.SafeClientEvent
 import com.lambda.client.gui.hudgui.LabelHud
 import com.lambda.client.manager.managers.OnlineTimeManager
+import java.time.Duration
 
 internal object OnlineTime: LabelHud(
     name = "OnlineTime",
@@ -10,9 +12,23 @@ internal object OnlineTime: LabelHud(
     description = "Displays how long you have been online"
 ) {
     override fun SafeClientEvent.updateText() {
-        OnlineTimeManager.getOnlineTime()?.let { onlineTime ->
-            displayText.add("Online:", secondaryColor)
-            displayText.add(onlineTime.toString(), primaryColor)
+        val onlineTime = OnlineTimeManager.getOnlineTime()
+        displayText.add("Online:", secondaryColor)
+        displayText.add(formatDuration(onlineTime), primaryColor)
+    }
+
+    // avoiding kotlin.time.Duration.toString() because it shows down to milliseconds
+    private fun formatDuration(duration: Duration): String {
+        val secondsInMinute = 60L
+        val secondsInHour = secondsInMinute * 60L
+        val seconds = duration.seconds % secondsInMinute
+        val minutes = duration.seconds / secondsInMinute % secondsInMinute
+        val hours = duration.seconds / secondsInHour % 24L
+        return buildString {
+            if (hours > 0) append(grammar(hours.toInt(), "hour", "hours") + ", ")
+            if (minutes > 0) append(grammar(minutes.toInt(), "minute", "minutes") + ", ")
+            append(grammar(seconds.toInt(), "second", "seconds"))
         }
     }
+
 }
