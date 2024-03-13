511a23794af930ea435926ca7d206c631c44a021
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-03-13 18:34:20 -0400

    Invalid KDoc

--- common/src/main/kotlin/com/lambda/util/FolderRegister.kt
+++ common/src/main/kotlin/com/lambda/util/FolderRegister.kt
@@ -9,7 +9,7 @@ import java.io.File
 /**
  * The [FolderRegister] object is responsible for managing the directory structure of the application.
  *
- * @property minecraft The root directory of the Minecraft client. It is retrieved using the `runDirectory` property of the Minecraft client instance.
+ * @property minecraft The root directory of the Minecraft client. It is retrieved using the [Platform.getGameFolder] function of the Architectury API.
  * @property lambda The directory for the Lambda client, located within the Minecraft directory.
  * @property config The directory for storing configuration files, located within the Lambda directory.
  */
