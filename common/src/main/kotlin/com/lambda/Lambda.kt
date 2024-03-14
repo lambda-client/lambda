cdd52c8bc497463a1ed96208c24fc260f009eb7b
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-03-13 22:41:04 -0400

    Build env logger level

--- common/src/main/kotlin/com/lambda/Lambda.kt
+++ common/src/main/kotlin/com/lambda/Lambda.kt
@@ -5,20 +5,23 @@ import com.google.gson.GsonBuilder
 import com.lambda.config.serializer.BlockPosSerializer
 import com.lambda.config.serializer.BlockSerializer
 import com.lambda.config.serializer.ColorSerializer
+import dev.architectury.platform.Platform
 import net.minecraft.block.Block
 import net.minecraft.client.MinecraftClient
 import net.minecraft.util.math.BlockPos
+import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.core.Logger
 import java.awt.Color
+
 
 object Lambda {
     const val MOD_NAME = "Lambda"
     const val MOD_ID = "lambda"
     const val SYMBOL = "λ"
     val VERSION: String = LoaderInfo.getVersion()
-
-    val LOG: Logger = LogManager.getLogger(SYMBOL)
+    val LOG = (LogManager.getLogger(SYMBOL) as Logger)
+        .apply { level = if (Platform.isDevelopmentEnvironment()) Level.DEBUG else Level.INFO }
     val mc: MinecraftClient by lazy { MinecraftClient.getInstance() }
 
     val gson: Gson = GsonBuilder()
@@ -28,7 +31,5 @@ object Lambda {
         .registerTypeAdapter(Block::class.java, BlockSerializer)
         .create()
 
-    fun initialize() {
-        Loader.initialize()
-    }
+    fun initialize() = Loader.initialize()
 }
