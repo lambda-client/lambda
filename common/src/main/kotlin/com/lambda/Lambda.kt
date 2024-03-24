34e800e6d30f9160dd707ec7930fa77911a87d2f
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-03-23 21:13:22 -0400

    Fix: Lambda main

--- common/src/main/kotlin/com/lambda/Lambda.kt
+++ common/src/main/kotlin/com/lambda/Lambda.kt
@@ -2,9 +2,9 @@ package com.lambda
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.lambda.config.serializer.BlockPosSerializer
-import com.lambda.config.serializer.BlockSerializer
-import com.lambda.config.serializer.ColorSerializer
+import com.lambda.config.serializer.*
+import com.lambda.module.tag.ModuleTag
+import com.lambda.util.KeyCode
 import net.minecraft.block.Block
 import net.minecraft.client.MinecraftClient
 import net.minecraft.util.math.BlockPos
@@ -19,10 +19,12 @@ object Lambda {
     const val SYMBOL = "λ"
     val VERSION: String = LoaderInfo.getVersion()
     val LOG: Logger = LogManager.getLogger(SYMBOL)
-    val mc: MinecraftClient by lazy { MinecraftClient.getInstance() }
+    @JvmStatic val mc: MinecraftClient by lazy { MinecraftClient.getInstance() }
 
     val gson: Gson = GsonBuilder()
         .setPrettyPrinting()
+        .registerTypeAdapter(ModuleTag::class.java, ModuleTagSerializer)
+        .registerTypeAdapter(KeyCode::class.java, KeyCodeSerializer)
         .registerTypeAdapter(Color::class.java, ColorSerializer)
         .registerTypeAdapter(BlockPos::class.java, BlockPosSerializer)
         .registerTypeAdapter(Block::class.java, BlockSerializer)
