0f110639771b2244480bc274ac8b6dcb70109ee5
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-03-27 18:53:20 -0400

    Feature: Discord RPC

--- common/src/main/kotlin/com/lambda/module/modules/client/DiscordRPC.kt
+++ common/src/main/kotlin/com/lambda/module/modules/client/DiscordRPC.kt
@@ -9,10 +9,16 @@ import com.lambda.module.tag.ModuleTag
 import com.lambda.threading.onShutdown
 import com.lambda.threading.runConcurrent
 import com.lambda.util.Communication.info
+import com.lambda.util.Communication.toast
 import com.lambda.util.Nameable
 import com.lambda.util.StringUtils.capitalize
+import com.lambda.util.text.ClickEvents
+import com.lambda.util.text.buildText
+import com.lambda.util.text.clickEvent
+import com.lambda.util.text.literal
 import dev.cbyrne.kdiscordipc.KDiscordIPC
 import dev.cbyrne.kdiscordipc.core.event.DiscordEvent
+import dev.cbyrne.kdiscordipc.core.event.data.ActivityInviteEventData
 import dev.cbyrne.kdiscordipc.core.event.impl.ActivityInviteEvent
 import dev.cbyrne.kdiscordipc.core.event.impl.ActivityJoinEvent
 import dev.cbyrne.kdiscordipc.core.event.impl.ErrorEvent
@@ -28,29 +34,31 @@ object DiscordRPC : Module(
     name = "DiscordRPC",
     description = "Discord Rich Presence configuration",
     defaultTags = setOf(ModuleTag.CLIENT),
-//    enabledByDefault = true, // ToDo: Enable before release
+    //enabledByDefault = true, // ToDo: Enable before release
 ) {
-//    private val playDetails by setting("Details", "Playing on Lambda")
-//    private val playState by setting("Play State", "Playing")
 
-//    private var joinSecret by setting("Join Secret", UUID.randomUUID().toString())
-//    private var partyId by setting("Party ID", UUID.randomUUID().toString())
-//    private var partySize by setting("Party Size", 1, 1..16, 1)
-//    private var partyMax by setting("Party Max Size", 16, 2..16, 1)
+    private var joinSecret by setting("Join Secret", UUID.randomUUID().toString()) // TODO: Server to generate this
+    private var partyId by setting("Party ID", UUID.randomUUID().toString())
+    private var partySize by setting("Party Size", 1, 1..16, 1)
+    private var partyMax by setting("Party Max Size", 16, 2..16, 1)
 
     private val confirmCoordinates by setting("Show Coordinates", false)
-    private val confirmServer by setting("Show Server", false)
+    private val confirmServer by setting("Expose server", false, description = "Allow to show what server you are on and allow to join parties.")
+    private val enableParty by setting("Enable Party", true, description = "Will allow you to create and join parties but cannot have buttons.")
+    private val showTime by setting("Show Time", true, description = "Show how long you have been playing for.")
 
     private val line1Left by setting("Line 1 Left", LineInfo.WORLD)
     private val line1Right by setting("Line 1 Right", LineInfo.USERNAME)
     private val line2Left by setting("Line 2 Left", LineInfo.DIMENSION)
     private val line2Right by setting("Line 2 Right", LineInfo.FPS)
+
     // Max update delay is 5 per 20 seconds -> every 4 seconds
     private val delay by setting("Update Delay", 4, 4..60, 1, unit = "s")
 
     private val rpc = KDiscordIPC(Lambda.APP_ID, scope = ioScope)
     private val startup = System.currentTimeMillis()
-//    private var lastInviter: User? = null
+    private var lastInviter: User? = null
+    private var lastInvite: ActivityInviteEventData? = null
 
     private enum class LineInfo(val value: () -> String) : Nameable {
         VERSION({ Lambda.VERSION }),
@@ -118,11 +126,15 @@ object DiscordRPC : Module(
                 largeImage("lambda", Lambda.VERSION)
                 smallImage("https://mc-heads.net/avatar/${mc.gameProfile.id}/nohelm", mc.gameProfile.name)
 
-//                party(partyId, partySize, partyMax)
-//                secrets(joinSecret)
-                button("Download", "https://modrinth.com/") // ToDo: Add real link
+                if (enableParty && false) { // TODO: Implement parties
+                    // TODO: Only enable if the player is in a server
+                    party(partyId, partySize, partyMax)
+                    secrets(joinSecret)
+                } else {
+                    button("Download", "https://modrinth.com/") // ToDo: Add real link
+                }
 
-                timestamps(startup)
+                if (showTime) timestamps(startup)
             }
         }
     }
@@ -131,27 +143,34 @@ object DiscordRPC : Module(
         on<ReadyEvent> {
             Lambda.LOG.info("Discord RPC connected to ${data.user.username}.")
 
-//            subscribe(DiscordEvent.ActivityJoinRequest)
-//            subscribe(DiscordEvent.ActivityJoin)
-//            subscribe(DiscordEvent.ActivityInvite)
+            subscribe(DiscordEvent.ActivityJoinRequest)
+            subscribe(DiscordEvent.ActivityJoin)
+            subscribe(DiscordEvent.ActivityInvite)
         }
 
-//        on<ActivityInviteEvent> {
-//            lastInviter = data.user
-//            info("${lastInviter?.username} has invited you to play")
-//            activityManager.acceptInvite(data) // TODO: Click button to join
-//        }
-//
-//        on<ActivityJoinEvent> {
-//            joinSecret = data.secret
-//            info("Joined ${lastInviter?.username}'s party.") // TODO: Join server button
-//        }
-//
-//        on<SetActivityPacket> {
-//            partyId = data?.party?.id ?: partyId
-//            partySize = data?.party?.size?.currentSize ?: partySize
-//            partyMax = data?.party?.size?.maxSize ?: partyMax
-//        }
+        on<ActivityInviteEvent> {
+            lastInviter = data.user
+
+            info(buildText {
+                clickEvent(ClickEvents.runCommand(";rpc accept")) {
+                    literal("Click to join ${data.user.username}'s party.")
+                }
+            }) // TODO: Possibility to block users
+
+            toast("You have been invited to play by ${lastInviter?.username}") // TODO: Custom toast ?
+            lastInvite = data
+        }
+
+        on<ActivityJoinEvent> {
+            joinSecret = data.secret
+            info("Joined ${lastInviter?.username}'s party.")
+        }
+
+        on<SetActivityPacket> {
+            partyId = data?.party?.id ?: partyId
+            partySize = data?.party?.size?.currentSize ?: partySize
+            partyMax = data?.party?.size?.maxSize ?: partyMax
+        }
 
         on<ErrorEvent> {
             Lambda.LOG.error("Discord RPC error: ${data.message}")
