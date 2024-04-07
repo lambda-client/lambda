1f6e1086d675fe65f47ce5ccd77a9ffcd8e3135a
Blade-gl <bladecore.kt@gmail.com>
2024-04-07 22:14:16 +0300

    Rotation Smoothness (LOCK Mode)

--- common/src/main/java/com/lambda/mixin/render/CameraMixin.java
+++ common/src/main/java/com/lambda/mixin/render/CameraMixin.java
@@ -1,16 +1,21 @@
 package com.lambda.mixin.render;
 
+import com.lambda.interaction.RotationManager;
 import com.lambda.module.modules.player.Freecam;
 import net.minecraft.client.render.Camera;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Camera.class)
-public class CameraMixin {
+public abstract class CameraMixin {
+    @Shadow
+    public abstract void setRotation(float yaw, float pitch);
+
     @Inject(method = "update", at = @At("TAIL"))
     private void onUpdate(
             BlockView area,
@@ -23,5 +28,12 @@ public class CameraMixin {
         if (!Freecam.INSTANCE.isEnabled()) return;
 
         Freecam.updateCam();
+    }
+
+    @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;setPos(DDD)V", shift = At.Shift.AFTER))
+    private void injectQuickPerspectiveSwap(BlockView area, Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo ci) {
+        var rot = RotationManager.getLockRotation();
+        if (rot == null) return;
+        setRotation(rot.getYawF(), rot.getPitchF());
     }
 }
