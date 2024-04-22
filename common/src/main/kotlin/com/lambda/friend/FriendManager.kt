5674bb398ab7832b7e11ed9225c325fee3fbff10
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-04-21 20:27:55 -0400

    Fix: Friends not saving

--- common/src/main/kotlin/com/lambda/friend/FriendManager.kt
+++ common/src/main/kotlin/com/lambda/friend/FriendManager.kt
@@ -3,26 +3,18 @@ package com.lambda.friend
 import com.lambda.config.Configurable
 import com.lambda.config.configurations.FriendConfig
 import com.lambda.core.Loadable
-import com.lambda.event.events.TickEvent
-import com.lambda.event.listener.SafeListener.Companion.listener
-import com.lambda.util.world.EntityUtils.getEntities
 import com.mojang.authlib.GameProfile
-import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.server.network.ServerPlayerEntity
-import net.minecraft.world.event.listener.EntityGameEventHandler
-import java.util.Arrays
 import java.util.UUID
 
 object FriendManager : Configurable(FriendConfig), Loadable {
     override val name = "FriendManager"
 
-    private val friends by setting("friends", arrayListOf<GameProfile>())
+    private var friends by setting("friends", listOf<GameProfile>())
 
-    fun add(vararg profile: GameProfile) =
-        friends.addAll(profile)
+    fun add(profile: GameProfile) = friends.add(profile)
 
-    fun remove(vararg profile: GameProfile) =
-        friends.removeAll(profile.toSet())
+    fun remove(profile: GameProfile) = friends.remove(profile)
 
     fun get(name: String) = friends.firstOrNull { it.name == name }
     fun get(uuid: UUID) = friends.firstOrNull { it.id == uuid }
@@ -35,6 +27,9 @@ object FriendManager : Configurable(FriendConfig), Loadable {
 
     val ServerPlayerEntity.isFriend: Boolean
         get() = contains(gameProfile)
+
+    fun ServerPlayerEntity.befriend() = add(gameProfile)
+    fun ServerPlayerEntity.unfriend() = remove(gameProfile)
 
     override fun load(): String {
         if (friends.isEmpty()) return "No friends loaded, damn bro you don't have to be antisocial online too,"
