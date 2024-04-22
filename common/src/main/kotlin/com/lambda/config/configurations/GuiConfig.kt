8d50d9615bdcef2b9f21112874a6c64c3753d843
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-04-21 20:37:24 -0400

    Make GuiConfig loadable

--- common/src/main/kotlin/com/lambda/config/configurations/GuiConfig.kt
+++ common/src/main/kotlin/com/lambda/config/configurations/GuiConfig.kt
@@ -1,9 +1,10 @@
 package com.lambda.config.configurations
 
 import com.lambda.config.Configuration
+import com.lambda.core.Loadable
 import com.lambda.util.FolderRegister
 
-object GuiConfig : Configuration() {
+object GuiConfig : Configuration(), Loadable {
     override val configName = "gui"
     override val primary = FolderRegister.config.resolve("$configName.json")
 }
