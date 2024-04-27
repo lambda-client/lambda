f8a89bf1f76ccec0b56396e57cfc95be6abc19bf
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-04-27 15:58:58 -0400

    Refactor: Faster rocket filter

--- common/src/main/kotlin/com/lambda/module/modules/movement/RocketExtend.kt
+++ common/src/main/kotlin/com/lambda/module/modules/movement/RocketExtend.kt
@@ -1,10 +1,12 @@
 package com.lambda.module.modules.movement
 
+import com.lambda.context.SafeContext
 import com.lambda.event.events.PacketEvent
 import com.lambda.event.listener.SafeListener.Companion.listener
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
 import com.lambda.threading.runSafe
+import com.lambda.util.collections.filterPointer
 import net.minecraft.entity.projectile.FireworkRocketEntity
 import net.minecraft.network.packet.c2s.common.CommonPongC2SPacket
 import net.minecraft.network.packet.s2c.play.EntitiesDestroyS2CPacket
@@ -25,24 +27,24 @@ object RocketExtend : Module(
             if (event.packet is PlayerPositionLookS2CPacket) reset()
 
             if (event.packet is EntitiesDestroyS2CPacket) {
-                val rockets = event.packet.entityIds.map(world::getEntityById)
-                    .filter { it is FireworkRocketEntity && it.shooter == player }
-                    .mapNotNull { it as? FireworkRocketEntity }
-                    .also { event.packet.entityIds.removeAll(it.map(FireworkRocketEntity::getId).toSet()) }
-                extendedRockets.addAll(rockets)
+                event.packet.entityIds.map(world::getEntityById)
+                    .filterPointer(extendedRockets, { event.packet.entityIds.removeInt(it.id) }) { rocket -> rocket.shooter == player }
             }
         }
 
         listener<PacketEvent.Send.Pre> { event ->
             if (event.packet !is CommonPongC2SPacket) return@listener
+
             if (extendedRockets.isEmpty()) {
                 lastPingTime = System.currentTimeMillis()
                 return@listener
             }
+
             if (System.currentTimeMillis() - lastPingTime > keepAliveTime * 1000) {
                 reset()
                 return@listener
             }
+
             pingPacket = event.packet
             event.cancel()
         }
@@ -52,7 +54,7 @@ object RocketExtend : Module(
         }
     }
 
-    private fun reset() = runSafe {
+    private fun SafeContext.reset() {
         extendedRockets.forEach(FireworkRocketEntity::discard)
         extendedRockets.clear()
         pingPacket?.let(connection::sendPacket)
