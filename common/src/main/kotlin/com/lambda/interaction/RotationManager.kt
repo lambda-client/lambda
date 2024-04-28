68527c310a4101bd97d668f384e630d441bbba60
Blade-gl <bladecore.kt@gmail.com>
2024-04-28 13:27:19 +0300

    Useless fix

--- common/src/main/kotlin/com/lambda/interaction/RotationManager.kt
+++ common/src/main/kotlin/com/lambda/interaction/RotationManager.kt
@@ -12,7 +12,6 @@ import com.lambda.event.listener.UnsafeListener.Companion.unsafeListener
 import com.lambda.interaction.rotation.Rotation
 import com.lambda.interaction.rotation.Rotation.Companion.angleDifference
 import com.lambda.interaction.rotation.Rotation.Companion.slerp
-import com.lambda.interaction.rotation.Rotation.Companion.lerp
 import com.lambda.interaction.rotation.RotationContext
 import com.lambda.interaction.rotation.RotationMode
 import com.lambda.module.modules.client.Baritone
@@ -54,15 +53,6 @@ object RotationManager : Loadable {
             runOnGameThread {
                 reset(Rotation(packet.yaw, packet.pitch))
             }
-        }
-
-        listener<RenderEvent.UpdateTarget> {
-            val context = currentContext ?: return@listener
-            if (context.config.rotationMode != RotationMode.LOCK) return@listener
-            val interpolation = prevRotation.lerp(currentRotation, mc.tickDelta.toDouble())
-
-            player.yaw = interpolation.yawF
-            player.pitch = interpolation.pitchF
         }
 
         unsafeListener<ConnectionEvent.Disconnect> {
