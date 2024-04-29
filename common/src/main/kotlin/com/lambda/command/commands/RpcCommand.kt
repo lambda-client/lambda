776f3c604ea613e76837f61e7a90280964770247
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-04-29 19:55:52 -0400

    Fixed rpc command builder

--- common/src/main/kotlin/com/lambda/command/commands/RpcCommand.kt
+++ common/src/main/kotlin/com/lambda/command/commands/RpcCommand.kt
@@ -1,12 +1,10 @@
 package com.lambda.command.commands
 
-import com.lambda.brigadier.CommandResult
+import com.lambda.brigadier.*
+import com.lambda.brigadier.argument.literal
 import com.lambda.brigadier.argument.string
 import com.lambda.brigadier.argument.value
 import com.lambda.brigadier.argument.word
-import com.lambda.brigadier.execute
-import com.lambda.brigadier.executeWithResult
-import com.lambda.brigadier.required
 import com.lambda.command.CommandManager.register
 import com.lambda.command.LambdaCommand
 import com.lambda.module.modules.client.DiscordRPC
@@ -14,30 +12,19 @@ import com.lambda.module.modules.client.DiscordRPC
 object RpcCommand : LambdaCommand {
     override val name = "rpc"
 
-    // TODO: What the fuck am I even doing here ??
     init {
         register(name) {
-            required(word("action")) { action ->
-                val actions = listOf("join", "accept")
-
-                suggests { _, builder ->
-                    actions.forEach {
-                        builder.suggest(it)
+            required(literal("join")) {
+                required(word("id")) { id ->
+                    execute {
+                        DiscordRPC.join(id().value())
                     }
-                    builder.buildFuture()
                 }
+            }
 
-                executeWithResult {
-                    val action = action().value()
-                    if (action !in actions) {
-                        return@executeWithResult CommandResult.failure("Invalid action $action. Did you mean ${actions.joinToString()}?")
-                    }
-
-                    when (action) {
-                        //"join" -> DiscordRPC.join()
-                        "accept" -> DiscordRPC.accept()
-                    }
-                    CommandResult.success()
+            required(literal("accept")) {
+                execute {
+                    DiscordRPC.accept()
                 }
             }
         }
