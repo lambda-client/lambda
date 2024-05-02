81b4bde4b87466e219dd521c430d54223bd5b98c
Blade-gl <bladecore.kt@gmail.com>
2024-05-02 13:52:08 +0300

    oopsie

--- common/src/main/kotlin/com/lambda/gui/impl/clickgui/windows/ModuleWindow.kt
+++ common/src/main/kotlin/com/lambda/gui/impl/clickgui/windows/ModuleWindow.kt
@@ -2,10 +2,8 @@ package com.lambda.gui.impl.clickgui.windows
 
 import com.lambda.gui.api.GuiEvent
 import com.lambda.gui.api.component.ListWindow
-import com.lambda.gui.api.component.WindowComponent
 import com.lambda.gui.impl.clickgui.AbstractClickGui
 import com.lambda.gui.impl.clickgui.buttons.ModuleButton
-import com.lambda.module.modules.client.ClickGui
 
 abstract class ModuleWindow(
     override var title: String,
@@ -15,12 +13,8 @@ abstract class ModuleWindow(
 ) : ListWindow<ModuleButton>(owner) {
     override fun onEvent(e: GuiEvent) {
         if (e is GuiEvent.Tick) {
-            contentComponents.children.apply {
-                sortBy {
-                    it.module.name
-                }
-
-
+            contentComponents.children.sortBy {
+                it.module.name
             }
         }
 
