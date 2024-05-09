36353e043ffc63077eb1ef131daa5a3b5aa06ee2
Constructor <fractalminds@protonmail.com>
2024-05-09 05:41:35 +0200

    SoundUtil and alias

---
+++ common/src/main/kotlin/com/lambda/util/primitives/extension/Aliases.kt
@@ -0,0 +1,6 @@
+package com.lambda.util.primitives.extension
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import net.minecraft.command.CommandSource
+
+typealias CommandBuilder = LiteralArgumentBuilder<CommandSource>
