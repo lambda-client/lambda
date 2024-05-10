8b60754fa492dc07bb95626b76875b73d1265dff
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-05-10 19:13:13 -0400

    Moved friend manager to core

--- common/src/main/kotlin/com/lambda/core/Loader.kt
+++ common/src/main/kotlin/com/lambda/core/Loader.kt
@@ -3,17 +3,12 @@ package com.lambda.core
 import com.lambda.Lambda
 import com.lambda.Lambda.LOG
 import com.lambda.command.CommandManager
-import com.lambda.config.configurations.GuiConfig
-import com.lambda.friend.FriendManager
 import com.lambda.graphics.renderer.gui.font.LambdaFont
 import com.lambda.gui.impl.clickgui.GuiConfigurable
-import com.lambda.gui.impl.clickgui.LambdaClickGui
 import com.lambda.interaction.PlayerPacketManager
 import com.lambda.interaction.RotationManager
 import com.lambda.module.ModuleRegistry
 import com.lambda.util.Communication.ascii
-import com.mojang.authlib.GameProfile
-import java.util.*
 import kotlin.system.measureTimeMillis
 
 object Loader {
