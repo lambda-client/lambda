f8034ed56a15eeff708178965a077600be377d75
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-05-17 20:24:51 -0400

    Fix: Better plugin loader

--- common/src/main/kotlin/com/lambda/plugin/PluginRegistry.kt
+++ common/src/main/kotlin/com/lambda/plugin/PluginRegistry.kt
@@ -2,89 +2,56 @@ package com.lambda.plugin
 
 import com.lambda.Lambda.LOG
 import com.lambda.core.Loadable
+import com.lambda.plugin.api.Plugin
 import com.lambda.util.FolderRegister.listRecursive
 import com.lambda.util.FolderRegister.mods
+import org.reflections.Reflections
+import org.reflections.scanners.Scanners
+import org.reflections.util.ConfigurationBuilder
 import java.io.File
+import java.lang.reflect.Method
+import java.net.URI
+import java.net.URL
+import java.net.URLClassLoader
 import java.util.jar.JarFile
 
 object PluginRegistry : Loadable {
-    private val classLoaderError = """
-                An error occurred while retrieving the thread class loader.
-                This likely mean that newer versions of the mod loader
-                have changed the way mods are loaded, affecting the plugin system.
-                Please report this error to the Lambda developers with the following information:
-                - The version of Lambda
-                - The version of the mod loader (ex. Fabric, Forge, etc.)
-                - The version of Minecraft you are using
-                - The version of Java you are using
-                
-                Plugin: %s
-                Stacktrace:
-                %s
-                """.trimIndent()
+    val plugins = mutableListOf<Plugin>()
 
     private val loadingError = """
                 An error occurred while loading a plugin.
-                If you are a developer, please check the plugin's main class and load method.
+                If you are a developer, please check the plugin's main class, instance or load method.
                 If you are a regular user, please report this issue to the plugin developer.
                 
                 Plugin: %s
                 Error: %s
-                Stacktrace:
-                %s
                 """.trimIndent()
 
-    private fun loadPlugin(file: File) {
-        runCatching {
-            val jar = JarFile(file)
-
-            val mainClass = jar.manifest.mainAttributes.getValue("Main-Class")
-                ?: return LOG.error("The plugin $jar does not have a main class")
-
-            val loader = PluginLoader(jar,
-                Thread.currentThread().contextClassLoader ?:
-                        return classLoaderError.format(
-                            file,
-                            Thread.currentThread().stackTrace.joinToString("\n")
-                        )
-                            .split("\n")
-                            .forEach(LOG::error))
-
-            val loadClass = loader.loadClass(mainClass)
-
-            val loadInstance =
-                loadClass.declaredFields.firstOrNull { it.name == "INSTANCE" }?.get(null)
-                    ?: loadClass.constructors.firstOrNull()?.newInstance()
-                        ?: return LOG.error("The plugin $jar does not have an object instance or a public constructor")
-            val loadMethod =
-                loadClass?.methods?.find { it.name == "load" }
-                    ?: return LOG.warn("The plugin $jar does not have a load method")
-
-            loadMethod.invoke(loadInstance)
-        }.onFailure {
-            loadingError.format(
-                file,
-                it.message,
-                it.stackTraceToString()
-            )
-                .split("\n")
-                .forEach(LOG::error)
-        }
-    }
-
     override fun load(): String {
-        val plugins = mods
-            .listRecursive {
-                it.isFile && it.extension == "jar"
-                        // Not a fan of creating a new JarFile instance every iteration
-                        && JarFile(it).manifest.mainAttributes.getValue("Lambda-Plugin") == "true"
-            }
-        val size = plugins.count()
+        Reflections(
+            ConfigurationBuilder()
+                .addUrls(
+                    mods.listRecursive { it.extension == "jar" }
+                        .map { it.toURI().toURL() }
+                        .toList()
+                )
+                .addScanners(Scanners.SubTypes)
+        ).getSubTypesOf(Plugin::class.java).forEach { pluginClass ->
+            val instance = (pluginClass.declaredFields.find { it.name == "INSTANCE" }?.get(null)
+                ?: pluginClass.constructors.firstOrNull()?.newInstance()) as? Plugin
+                ?: null.also {
+                    loadingError.format(
+                        pluginClass,
+                        "The plugin does not have an object instance or a public constructor"
+                    ).also { LOG.warn(it) }
+                }
 
-        plugins.forEach(::loadPlugin)
+            if (instance == null) return@forEach
 
-        val plural = if (size == 1) "" else "s"
+            instance.load()
+            plugins.add(instance)
+        }
 
-        return "Loaded $size plugin$plural"
+        return "Registered ${plugins.size} plugins"
     }
 }
