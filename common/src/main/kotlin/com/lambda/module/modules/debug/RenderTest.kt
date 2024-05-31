6d65b572731d51bb45f3e57faeaf214e991419ab
Constructor <fractalminds@protonmail.com>
2024-05-31 19:31:06 +0200

    Comment out old ESP test

--- common/src/main/kotlin/com/lambda/module/modules/debug/RenderTest.kt
+++ common/src/main/kotlin/com/lambda/module/modules/debug/RenderTest.kt
@@ -15,31 +15,31 @@ object RenderTest : Module(
     private val test31 by setting("Holla huh 1", true, visibility = { !test1 })
     private val test32 by setting("Holla buh 2", true, visibility = { !test1 })
 
-    private val filled = DynamicFilledRenderer()
-    private val outline = DynamicOutlineRenderer()
-    private val color = Color(60, 200, 60)
-
-    init {
-        filled.build {
-            val flag = mc.crosshairTarget?.blockResult?.blockPos?.let { box = Box(it) } != null
-            color = color.setAlpha((color.a + flag.toIntSign() * 0.05).coerceAtMost(0.2))
-        }
-
-        outline.build {
-            val block = mc.crosshairTarget?.blockResult
-            val flag = block?.blockPos?.let { box = Box(it) } != null
-            color = color.setAlpha(color.a + flag.toIntSign() * 0.2)
-            sides = block?.side?.mask ?: sides
-        }
-
-        listener<TickEvent.Pre> {
-            filled.update()
-            outline.update()
-        }
-
-        listener<RenderEvent.World> {
-            filled.render()
-            outline.render()
-        }
-    }
+//    private val filled = DynamicFilledRenderer()
+//    private val outline = DynamicOutlineRenderer()
+//    private val color = Color(60, 200, 60)
+//
+//    init {
+//        filled.build {
+//            val flag = mc.crosshairTarget?.blockResult?.blockPos?.let { box = Box(it) } != null
+//            color = color.setAlpha((color.a + flag.toIntSign() * 0.05).coerceAtMost(0.2))
+//        }
+//
+//        outline.build {
+//            val block = mc.crosshairTarget?.blockResult
+//            val flag = block?.blockPos?.let { box = Box(it) } != null
+//            color = color.setAlpha(color.a + flag.toIntSign() * 0.2)
+//            sides = block?.side?.mask ?: sides
+//        }
+//
+//        listener<TickEvent.Pre> {
+//            filled.update()
+//            outline.update()
+//        }
+//
+//        listener<RenderEvent.World> {
+//            filled.render()
+//            outline.render()
+//        }
+//    }
 }
