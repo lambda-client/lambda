2cd6e426a26b34e40f11969c1ad11e7a9d8608db
Constructor <fractalminds@protonmail.com>
2024-05-31 17:57:32 +0200

    Remove TridentFlight (sry Kamigen)

--- common/src/main/java/com/lambda/mixin/items/TridentMixin.java
+++ common/src/main/java/com/lambda/mixin/items/TridentMixin.java
@@ -11,14 +11,27 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public class TridentMixin {
     // Forge doesn't support the @ModityArgs annotation, so we have to chain multiple @ModifyArg
     @ModifyArg(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addVelocity(DDD)V"), index = 0)
-    private double modifyVelocity0(double velocity) { return TridentBoost.INSTANCE.isEnabled() ? velocity * TridentBoost.INSTANCE.getTridentSpeed() : velocity; }
+    private double modifyVelocity0(double velocity) {
+        return TridentBoost.INSTANCE.isEnabled() ? velocity * TridentBoost.getTridentSpeed() : velocity;
+    }
 
     @ModifyArg(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addVelocity(DDD)V"), index = 1)
-    private double modifyVelocity1(double velocity) { return TridentBoost.INSTANCE.isEnabled() ? velocity * TridentBoost.INSTANCE.getTridentSpeed() : velocity; }
+    private double modifyVelocity1(double velocity) {
+        return TridentBoost.INSTANCE.isEnabled() ? velocity * TridentBoost.getTridentSpeed() : velocity;
+    }
 
     @ModifyArg(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addVelocity(DDD)V"), index = 2)
-    private double modifyVelocity2(double velocity) { return TridentBoost.INSTANCE.isEnabled() ? velocity * TridentBoost.INSTANCE.getTridentSpeed() : velocity; }
+    private double modifyVelocity2(double velocity) {
+        return TridentBoost.INSTANCE.isEnabled() ? velocity * TridentBoost.getTridentSpeed() : velocity;
+    }
 
     @ModifyExpressionValue(method = {"onStoppedUsing", "use"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
-    private boolean modifyIsTouchingWaterOrRain(boolean original) { return TridentBoost.INSTANCE.isEnabled() ? TridentBoost.INSTANCE.getForceUse() : original; }
+    private boolean modifyIsTouchingWaterOrRain(boolean original) {
+        return TridentBoost.INSTANCE.isEnabled() && TridentBoost.getForceUse() || original;
+    }
+
+    @ModifyArg(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;useRiptide(I)V"))
+    private int modifyRiptide(int riptide) {
+        return TridentBoost.INSTANCE.isEnabled() ? TridentBoost.getDelay() : riptide;
+    }
 }
