bd00fb4414279c6aecdab84d71b68bfd967218a1
Constructor <fractalminds@protonmail.com>
2024-06-07 03:12:08 +0200

    Nuker floor filling

--- common/src/main/kotlin/com/lambda/module/modules/player/Nuker.kt
+++ common/src/main/kotlin/com/lambda/module/modules/player/Nuker.kt
@@ -1,55 +1,51 @@
 package com.lambda.module.modules.player
 
-import com.lambda.interaction.construction.DynamicBlueprint
 import com.lambda.interaction.construction.DynamicBlueprint.Companion.blueprintOnTick
 import com.lambda.interaction.construction.verify.TargetState
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
-import com.lambda.task.Task
 import com.lambda.task.Task.Companion.emptyTask
 import com.lambda.task.tasks.BuildStructure.Companion.buildStructure
 import com.lambda.util.BlockUtils.blockPos
 import com.lambda.util.BlockUtils.blockState
-import com.lambda.util.BlockUtils.instantBreakable
-import com.lambda.util.KeyCode
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.Vec3i
 
 object Nuker : Module(
     name = "Nuker",
     description = "Breaks blocks around you",
     defaultTags = setOf(ModuleTag.PLAYER, ModuleTag.AUTOMATION)
 ) {
+    private val height by setting("Height", 4, 1..8, 1)
+    private val width by setting("Width", 4, 1..8, 1)
     private val flatten by setting("Flatten", true)
     private val onlyBreakInstant by setting("Only Break Instant", true)
     private val fillFloor by setting("Fill Floor", false)
 
-    private val range = Vec3i(4, 4, 4) // TODO: Customizable
-    private var task: Task<*> = emptyTask()
+    private var task = emptyTask()
 
     init {
         onEnable {
             task = buildStructure(
                 pathing = false,
-                finishOnDone = false
+                finishOnDone = false,
+                cancelOnUnsolvable = false
             ) {
                 blueprintOnTick { _ ->
-                    val selection = BlockPos.iterateOutwards(player.blockPos, range.x, range.y, range.z)
+                    val selection = BlockPos.iterateOutwards(player.blockPos, width, height, width)
                         .asSequence()
                         .map { it.blockPos }
                         .filter { !world.isAir(it) }
                         .filter { !flatten || it.y >= player.blockPos.y }
-                        .filter { !onlyBreakInstant || instantBreakable(it.blockState(world), it) }
+                        .filter { !onlyBreakInstant || it.blockState(world).getHardness(world, it) <= 1 }
+                        .filter { it.blockState(world).getHardness(world, it) >= 0 }
                         .associateWith { TargetState.Air }
 
-//                    if (fillFloor) {
-//                        // ToDo: Use smarter iteration pattern
-//                        val floor = BlockPos.iterateOutwards(player.blockPos, range.x, range.y, range.z)
-//                            .filter { it.y == player.blockPos.down().y }
-//                            .map { it.blockPos }
-//                            .associateWith { TargetState.Solid }
-//                        return@DynamicBlueprint selection + floor
-//                    }
+                    if (fillFloor) {
+                        val floor = BlockPos.iterateOutwards(player.blockPos.down(), width, 0, width)
+                            .map { it.blockPos }
+                            .associateWith { TargetState.Solid }
+                        return@blueprintOnTick selection + floor
+                    }
 
                     selection
                 }
