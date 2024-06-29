1b58a94f0447231e5b05968d8fb05894b9636b9a
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-06-28 21:24:20 -0400

    Bumped java version to 22

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ allprojects {
     java {
         withSourcesJar()
 
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
     }
 }
