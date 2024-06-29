41e9a7e41abbffebfec5d402527f762973d8bac0
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-06-28 21:20:29 -0400

    Bumped kff to 5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ allprojects {
     java {
         withSourcesJar()
 
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
