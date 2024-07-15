5d735a4a4ad525b995be44d1f44805dddd2d5540
beanbag44 <pidgeonmaster64@gmail.com>
2024-07-15 21:40:37 +0100

    - forgor to remove unused import

--- common/src/main/kotlin/com/lambda/module/modules/player/PacketMine.kt
+++ common/src/main/kotlin/com/lambda/module/modules/player/PacketMine.kt
@@ -29,7 +29,6 @@ import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket
 import net.minecraft.registry.tag.FluidTags
 import net.minecraft.screen.slot.SlotActionType
 import net.minecraft.state.property.Properties
-import net.minecraft.text.Text
 import net.minecraft.util.Hand
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Box
