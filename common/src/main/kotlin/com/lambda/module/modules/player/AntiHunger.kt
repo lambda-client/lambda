196170ab1414d02b870f19aa0df2b6e9ee6d6c12
Blade-gl <bladecore.kt@gmail.com>
2024-07-22 22:12:53 +0300

    Using player packet manager instead

--- common/src/main/kotlin/com/lambda/module/modules/player/AntiHunger.kt
+++ common/src/main/kotlin/com/lambda/module/modules/player/AntiHunger.kt
@@ -1,11 +1,9 @@
 package com.lambda.module.modules.player
 
-import com.lambda.event.events.PacketEvent
+import com.lambda.event.events.PlayerPacketEvent
 import com.lambda.event.listener.SafeListener.Companion.listener
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
-import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
 
 object AntiHunger : Module(
     name = "AntiHunger",
@@ -15,20 +13,9 @@ object AntiHunger : Module(
     private val cancelMovementState by setting("Cancel Movement State", true)
     
     init {
-        listener<PacketEvent.Send.Pre> {
-            when (it.packet) {
-                is ClientCommandC2SPacket -> {
-                    if (cancelMovementState &&
-                        (it.packet.mode == ClientCommandC2SPacket.Mode.START_SPRINTING ||
-                                it.packet.mode == ClientCommandC2SPacket.Mode.STOP_SPRINTING)) {
-                        it.cancel()
-                    }
-                }
-                
-                is PlayerMoveC2SPacket -> {
-                    it.packet.onGround = (player.fallDistance <= 0 || interaction.isBreakingBlock) && player.isFallFlying
-                }
-            }
+        listener<PlayerPacketEvent.Pre> { event ->
+            if (cancelMovementState) event.isSprinting = false
+            event.onGround = (player.fallDistance <= 0 || interaction.isBreakingBlock) && player.isFallFlying
         }
     }
 }
