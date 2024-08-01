f70bb3a8ad487a20bf97260aaa71d49a5b4db528
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-08-01 11:23:43 -0400

    Removed slf4j nop

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -35,7 +35,7 @@ loom {
 repositories {
     // You can add more repositories here if you plan
     // on using environment-specific dependencies.
-    // If you simply want to add a global repository,
+    // If you simply want to add a global plugin repository,
     // you can add it to the `settings.gradle.kts` file
     // in the base of the project and gradle will do the
     // rest for you.
@@ -89,9 +89,6 @@ dependencies {
     // MixinExtras
     implementation("io.github.llamalad7:mixinextras-forge:$mixinExtrasVersion")
     compileOnly(annotationProcessor("io.github.llamalad7:mixinextras-common:$mixinExtrasVersion")!!)
-
-    // Disable reflections logging
-    include("org.slf4j:slf4j-nop:2.0.13")
 
     // Common (Do not touch)
     common(project(":common", configuration = "namedElements")) { isTransitive = false }
