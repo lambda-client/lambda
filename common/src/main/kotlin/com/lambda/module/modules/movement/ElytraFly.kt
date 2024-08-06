0aed9ba6145a4aa3a3d8edc69b8a9a6ac551f692
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-06 14:34:33 -0400

    refactor: module style

--- common/src/main/kotlin/com/lambda/module/modules/movement/ElytraFly.kt
+++ common/src/main/kotlin/com/lambda/module/modules/movement/ElytraFly.kt
@@ -1,24 +1,40 @@
-package com.lambda.module.modules.movement;
+package com.lambda.module.modules.movement
+
 import com.lambda.event.events.MovementEvent
+import com.lambda.event.listener.SafeListener.Companion.listener
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
-import com.lambda.event.listener.SafeListener.Companion.listener
 import com.lambda.util.player.MovementUtils.addSpeed
 
 object ElytraFly : Module(
     name = "ElytraFly",
-    description = "Elytra Go Brrr",
+    description = "Allows you to fly with an elytra",
     defaultTags = setOf(ModuleTag.MOVEMENT, ModuleTag.GRIM)
 ) {
-    private val speed by setting("Speed", 0.0, 0.0..0.5, 0.005)
+    // private val page by setting("Page", Page.GENERAL) // Uncomment when needed
+    private val mode by setting("Mode", Mode.BOOST)
+
+    private val speed by setting("Speed", 0.0, 0.0..0.5, 0.005, description = "Speed to add when flying") { mode == Mode.BOOST }
+
     init {
         listener<MovementEvent.Pre> {
-            if (player.isFallFlying && !player.isUsingItem) {
-                addSpeed(speed)
+            when (mode) {
+                Mode.BOOST -> {
+                    if (player.isFallFlying && !player.isUsingItem) {
+                        addSpeed(speed)
+                    }
+                }
             }
         }
     }
+
+    private enum class Page {
+        GENERAL,
+        // Add more when needed
+    }
+
+    enum class Mode {
+        BOOST,
+        // Add more when needed
+    }
 }
-
-
-
