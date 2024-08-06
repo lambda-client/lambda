82f41533492aaaf8e17f23f26f9b10b5d3db0004
Camii2407 <camii@spawnmasons.dev>
2024-08-06 18:25:33 +0100

    lol elytrafly mode boost since i messed last one up somehow

--- common/src/main/kotlin/com/lambda/module/modules/movement/ElytraFly.kt
+++ common/src/main/kotlin/com/lambda/module/modules/movement/ElytraFly.kt
@@ -1,4 +1,24 @@
-package com.lambda.module.modules.movement
+package com.lambda.module.modules.movement;
+import com.lambda.event.events.MovementEvent
+import com.lambda.module.Module
+import com.lambda.module.tag.ModuleTag
+import com.lambda.event.listener.SafeListener.Companion.listener
+import com.lambda.util.player.MovementUtils.addSpeed
 
-class ElytraFly {
+object ElytraFly : Module(
+    name = "ElytraFly",
+    description = "Elytra Go Brrr",
+    defaultTags = setOf(ModuleTag.MOVEMENT, ModuleTag.GRIM)
+) {
+    private val speed by setting("Speed", 0.0, 0.0..0.5, 0.005)
+    init {
+        listener<MovementEvent.Pre> {
+            if (player.isFallFlying && !player.isUsingItem) {
+                addSpeed(speed)
+            }
+        }
+    }
 }
+
+
+
