5fdde8d250c6f5e1b3ed3dafd493b32b5e08986f
Camii2407 <camii@spawnmasons.dev>
2024-08-06 17:45:42 +0100

    working coords reply in auto reply

--- common/src/main/kotlin/com/lambda/module/modules/player/AutoReply.kt
+++ common/src/main/kotlin/com/lambda/module/modules/player/AutoReply.kt
@@ -5,14 +5,16 @@ import com.lambda.event.listener.SafeListener.Companion.listener
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
 import com.lambda.util.Formatting.string
-import net.minecraft.network.packet.s2c.play.ChatMessageS2CPacket
+import com.lambda.util.StringUtils.capitalize
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket
+import net.minecraft.util.math.Vec3d
 
 object AutoReply : Module(
     name = "AutoReply",
     description = "Automatically Replies to messages",
     defaultTags = setOf(ModuleTag.PLAYER)
 ){
+    private val range by setting("Reply range", 5000, 0..25000, 250, "Replies with coords if player is within range to spawn", " Blocks")
     init{
         listener<PacketEvent.Receive.Pre>{ event ->
             val packet = event.packet
@@ -20,8 +22,9 @@ object AutoReply : Module(
             val msg = packet.content.string
             if (!msg.contains("Coords", true)) return@listener
             if (!msg.contains(" whispers: ")) return@listener
+            if (!player.pos.isInRange(Vec3d.ZERO, range.toDouble())) return@listener
             val sender = msg.split(" whispers: ").first()
-            connection.sendChatCommand("w $sender ${player.pos.string} [${world.dimension}]")
+            connection.sendChatCommand("w $sender ${player.blockPos.string} [${world.dimensionKey.value.path.capitalize()}]")
         }
     }
 }
--- common/src/main/kotlin/com/lambda/util/Formatting.kt
+++ common/src/main/kotlin/com/lambda/util/Formatting.kt
@@ -1,5 +1,6 @@
 package com.lambda.util
 
+import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -14,6 +15,9 @@ object Formatting {
         val format = "%.${decimals}f"
         return "(${format.format(x)}, ${format.format(y)}, ${format.format(z)})"
     }
+
+    val BlockPos.string: String
+        get() = "X: ${x} Y: ${y} Z: ${z}"
 
     fun getTime(formatter: DateTimeFormatter = DateTimeFormatter.RFC_1123_DATE_TIME): String {
         val localDateTime = LocalDateTime.now()
