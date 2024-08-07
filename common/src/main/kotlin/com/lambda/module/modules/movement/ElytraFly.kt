5a7bf4a29450aa0b9111be50ba7e5f16397d4039
Camii2407 <camii@spawnmasons.dev>
2024-08-07 22:42:27 +0100

    added Mute Elytra option to ElytraFly module

--- common/src/main/kotlin/com/lambda/module/modules/movement/ElytraFly.kt
+++ common/src/main/kotlin/com/lambda/module/modules/movement/ElytraFly.kt
@@ -1,10 +1,13 @@
 package com.lambda.module.modules.movement
 
+import com.lambda.event.events.ClientEvent
 import com.lambda.event.events.MovementEvent
+import com.lambda.event.events.TickEvent
 import com.lambda.event.listener.SafeListener.Companion.listener
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
 import com.lambda.util.player.MovementUtils.addSpeed
+import net.minecraft.sound.SoundEvents
 
 object ElytraFly : Module(
     name = "ElytraFly",
@@ -15,6 +18,7 @@ object ElytraFly : Module(
     private val mode by setting("Mode", Mode.BOOST)
 
     private val speed by setting("Speed", 0.0, 0.0..0.5, 0.005, description = "Speed to add when flying") { mode == Mode.BOOST }
+    private val elytraMute by setting("Mute Elytra", false, "Mutes elytra sound")
 
     init {
         listener<MovementEvent.Pre> {
@@ -25,6 +29,12 @@ object ElytraFly : Module(
                     }
                 }
             }
+        }
+
+        listener<ClientEvent.Sound> { event ->
+            if (!elytraMute) return@listener
+            if (event.sound.id != SoundEvents.ITEM_ELYTRA_FLYING.id) return@listener
+            event.cancel()
         }
     }
 
