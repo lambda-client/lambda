ec587e016ece54ac585693c6fdcebd312b15b9fc
Constructor <Avanatiker@users.noreply.github.com>
2024-08-17 05:24:10 +0200

    Feature: PacketMine (#20)
    Co-authored-by: Kamigen <46357922+Edouard127@users.noreply.github.com>
    Co-authored-by: beanbag44 <pidgeonmaster64@gmail.com>

--- common/src/main/kotlin/com/lambda/event/events/InteractionEvent.kt
+++ common/src/main/kotlin/com/lambda/event/events/InteractionEvent.kt
@@ -1,12 +1,42 @@
 package com.lambda.event.events
 
 import com.lambda.event.Event
+import com.lambda.event.callback.Cancellable
+import com.lambda.event.callback.ICancellable
 import net.minecraft.client.world.ClientWorld
 import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 
 sealed class InteractionEvent : Event {
     class Block(
         val world: ClientWorld,
         val blockHitResult: BlockHitResult
     ) : InteractionEvent()
+
+    sealed class BlockAttack : InteractionEvent() {
+        class Pre(
+            val pos: BlockPos,
+            val side: Direction
+        ) : BlockAttack(), ICancellable by Cancellable()
+
+        class Post(
+            val pos: BlockPos,
+            val side: Direction
+        ) : BlockAttack()
+    }
+
+    sealed class BreakingProgress : InteractionEvent() {
+        class Pre(
+            val pos: BlockPos,
+            val side: Direction,
+            var progress: Float,
+        ) : BreakingProgress(), ICancellable by Cancellable()
+
+        class Post(
+            val pos: BlockPos,
+            val side: Direction,
+            val progress: Float,
+        ) : BreakingProgress()
+    }
 }
