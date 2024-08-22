ea972be77204e4f0e97fb2de1f95589b18ee54b3
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-21 20:16:07 -0400

    todo: pixel buffer object

---
+++ common/src/main/kotlin/com/lambda/graphics/buffer/PixelBuffer.kt
@@ -0,0 +1,66 @@
+package com.lambda.graphics.buffer
+
+import org.lwjgl.opengl.GL45C.*
+import java.nio.ByteBuffer
+
+// NOT TESTED
+class PixelBuffer(
+    width: Int,
+    height: Int
+) {
+    private val pboIds = IntArray(2) { 0 }
+    private var index = 0
+
+    fun upload(data: ByteBuffer, block: () -> Unit) {
+        // Bind the current PBO for writing
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pboIds[index])
+
+        // Map the buffer and copy data into it
+        val bufferData = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY) as ByteBuffer
+        bufferData.put(data)
+        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER)
+
+        // Process the data
+        block()
+
+        // Unbind the buffer
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0)
+
+        // Switch to the other PBO
+        index = (index + 1) % 2
+    }
+
+    fun download(): ByteBuffer {
+        // Bind the current PBO for reading
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[index])
+
+        // Map the buffer and copy data from it
+        val bufferData = glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY) as ByteBuffer
+        val data = bufferData.slice()
+
+        // Unbind the buffer
+        glUnmapBuffer(GL_PIXEL_PACK_BUFFER)
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, 0)
+
+        return data
+    }
+
+    fun finalize() {
+        // Delete the PBOs
+        glDeleteBuffers(pboIds)
+    }
+
+    init {
+        // Generate the PBOs
+        glGenBuffers(pboIds)
+
+        // Fill the buffers with null data to allocate the memory spaces
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[0])
+        glBufferData(GL_PIXEL_PACK_BUFFER, width * height * 4L, GL_DYNAMIC_READ)
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[1])
+        glBufferData(GL_PIXEL_PACK_BUFFER, width * height * 4L, GL_DYNAMIC_READ)
+
+        // Unbind the buffer
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, 0)
+    }
+}
