3247c70e7427f4209d94c6fcc8dbb2cff56a1c97
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-24 14:12:29 -0400

    wrong import location

--- common/src/main/kotlin/com/lambda/module/modules/movement/Speed.kt
+++ common/src/main/kotlin/com/lambda/module/modules/movement/Speed.kt
@@ -22,7 +22,7 @@ import com.lambda.util.player.MovementUtils.newMovementInput
 import com.lambda.util.player.MovementUtils.roundedForward
 import com.lambda.util.player.MovementUtils.roundedStrafing
 import com.lambda.util.player.MovementUtils.setSpeed
-import com.lambda.util.primitives.extension.contains
+import com.lambda.util.extension.contains
 import com.lambda.util.world.entitySearch
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.decoration.ArmorStandEntity
