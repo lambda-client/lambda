86a7969caefa2c1eec9c6c1566effe0c40643226
pothemagicdragon <pothemagicdragon1@gmail.com>
2024-08-24 11:03:12 -0600

    Fixes fetching player skins for FakePlayer.kt

--- common/src/main/kotlin/com/lambda/module/modules/combat/FakePlayer.kt
+++ common/src/main/kotlin/com/lambda/module/modules/combat/FakePlayer.kt
@@ -1,21 +1,14 @@
 package com.lambda.module.modules.combat
 
-import com.google.gson.annotations.SerializedName
 import com.lambda.context.SafeContext
 import com.lambda.http.Method
 import com.lambda.http.request
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
-import com.lambda.threading.runSafe
 import com.lambda.threading.runSafeConcurrent
-import com.lambda.threading.runSafeGameConcurrent
 import com.mojang.authlib.GameProfile
-import com.mojang.authlib.minecraft.MinecraftProfileTexture
-import com.mojang.authlib.properties.Property
 import net.minecraft.client.network.OtherClientPlayerEntity
 import net.minecraft.client.network.PlayerListEntry
-import net.minecraft.client.texture.PlayerSkinProvider
-import net.minecraft.client.world.ClientWorld
 import net.minecraft.entity.Entity
 import java.util.*
 
@@ -35,27 +28,18 @@ object FakePlayer : Module(
                 return@onEnable spawnPlayer(fakePlayer!!.gameProfile)
 
             runSafeConcurrent {
-                val profile =
+                var profile = GameProfile(UUID.randomUUID(), playerName)
+
+                profile =
                     request("https://api.mojang.com/users/profiles/minecraft/$playerName") {
                         method(Method.GET)
                     }.json<GameProfile>()
-                        .data ?: return@runSafeConcurrent spawnPlayer(GameProfile(UUID.randomUUID(), playerName))
+                        .data ?: profile
 
-                val properties =
-                    request("https://sessionserver.mojang.com/session/minecraft/profile/${profile.id}") {
-                        method(Method.GET)
-                    }.json<PlayerSkinProviderKey>()
-                        .data ?: return@runSafeConcurrent spawnPlayer(profile)
+                 profile = mc.sessionService.fetchProfile(profile.id, true)?.profile ?: profile
 
-                val textureProperty = properties.textureProperty ?: return@runSafeConcurrent spawnPlayer(profile)
-                val textures = mc.sessionService.unpackTextures(textureProperty)
-
-                // Fetch and cache the skin textures
-                mc.skinProvider.fetchSkinTextures(profile.id, textures)
-
-                // Hack the game profile to include the skin textures
-                profile.properties.put("textures", textureProperty)
-
+                // This is the cache that mc pulls profile data from when it fetches skins.
+                mc.networkHandler?.playerListEntries?.put(profile.id, PlayerListEntry(profile, false))
                 spawnPlayer(profile)
             }
         }
@@ -77,16 +61,7 @@ object FakePlayer : Module(
         world.addEntity(fakePlayer)
     }
 
-    private fun SafeContext.deletePlayer() {
+    private fun deletePlayer() {
         fakePlayer?.setRemoved(Entity.RemovalReason.DISCARDED)
-    }
-
-    private data class PlayerSkinProviderKey(
-        @SerializedName("id") val id: String,
-        @SerializedName("name") val name: String,
-        @SerializedName("properties") val properties: List<Property>
-    ) {
-        val textureProperty: Property?
-            get() = properties.firstOrNull { it.name == "textures" }
     }
 }
