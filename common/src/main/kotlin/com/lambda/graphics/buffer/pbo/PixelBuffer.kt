06aba2d8aa6ea37cc2f37f906ca0136062eb1ff4
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-23 20:35:03 -0400

    added upload record time

--- common/src/main/kotlin/com/lambda/graphics/buffer/pbo/PixelBuffer.kt
+++ common/src/main/kotlin/com/lambda/graphics/buffer/pbo/PixelBuffer.kt
@@ -1,10 +1,8 @@
 package com.lambda.graphics.buffer.pbo
 
 import com.lambda.graphics.buffer.BufferUsage
-import com.lambda.graphics.texture.TextureUtils.setupTexture
 import org.lwjgl.opengl.GL45C.*
 import java.nio.ByteBuffer
-import kotlin.system.measureNanoTime
 
 class PixelBuffer(
     private val width: Int,
@@ -16,62 +14,58 @@ class PixelBuffer(
     private var writeIdx = 0 // Used to copy pixels from the PBO to the texture
     private var uploadIdx = 0 // Used to upload data to the PBO
 
-    fun mapTexture(id: Int, buffer: ByteBuffer) {
-        val time = measureNanoTime {
-            upload(buffer) { allocate ->
-                // Bind the texture
-                glBindTexture(GL_TEXTURE_2D, id)
+    private val queryId = glGenQueries() // Used to measure the time taken to upload data to the PBO
+    val uploadTime get() = IntArray(1).also { glGetQueryObjectiv(queryId, GL_QUERY_RESULT, it) }[0]
 
-                if (allocate) {
-                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    fun mapTexture(id: Int, buffer: ByteBuffer) =
+        upload(buffer) {
+            // Bind the texture
+            glBindTexture(GL_TEXTURE_2D, id)
 
-                    // Allocate the texture memory
-                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0)
-                }
-                else {
-                    // Update the texture
-                    glTextureSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, 0)
-                }
+            // Perform the actual data transfer to the GPU
+            glTextureSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, 0)
+        }
+
+    fun upload(data: ByteBuffer, process: () -> Unit) =
+        recordTransfer {
+            uploadIdx = (writeIdx + 1) % buffers
+
+            // Bind the next PBO to update pixel values
+            glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pboIds[writeIdx])
+
+            // Map the buffer into the memory
+            val bufferData = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY)
+            if (bufferData != null) {
+                bufferData.put(data)
+
+                // Release the buffer
+                glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER)
+            } else {
+                println("Failed to map the PBO")
             }
+
+            // Bind the current PBO for writing
+            glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pboIds[uploadIdx])
+
+            // Copy the pixel values from the PBO to the texture
+            process()
+
+            // Unbind the PBO
+            glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0)
+
+            // Swap the indices
+            writeIdx = uploadIdx
         }
 
-        println("PBO => texture $id took $time nanoseconds")
-    }
+    private fun recordTransfer(block: () -> Unit) {
+        // Start the timer
+        glBeginQuery(GL_TIME_ELAPSED, queryId)
 
-    fun upload(data: ByteBuffer, process: (Boolean) -> Unit) {
-        uploadIdx = (writeIdx + 1) % buffers
+        // Perform the transfer
+        block()
 
-        // Bind the next PBO to update pixel values
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pboIds[uploadIdx])
-
-        // Allocate the memory for the buffer
-        process(true)
-
-        // Map the buffer into the memory
-        val bufferData = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY)
-        if (bufferData != null) {
-            bufferData.put(data)
-
-            // Release the buffer
-            glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER)
-        } else {
-            println("Failed to map the PBO")
-        }
-
-        // Bind the current PBO for writing
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pboIds[writeIdx])
-
-        // Copy the pixel values from the PBO to the texture
-        process(false)
-
-        // Unbind the PBO
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0)
-
-        println("Uploaded data to PBO $uploadIdx at $bufferData")
-
-        // Swap the indices
-        writeIdx = uploadIdx
+        // Stop the timer
+        glEndQuery(GL_TIME_ELAPSED)
     }
 
     // Called when no references to the object exist
@@ -89,7 +83,7 @@ class PixelBuffer(
         // Fill the buffers with null data to allocate the memory spaces
         repeat(buffers) {
             glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pboIds[it])
-            glBufferData(GL_PIXEL_UNPACK_BUFFER, width * height * 4L, GL_STREAM_DRAW)
+            glBufferData(GL_PIXEL_UNPACK_BUFFER, width * height * 4L, bufferUsage.gl)
         }
 
         glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0) // Unbind the buffer
