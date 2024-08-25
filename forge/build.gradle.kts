653c32dac1767d7ca6249c434fcf489f7fc3477d
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-25 17:13:15 -0400

    Revert "bump shadow"
    This reverts commit 707143d29d354bf9ae0493c93782bae3ccc48280.

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -8,7 +8,7 @@ val discordIPCVersion: String by project
 base.archivesName = "${base.archivesName.get()}-forge"
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "8.3.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 architectury {
