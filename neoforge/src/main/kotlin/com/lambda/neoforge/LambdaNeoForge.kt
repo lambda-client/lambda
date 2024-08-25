1dc796f5a3e0fdaecddaccfc1a392fe67e2c52bb
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-25 16:53:57 -0400

    fix: neoforge client initilization

--- neoforge/src/main/kotlin/com/lambda/neoforge/LambdaNeoForge.kt
+++ neoforge/src/main/kotlin/com/lambda/neoforge/LambdaNeoForge.kt
@@ -12,12 +12,14 @@ import net.minecraft.util.Identifier
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.common.Mod.EventBusSubscriber
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
 import net.neoforged.neoforge.registries.RegisterEvent
 
 @Mod(Lambda.MOD_ID)
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 object LambdaNeoForge {
-    init {
+    @SubscribeEvent
+    fun onClient(event: FMLClientSetupEvent) {
         Lambda.initialize()
         LOG.info("$MOD_NAME Forge $VERSION initialized.")
     }
