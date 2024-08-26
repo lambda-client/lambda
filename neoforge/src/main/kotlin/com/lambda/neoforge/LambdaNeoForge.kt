311d02113b624d6270d9141fafbe70f4f476f439
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-26 15:30:36 -0400

    fix: neoforge prod crash

--- neoforge/src/main/kotlin/com/lambda/neoforge/LambdaNeoForge.kt
+++ neoforge/src/main/kotlin/com/lambda/neoforge/LambdaNeoForge.kt
@@ -6,17 +6,22 @@ import com.lambda.Lambda.MOD_NAME
 import com.lambda.Lambda.VERSION
 import com.lambda.core.registry.RegistryController
 import com.lambda.core.registry.RegistryWrapper
+import com.lambda.graphics.RenderMain
 import net.minecraft.registry.Registry
 import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.util.Identifier
+import net.neoforged.api.distmarker.Dist
+import net.neoforged.api.distmarker.OnlyIn
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.Mod
-import net.neoforged.fml.common.Mod.EventBusSubscriber
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
+import net.neoforged.neoforge.client.event.RenderGuiEvent
 import net.neoforged.neoforge.registries.RegisterEvent
+import thedarkcolour.kotlinforforge.neoforge.forge.FORGE_BUS
+import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
 @Mod(Lambda.MOD_ID)
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+@OnlyIn(Dist.CLIENT)
 object LambdaNeoForge {
     @SubscribeEvent
     fun onClient(event: FMLClientSetupEvent) {
@@ -31,5 +36,17 @@ object LambdaNeoForge {
                 return Registry.registerReference(event.registry as Registry<T>, id, value)
             }
         })
+    }
+
+    // Most events here are hooked due to neoforge not caring about others
+    // and directly patching the minecraft classes.
+    private object ClientEvents {
+        @SubscribeEvent
+        fun onHudRender(event: RenderGuiEvent.Post) { RenderMain.render2D() }
+    }
+
+    init {
+        MOD_BUS.register(this)
+        FORGE_BUS.register(ClientEvents)
     }
 }
