d04a773116c4795a7c3ad20d373dbb622caed55d
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-26 15:25:44 -0400

    fix: 2d renderer forge

--- forge/src/main/kotlin/com/lambda/forge/LambdaForge.kt
+++ forge/src/main/kotlin/com/lambda/forge/LambdaForge.kt
@@ -6,15 +6,18 @@ import com.lambda.Lambda.MOD_NAME
 import com.lambda.Lambda.VERSION
 import com.lambda.core.registry.RegistryController
 import com.lambda.core.registry.RegistryWrapper
+import com.lambda.graphics.RenderMain
 import net.minecraft.registry.Registry
 import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.util.Identifier
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.api.distmarker.OnlyIn
+import net.minecraftforge.client.event.RenderGuiEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.registries.RegisterEvent
+import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 
@@ -36,7 +39,15 @@ object LambdaForge {
         })
     }
 
+    // Most events here are hooked due to forge not caring about others
+    // and directly patching the minecraft classes.
+    private object ClientEvents {
+        @SubscribeEvent
+        fun onHudRender(event: RenderGuiEvent.Post) { RenderMain.render2D() }
+    }
+
     init {
         MOD_BUS.register(this)
+        FORGE_BUS.register(ClientEvents)
     }
 }
