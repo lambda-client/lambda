094238fb0bba5b8776a08dd31a545149960bf575
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-09-06 18:21:40 -0400

    bump kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val Project.loom: LoomGradleExtensionAPI
     get() = (this as ExtensionAware).extensions.getByName("loom") as LoomGradleExtensionAPI
 
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.20"
     id("org.jetbrains.dokka") version "1.9.20"
     id("architectury-plugin") version "3.4-SNAPSHOT"
     id("dev.architectury.loom") version "1.7-SNAPSHOT" apply false
