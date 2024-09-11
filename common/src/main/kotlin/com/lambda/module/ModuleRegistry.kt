b19945f163ba6ea61f029470114b8e6ffa8320cb
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-09-11 19:20:42 -0400

    [1.20.x] [All] Bug: Inconsistent run in dev environment (#70)
    After multiple hours of excruciating pain and mental breakdowns, I have
    found that `org.reflections` was schizoing about input filters.
    
    The fix ?
    Don't filter anything, just don't.

--- common/src/main/kotlin/com/lambda/module/ModuleRegistry.kt
+++ common/src/main/kotlin/com/lambda/module/ModuleRegistry.kt
@@ -10,9 +10,7 @@ import org.reflections.util.ConfigurationBuilder
  * The [ModuleRegistry] object is responsible for managing all [Module] instances in the system.
  */
 object ModuleRegistry : Loadable {
-    val modules = getInstances<Module> {
-        forPackages("com.lambda.module.modules"); filterInputsBy { it.contains("com.lambda") }
-    }.toMutableList()
+    val modules = getInstances<Module> { forPackages("com.lambda.module.modules") }.toMutableList()
 
     val moduleNames: Set<String>
         get() = modules.map { it.name }.toSet()
