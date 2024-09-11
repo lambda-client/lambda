5b5e7e821b6c60547094c296585ef9b5464c6973
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-09-11 19:05:30 -0400

    fix #69

--- common/src/main/kotlin/com/lambda/util/reflections/ReflectionsConfigDsl.kt
+++ common/src/main/kotlin/com/lambda/util/reflections/ReflectionsConfigDsl.kt
@@ -12,6 +12,11 @@ annotation class ReflectionsDsl
  * A DSL class to configure the Reflections library for runtime scanning.
  * This class allows you to specify packages, scanners, URLs, filters, class loaders, and other options
  * to build a Reflections configuration.
+ *
+ * Important!!
+ * Be aware that using Reflections can lead to unpredictable and hard-to-debug issues.
+ * Furthermore, the library we're relying on is outdated and no longer maintained.
+ * For more reliable methods, refer to Reflections.kt.
  */
 @ReflectionsDsl
 class ReflectionConfigDsl {
@@ -19,72 +24,50 @@ class ReflectionConfigDsl {
     private val scanners = mutableListOf<Scanners>()
     private val urls = mutableListOf<URL>()
     private val classLoaders = mutableListOf<ClassLoader>()
-    private var parallel = false
+
     private var shouldExpandSuperTypes = true
 
     /**
      * Specifies the packages to be scanned.
      *
      * @param packages A vararg of package names to be included in the scanning process.
-     * @return The current instance of [ReflectionConfigDsl] for method chaining.
      */
-    fun forPackages(vararg packages: String): ReflectionConfigDsl {
+    fun forPackages(vararg packages: String) {
         this.packages += packages
-        return this
     }
 
     /**
      * Adds scanners to the configuration.
      *
      * @param scanners A vararg of [Scanners] to be used for scanning classes, methods, etc.
-     * @return The current instance of [ReflectionConfigDsl] for method chaining.
      */
-    fun addScanners(vararg scanners: Scanners): ReflectionConfigDsl {
+    fun addScanners(vararg scanners: Scanners) {
         this.scanners += scanners
-        return this
     }
 
     /**
      * Adds URLs to the configuration.
      *
      * @param urls A vararg of [URL] to be included in the scanning process.
-     * @return The current instance of [ReflectionConfigDsl] for method chaining.
      */
-    fun addUrls(vararg urls: URL): ReflectionConfigDsl {
+    fun addUrls(vararg urls: URL) {
         this.urls += urls
-        return this
     }
 
     /**
      * Adds class loaders to the configuration.
      *
      * @param classLoaders A vararg of [ClassLoader] to be used in the scanning process.
-     * @return The current instance of [ReflectionConfigDsl] for method chaining.
      */
-    fun addClassLoaders(vararg classLoaders: ClassLoader): ReflectionConfigDsl {
+    fun addClassLoaders(vararg classLoaders: ClassLoader) {
         this.classLoaders += classLoaders
-        return this
     }
 
     /**
-     * Enables parallel scanning.
-     *
-     * @return The current instance of [ReflectionConfigDsl] for method chaining.
+     * Expand super types during the scan
      */
-    fun parallel(): ReflectionConfigDsl {
-        parallel = true
-        return this
-    }
-
-    /**
-     * Sets whether to expand super types during scanning.
-     *
-     * @param value A [Boolean] indicating whether to expand super types.
-     * @return The current instance of [ReflectionConfigDsl] for method chaining.
-     */
-    fun expandSuperTypes(value: Boolean): ReflectionConfigDsl {
-        shouldExpandSuperTypes = value
-        return this
+    fun expandSuperTypes(shouldExpand: Boolean) {
+        shouldExpandSuperTypes = shouldExpand
     }
 
     /**
@@ -93,7 +76,6 @@ class ReflectionConfigDsl {
     fun build(): ConfigurationBuilder = ConfigurationBuilder()
         .forPackages(*packages.toTypedArray())
         .addUrls(*urls.toTypedArray())
-        .filterInputsBy { it.contains("com.lambda") }
         .addClassLoaders(*classLoaders.toTypedArray())
         .setExpandSuperTypes(shouldExpandSuperTypes)
 }
