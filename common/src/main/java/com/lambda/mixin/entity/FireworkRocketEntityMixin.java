0b9f4757a7735881b62747111d6238e7d2c56159
Constructor <fractalminds@protonmail.com>
2024-09-20 02:27:48 +0200

    Refactor

--- common/src/main/java/com/lambda/mixin/entity/FireworkRocketEntityMixin.java
+++ common/src/main/java/com/lambda/mixin/entity/FireworkRocketEntityMixin.java
@@ -1,24 +1,15 @@
 package com.lambda.mixin.entity;
 
-import com.lambda.module.Module;
 import com.lambda.module.modules.movement.ElytraFly;
-import com.lambda.util.Nameable;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import javax.annotation.Nullable;
-
 @Mixin(FireworkRocketEntity.class)
 public class FireworkRocketEntityMixin {
-
-
-    @Shadow @Nullable public LivingEntity shooter;
-
     @Redirect(
             method = "tick",
             at = @At(
@@ -26,26 +17,8 @@ public class FireworkRocketEntityMixin {
                     target = "Lnet/minecraft/entity/LivingEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"
             )
     )
-    private void redirectSetVelocity(LivingEntity instance, Vec3d vec3d) {
-
-        if (ElytraFly.INSTANCE.isEnabled() && ElytraFly.INSTANCE.getMode().equals(ElytraFly.Mode.ROCKET_BOOST)) {
-
-            double speedMultiplier = ElytraFly.INSTANCE.getRocketSpeed();
-
-            assert shooter != null;
-            Vec3d rotationVector = shooter.getRotationVector();
-            Vec3d currentVelocity = shooter.getVelocity();
-
-            double d = 1.5 * speedMultiplier;
-            double e = 0.1 * speedMultiplier;
-
-            Vec3d newVelocity = currentVelocity.add(
-                    rotationVector.x * e + (rotationVector.x * d - currentVelocity.x) * 0.5,
-                    rotationVector.y * e + (rotationVector.y * d - currentVelocity.y) * 0.5,
-                    rotationVector.z * e + (rotationVector.z * d - currentVelocity.z) * 0.5
-            );
-
-            this.shooter.setVelocity(newVelocity);
-        }
+    private void redirectSetVelocity(LivingEntity shooter, Vec3d vec3d) {
+        if (!ElytraFly.getDoBoost()) return;
+        ElytraFly.boostRocket(shooter);
     }
 }
--- common/src/main/kotlin/com/lambda/module/modules/movement/ElytraFly.kt
+++ common/src/main/kotlin/com/lambda/module/modules/movement/ElytraFly.kt
@@ -5,7 +5,9 @@ import com.lambda.event.events.MovementEvent
 import com.lambda.event.listener.SafeListener.Companion.listener
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
+import com.lambda.threading.runSafe
 import com.lambda.util.player.MovementUtils.addSpeed
+import net.minecraft.entity.LivingEntity
 import net.minecraft.sound.SoundEvents
 
 object ElytraFly : Module(
@@ -14,29 +16,23 @@ object ElytraFly : Module(
     defaultTags = setOf(ModuleTag.MOVEMENT, ModuleTag.GRIM)
 ) {
     // private val page by setting("Page", Page.GENERAL) // Uncomment when needed
-    val mode by setting("Mode", Mode.BOOST)
 
-    private val speed by setting("Speed", 0.02, 0.0..0.5, 0.005, description = "Speed to add when flying") { mode == Mode.BOOST }
+    private val playerBoost by setting("Player Boost", true, description = "Boosts the player when flying")
+    private val playerSpeed by setting("Player Speed", 0.02, 0.0..0.5, 0.005, description = "Speed to add when flying") { playerBoost }
+    private val rocketBoost by setting("Rocket Boost", false, description = "Boosts the player when using a firework")
+    private val rocketSpeed by setting("Rocket Speed", 2.0, 0.0 ..2.0, description = "Speed multiplier that the rocket gives you") { rocketBoost }
+
     private val mute by setting("Mute Elytra", false, "Mutes the elytra sound when gliding")
 
-    val rocketSpeed by setting("Rocket Speed", 2.0, 0.0 ..5.0, description = "Speed multiplier that the rocket gives you") { mode == Mode.ROCKET_BOOST }
+    @JvmStatic
+    val doBoost: Boolean get() = isEnabled && playerBoost
 
     init {
         listener<MovementEvent.Pre> {
-            when (mode) {
-                Mode.BOOST -> {
-                    if (player.isFallFlying && !player.isUsingItem) {
-                        addSpeed(speed)
-                    }
-                }
-
-                Mode.ROCKET_BOOST -> {
-
-
-                }
+            if (playerBoost && player.isFallFlying && !player.isUsingItem) {
+                addSpeed(playerSpeed)
             }
         }
-
 
         listener<ClientEvent.Sound> { event ->
             if (!mute) return@listener
@@ -45,14 +41,27 @@ object ElytraFly : Module(
         }
     }
 
-    private enum class Page {
-        GENERAL,
-        // Add more when needed
+    @JvmStatic
+    fun boostRocket(shooter: LivingEntity) {
+        runSafe {
+            if (shooter != player) return@runSafe
+
+            val vec = player.rotationVector
+            val velocity = player.velocity
+
+            val d = 1.5 * rocketSpeed
+            val e = 0.1 * rocketSpeed
+
+            player.velocity = velocity.add(
+                vec.x * e + (vec.x * d - velocity.x) * 0.5,
+                vec.y * e + (vec.y * d - velocity.y) * 0.5,
+                vec.z * e + (vec.z * d - velocity.z) * 0.5
+            )
+        }
     }
 
-    enum class Mode {
-        BOOST,
-        ROCKET_BOOST,
+    private enum class Page {
+        GENERAL,
         // Add more when needed
     }
 }
