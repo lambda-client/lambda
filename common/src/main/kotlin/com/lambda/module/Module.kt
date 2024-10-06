8bb124aca7735fd0af9de81c587d1ce95e45f325
Constructor <fractalminds@protonmail.com>
2024-10-06 04:52:23 +0200

    Fix ClickGUI keybind again

--- common/src/main/kotlin/com/lambda/module/Module.kt
+++ common/src/main/kotlin/com/lambda/module/Module.kt
@@ -14,7 +14,6 @@ import com.lambda.event.listener.Listener
 import com.lambda.event.listener.SafeListener
 import com.lambda.event.listener.SafeListener.Companion.listener
 import com.lambda.event.listener.UnsafeListener
-import com.lambda.gui.api.LambdaGui
 import com.lambda.gui.impl.clickgui.buttons.ModuleButton
 import com.lambda.module.modules.client.ClickGui
 import com.lambda.module.tag.ModuleTag
@@ -116,9 +115,9 @@ abstract class Module(
             if (mc.options.commandKey.isPressed) return@listener
             if (keybind == KeyCode.UNBOUND) return@listener
             if (event.translated != keybind) return@listener
-            if (mc.currentScreen is ChatScreen) return@listener
+            if (mc.currentScreen != null) return@listener
 
-            if (mc.currentScreen == null || this@Module is ClickGui) toggle()
+            toggle()
         }
 
         onEnable {
