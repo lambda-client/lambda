f3c583b78982a5be2e7f305b879605dc12c05e90
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-11-20 18:32:15 -0500

    rpc -> api cname

--- common/src/main/kotlin/com/lambda/module/modules/client/DiscordRPC.kt
+++ common/src/main/kotlin/com/lambda/module/modules/client/DiscordRPC.kt
@@ -67,7 +67,7 @@ object DiscordRPC : Module(
     private val showTime by setting("Show Time", true, description = "Show how long you have been playing for.") { page == Page.General }
 
     /* Technical settings */
-    private var rpcServer by setting("RPC Server", "https://rpc.lambda-client.org") { page == Page.Settings }
+    private var rpcServer by setting("RPC Server", "https://api.lambda-client.org") { page == Page.Settings }
     private var apiVersion by setting("API Version", ApiVersion.V1) { page == Page.Settings }
     private val delay by setting("Update Delay", 15000, 15000..30000, 100, unit = "ms", visibility = { page == Page.Settings })
 
