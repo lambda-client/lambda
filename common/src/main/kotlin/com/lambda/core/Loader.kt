c8a099bc598efe6cc369dca155fbc0217c2b67a6
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-11-25 15:34:40 -0500

    use reflections for loadables
    I swear I already did that a long time ago, well too bad I guess

--- common/src/main/kotlin/com/lambda/core/Loader.kt
+++ common/src/main/kotlin/com/lambda/core/Loader.kt
@@ -19,18 +19,8 @@ package com.lambda.core
 
 import com.lambda.Lambda
 import com.lambda.Lambda.LOG
-import com.lambda.command.CommandRegistry
-import com.lambda.friend.FriendRegistry
-import com.lambda.graphics.renderer.gui.font.LambdaEmoji
-import com.lambda.graphics.renderer.gui.font.LambdaFont
-import com.lambda.gui.GuiConfigurable
-import com.lambda.gui.HudGuiConfigurable
-import com.lambda.interaction.PlayerPacketManager
-import com.lambda.interaction.RotationManager
-import com.lambda.interaction.material.ContainerManager
-import com.lambda.module.ModuleRegistry
-import com.lambda.sound.SoundRegistry
 import com.lambda.util.Communication.ascii
+import com.lambda.util.reflections.getInstances
 import kotlin.system.measureTimeMillis
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
@@ -41,21 +31,7 @@ object Loader {
     val runtime: String
         get() = "${(System.currentTimeMillis() - started).toDuration(DurationUnit.MILLISECONDS)}"
 
-    private val loadables = listOf(
-        ModuleRegistry,
-        CommandRegistry,
-        RotationManager,
-        PlayerPacketManager,
-        LambdaFont.Loader,
-        LambdaEmoji.Loader,
-        GuiConfigurable,
-        HudGuiConfigurable,
-        FriendRegistry,
-        SoundRegistry,
-        TimerManager,
-        PingManager,
-        ContainerManager
-    )
+    private val loadables = getInstances<Loadable> { forPackages("com.lambda") }
 
     fun initialize() {
         ascii.split("\n").forEach { LOG.info(it) }
