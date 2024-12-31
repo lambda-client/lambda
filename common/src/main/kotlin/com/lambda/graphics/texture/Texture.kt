b52727dd336a3639ad21b9cef7afd1153f2c99ef
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-12-31 11:51:49 -0500

    moved the texture options before the tex call

--- common/src/main/kotlin/com/lambda/graphics/texture/Texture.kt
+++ common/src/main/kotlin/com/lambda/graphics/texture/Texture.kt
@@ -74,8 +74,8 @@ open class Texture(
         initialized = true
 
         // Set this mipmap to `offset` to define the original texture
-        glTexImage2D(GL_TEXTURE_2D, offset, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, readImage(image))
         setupTexture(GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, offset, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, readImage(image))
         if (levels > 1) glGenerateMipmap(GL_TEXTURE_2D) // This take the derived values GL_TEXTURE_BASE_LEVEL and GL_TEXTURE_MAX_LEVEL to generate the stack
     }
 
