07b4f4107ffef7622e20aa5ccca7af8ebb4c88d7
Constructor <fractalminds@protonmail.com>
2025-01-09 00:31:19 +0100

    Proper FastVector mesh

--- common/src/main/kotlin/com/lambda/graphics/renderer/esp/DirectionMask.kt
+++ common/src/main/kotlin/com/lambda/graphics/renderer/esp/DirectionMask.kt
@@ -19,6 +19,8 @@ package com.lambda.graphics.renderer.esp
 
 import com.lambda.util.world.FastVector
 import com.lambda.util.world.offset
+import com.lambda.util.world.toBlockPos
+import com.lambda.util.world.toFastVec
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 
@@ -41,15 +43,8 @@ object DirectionMask {
     fun Int.exclude(direction: Direction) = exclude(direction.mask)
     fun Int.hasDirection(dir: Int) = (this and dir) != 0
 
-    fun buildSideMesh(position: BlockPos, filter: (BlockPos) -> Boolean): Int {
-        var sides = ALL
-
-        Direction.entries
-            .filter { filter(position.offset(it)) }
-            .forEach { sides = sides.exclude(it.mask) }
-
-        return sides
-    }
+    fun buildSideMesh(position: BlockPos, filter: (BlockPos) -> Boolean) =
+        buildSideMesh(position.toFastVec()) { filter(it.toBlockPos()) }
 
     fun buildSideMesh(position: FastVector, filter: (FastVector) -> Boolean): Int {
         var sides = ALL
