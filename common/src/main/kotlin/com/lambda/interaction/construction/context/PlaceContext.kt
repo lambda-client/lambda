3d84218c4af9fe46a27bd60dcc8232f13e861c96
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2025-03-08 19:21:38 -0500

    Remove swing hand check

--- common/src/main/kotlin/com/lambda/interaction/construction/context/PlaceContext.kt
+++ common/src/main/kotlin/com/lambda/interaction/construction/context/PlaceContext.kt
@@ -17,7 +17,6 @@
 
 package com.lambda.interaction.construction.context
 
-import com.lambda.brigadier.argument.integer
 import com.lambda.config.groups.BuildConfig
 import com.lambda.context.SafeContext
 import com.lambda.graphics.renderer.esp.DirectionMask
@@ -59,10 +58,8 @@ data class PlaceContext(
                 player, hand, result
             )
 
-            actionResult.
-
             if (actionResult.isAccepted) {
-                if (actionResult.shouldSwingHand() && swingHand) {
+                if (swingHand) {
                     player.swingHand(hand)
                 }
 
