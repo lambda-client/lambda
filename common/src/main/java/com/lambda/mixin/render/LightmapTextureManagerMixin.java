5d19c26bd6397bf829deb94929fabcec23650ca0
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2025-03-08 20:56:01 -0500

    Fix fullbright

--- common/src/main/java/com/lambda/mixin/render/LightmapTextureManagerMixin.java
+++ common/src/main/java/com/lambda/mixin/render/LightmapTextureManagerMixin.java
@@ -20,23 +20,27 @@ package com.lambda.mixin.render;
 import com.lambda.module.modules.render.Fullbright;
 import com.lambda.module.modules.render.NoRender;
 import com.lambda.module.modules.render.XRay;
+import net.minecraft.client.gl.SimpleFramebuffer;
 import net.minecraft.client.render.LightmapTextureManager;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LightmapTextureManager.class)
 public class LightmapTextureManagerMixin {
-    // TODO: Fix this
-    /*@ModifyArg(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/texture/NativeImage;setColor(III)V"), index = 2)
-    private int updateModify(int color) {
+    @Shadow @Final private SimpleFramebuffer lightmapFramebuffer;
+
+    @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/SimpleFramebuffer;endWrite()V", shift = At.Shift.BEFORE))
+    private void updateModify(CallbackInfo ci) {
         if (Fullbright.INSTANCE.isEnabled() || XRay.INSTANCE.isEnabled()) {
-            return 0xFFFFFFFF;
+            lightmapFramebuffer.clear();
         }
-        return color;
-    }*/
+    }
 
     @Inject(method = "getDarknessFactor(F)F", at = @At("HEAD"), cancellable = true)
     private void getDarknessFactor(float tickDelta, CallbackInfoReturnable<Float> info) {
