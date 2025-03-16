0692abc5a4a4702eb9be120131583bdc5c00783d
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2025-03-16 15:33:59 -0400

    Test test fail

--- common/src/test/kotlin/FastVectorTest.kt
+++ common/src/test/kotlin/FastVectorTest.kt
@@ -47,6 +47,7 @@ class FastVectorTest {
 
         val fastVec = fastVectorOf(x, y, z)
 
+        assertEquals(true, false)
         assertEquals(x, fastVec.x)
         assertEquals(y, fastVec.y)
         assertEquals(z, fastVec.z)
