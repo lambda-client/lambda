652613832a42819890173cc0f1fd57a2d90d6629
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2025-03-30 16:50:42 -0400

    Better map tooltip

--- common/src/main/kotlin/com/lambda/module/modules/render/MapPreview.kt
+++ common/src/main/kotlin/com/lambda/module/modules/render/MapPreview.kt
@@ -24,9 +24,14 @@ import com.mojang.blaze3d.systems.RenderSystem
 import net.minecraft.client.font.TextRenderer
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.tooltip.TooltipComponent
+import net.minecraft.client.render.MapRenderState
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.component.DataComponentTypes
+import net.minecraft.component.type.MapIdComponent
 import net.minecraft.item.FilledMapItem
 import net.minecraft.item.ItemStack
 import net.minecraft.item.map.MapState
+import net.minecraft.item.tooltip.TooltipData
 import net.minecraft.util.Identifier
 
 
@@ -35,20 +40,17 @@ object MapPreview : Module(
     description = "Preview maps in your inventory",
     defaultTags = setOf(ModuleTag.RENDER)
 ) {
-    private val background = Identifier("textures/map/map_background.png")
+    private val background = Identifier.ofVanilla("textures/map/map_background.png")
 
-    // The map component is added via the draw context mixin, thanks mojang
-    class MapComponent(val stack: ItemStack) : TooltipComponent {
+    class MapComponent(val stack: ItemStack) : TooltipData, TooltipComponent {
         val state: MapState?
             get() = FilledMapItem.getMapState(stack, mc.world)
 
-        val mapId: Int?
-            get() = FilledMapItem.getMapId(stack)
+        val mapId: MapIdComponent?
+            get() = stack.getOrDefault(DataComponentTypes.MAP_ID, null)
 
-        override fun drawItems(fontRenderer: TextRenderer, x: Int, y: Int, context: DrawContext) {
+        override fun drawItems(textRenderer: TextRenderer, x: Int, y: Int, width: Int, height: Int, context: DrawContext) {
             mapId?.let { id ->
-                // Values taken from net.minecraft.client.render.item.HeldItemRenderer.renderFirstPersonMap
-
                 val matrices = context.matrices
 
                 matrices.push()
@@ -56,14 +58,18 @@ object MapPreview : Module(
                 matrices.scale(0.7f, 0.7f, 1f)
 
                 RenderSystem.enableBlend()
-                context.drawTexture(background, -7, -7, 0f, 0f, 142, 142, 142, 142)
+                context.drawTexture(RenderLayer::getGuiTextured, background, -7, -7, 0f, 0f, 142, 142, 142, 142)
 
                 matrices.translate(0.0, 0.0, 1.0)
-                mc.gameRenderer.mapRenderer.draw(matrices, context.vertexConsumers, id, state, true, 240)
+
+                val renderState = MapRenderState()
+                mc.mapRenderer.update(id, state, renderState)
+                context.draw { mc.mapRenderer.draw(renderState, matrices, it, true, 0xF000F0) }
+                matrices.pop()
             }
         }
 
-        override fun getHeight(): Int {
+        override fun getHeight(textRenderer: TextRenderer): Int {
             return if (FilledMapItem.getMapState(stack, mc.world) != null) 100
             else 0
         }
