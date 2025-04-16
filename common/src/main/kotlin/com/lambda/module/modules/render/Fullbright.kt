01ac6b07ffc6944c59d13c7932a2cf0e9a544a28
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2025-04-16 18:52:47 -0400

    Fixed fullbright for shaders

--- common/src/main/kotlin/com/lambda/module/modules/render/Fullbright.kt
+++ common/src/main/kotlin/com/lambda/module/modules/render/Fullbright.kt
@@ -17,11 +17,35 @@
 
 package com.lambda.module.modules.render
 
+import com.lambda.context.SafeContext
+import com.lambda.event.events.TickEvent
+import com.lambda.event.listener.SafeListener.Companion.listenOnce
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
 
 object Fullbright : Module(
     name = "Fullbright",
     description = "Makes everything brighter",
     defaultTags = setOf(ModuleTag.RENDER)
-)
+) {
+    private val nightVision by setting("Night Vision", false, description = "Adds the night vision effect client-side")
+        .onValueChange { _, to -> setNightVision(to) }
+
+    private val instance = StatusEffectInstance(StatusEffects.NIGHT_VISION, -1, 1, false, false)
+
+    init {
+        listenOnce<TickEvent.Pre> {
+            if (nightVision && !player.hasStatusEffect(StatusEffects.NIGHT_VISION)) setNightVision(true)
+
+            // Destroy the listener
+            true
+        }
+    }
+
+    private fun SafeContext.setNightVision(value: Boolean) {
+        if (value) player.addStatusEffect(instance)
+        else player.removeStatusEffect(StatusEffects.NIGHT_VISION)
+    }
+}
