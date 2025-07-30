05afa476a2ce2153842ab9afc151f29a9c42053a
beanbag44 <pidgeonmaster64@gmail.com>
2025-07-30 02:03:17 +0100

    check entity before setting pitch lol

--- common/src/main/java/com/lambda/mixin/render/LivingEntityRendererMixin.java
+++ common/src/main/java/com/lambda/mixin/render/LivingEntityRendererMixin.java
@@ -17,6 +17,7 @@
 
 package com.lambda.mixin.render;
 
+import com.lambda.Lambda;
 import com.lambda.interaction.request.rotating.RotationManager;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.entity.LivingEntity;
@@ -39,6 +40,7 @@ public class LivingEntityRendererMixin<T extends LivingEntity> {
      */
     @Redirect(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F", ordinal = 0), require = 0)
     private float injectRotationPitch(float g, float f, float s) {
+        if ((Object) this != Lambda.getMc().player) return MathHelper.lerp(g, f, s);
         Float headPitch = RotationManager.getHeadPitch();
         if (headPitch != null) {
             return MathHelper.lerp(g, RotationManager.INSTANCE.getPrevServerRotation().getPitchF(), headPitch);
