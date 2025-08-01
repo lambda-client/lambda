954342b791ef542b25ab901774096536d3ca8b6c
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2025-08-01 14:19:14 -0400

    Fixed ghost cast issue

--- src/main/kotlin/com/lambda/util/player/SlotUtils.kt
+++ src/main/kotlin/com/lambda/util/player/SlotUtils.kt
@@ -23,11 +23,11 @@ import net.minecraft.item.ItemStack
 import net.minecraft.screen.slot.SlotActionType
 
 object SlotUtils {
-    val ClientPlayerEntity.hotbar: List<ItemStack> get() = inventory.mainStacks.slice(0..8)
-    val ClientPlayerEntity.storage: List<ItemStack> get() = inventory.mainStacks.slice(9..35)
+    val ClientPlayerEntity.hotbar: List<ItemStack> get() = inventory.main.slice(0..8)
+    val ClientPlayerEntity.storage: List<ItemStack> get() = inventory.main.slice(9..35)
     val ClientPlayerEntity.equipment: List<ItemStack> get() = inventory.equipment.map.map { it.value }
-    val ClientPlayerEntity.hotbarAndStorage: List<ItemStack> get() = inventory.mainStacks
-    val ClientPlayerEntity.combined: List<ItemStack> get() = hotbarAndStorage// + equipment
+    val ClientPlayerEntity.hotbarAndStorage: List<ItemStack> get() = inventory.main
+    val ClientPlayerEntity.combined: List<ItemStack> get() = hotbarAndStorage + equipment
 
 
     fun SafeContext.clickSlot(
