ab924867fc5eb9ec7da11325fdaaf4826a864a89
beanbag44 <pidgeonmaster64@gmail.com>
2025-08-10 00:27:05 +0100

    default fudge factor to 2

--- src/main/kotlin/com/lambda/config/groups/BreakSettings.kt
+++ src/main/kotlin/com/lambda/config/groups/BreakSettings.kt
@@ -45,7 +45,7 @@ class BreakSettings(
 
     // Fixes / Delays
     override val breakThreshold by c.setting("Break Threshold", 0.70f, 0.1f..1.0f, 0.01f, "The break amount at which the block is considered broken") { vis() && page == Page.General }
-    override val fudgeFactor by c.setting("Fudge Factor", 1, 0..5, 1, "The amount of ticks to give double, aka secondary breaks extra for the server to recognise the break") { vis() && page == Page.General }
+    override val fudgeFactor by c.setting("Fudge Factor", 2, 0..5, 1, "The amount of ticks to give double, aka secondary breaks extra for the server to recognise the break") { vis() && page == Page.General }
 //    override val desyncFix by c.setting("Desync Fix", false, "Predicts if the players breaking will be slowed next tick as block break packets are processed using the players next position") { vis() && page == Page.General }
     override val breakDelay by c.setting("Break Delay", 0, 0..6, 1, "The delay between breaking blocks", " ticks") { vis() && page == Page.General }
 
