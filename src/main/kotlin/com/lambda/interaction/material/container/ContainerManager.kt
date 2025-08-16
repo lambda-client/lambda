9a8a8a7f5a68e376e8489334ee111b2c2733ab68
beanbag44 <pidgeonmaster64@gmail.com>
2025-08-16 20:01:08 +0100

    containers with a disposable stack greater than 0

--- src/main/kotlin/com/lambda/interaction/material/container/ContainerManager.kt
+++ src/main/kotlin/com/lambda/interaction/material/container/ContainerManager.kt
@@ -124,7 +124,7 @@ object ContainerManager : Loadable {
             .filter { inventory.containerSelection.matches(it) }
 
     fun findDisposable(inventory: InventoryConfig = TaskFlowModule.inventory) = container().find { container ->
-        inventory.disposables.any { container.materialAvailable(it.asItem().select()) >= 0 }
+        inventory.disposables.any { container.materialAvailable(it.asItem().select()) > 0 }
     }
 
     class NoContainerFound(selection: StackSelection) : Exception("No container found matching $selection")
