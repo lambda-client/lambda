5c119f281c1e0a2405553a2953d2766f153dabce
beanbag44 <pidgeonmaster64@gmail.com>
2025-09-07 03:35:55 +0100

    constructor told me to do this

--- src/main/kotlin/com/lambda/module/modules/player/InventoryMove.kt
+++ src/main/kotlin/com/lambda/module/modules/player/InventoryMove.kt
@@ -18,10 +18,11 @@
 package com.lambda.module.modules.player
 
 import com.lambda.Lambda.mc
+import com.lambda.event.events.UpdateManagerEvent
+import com.lambda.event.listener.SafeListener.Companion.listen
 import com.lambda.gui.LambdaScreen
 import com.lambda.interaction.request.rotating.Rotation
 import com.lambda.interaction.request.rotating.RotationConfig
-import com.lambda.interaction.request.rotating.RotationManager.onRotate
 import com.lambda.interaction.request.rotating.RotationMode
 import com.lambda.interaction.request.rotating.visibilty.lookAt
 import com.lambda.module.Module
@@ -67,8 +68,8 @@ object InventoryMove : Module(
                 this == null
 
     init {
-        onRotate {
-            if (!arrowKeys || mc.currentScreen.hasInputOrNull) return@onRotate
+        listen<UpdateManagerEvent.Rotation> {
+            if (!arrowKeys || mc.currentScreen.hasInputOrNull) return@listen
 
             val pitch = (isKeyPressed(GLFW_KEY_DOWN, GLFW_KEY_KP_2).toFloatSign() -
                     isKeyPressed(GLFW_KEY_UP, GLFW_KEY_KP_8).toFloatSign()) * speed
