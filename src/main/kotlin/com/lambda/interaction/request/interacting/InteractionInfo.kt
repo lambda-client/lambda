ca437faf1c8378a21ff61daf000ec1deb97c8dfe
beanbag44 <pidgeonmaster64@gmail.com>
2025-09-09 02:20:59 +0100

    InteractionInfo toLogContext

--- src/main/kotlin/com/lambda/interaction/request/interacting/InteractionInfo.kt
+++ src/main/kotlin/com/lambda/interaction/request/interacting/InteractionInfo.kt
@@ -20,9 +20,17 @@ package com.lambda.interaction.request.interacting
 import com.lambda.interaction.construction.context.BuildContext
 import com.lambda.interaction.construction.context.InteractionContext
 import com.lambda.interaction.request.ActionInfo
+import com.lambda.interaction.request.LogContext
+import com.lambda.interaction.request.LogContext.Companion.buildLogContext
 
 data class InteractionInfo(
     override val context: InteractionContext,
     override val pendingInteractionsList: MutableCollection<BuildContext>,
     private val config: InteractConfig
-) : ActionInfo, InteractConfig by config
+) : ActionInfo, InteractConfig by config, LogContext {
+    override fun toLogContext() =
+        buildLogContext {
+            text("Interaction Info:")
+            pushTab()
+        }
+}
