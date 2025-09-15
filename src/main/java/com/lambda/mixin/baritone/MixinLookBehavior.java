e3119d4bd626efdd21788b863ba69a37115d4f09
Constructor <fractalminds@protonmail.com>
2025-09-15 02:06:05 +0200

    Fix Baritone strafe and add hud info

--- src/main/java/com/lambda/mixin/baritone/MixinLookBehavior.java
+++ src/main/java/com/lambda/mixin/baritone/MixinLookBehavior.java
@@ -36,7 +36,7 @@ public class MixinLookBehavior {
         LookBehavior instance = ((LookBehavior) (Object) this);
         if (instance.baritone != BaritoneManager.getPrimary()) return;
 
-        RotationManager.handleBaritoneRotation(rotation.getYaw(), rotation.getPitch());
+        RotationManager.handleBaritoneRotation(rotation.getYaw());
         ci.cancel();
     }
 
