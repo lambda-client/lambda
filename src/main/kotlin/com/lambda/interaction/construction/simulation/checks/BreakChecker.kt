7d7cbc37a367790e5f2291013e25c2be5a5c66dc
beanbag44 <pidgeonmaster64@gmail.com>
2025-10-26 15:56:35 +0000

    isEmpty check after fluid checks in break checker

--- src/main/kotlin/com/lambda/interaction/construction/simulation/checks/BreakChecker.kt
+++ src/main/kotlin/com/lambda/interaction/construction/simulation/checks/BreakChecker.kt
@@ -85,8 +85,6 @@ class BreakChecker @SimCheckerDsl private constructor(simInfo: SimInfo)
     }
 
     private suspend fun AutomatedSafeContext.checkBreaks(): Boolean {
-        if (state.isEmpty) return false
-
         if (breakConfig.avoidSupporting) player.supportingBlockPos.getOrNull()?.let { support ->
             if (support != pos) return@let
             result(BreakResult.PlayerOnTop(pos, state))
@@ -97,6 +95,8 @@ class BreakChecker @SimCheckerDsl private constructor(simInfo: SimInfo)
             result(BreakResult.Submerge(pos, state))
             return simInfo(pos, state, TargetState.Solid(emptySet()))?.checkPlacements() ?: true
         }
+
+        if (state.isEmpty) return false
 
         if (breakConfig.avoidLiquids && affectsFluids()) return true
 
