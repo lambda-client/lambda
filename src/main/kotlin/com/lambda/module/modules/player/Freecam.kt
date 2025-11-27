169308d1e61f196e158744b2c3dd6516714bdc77
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2025-11-27 23:46:36 +0100

    - Make rotations for looking at target smooth - Fix LookAtTarget not working due to conflict with RotationManager

--- src/main/kotlin/com/lambda/module/modules/player/Freecam.kt
+++ src/main/kotlin/com/lambda/module/modules/player/Freecam.kt
@@ -30,6 +30,8 @@ import com.lambda.interaction.request.rotating.visibilty.lookAt
 import com.lambda.interaction.request.rotating.visibilty.lookAtHit
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
+import com.lambda.util.Describable
+import com.lambda.util.NamedEnum
 import com.lambda.util.extension.rotation
 import com.lambda.util.math.interpolate
 import com.lambda.util.math.plus
@@ -45,6 +47,10 @@ import com.lambda.util.player.MovementUtils.roundedStrafing
 import com.lambda.util.player.MovementUtils.verticalMovement
 import com.lambda.util.world.raycast.RayCastUtils.orMiss
 import net.minecraft.client.option.Perspective
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.hit.HitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3d
 
 object Freecam : Module(
@@ -57,9 +63,9 @@ object Freecam : Module(
     private val sprint by setting("Sprint Multiplier", 3.0, 0.1..10.0, 0.1, description = "Set below 1.0 to fly slower on sprint.")
     private val reach by setting("Reach", 10.0, 1.0..100.0, 1.0, "Freecam reach distance")
     private val rotateMode by setting("Rotate Mode", FreecamRotationMode.None, "Rotation mode")
-    private val relative by setting("Relative", false, "Moves freecam relative to player position").onValueChange { _, it ->
-        if (it) lastPlayerPosition = player.pos
-    }
+        .onValueChange { _, it -> if (it == FreecamRotationMode.LookAtTarget) mc.crosshairTarget = BlockHitResult.createMissed(Vec3d.ZERO, Direction.UP, BlockPos.ORIGIN) }
+    private val relative by setting("Relative", false, "Moves freecam relative to player position")
+        .onValueChange { _, it -> if (it) lastPlayerPosition = player.pos }
     private val keepYLevel by setting("Keep Y Level", false, "Don't change the camera y-level on player movement") { relative }
 
     override val rotationConfig = RotationConfig.Instant(RotationMode.Lock)
@@ -107,7 +113,14 @@ object Freecam : Module(
             when (rotateMode) {
                 FreecamRotationMode.None -> return@listen
                 FreecamRotationMode.KeepRotation -> lookAt(rotation).requestBy(this@Freecam)
-                FreecamRotationMode.LookAtTarget -> mc.crosshairTarget?.let { lookAtHit(it)?.requestBy(this@Freecam) }
+                FreecamRotationMode.LookAtTarget ->
+                    mc.crosshairTarget?.let {
+                        when (it) {
+                            is BlockHitResult -> lookAt(it.pos).requestBy(this@Freecam)
+                            else -> lookAtHit(it)?.requestBy(this@Freecam)
+
+                    }
+                }
             }
         }
 
@@ -150,12 +163,12 @@ object Freecam : Module(
             }
         }
 
-        listen<RenderEvent.UpdateTarget> {
-            it.cancel()
-
+        listen<RenderEvent.UpdateTarget>(priority = 1) { event -> // Higher priority then RotationManager to run before RotationManager modifies mc.crosshairTarget
             mc.crosshairTarget = rotation
                 .rayCast(reach, lerpPos)
                 .orMiss // Can't be null (otherwise mc will spam "Null returned as 'hitResult', this shouldn't happen!")
+
+            mc.crosshairTarget?.let { if (it.type != HitResult.Type.MISS) event.cancel() }
         }
     }
 
