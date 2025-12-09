fa2e846ee8b603797d77a35401eb18ea813b8c57
Constructor <fractalminds@protonmail.com>
2025-12-09 20:18:56 +0100

    Get rid of all @Redirects

--- src/main/java/com/lambda/mixin/world/BlockCollisionSpliteratorMixin.java
+++ src/main/java/com/lambda/mixin/world/BlockCollisionSpliteratorMixin.java
@@ -24,17 +24,18 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.BlockCollisionSpliterator;
 import net.minecraft.world.CollisionView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockCollisionSpliterator.class)
 public abstract class BlockCollisionSpliteratorMixin<T extends AbstractIterator<T>> {
-    @Redirect(method = "computeNext", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/ShapeContext;getCollisionShape(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/CollisionView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/shape/VoxelShape;"))
-    private VoxelShape collisionShapeRedirect(ShapeContext instance, BlockState blockState, CollisionView collisionView, BlockPos blockPos) {
-        VoxelShape collisionShape = instance.getCollisionShape(blockState, collisionView, blockPos);
+    @WrapOperation(method = "computeNext", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/ShapeContext;getCollisionShape(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/CollisionView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/shape/VoxelShape;"))
+    private VoxelShape wrapCollisionShape(ShapeContext instance, BlockState blockState, CollisionView collisionView, BlockPos blockPos, Operation<VoxelShape> original) {
+        VoxelShape collisionShape = original.call(instance, blockState, collisionView, blockPos);
         WorldEvent.Collision event = EventFlow.post(new WorldEvent.Collision(blockPos, blockState, collisionShape));
         return event.getShape();
     }
