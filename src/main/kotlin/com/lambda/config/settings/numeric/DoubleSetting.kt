3b5ec63548315c9409c2cdd079dc9929bae2e59f
cattyn <cvenby@gmail.com>
2025-12-14 04:22:44 +0300

    fixed incorrect rounding with imgui sliders

--- src/main/kotlin/com/lambda/config/settings/numeric/DoubleSetting.kt
+++ src/main/kotlin/com/lambda/config/settings/numeric/DoubleSetting.kt
@@ -26,6 +26,7 @@ import com.lambda.config.Setting
 import com.lambda.config.settings.NumericSetting
 import com.lambda.gui.dsl.ImGuiBuilder
 import com.lambda.util.extension.CommandBuilder
+import com.lambda.util.math.MathUtils.roundToStep
 import net.minecraft.command.CommandRegistryAccess
 import kotlin.math.roundToInt
 
@@ -46,7 +47,9 @@ class DoubleSetting(
     private var valueIndex: Int
         get() = ((value - range.start) / step).roundToInt()
         set(index) {
-            value = (range.start + index * step).coerceIn(range)
+            value = (range.start + index * step)
+	            .roundToStep(step)
+	            .coerceIn(range)
         }
 
 	context(setting: Setting<*, Double>)
