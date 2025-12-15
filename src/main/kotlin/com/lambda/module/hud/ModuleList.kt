28d6811aa049b53bffd6dd9533f64dc13a7847b2
cattyn <cvenby@gmail.com>
2025-12-15 09:55:04 +0300

    refactor(ModuleList): move color into if statement

--- src/main/kotlin/com/lambda/module/hud/ModuleList.kt
+++ src/main/kotlin/com/lambda/module/hud/ModuleList.kt
@@ -41,10 +41,11 @@ object ModuleList : HudModule(
 
         enabled.forEach {
             text(it.name); sameLine()
-            val color = if (it.keybind.key == 0 && it.keybind.mouse == -1) Color.RED else Color.GREEN
 
-	        if (showKeybind)
-				withStyleColor(ImGuiCol.Text, color) { text(" [${it.keybind.name}]") }
+	        if (showKeybind) {
+		        val color = if (it.keybind.key == 0 && it.keybind.mouse == -1) Color.RED else Color.GREEN
+		        withStyleColor(ImGuiCol.Text, color) { text(" [${it.keybind.name}]") }
+	        }
         }
     }
 }
