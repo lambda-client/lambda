cdea7aff8de046191135742220b904c7aa5ff9a1
cattyn <60744119+cattyngmd@users.noreply.github.com>
2025-12-16 01:13:24 +0300

    fix(ModuleList): fix `sameLine` being invoked with `Show Keybin` off

--- src/main/kotlin/com/lambda/module/hud/ModuleList.kt
+++ src/main/kotlin/com/lambda/module/hud/ModuleList.kt
@@ -40,10 +40,12 @@ object ModuleList : HudModule(
             .filter { it.isVisible }
 
         enabled.forEach {
-            text(it.name); sameLine()
+            text(it.name)
 
 	        if (showKeybind) {
 		        val color = if (it.keybind.key == 0 && it.keybind.mouse == -1) Color.RED else Color.GREEN
+
+						sameLine()
 		        withStyleColor(ImGuiCol.Text, color) { text(" [${it.keybind.name}]") }
 	        }
         }
