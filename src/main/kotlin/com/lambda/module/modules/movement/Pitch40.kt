a4d5ed116f19fa237ff84dbfca24b6c638bb1755
beanbag44 <107891830+beanbag44@users.noreply.github.com>
2025-12-15 05:23:05 +0000

    use rotation requests in pitch 40

--- src/main/kotlin/com/lambda/module/modules/movement/Pitch40.kt
+++ src/main/kotlin/com/lambda/module/modules/movement/Pitch40.kt
@@ -17,8 +17,12 @@
 
 package com.lambda.module.modules.movement
 
+import com.lambda.config.AutomationConfig.Companion.setDefaultAutomationConfig
+import com.lambda.config.applyEdits
 import com.lambda.event.events.TickEvent
 import com.lambda.event.listener.SafeListener.Companion.listen
+import com.lambda.interaction.managers.rotating.Rotation
+import com.lambda.interaction.managers.rotating.visibilty.lookAt
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
 import com.lambda.util.Communication.info
@@ -27,11 +31,6 @@ import net.minecraft.client.network.ClientPlayerEntity
 import net.minecraft.text.Text.literal
 import net.minecraft.util.math.Vec3d
 
-
-/*
-  * @author IceTank
-  * @since 07.12.2025
-*/
 object Pitch40 : Module(
 	name = "Pitch40",
 	description = "Allows you to fly forever",
@@ -39,36 +38,42 @@ object Pitch40 : Module(
 ) {
 	val logHeightGain by setting("Log Height Gain", true, "Logs the height gained each cycle to the chat")
 
-	val PitchUpDefault = -49f  // Start angle when going back up. negative pitch = looking up
-	val PitchDownDefault = 33f // Best angle for getting speed
-	val PitchAngleChangeSpeed = 0.5f
-	val PitchUpSpeedThreshold = 45f
+	const val PITCH_UP_DEFAULT = -49f  // Start angle when going back up. negative pitch = looking up
+	const val PITCH_DOWN_DEFAULT = 33f // Best angle for getting speed
+	const val PITCH_ANGLE_CHANGE_SPEED = 0.5f
+	const val PITCH_UP_SPEED_THRESHOLD = 45f
 
 	var state = Pitch40State.GainSpeed
-	var lastAngle = PitchUpDefault
+	var lastAngle = PITCH_UP_DEFAULT
 
-	var lastPos = Vec3d.ZERO
+	var lastPos: Vec3d = Vec3d.ZERO
 	var lastY = 0.0
 
 	init {
+		setDefaultAutomationConfig {
+			applyEdits {
+				hideAllGroupsExcept(rotationConfig)
+			}
+		}
+
 		listen<TickEvent.Pre> {
 			when (state) {
 				Pitch40State.GainSpeed -> {
-					player.pitch = PitchDownDefault
-					if (player.flySpeed() > PitchUpSpeedThreshold) {
+					lookAt(Rotation(player.yaw, PITCH_DOWN_DEFAULT)).requestBy(this@Pitch40)
+					if (player.flySpeed() > PITCH_UP_SPEED_THRESHOLD) {
 						state = Pitch40State.PitchUp
 					}
 				}
 				Pitch40State.PitchUp -> {
 					lastAngle -= 5f
-					player.pitch = lastAngle
-					if (lastAngle <= PitchUpDefault) {
+					lookAt(Rotation(player.yaw, lastAngle)).requestBy(this@Pitch40)
+					if (lastAngle <= PITCH_UP_DEFAULT) {
 						state = Pitch40State.FlyUp
 					}
 				}
 				Pitch40State.FlyUp -> {
-					lastAngle += PitchAngleChangeSpeed
-					player.pitch = lastAngle
+					lastAngle += PITCH_ANGLE_CHANGE_SPEED
+					lookAt(Rotation(player.yaw, lastAngle)).requestBy(this@Pitch40)
 					if (lastAngle >= 0f) {
 						state = Pitch40State.GainSpeed
 						if (logHeightGain)
@@ -84,7 +89,7 @@ object Pitch40 : Module(
 		onEnable {
 			state = Pitch40State.GainSpeed
 			lastPos = player.pos
-			lastAngle = PitchUpDefault
+			lastAngle = PITCH_UP_DEFAULT
 		}
 	}
 
