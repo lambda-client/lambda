bc3cc30ddcce2e44aa11f474fc044577bfb195e2
beanbag44 <107891830+beanbag44@users.noreply.github.com>
2025-12-20 08:45:36 +0000

    fix mixin class names and move remap = false to the annotation

--- src/main/java/com/lambda/mixin/baritone/LookBehaviourMixin.java
+++ src/main/java/com/lambda/mixin/baritone/LookBehaviourMixin.java
@@ -28,10 +28,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LookBehavior.class)
-public class MixinLookBehavior {
+@Mixin(value = LookBehavior.class, remap = false)
+public class LookBehaviourMixin {
     // Redirect baritone's rotations into our rotation engine
-    @Inject(method = "updateTarget", at = @At("HEAD"), remap = false, cancellable = true)
+    @Inject(method = "updateTarget", at = @At("HEAD"), cancellable = true)
     void onTargetUpdate(Rotation rotation, boolean blockInteract, CallbackInfo ci) {
         LookBehavior instance = ((LookBehavior) (Object) this);
         if (instance.baritone != BaritoneManager.getPrimary()) return;
@@ -40,7 +40,7 @@ public class MixinLookBehavior {
         ci.cancel();
     }
 
-    @Inject(method = "onPlayerUpdate", at = @At("HEAD"), remap = false, cancellable = true)
+    @Inject(method = "onPlayerUpdate", at = @At("HEAD"), cancellable = true)
     void onUpdate(PlayerUpdateEvent event, CallbackInfo ci) {
         LookBehavior instance = ((LookBehavior) (Object) this);
         if (instance.baritone != BaritoneManager.getPrimary()) return;
@@ -48,7 +48,7 @@ public class MixinLookBehavior {
         ci.cancel();
     }
 
-    @Inject(method = "onPlayerRotationMove", at = @At("HEAD"), remap = false, cancellable = true)
+    @Inject(method = "onPlayerRotationMove", at = @At("HEAD"), cancellable = true)
     void onMovementUpdate(RotationMoveEvent event, CallbackInfo ci) {
         LookBehavior instance = ((LookBehavior) (Object) this);
         if (instance.baritone != BaritoneManager.getPrimary()) return;
