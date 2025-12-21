edb15e2096d6abd31225d082e86754ba182896f5
Emy 💜 <swag@emy.systems>
2025-12-21 10:00:28 -0500

    removed unused font loading

--- src/main/java/com/lambda/mixin/render/ChatInputSuggestorMixin.java
+++ src/main/java/com/lambda/mixin/render/ChatInputSuggestorMixin.java
@@ -17,42 +17,23 @@
 
 package com.lambda.mixin.render;
 
-import com.google.common.base.Strings;
 import com.lambda.command.CommandManager;
-import com.lambda.graphics.renderer.gui.font.core.LambdaAtlas;
-import com.lambda.module.modules.client.LambdaMoji;
-import com.lambda.module.modules.client.StyleEditor;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.minecraft.client.gui.screen.ChatInputSuggestor;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.command.CommandSource;
-import org.jetbrains.annotations.Nullable;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 @Mixin(ChatInputSuggestor.class)
 public abstract class ChatInputSuggestorMixin {
-    @Unique private static final Pattern COLON_PATTERN = Pattern.compile("(:[a-zA-Z0-9_]+)");
-    @Unique private static final Pattern EMOJI_PATTERN = Pattern.compile("(:)([a-zA-Z0-9_]+)(:)");
     @Shadow @Final TextFieldWidget textField;
-    @Shadow private @Nullable CompletableFuture<Suggestions> pendingSuggestions;
 
     @Shadow
     public abstract void show(boolean narrateFirstSuggestion);
@@ -66,65 +47,5 @@ public abstract class ChatInputSuggestorMixin {
     @WrapOperation(method = "refresh", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;getCommandDispatcher()Lcom/mojang/brigadier/CommandDispatcher;"))
     private CommandDispatcher<CommandSource> wrapRefresh(ClientPlayNetworkHandler instance, Operation<CommandDispatcher<CommandSource>> original) {
         return (CommandDispatcher<CommandSource>) CommandManager.INSTANCE.currentDispatcher(textField.getText());
-    }
-
-    @Inject(method = "refresh", at = @At("TAIL"))
-    private void refreshEmojiSuggestion(CallbackInfo ci) {
-        if (!LambdaMoji.INSTANCE.isEnabled() ||
-                !LambdaMoji.INSTANCE.getSuggestions()) return;
-
-        String typing = textField.getText();
-
-        // Don't suggest emojis in commands
-        if (CommandManager.INSTANCE.isCommand(typing) ||
-                CommandManager.INSTANCE.isLambdaCommand(typing)) return;
-
-        int cursor = textField.getCursor();
-        String textToCursor = typing.substring(0, cursor);
-        if (textToCursor.isEmpty()) return;
-
-        // Most right index at the left of the regex expression
-        int start = neoLambda$getLastColon(textToCursor);
-        if (start == -1) return;
-
-        Matcher emojiMatcher = EMOJI_PATTERN.matcher(textToCursor);
-        Map<String, ?> emojiKeys = LambdaAtlas.INSTANCE.getKeys(StyleEditor.INSTANCE.getEmojiFont());
-        while (emojiMatcher.find()) {
-            int openingColon = emojiMatcher.start(1);
-            String key = emojiMatcher.group(2);
-            int closingColon = emojiMatcher.end(3);
-
-            if (emojiKeys.containsKey(key) && start >= openingColon && start < closingColon) {
-                // If the colon is part of a previous valid emoji, return
-                return;
-            }
-        }
-
-        String emojiString = textToCursor.substring(start + 1);
-
-        Stream<String> results = emojiKeys.keySet().stream()
-                .filter(s -> s.startsWith(emojiString))
-                .map(s -> s + ":");
-
-        pendingSuggestions = CommandSource.suggestMatching(results, new SuggestionsBuilder(textToCursor, start + 1));
-        pendingSuggestions.thenRun(() -> {
-            if (!pendingSuggestions.isDone()) return;
-
-            show(false);
-        });
-    }
-
-    @Unique
-    private int neoLambda$getLastColon(String input) {
-        if (Strings.isNullOrEmpty(input)) return -1;
-
-        int i = -1;
-        Matcher matcher = COLON_PATTERN.matcher(input);
-
-        while (matcher.find()) {
-            i = matcher.start();
-        }
-
-        return i;
     }
 }
