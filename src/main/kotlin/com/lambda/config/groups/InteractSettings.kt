a52bbd74f75ee891f8a7db42e2f8fe93689660b7
Emy 💜 <swag@emy.systems>
2025-12-21 16:21:17 -0500

    Fix: CollectionSettings mapping stringified values instead of converting the collection to a tree (#204)
    This pull request modifies slightly comparable collection settings by
    converting the entire tree directly instead of mapping the collection
    values to their stringified representations. Collection setting of
    comparable types was removed and a new flag was introduce to chose the
    setting core before creating the setting. This pull request also fixes
    the friend list being serialized using the class collection setting
    core.

--- src/main/kotlin/com/lambda/config/groups/InteractSettings.kt
+++ src/main/kotlin/com/lambda/config/groups/InteractSettings.kt
@@ -34,7 +34,7 @@ class InteractSettings(
     override val airPlace by c.setting("Air Place", AirPlaceMode.None, "Allows for placing blocks without adjacent faces").group(baseGroup).index()
     override val axisRotateSetting by c.setting("Axis Rotate", true, "Overrides the Rotate For Place setting and rotates the player on each axis to air place rotational blocks") { airPlace.isEnabled }.group(baseGroup).index()
     override val sorter by c.setting("Interaction Sorter", ActionConfig.SortMode.Tool, "The order in which placements are performed").group(baseGroup).index()
-    override val tickStageMask by c.setting("Interaction Stage Mask", setOf(TickEvent.Input.Post), ALL_STAGES.toSet(), description = "The sub-tick timing at which place actions are performed").group(baseGroup).index()
+    override val tickStageMask by c.setting("Interaction Stage Mask", setOf(TickEvent.Input.Post), ALL_STAGES.toSet(), "The sub-tick timing at which place actions are performed", displayClassName = true).group(baseGroup).index()
     override val interactConfirmationMode by c.setting("Interact Confirmation", InteractConfirmationMode.PlaceThenAwait, "Wait for block placement confirmation").group(baseGroup).index()
     override val interactDelay by c.setting("Interact Delay", 0, 0..3, 1, "Tick delay between interacting with another block").group(baseGroup).index()
     override val interactionsPerTick by c.setting("Interactions Per Tick", 1, 1..30, 1, "Maximum instant block places per tick").group(baseGroup).index()
