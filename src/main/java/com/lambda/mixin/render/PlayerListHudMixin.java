65d40f5f99840659657640f9fc5292bd682b1d1d
beanbag44 <107891830+beanbag44@users.noreply.github.com>
2026-01-03 07:42:58 +0000

    implement ExtraTab rows and add friends only, highlight friends, and friend color settings

--- src/main/java/com/lambda/mixin/render/PlayerListHudMixin.java
+++ src/main/java/com/lambda/mixin/render/PlayerListHudMixin.java
@@ -17,15 +17,26 @@
 
 package com.lambda.mixin.render;
 
+import com.lambda.friend.FriendManager;
 import com.lambda.module.modules.render.ExtraTab;
+import com.lambda.util.text.TextBuilder;
+import com.lambda.util.text.TextDslKt;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import kotlin.Unit;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Comparator;
@@ -45,9 +56,35 @@ public class PlayerListHudMixin {
                 client.player.networkHandler
                         .getListedPlayerListEntries()
                         .stream()
+                        .filter(entry -> !ExtraTab.getFriendsOnly() || FriendManager.INSTANCE.isFriend(entry.getProfile()))
                         .sorted(ENTRY_ORDERING)
                         .limit(ExtraTab.getTabEntries())
                         .toList()
         );
+    }
+
+    @ModifyConstant(method = "render", constant = @Constant(intValue = 20))
+    private int modifyRowLimit(int original) {
+        return ExtraTab.INSTANCE.isEnabled() ? ExtraTab.getRows() : original;
+    }
+
+    @ModifyExpressionValue(method = "getPlayerName", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;copy()Lnet/minecraft/text/MutableText;"))
+    private MutableText modifyPlayerName(MutableText original) { return modifyName(original); }
+
+    @ModifyExpressionValue(method = "getPlayerName", at = @At(value = "INVOKE", target = "Lnet/minecraft/scoreboard/Team;decorateName(Lnet/minecraft/scoreboard/AbstractTeam;Lnet/minecraft/text/Text;)Lnet/minecraft/text/MutableText;"))
+    private MutableText modifyDecorateName(MutableText original) { return modifyName(original); }
+
+    @Unique
+    private @Nullable MutableText modifyName(Text original) {
+        if (ExtraTab.INSTANCE.isDisabled() ||
+                !ExtraTab.getHighlightFriends() ||
+                !FriendManager.INSTANCE.isFriend(original.getString())) return original.copy();
+        var newText = original.copy();
+        var textBuilder = new TextBuilder();
+        TextDslKt.color(textBuilder, ExtraTab.getFriendColor(), builder -> {
+            builder.styleAndAppend(newText);
+            return Unit.INSTANCE;
+        });
+        return textBuilder.getText();
     }
 }
