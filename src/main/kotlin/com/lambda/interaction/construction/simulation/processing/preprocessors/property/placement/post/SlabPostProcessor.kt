7389e701fa899122ebaf2fa631ae692ba6ebf878
beanbag44 <107891830+beanbag44@users.noreply.github.com>
2026-01-28 02:12:11 +0000

    slab post processing to avoid breaking incomplete double slabs

---
+++ src/main/kotlin/com/lambda/interaction/construction/simulation/processing/preprocessors/property/placement/post/SlabPostProcessor.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2026 Lambda
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.lambda.interaction.construction.simulation.processing.preprocessors.property.placement.post
+
+import com.lambda.context.SafeContext
+import com.lambda.interaction.construction.simulation.processing.PreProcessingInfoAccumulator
+import com.lambda.interaction.construction.simulation.processing.PropertyPostProcessor
+import net.minecraft.block.BlockState
+import net.minecraft.block.SlabBlock
+import net.minecraft.block.enums.SlabType
+import net.minecraft.state.property.Properties
+import net.minecraft.util.math.BlockPos
+
+// Collected using reflections and then accessed from a collection in ProcessorRegistry
+@Suppress("unused")
+object SlabPostProcessor : PropertyPostProcessor {
+	override fun acceptsState(state: BlockState, targetState: BlockState) =
+		targetState.block is SlabBlock && targetState.get(Properties.SLAB_TYPE) == SlabType.DOUBLE &&
+				state.block == targetState.block &&
+				state.get(Properties.SLAB_TYPE) != SlabType.DOUBLE
+
+	context(safeContext: SafeContext)
+	override fun PreProcessingInfoAccumulator.preProcess(state: BlockState, targetState: BlockState, pos: BlockPos) {
+		addIgnores(Properties.SLAB_TYPE)
+	}
+}
