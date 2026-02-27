7f03cd1486095ffe606b1d84c5c1565c0caae412
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2026-02-27 21:10:18 +0100

    Fix pr code comments

--- src/main/kotlin/com/lambda/module/modules/movement/AutoMount.kt
+++ src/main/kotlin/com/lambda/module/modules/movement/AutoMount.kt
@@ -17,23 +17,18 @@
 
 package com.lambda.module.modules.movement
 
-import com.lambda.config.AutomationConfig
+import com.lambda.config.AutomationConfig.Companion.setDefaultAutomationConfig
 import com.lambda.context.SafeContext
 import com.lambda.event.events.TickEvent
 import com.lambda.event.listener.SafeListener.Companion.listen
-import com.lambda.interaction.managers.rotating.visibilty.VisibilityChecker
 import com.lambda.interaction.managers.rotating.visibilty.VisibilityChecker.findRotation
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
 import com.lambda.threading.runSafeAutomated
-import com.lambda.util.Communication.debug
 import com.lambda.util.Communication.info
-import com.lambda.util.EntityUtils
 import com.lambda.util.Timer
-import com.lambda.util.math.dist
 import com.lambda.util.world.fastEntitySearch
 import net.minecraft.entity.Entity
-import net.minecraft.entity.EntityType
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket
 import net.minecraft.registry.Registries
 import net.minecraft.util.Hand
@@ -47,22 +42,17 @@ class AutoMount : Module(
 ) {
 	var autoRemount by setting("Auto Remount", false, description = "Automatically remounts if you get off")
 	var autoMountEntities by setting("Auto Mount Entities", true, description = "Automatically mounts nearby entities in range")
-	var autoMountEntityList by setting("Auto Mount Entity List",
-		mutableListOf(),
-		Registries.ENTITY_TYPE.toList()
-	) { autoMountEntities }
+	var autoMountEntityList by setting("Auto Mount Entity List", mutableListOf(), Registries.ENTITY_TYPE.toList()) { autoMountEntities }
 
 	var interval by setting("Interval", 50, 1..200, 1, unit = "ms", description = "Interact interval")
 	var range by setting("Range", 4.0, 1.0..20.0, 0.1, description = "Mount range")
-
-	override var automationConfig = AutomationConfig(
-		name = "AutoMount"
-	)
+	var debug by setting("Debug", false, description = "Print debug messages")
 
 	val intervalTimer = Timer()
 	var lastEntity: Entity? = null
 
 	init {
+		setDefaultAutomationConfig("AutoMount")
 		onEnable {
 			intervalTimer.reset()
 			lastEntity = null
@@ -80,24 +70,27 @@ class AutoMount : Module(
 					entity.firstOrNull()?.let {
 						intervalTimer.reset()
 						interactEntity(it)
-						debug("Mounting ${it.name}")
+						if (debug) info("Mounting ${it.name}")
 					}
 				}
 			}
-			if (autoRemount) {
-				if (player.vehicle != null) {
-					lastEntity = player.vehicle
-				} else {
-					lastEntity?.let {
-						if (it.isRemoved || it.distanceTo(player) > range) {
-							lastEntity = null
-						} else {
-							if (canRide(it)) {
-								intervalTimer.reset()
-								interactEntity(it)
-							}
-						}
-					}
+			if (!autoRemount) {
+				return@listen
+			}
+			if (player.vehicle != null) {
+				lastEntity = player.vehicle
+			}
+			if (lastEntity == null) {
+				return@listen
+			}
+			lastEntity?.let {
+				if (it.isRemoved || it.distanceTo(player) > range) {
+					lastEntity = null
+					return@let
+				}
+				if (canRide(it)) {
+					intervalTimer.reset()
+					interactEntity(it)
 				}
 			}
 		}
@@ -108,7 +101,5 @@ class AutoMount : Module(
 		mc.networkHandler?.sendPacket(PlayerInteractEntityC2SPacket.interact(entity, false, Hand.MAIN_HAND))
 	}
 
-	private fun SafeContext.canRide(entity: Entity): Boolean {
-		return entity.canAddPassenger(player)
-	}
+	private fun SafeContext.canRide(entity: Entity) = entity.canAddPassenger(player)
 }
