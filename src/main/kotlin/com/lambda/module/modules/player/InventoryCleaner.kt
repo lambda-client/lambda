5bbf1e406461c9aebd6b87c8a5229b0ee7dcded9
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2026-02-27 21:41:28 +0100

    Fix pr code comments

--- src/main/kotlin/com/lambda/module/modules/player/InventoryCleaner.kt
+++ src/main/kotlin/com/lambda/module/modules/player/InventoryCleaner.kt
@@ -34,7 +34,9 @@ import net.minecraft.registry.Registries.ITEM
 import net.minecraft.screen.GenericContainerScreenHandler
 import net.minecraft.screen.ScreenHandler
 import net.minecraft.screen.slot.SlotActionType
+import net.minecraft.world.GameMode
 
+@Suppress("unused")
 object InventoryCleaner : Module(
 	name = "InventoryCleaner",
 	description = """
@@ -42,22 +44,13 @@ object InventoryCleaner : Module(
 		""".trimIndent(),
 	tag = ModuleTag.PLAYER
 ) {
-	private val itemsCanTrash by setting(
-		"Items Can Trash", setOf<Item>(Items.NETHERRACK, Items.COBBLESTONE), ITEM.toSet(),
-		description = """A list of items that the module can trash when trying to make space in the inventory.
-		""".trimIndent()
-	)
+	private val itemsCanTrash by setting("Items Can Trash", setOf<Item>(Items.NETHERRACK, Items.COBBLESTONE), ITEM.toSet(), description = "A list of items that the module can trash when trying to make space in the inventory.")
 
-	private val dropOnContainerClick by setting(
-		"Drop On Container Click", true,
-		description = """If enabled, shift + left clicking an item in a container will drop a trash item out of the inventory if the player inventory is full.
-		""".trimIndent()
-	)
+	private val dropOnContainerClick by setting("Drop On Container Click", true, description = "If enabled, shift + left clicking an item in a container will drop a trash item out of the inventory if the player inventory is full.")
 	private val dropToPickup by setting(
-		"Drop To Pickup", true,
-		description = """If enabled, the module will drop a trash item out of the inventory when the player is in pickup range of an item to pickup.
-		Items to pickup can be configured in the 'Items to pickup' setting. Trash items to drop can be configured in the 'Items can trash' setting.
-		""".trimIndent()
+		"Drop To Pickup",
+		true,
+		description = "If enabled, the module will drop a trash item out of the inventory when the player is in pickup range of an item to pickup.\nItems to pickup can be configured in the 'Items to pickup' setting.\nTrash items to drop can be configured in the 'Items can trash' setting."
 	)
 	private val itemsToPickup by setting("Items To Pickup", setOf(), ITEM.toSet()) { dropToPickup }
 
@@ -71,48 +64,50 @@ object InventoryCleaner : Module(
 				return@listen
 			}
 
-			if (event.screenHandler is GenericContainerScreenHandler) {
-				val sh = event.screenHandler
-				val rows = sh.rows
+			val sh = event.screenHandler
+			if (sh !is GenericContainerScreenHandler) {
+				return@listen
+			}
+			val rows = sh.rows
 
-				if (event.slotId >= rows * 9) {
-					// Clicked in inventory, not in container
-					return@listen
-				}
+			if (event.slotId >= rows * 9) {
+				// Clicked in inventory, not in container
+				return@listen
+			}
 
-				if (sh.slots.subList(rows * 9, sh.slots.size).any { !it.hasStack() }) {
-					// There is empty space in the inventory, no need to trash
-					return@listen
-				}
+			if (sh.slots.subList(rows * 9, sh.slots.size).any { !it.hasStack() }) {
+				// There is empty space in the inventory, no need to trash
+				return@listen
+			}
 
-				StackSelection.selectStack {
-					isOneOfItems(itemsCanTrash)
-				}.filterSlots(InventoryContainer.slots).firstOrNull()?.let { trashSlot ->
-					inventoryRequest {
-						pickup(event.slotId, 0)
-						pickup(trashSlot.id, 0)
-						pickup(ScreenHandler.EMPTY_SPACE_SLOT_INDEX, 0)
-					}.submit(true)
-					event.cancel()
-				}
+			StackSelection.selectStack {
+				isOneOfItems(itemsCanTrash)
+			}.filterSlots(InventoryContainer.slots).firstOrNull()?.let { trashSlot ->
+				inventoryRequest {
+					pickup(event.slotId, 0)
+					pickup(trashSlot.id, 0)
+					pickup(ScreenHandler.EMPTY_SPACE_SLOT_INDEX, 0)
+				}.submit(true)
+				event.cancel()
 			}
 		}
 	}
 
 	fun SafeContext.checkShouldTrashSomething() {
-		if (player.health > 0.0f && !player.isSpectator && dropToPickup && !player.isCreative) {
-			val vehicle = player.vehicle
-			val box = if (vehicle != null && !vehicle.isRemoved) {
-				player.boundingBox.union(vehicle.boundingBox).expand(1.0, 0.0, 1.0)
-			} else {
-				player.boundingBox.expand(1.0, 0.5, 1.0)
-			}
-			val items = world.getEntitiesByType<ItemEntity>(EntityType.ITEM, box) { entity ->
-				itemsToPickup.contains(entity.stack.item) && entity.isOnGround
-			}.map { i -> i.stack.item }
-			if (items.isNotEmpty() && InventoryContainer.spaceAvailable(StackSelection.selectStack { isOneOfItems(items) }) <= 0) {
-				dropOneTrashStack()
-			}
+		if (!dropToPickup || !player.isAlive || player.gameMode != GameMode.SURVIVAL) {
+			return
+		}
+		val vehicle = player.vehicle
+		val box = if (vehicle != null && !vehicle.isRemoved) {
+			player.boundingBox.union(vehicle.boundingBox).expand(1.0, 0.0, 1.0)
+		} else {
+			player.boundingBox.expand(1.0, 0.5, 1.0)
+		}
+		val items = world.getEntitiesByType<ItemEntity>(EntityType.ITEM, box) { entity ->
+			itemsToPickup.contains(entity.stack.item) && entity.isOnGround
+		}.map { i -> i.stack.item }
+		if (items.isNotEmpty() && InventoryContainer.spaceAvailable(StackSelection.selectStack { isOneOfItems(items) }) <= 0) {
+			dropOneTrashStack()
 		}
 	}
 
