5692b8b5e9ed4c4de14567b54fa64ad046bef2d3
Sophie <sophimoo@protonmail.com>
2026-07-05 06:42:53 +0200

    dont track shell

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    jdk21
-  ];
-  shellHook = ''
-    export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:}${pkgs.libglvnd}/lib"
-  '';
-}
