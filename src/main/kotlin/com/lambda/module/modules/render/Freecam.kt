57d5269da98a879cd1323945be97abefa2abf89c
pothemagicdragon <pothemagicdragon1@gmail.com>
2026-09-03 09:41:03 -0700

    updates comment

--- src/main/kotlin/com/lambda/module/modules/render/Freecam.kt
+++ src/main/kotlin/com/lambda/module/modules/render/Freecam.kt
@@ -158,7 +158,7 @@ object Freecam : Module(
 			it.cancel()
 		}
 
-		listen<MovementEvent.InputUpdate>({ 1 }) { event -> // Higher priority than movement modules so their input survives the cancel below
+		listen<MovementEvent.InputUpdate>({ 1 }) { event -> // Higher priority than auto walk so it's input survives the cancel below
 			mc.options.perspective = Perspective.FIRST_PERSON
 
 			// Don't block baritone from working
